48c957028de2a2670dbc5412747ffbc84ee3714c
Stuart Pernsteiner <spernsteiner@galois.com>
2017-07-11 15:27:56 -0400

    add more cases to mark_uses

--- src/mark_adjust.rs
+++ src/mark_adjust.rs
@@ -27,14 +27,19 @@ impl<'a, 'hir, 'gcx, 'tcx> MarkUseVisitor<'a, 'hir, 'gcx, 'tcx> {
     fn handle_qpath(&mut self, use_id: NodeId, hp: &hir::QPath) {
         match hp {
             &hir::QPath::Resolved(_, ref path) => {
-                println!("found qpath::resolved {:?}", path);
                 if let Some(def_id) = path.def.opt_def_id() {
-                    println!("  def_id {:?}", def_id);
                     if let Some(id) = self.cx.hir_map().as_local_node_id(def_id) {
-                        println!("    node id {:?}", id);
                         if self.cx.marked(id, self.label) {
-                            println!("      insert!!");
                             self.uses.insert(use_id);
+                        }
+
+                        // For struct and node constructors, also check the parent item
+                        if matches!([path.def] Def::StructCtor(..)) ||
+                           matches!([path.def] Def::VariantCtor(..)) {
+                            let parent_id = self.cx.hir_map().get_parent(id);
+                            if self.cx.marked(parent_id, self.label) {
+                                self.uses.insert(use_id);
+                            }
                         }
                     }
                 }
@@ -45,24 +50,94 @@ impl<'a, 'hir, 'gcx, 'tcx> MarkUseVisitor<'a, 'hir, 'gcx, 'tcx> {
 }
 
 impl<'a, 'hir, 'gcx, 'tcx, 's> Visitor<'s> for MarkUseVisitor<'a, 'hir, 'gcx, 'tcx> {
+    // We currently handle exprs, pats, and tys.  There are more cases (see comment in
+    // path_edit.rs), but this should be sufficient for now.
     fn visit_expr(&mut self, x: &'s Expr) {
+        let hir = if let Some(node) = self.cx.hir_map().find(x.id) {
+            expect!([node] hir::map::NodeExpr(y) => y)
+        } else {
+            visit::walk_expr(self, x);
+            return;
+        };
+
         match x.node {
             ExprKind::Path(_, _) => {
-                match self.cx.hir_map().expect_expr(x.id).node {
-                    hir::ExprPath(ref hp) => self.handle_qpath(x.id, hp),
-                    ref n => panic!("unexpected hir node for ExprKind::Path: {:?}", n),
-                }
+                expect!([hir.node] hir::ExprPath(ref hp) => {
+                    println!("looking at ExprPath {:?}", x);
+                    self.handle_qpath(x.id, hp);
+                });
             },
+
             ExprKind::Struct(_, _, _) => {
-                match self.cx.hir_map().expect_expr(x.id).node {
-                    hir::ExprStruct(ref hp, _, _) => self.handle_qpath(x.id, hp),
-                    ref n => panic!("unexpected hir node for ExprKind::Struct: {:?}", n),
-                }
+                expect!([hir.node] hir::ExprStruct(ref hp, _, _) => {
+                    println!("looking at ExprStruct {:?}", x);
+                    self.handle_qpath(x.id, hp);
+                });
             },
+
             _ => {},
         }
 
         visit::walk_expr(self, x);
+    }
+
+    fn visit_pat(&mut self, x: &'s Pat) {
+        let hir = if let Some(node) = self.cx.hir_map().find(x.id) {
+            expect!([node] hir::map::NodePat(y) => y,
+                           hir::map::NodeLocal(y) => y)
+        } else {
+            visit::walk_pat(self, x);
+            return;
+        };
+
+        match x.node {
+            PatKind::Struct(_, _, _) => {
+                expect!([hir.node] hir::PatKind::Struct(ref hp, _, _) => {
+                    println!("looking at PatStruct {:?}", x);
+                    self.handle_qpath(x.id, hp);
+                });
+            },
+
+            PatKind::TupleStruct(_, _, _) => {
+                expect!([hir.node] hir::PatKind::TupleStruct(ref hp, _, _) => {
+                    println!("looking at PatTupleStruct {:?}", x);
+                    self.handle_qpath(x.id, hp);
+                });
+            },
+
+            PatKind::Path(_, _) => {
+                expect!([hir.node] hir::PatKind::Path(ref hp) => {
+                    println!("looking at PatPath {:?}", x);
+                    self.handle_qpath(x.id, hp);
+                });
+            },
+
+            _ => {},
+        }
+
+        visit::walk_pat(self, x);
+    }
+
+    fn visit_ty(&mut self, x: &'s Ty) {
+        let hir = if let Some(node) = self.cx.hir_map().find(x.id) {
+            expect!([node] hir::map::NodeTy(y) => y)
+        } else {
+            visit::walk_ty(self, x);
+            return;
+        };
+
+        match x.node {
+            TyKind::Path(_, _) => {
+                expect!([hir.node] hir::TyPath(ref hp) => {
+                    println!("looking at TyPath {:?}", x);
+                    self.handle_qpath(x.id, hp);
+                });
+            },
+
+            _ => {},
+        }
+
+        visit::walk_ty(self, x);
     }
 }
 
