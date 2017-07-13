e1d4900af0852c3587d07c338416fe96c3cc5a4e
Stuart Pernsteiner <spernsteiner@galois.com>
2017-07-13 15:47:46 -0400

    uncomment part of tests/reconstruct_for_range.rs

--- tests/reconstruct_for_range.rs
+++ tests/reconstruct_for_range.rs
@@ -7,13 +7,9 @@ fn main() {
         i = i + 1;
     }
 
-    /*
-    // This part of the test is disabled because it triggers a bug in libsyntax `pprust`: the
-    // pretty printer does not put parentheses around `0..10` in `(0..10).step_by(2)`.
     i = 0;
     'a: while (i < 10) {
         println!("{}", i);
         i = i + 2;
     }
-    */
 }
