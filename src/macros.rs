163ea5e378b4ae92131f3dd4f20e0b582970ba1e
Stuart Pernsteiner <spernsteiner@galois.com>
2017-07-24 08:57:57 -0400

    export macros for use in plugins

--- src/macros.rs
+++ src/macros.rs
@@ -1,3 +1,4 @@
+#[macro_export]
 macro_rules! match_or {
     ([$e:expr] $($arm_pat:pat => $arm_body:expr),*; $or:expr) => {
         match $e {
@@ -7,6 +8,7 @@ macro_rules! match_or {
     };
 }
 
+#[macro_export]
 macro_rules! expect {
     ([$e:expr] $arm_pat:pat => $arm_body:expr) => {
         match_or!([$e] $arm_pat => $arm_body;
@@ -19,12 +21,14 @@ macro_rules! expect {
     };
 }
 
+#[macro_export]
 macro_rules! unpack {
     ([$e:expr] $enum_:ident :: $variant:ident ( $($arg:ident),* )) => {
         let ($($arg,)*) = expect!([$e] $enum_::$variant($($arg),*) => ($($arg,)*));
     };
 }
 
+#[macro_export]
 macro_rules! matches {
     ([$e:expr] $($pat:pat),*) => {
         match $e {
