aede11ebcd3abdf92ff7afc56263377df7664e2f
Andrei Homescu <ah@immunant.com>
2017-08-18 18:22:49 -0700

    Implemented cross-check overrides

--- rust-cross-checker/src/lib.rs
+++ rust-cross-checker/src/lib.rs
@@ -14,19 +14,78 @@ use syntax::symbol::Symbol;
 
 fn expand_cross_checks(cx: &mut ExtCtxt,
                        _sp: Span,
-                       _meta_item: &ast::MetaItem,
+                       mi: &ast::MetaItem,
                        item: Annotatable) -> Annotatable {
+    let config = CrossCheckConfig::new(mi);
     match item {
         Annotatable::Item(i) => Annotatable::Item(
-            CrossChecker{ cx: cx, ident: i.ident }.fold_item(i).expect_one("too many items returned")),
+            CrossChecker{ cx: cx, config: config, ident: i.ident }
+            .fold_item(i).expect_one("too many items returned")),
         // TODO: handle TraitItem
         // TODO: handle ImplItem
         _ => panic!("Unexpected item: {:?}", item),
     }
 }
 
+struct CrossCheckConfig {
+    enabled: bool,
+    name: Option<String>,
+    id: Option<u32>,
+}
+
+impl CrossCheckConfig {
+    fn new(mi: &ast::MetaItem) -> CrossCheckConfig {
+        assert!(mi.name == "cross_check");
+        let mut res = CrossCheckConfig {
+            enabled: true,
+            name: None,
+            id: None,
+        };
+        match mi.node {
+            ast::MetaItemKind::Word => { } // Use the defaults for #[cross_check]
+            ast::MetaItemKind::List(ref items) => {
+                for ref nested_item in items {
+                    if let Some(ref item) = nested_item.meta_item() {
+                        match item.name.as_str().as_ref() {
+                            "never" |
+                            "disable" |
+                            "no" => {
+                                res.enabled = false
+                            }
+                            "always" |
+                            "enable" |
+                            "yes" => {
+                                res.enabled = true
+                            }
+                            "name" => {
+                                res.name = item.value_str().map(|s| String::from(s.as_str().as_ref()))
+                            }
+                            "id" => {
+                                if let Some(val) = item.value_str() {
+                                    // FIXME: for now, we only support decimal ids
+                                    // It would be nice to use libsyntax's integer parser
+                                    let id = val.as_str().parse::<u32>();
+                                    if let Ok(id) = id {
+                                        res.id = Some(id)
+                                    } else {
+                                        println!("Warning!!! Could not parse cross_check id: \"{}\"", val);
+                                    }
+                                }
+                            }
+                            name@_ => panic!("Unknown cross_check item: {}", name)
+                        }
+                    }
+                }
+            }
+            _ => panic!("Unknown cross_check item: {:?}", mi.node)
+        }
+        res
+    }
+}
+
 struct CrossChecker<'a, 'cx: 'a> {
     cx: &'a mut ExtCtxt<'cx>,
+    config: CrossCheckConfig,
     ident: ast::Ident,
 }
 
@@ -36,13 +95,21 @@ fn djb2_hash(s: &str) -> u32 {
 
 impl<'a, 'cx> Folder for CrossChecker<'a, 'cx> {
     fn fold_item_kind(&mut self, ik: ast::ItemKind) -> ast::ItemKind {
-        if let ast::ItemKind::Fn(fn_decl, unsafety, constness, abi, generics, block) = ik {
+        if !self.config.enabled {
+            fold::noop_fold_item_kind(ik, self)
+        } else if let ast::ItemKind::Fn(fn_decl, unsafety, constness, abi, generics, block) = ik {
             // Add the cross-check to the beginning of the function
             // TODO: only add the checks to C abi functions???
-            // TODO: allow clients to specify the id or name manually, like this:
+            // Allow clients to specify the id or name manually, like this:
             // #[cross_check(name = "foo")]
-            // #[cross_check(id = 0x12345678u32)]
-            let check_id = djb2_hash(&*self.ident.name.as_str());
+            // #[cross_check(id = "12345678")]
+            let check_id = if let Some(id) = self.config.id {
+                id
+            } else if let Some(ref name) = self.config.name {
+                djb2_hash(name)
+            } else {
+                djb2_hash(self.ident.name.as_str().as_ref())
+            };
             let checked_block = self.fold_block(block).map(|block| {
                 quote_block!(self.cx, {
                     unsafe { rb_xcheck($check_id); }
