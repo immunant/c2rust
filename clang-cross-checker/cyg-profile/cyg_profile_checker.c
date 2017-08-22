dc08d0442475a51097ad57362e33ff2db4941bb7
Andrei Homescu <ah@immunant.com>
2017-08-22 14:10:40 -0700

    Cleaned up hash computation code

--- clang-cross-checker/cyg-profile/cyg_profile_checker.c
+++ clang-cross-checker/cyg-profile/cyg_profile_checker.c
@@ -19,8 +19,8 @@ void __cyg_profile_func_enter(void *, void *) __attribute__((no_instrument_funct
 void __cyg_profile_func_enter(void *func,  void *caller) {
     Dl_info func_info, caller_info;
     dladdr(func, &func_info);
-    printf("Called %s@%p hash:%" PRIx32 "\n",
-           func_info.dli_sname, func,
-           djb2_hash(func_info.dli_sname));
-    // TODO: compute djb2 hash of name, then call rb_xcheck
+    const char *func_name = func_info.dli_sname;
+    uint32_t func_hash = djb2_hash(func_name);
+    printf("Called %s@%p hash:%" PRIx32 "\n", func_name, func, func_hash);
+    // TODO: call rb_xcheck on hash
 }
