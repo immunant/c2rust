d8daae1e1adb958038f3f61855e52ef959de018c
Andrei Homescu <ah@immunant.com>
2017-08-22 14:07:58 -0700

    Added djb2 hash function to cross checker

--- clang-cross-checker/cyg-profile/cyg_profile_checker.c
+++ clang-cross-checker/cyg-profile/cyg_profile_checker.c
@@ -1,15 +1,26 @@
 #define _GNU_SOURCE
 #include <dlfcn.h>
 #include <stdio.h>
+#include <stdint.h>
+#include <inttypes.h>
+
+uint32_t djb2_hash(const unsigned char *str) {
+    uint32_t hash = 5381UL;
+    int c;
+
+    while (c = *str++)
+        hash = ((hash << 5) + hash) + c; /* hash * 33 + c */
+
+    return hash;
+}
 
 void __cyg_profile_func_enter (void *, void *) __attribute__((no_instrument_function, visibility("default")));
 
 void __cyg_profile_func_enter (void *func,  void *caller) {
     Dl_info func_info, caller_info;
     dladdr(func, &func_info);
-    dladdr(caller, &caller_info);
-    printf("Called %s@%p from %s@%p\n",
+    printf("Called %s@%p hash:%" PRIx32 "\n",
            func_info.dli_sname, func,
-           caller_info.dli_sname, caller);
+           djb2_hash(func_info.dli_sname));
     // TODO: compute djb2 hash of name, then call rb_xcheck
 }
