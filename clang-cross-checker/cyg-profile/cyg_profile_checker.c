b5156a2708e9215b00725b9aad22472b72432826
Andrei Homescu <ah@immunant.com>
2017-08-22 14:24:44 -0700

    Import and call rb_xcheck (if available)

--- clang-cross-checker/cyg-profile/cyg_profile_checker.c
+++ clang-cross-checker/cyg-profile/cyg_profile_checker.c
@@ -16,11 +16,13 @@ uint32_t djb2_hash(const unsigned char *str) {
 
 void __cyg_profile_func_enter(void *, void *) __attribute__((no_instrument_function, visibility("default")));
 
+extern void rb_xcheck(unsigned long) __attribute__((weak));
+
 void __cyg_profile_func_enter(void *func,  void *caller) {
     Dl_info func_info, caller_info;
     dladdr(func, &func_info);
     const char *func_name = func_info.dli_sname;
     uint32_t func_hash = djb2_hash(func_name);
-    printf("Called %s@%p hash:%" PRIx32 "\n", func_name, func, func_hash);
-    // TODO: call rb_xcheck on hash
+    if (rb_xcheck)
+        rb_xcheck(func_hash);
 }
