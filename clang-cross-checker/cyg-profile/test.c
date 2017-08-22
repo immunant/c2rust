55cb40befe718fb3400d522e0f62f5d7cab411e2
Andrei Homescu <ah@immunant.com>
2017-08-22 14:08:13 -0700

    Added fibonacci test function to test.c

--- clang-cross-checker/cyg-profile/test.c
+++ clang-cross-checker/cyg-profile/test.c
@@ -1,6 +1,16 @@
 #include <stdio.h>
+#include <stdint.h>
+
+uint64_t fibo(uint64_t n) {
+    if (n <= 1) {
+        return 1;
+    } else {
+        return fibo(n - 1) + fibo(n - 2);
+    }
+}
 
 int main() {
     printf("Profiling test!!!\n");
+    fibo(4);
     return 0;
 }
