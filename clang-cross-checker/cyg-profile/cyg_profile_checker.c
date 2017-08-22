a3e91cbe6f2c54130e22ae74327d30834b948bc3
Andrei Homescu <ah@immunant.com>
2017-08-22 14:09:26 -0700

    Consistent spacing in function declarations

--- clang-cross-checker/cyg-profile/cyg_profile_checker.c
+++ clang-cross-checker/cyg-profile/cyg_profile_checker.c
@@ -14,9 +14,9 @@ uint32_t djb2_hash(const unsigned char *str) {
     return hash;
 }
 
-void __cyg_profile_func_enter (void *, void *) __attribute__((no_instrument_function, visibility("default")));
+void __cyg_profile_func_enter(void *, void *) __attribute__((no_instrument_function, visibility("default")));
 
-void __cyg_profile_func_enter (void *func,  void *caller) {
+void __cyg_profile_func_enter(void *func,  void *caller) {
     Dl_info func_info, caller_info;
     dladdr(func, &func_info);
     printf("Called %s@%p hash:%" PRIx32 "\n",
