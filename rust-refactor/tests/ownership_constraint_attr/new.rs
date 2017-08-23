2eddd2f85de6c9ebb7edee0c261477181f877377
Stuart Pernsteiner <spernsteiner@galois.com>
2017-08-23 17:01:17 -0400

    add test for ownership annotations

---
+++ rust-refactor/tests/ownership_constraint_attr/new.rs
@@ -0,0 +1,42 @@
+#![feature(custom_attribute, attr_literals)]
+
+use std::os::raw::c_void;
+
+extern "C" {
+    fn free(ptr: *mut c_void);
+}
+
+struct RefCounted {
+    #[ownership_static()]
+    ref_count: usize,
+    #[ownership_static()]
+    data: i32,
+}
+
+#[a]
+#[b]
+#[ownership_constraints(le(MOVE, _1))]
+#[ownership_mono("0", READ, MOVE)]
+unsafe fn inc_ref(rc: *mut RefCounted) -> *mut RefCounted {
+    (*rc).ref_count += 1;
+    rc
+}
+
+#[ownership_constraints(le(WRITE, _0), le(MOVE, _0))]
+#[ownership_mono("0", MOVE)]
+unsafe fn dec_ref(rc: *mut RefCounted) {
+    (*rc).ref_count -= 1;
+    if (*rc).ref_count == 0 {
+        free(rc as *mut c_void);
+    }
+}
+
+#[ownership_constraints()]
+#[ownership_mono("0", READ)]
+unsafe fn inc_dec(rc: *mut RefCounted) {
+    let rc2 = inc_ref(rc);
+    dec_ref(rc2);
+}
+
+#[ownership_constraints()]
+fn main() {}
---
+++ rust-refactor/tests/ownership_constraint_attr/old.rs
@@ -0,0 +1,35 @@
+#![feature(custom_attribute, attr_literals)]
+
+use std::os::raw::c_void;
+
+extern "C" {
+    fn free(ptr: *mut c_void);
+}
+
+struct RefCounted {
+    ref_count: usize,
+    data: i32,
+}
+
+#[a]
+// make sure the old attr gets removed
+#[ownership_constraints(le(MOVE, _1))]
+#[b]
+unsafe fn inc_ref(rc: *mut RefCounted) -> *mut RefCounted {
+    (*rc).ref_count += 1;
+    rc
+}
+
+unsafe fn dec_ref(rc: *mut RefCounted) {
+    (*rc).ref_count -= 1;
+    if (*rc).ref_count == 0 {
+        free(rc as *mut c_void);
+    }
+}
+
+unsafe fn inc_dec(rc: *mut RefCounted) {
+    let rc2 = inc_ref(rc);
+    dec_ref(rc2);
+}
+
+fn main() {}
