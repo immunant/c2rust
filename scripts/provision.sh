08aa74707ef23069dd83df189e230c7c2b0bf5bf
perl <perl@immunant.com>
2017-08-23 19:04:15 -0700

    [WIP] Vagrant test environment.

---
+++ scripts/provision.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+SCRIPT_DIR="$(dirname "$0")"
+
+# latest cmake directly from cmake.org -> /opt
+bash $SCRIPT_DIR/provision_cmake.sh
---
+++ scripts/provision_cmake.sh
@@ -0,0 +1,45 @@
+#!/bin/bash
+
+if [[ "$EUID" -ne 0 ]]
+  then echo "Please run as root"
+  exit
+fi
+
+# exit early if cmake in path is the one to be installed
+if type "cmake" > /dev/null 2>&1; then
+  # CMAKE_VERSION=$(cmake --version)
+  cmake --version | grep -q "3.7.2" && { echo >&2 "Cmake requirements already met. Nothing to do."; exit 0; }
+fi
+
+CMAKE_URL="https://cmake.org/files/v3.7/cmake-3.7.2-Linux-x86_64.sh"
+CMAKE_SCRIPT=${CMAKE_URL##*/}
+CMAKE_PREFIX=/opt/$CMAKE_SCRIPT
+CMAKE_SHA256="2e250d9a23764a9c54262c1ddc275b02666b81e2cbe05459d15906825655874b  $CMAKE_SCRIPT"
+
+WORK_DIR=`mktemp -d` && cd $WORK_DIR
+
+# deletes the temp directory
+function cleanup {
+  rm -rf "$WORK_DIR"
+  echo "Deleted temp working directory $WORK_DIR"
+}
+
+# register cleanup function to be called on the EXIT signal
+trap cleanup EXIT
+
+echo "Downloading $CMAKE_SCRIPT"
+wget --quiet $CMAKE_URL || { echo >&2 "Cmake download failed. Aborting."; exit 1; }
+
+# check integrity of the downloaded script
+echo $CMAKE_SHA256 > cmake_sha256sum.txt
+sha256sum -c cmake_sha256sum.txt 2>&1 | grep OK || { echo >&2 "Checksum mismatch. Aborting."; exit 1; }
+
+chmod u+x $CMAKE_SCRIPT
+mkdir -p $CMAKE_PREFIX
+./$CMAKE_SCRIPT --prefix=$CMAKE_PREFIX --skip-license || { echo >&2 "Cmake installation failed. Aborting."; exit 1; }
+
+# on ubuntu, call update alternatives
+source /etc/os-release
+if [ "$NAME" == "Ubuntu" ]; then
+  update-alternatives --install /usr/bin/cmake cmake "$CMAKE_PREFIX/bin/cmake" 1 --slave /usr/bin/ccmake ccmake "$CMAKE_PREFIX/bin/ccmake" --slave /usr/bin/cpack cpack "$CMAKE_PREFIX/bin/cpack" --slave /usr/bin/ctest ctest "$CMAKE_PREFIX/bin/ctest" --slave /usr/bin/cmake-gui cmake-gui "$CMAKE_PREFIX/bin/cmake-gui"
+fi
