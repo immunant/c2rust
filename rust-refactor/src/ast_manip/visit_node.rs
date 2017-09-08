ae220c4109570c2c9131dc80f6cbcf5d3b8367f3
Stuart Pernsteiner <spernsteiner@galois.com>
2017-09-08 09:30:44 -0400

    adjust visit_nodes macro syntax to match fold_nodes

--- rust-refactor/src/ast_manip/visit_node.rs
+++ rust-refactor/src/ast_manip/visit_node.rs
@@ -23,9 +23,9 @@ macro_rules! gen_visit_node_impl {
 
         fn $visit_thing:ident ( &mut $slf:ident,
                                 $arg:ident : &'ast $ArgTy:ty
-                                $( , $args:ident : $ArgTys:ty )* ) {
-            $finish:expr
-        }
+                                $( , $args:ident : $ArgTys:ty )* );
+
+        walk = $walk:expr;
     ) => {
         struct $NodeVisitor<F>
                 where F: FnMut(&$ArgTy) {
@@ -36,7 +36,7 @@ macro_rules! gen_visit_node_impl {
                 where F: FnMut(&$ArgTy) {
             fn $visit_thing(&mut $slf, $arg: &'ast $ArgTy, $($args: $ArgTys,)*) {
                 ($slf.callback)($arg);
-                $finish;
+                $walk;
             }
         }
 
@@ -48,7 +48,7 @@ macro_rules! gen_visit_node_impl {
         impl<'ast, F> Visitor<'ast> for $NodeVisitorPost<F>
                 where F: FnMut(&$ArgTy) {
             fn $visit_thing(&mut $slf, $arg: &'ast $ArgTy, $($args: $ArgTys,)*) {
-                $finish;
+                $walk;
                 ($slf.callback)($arg);
             }
         }
@@ -74,61 +74,53 @@ macro_rules! gen_visit_node_impl {
 // If you want to use `visit_nodes` with more node types, add more `gen_visit_node_impl!`
 // invocations below.
 
-// `gen_visit_node_impl!` works much like `gen_fold_node_impl!` in fold_node.rs, except that `walk`
-// is written directly as the function body instead.
-// (TODO: No reason not to make these consistent, by switching this one to `walk = ...` syntax.)
+// `gen_visit_node_impl!` works much like `gen_fold_node_impl!` in fold_node.rs.
 gen_visit_node_impl! {
     node = Expr;
     visitor = ExprNodeVisitor;
     visitor_post = ExprNodeVisitorPost;
-    fn visit_expr(&mut self, e: &'ast Expr) {
-        visit::walk_expr(self, e)
-    }
+    fn visit_expr(&mut self, e: &'ast Expr);
+    walk = visit::walk_expr(self, e);
 }
 
 gen_visit_node_impl! {
     node = Item;
     visitor = ItemNodeVisitor;
     visitor_post = ItemNodeVisitorPost;
-    fn visit_item(&mut self, i: &'ast Item) {
-        visit::walk_item(self, i)
-    }
+    fn visit_item(&mut self, i: &'ast Item);
+    walk = visit::walk_item(self, i);
 }
 
 gen_visit_node_impl! {
     node = Path;
     visitor = PathNodeVisitor;
     visitor_post = PathNodeVisitorPost;
-    fn visit_path(&mut self, p: &'ast Path, _id: NodeId) {
-        visit::walk_path(self, p)
-    }
+    fn visit_path(&mut self, p: &'ast Path, _id: NodeId);
+    walk = visit::walk_path(self, p);
 }
 
 gen_visit_node_impl! {
     node = Block;
     visitor = BlockNodeVisitor;
     visitor_post = BlockNodeVisitorPost;
-    fn visit_block(&mut self, b: &'ast Block) {
-        visit::walk_block(self, b)
-    }
+    fn visit_block(&mut self, b: &'ast Block);
+    walk = visit::walk_block(self, b);
 }
 
 gen_visit_node_impl! {
     node = Local;
     visitor = LocalNodeVisitor;
     visitor_post = LocalNodeVisitorPost;
-    fn visit_local(&mut self, l: &'ast Local) {
-        visit::walk_local(self, l)
-    }
+    fn visit_local(&mut self, l: &'ast Local);
+    walk = visit::walk_local(self, l);
 }
 
 gen_visit_node_impl! {
     node = ForeignItem;
     visitor = ForeignItemNodeVisitor;
     visitor_post = ForeignItemNodeVisitorPost;
-    fn visit_foreign_item(&mut self, i: &'ast ForeignItem) {
-        visit::walk_foreign_item(self, i)
-    }
+    fn visit_foreign_item(&mut self, i: &'ast ForeignItem);
+    walk = visit::walk_foreign_item(self, i);
 }
 
 /// Visit nodes of the callback's argument type within `target`.  This function performs a preorder
