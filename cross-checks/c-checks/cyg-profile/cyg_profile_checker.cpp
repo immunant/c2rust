80bfbfb4d18a3759123372441de3cd567e627d67
Andrei Homescu <ah@immunant.com>
2017-09-28 15:47:10 -0700

    Added compile-time option to enable/disable memoization of function hashes for cyg_profile_checker

--- cross-checks/c-checks/cyg-profile/cyg_profile_checker.cpp
+++ cross-checks/c-checks/cyg-profile/cyg_profile_checker.cpp
@@ -6,6 +6,8 @@
 #include <unordered_map>
 #include <mutex>
 
+#define MEMOIZE_FUNC_HASHES   1
+
 uint32_t djb2_hash(const uint8_t *str) {
     uint32_t hash = 5381;
     while (uint32_t c = static_cast<uint32_t>(*str++))
@@ -20,16 +22,20 @@ std::unordered_map<void*, uint32_t> hash_cache;
 std::mutex cache_mutex;
 
 uint32_t get_func_hash(void *func) {
+#if MEMOIZE_FUNC_HASHES
     std::lock_guard<std::mutex> lock(cache_mutex);
     auto it = hash_cache.find(func);
     if (it != hash_cache.end())
         return it->second;
+#endif
 
     Dl_info func_info, caller_info;
     dladdr(func, &func_info);
     auto *func_name = reinterpret_cast<const uint8_t*>(func_info.dli_sname);
     uint32_t func_hash = djb2_hash(func_name);
+#if MEMOIZE_FUNC_HASHES
     hash_cache[func] = func_hash;
+#endif
     return func_hash;
 }
 
