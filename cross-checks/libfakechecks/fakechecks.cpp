a8971f93fd8f22b0f92a925a877622744c6b19fe
Andrei Homescu <ah@immunant.com>
2017-09-28 18:28:27 -0700

    Fixed rb_xcheck API in libfakechecks

--- cross-checks/libfakechecks/fakechecks.cpp
+++ cross-checks/libfakechecks/fakechecks.cpp
@@ -1,6 +1,8 @@
 #include <cstdio>
+#include <cstddef>
+#include <cstdint>
 
 extern "C"
-void rb_xcheck(unsigned long item) {
-    fprintf(stderr, "XCHECK:%lu/0x%08lx\n", item, item);
+void rb_xcheck(uint8_t tag, uint64_t item) {
+    fprintf(stderr, "XCHECK(%hhd):%lu/0x%08lx\n", tag, item, item);
 }
