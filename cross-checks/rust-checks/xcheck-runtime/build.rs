4a433e25ca931fffc3905615c81f23ed8ea7ef13
Andrei Homescu <ah@immunant.com>
2017-10-03 19:06:44 -0700

    Add xcheck-runtime feature to use dlsym to locate rb_xcheck symbol at runtime, instead of always linking to libclevrbuf.so

--- cross-checks/rust-checks/xcheck-runtime/build.rs
+++ cross-checks/rust-checks/xcheck-runtime/build.rs
@@ -1,4 +1,14 @@
-fn main() {
+#[cfg(feature="xcheck-with-dlsym")]
+fn add_libclevrbuf() {
+}
+
+#[cfg(not(any(feature="xcheck-with-dlsym")))]
+fn add_libclevrbuf() {
+    // TODO: find a way to get the full path
     println!("cargo:rustc-link-lib=dylib=clevrbuf");
     println!("cargo:rustc-link-search=native=../../ReMon/libclevrbuf");
+}
+
+fn main() {
+    add_libclevrbuf();
 }
