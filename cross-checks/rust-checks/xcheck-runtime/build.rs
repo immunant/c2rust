29dec2af428683011898771942605c1b8f7bdead
Andrei Homescu <ah@immunant.com>
2017-10-04 17:26:41 -0700

    Build full path to libclevrbuf.so in xcheck-runtime build script

--- cross-checks/rust-checks/xcheck-runtime/build.rs
+++ cross-checks/rust-checks/xcheck-runtime/build.rs
@@ -4,9 +4,11 @@ fn add_libclevrbuf() {
 
 #[cfg(not(any(feature="xcheck-with-dlsym", feature="xcheck-with-weak")))]
 fn add_libclevrbuf() {
-    // TODO: find a way to get the full path
+    let here = ::std::path::PathBuf::from(::std::env::current_dir().unwrap());
+    let cross_checks_path = here.parent().unwrap().parent().unwrap();
+    let libclevrbuf_path = cross_checks_path.join("ReMon").join("libclevrbuf");
     println!("cargo:rustc-link-lib=dylib=clevrbuf");
-    println!("cargo:rustc-link-search=native=../../ReMon/libclevrbuf");
+    println!("cargo:rustc-link-search=native={}", libclevrbuf_path.display());
 }
 
 fn main() {
