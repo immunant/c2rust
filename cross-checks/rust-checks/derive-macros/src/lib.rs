d5f46d2d0dcfdaaddaab21fbd6123d14222e22d5
Andrei Homescu <ah@immunant.com>
2017-10-11 16:05:18 -0700

    Add in comment on hashing the name of each structure

--- cross-checks/rust-checks/derive-macros/src/lib.rs
+++ cross-checks/rust-checks/derive-macros/src/lib.rs
@@ -19,6 +19,7 @@ pub fn derive_xcheck_hash(input: TokenStream) -> TokenStream {
     // TODO: we should mark the impl below with #[automatically_derived],
     // but that currently causes an "unused attribute" warning
     //
+    // TODO: also hash in the name of the structure???
     let (impl_generics, ty_generics, where_clause) = struct_def.generics.split_for_impl();
     let ident = &struct_def.ident;
     let hash_impl = quote! {
