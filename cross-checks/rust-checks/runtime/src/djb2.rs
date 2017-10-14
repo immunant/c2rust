c106dd4bb461ce6bd13e5415949f25673602ff12
Andrei Homescu <ah@immunant.com>
2017-10-13 18:01:53 -0700

    Added some simple tests for djb2 hash function in runtime

--- cross-checks/rust-checks/runtime/src/djb2.rs
+++ cross-checks/rust-checks/runtime/src/djb2.rs
@@ -35,3 +35,28 @@ impl Hasher for Djb2Hasher {
             |h, c| h.wrapping_mul(33).wrapping_add(*c as u32));
     }
 }
+
+#[cfg(test)]
+mod tests {
+    use super::Hasher;
+    use super::Djb2Hasher;
+
+    fn djb2_string(s: &str) -> u32 {
+        let mut h = Djb2Hasher::new();
+        h.write(s.as_bytes());
+        h.get_hash()
+    }
+
+    #[test]
+    fn test_djb2() {
+        assert_eq!(djb2_string("a"),        0x0002b606u32);
+        assert_eq!(djb2_string("ab"),       0x00597728u32);
+        assert_eq!(djb2_string("abc"),      0x0b885c8bu32);
+        assert_eq!(djb2_string("abcd"),     0x7c93ee4fu32);
+        assert_eq!(djb2_string("abcde"),    0x0f11b894u32);
+        assert_eq!(djb2_string("abcdef"),   0xf148cb7au32);
+        assert_eq!(djb2_string("abcdefg"),  0x1a623b21u32);
+        assert_eq!(djb2_string("abcdefgh"), 0x66a99fa9u32);
+        assert_eq!(djb2_string("djb2"),     0x7c95b527u32);
+    }
+}
