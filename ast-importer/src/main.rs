bb8d27ff8d93532adfd3a68af0830f48bd6cae09
Alec Theriault <alec.theriault@gmail.com>
2017-10-27 08:56:57 -0700

    Added command line argument parser
    Various debug outputs are hidden behind debug options.

--- ast-importer/src/main.rs
+++ ast-importer/src/main.rs
@@ -1,4 +1,5 @@
 extern crate cbor;
+extern crate clap;
 extern crate syn;
 extern crate ast_importer;
 
@@ -14,49 +15,79 @@ use ast_importer::clang_ast::process;
 use ast_importer::c_ast::*;
 use ast_importer::c_ast::Printer;
 use ast_importer::clang_ast::AstContext;
+use clap::{Arg, App, SubCommand};
 
 fn main() {
 
-    let args: Vec<String> = env::args().collect();
+    let matches = App::new("AST Importer")
+        .version("0.1.0")
+        .author("Eric Mertens <emertens@galois.com>")
+        .arg(Arg::with_name("dump-untyped-clang-ast")
+            .long("ddump-untyped-clang-ast")
+            .help("Prints out CBOR based Clang AST")
+            .takes_value(false))
+        .arg(Arg::with_name("dump-typed-clang-ast")
+            .long("ddump-typed-clang-ast")
+            .help("Prints out the parsed typed Clang AST")
+            .takes_value(false))
+        .arg(Arg::with_name("pretty-typed-clang-ast")
+            .long("dpretty-typed-clang-ast")
+            .help("Pretty-prints out the parsed typed Clang AST")
+            .takes_value(false))
+        .arg(Arg::with_name("INPUT")
+            .help("Sets the input CBOR file to use")
+            .required(true)
+            .index(1))
+        .get_matches();
 
-    if args.len() < 2 {
-        println!("Usage: ast-importer FILENAME");
-        return;
-    }
+    let file = matches.value_of("INPUT").unwrap();
+    let dump_untyped_context = matches.is_present("dump-untyped-clang-ast");
+    let dump_typed_context = matches.is_present("dump-typed-clang-ast");
+    let pretty_typed_context = matches.is_present("pretty-typed-clang-ast");
 
-    let output = parse_and_dump(&args[1]);
-    match output {
-        Err(e) => println!("{:#?}", e),
+    // Extract from the CBOR file the untyped AST
+    let untyped_context = match parse_untyped_ast(file) {
+        Err(e) => panic!("{:#?}", e),
         Ok(cxt) => {
-            println!("{:#?}", cxt);
+            if dump_untyped_context {
+                println!("{:#?}", cxt);
+            }
+            cxt
+        },
+    };
 
-            let mut conv = ConversionContext::new(&cxt);
-            conv.convert(&cxt);
+    // Conditionally convert this to a typed AST
+    if dump_typed_context || pretty_typed_context {
+        let mut conv = ConversionContext::new(&untyped_context);
+        conv.convert(&untyped_context);
+
+        if dump_typed_context {
+            println!("Typed Clang AST");
+            println!("{:#?}", conv.typed_context);
+        }
+
+        if pretty_typed_context {
+            println!("Pretty-printed typed Clang AST");
             println!("{:#?}", Printer::new().print(&conv.typed_context));
-        },
+        }
     }
 
-    use syn::parse;
-    use quote::ToTokens;
-    use quote::Tokens;
-    if let parse::IResult::Done(_, t) = parse::ty("[u32; 10]") {
-        let mut tokens = Tokens::new();
-        t.to_tokens(&mut tokens);
-        println!("{}", tokens.as_str());
-    }
 
+//    use syn::parse;
+//    use quote::ToTokens;
+//    use quote::Tokens;
+//    if let parse::IResult::Done(_, t) = parse::ty("[u32; 10]") {
+//        let mut tokens = Tokens::new();
+//        t.to_tokens(&mut tokens);
+//        println!("{}", tokens.as_str());
+//    }
+
+    // Perform the translation
     use ast_importer::translator::translate;
-    let output = parse_and_dump(&args[1]);
-    match output {
-        Err(e) => println!("{:#?}", e),
-        Ok(cxt) => {
-            println!("{}", translate(cxt));
-        },
-    }
-
+    println!("{}", translate(untyped_context));
 }
 
-fn parse_and_dump(filename: &str) -> Result<AstContext, Error> {
+fn parse_untyped_ast(filename: &str) -> Result<AstContext, Error> {
     let mut f = File::open(filename)?;
     let mut buffer = vec![];
     f.read_to_end(&mut buffer)?;
