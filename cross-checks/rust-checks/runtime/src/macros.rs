113df2264380812883d718a04d50f1245181bce0
Andrei Homescu <ah@immunant.com>
2017-10-27 15:36:02 -0700

    Improved pattern replacement for cross_check_xxx! macros

--- cross-checks/rust-checks/runtime/src/macros.rs
+++ cross-checks/rust-checks/runtime/src/macros.rs
@@ -2,24 +2,24 @@
 #[macro_export]
 macro_rules! cross_check_raw {
     ($item:expr) => {
-        cross_check_raw!(UNKNOWN_TAG, $item);
+        cross_check_raw!(UNKNOWN_TAG, $item)
     };
     ($tag:ident, $item:expr) => {
-        $crate::xcheck::xcheck($crate::xcheck::$tag, $item as u64);
+        $crate::xcheck::xcheck($crate::xcheck::$tag, $item as u64)
     };
 }
 
 #[macro_export]
 macro_rules! cross_check_value {
    ($value:expr) => {
-       cross_check_value!(UNKNOWN_TAG, $value);
+       cross_check_value!(UNKNOWN_TAG, $value)
    };
-   ($tag:ident, $value:expr) => {
+   ($tag:ident, $value:expr) => {{
        use $crate::hash::CrossCheckHash;
        use $crate::hash::jodyhash::JodyHasher;
        use $crate::hash::simple::SimpleHasher;
        $crate::xcheck::xcheck(
            $crate::xcheck::$tag,
-           CrossCheckHash::cross_check_hash::<JodyHasher, SimpleHasher>(&$value));
-   };
+           CrossCheckHash::cross_check_hash::<JodyHasher, SimpleHasher>(&$value))
+   }};
 }
