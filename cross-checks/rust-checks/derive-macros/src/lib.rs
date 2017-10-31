fa3b6e3e8ea4a7d73f6159ea89915e9a3e3a6a7d
Andrei Homescu <ah@immunant.com>
2017-10-30 17:10:35 -0700

    Implement some #[cross_check_hash] attributes for structure fields: no/never/disable and custom

--- cross-checks/rust-checks/derive-macros/src/lib.rs
+++ cross-checks/rust-checks/derive-macros/src/lib.rs
@@ -8,6 +8,35 @@ extern crate quote;
 fn xcheck_hash_derive(s: synstructure::Structure) -> quote::Tokens {
     // Iterate through all fields, inserting the hash computation for each field
     let hash_fields = s.each(|f| {
+        let xcheck_hash_attr = f.ast().attrs.iter().find(
+            |f| f.name() == "cross_check_hash");
+        if let Some(ref attr) = xcheck_hash_attr {
+            if let syn::MetaItem::List(ref ident, ref items) = attr.value {
+                assert!(ident == "cross_check_hash");
+                for item in items {
+                    if let &syn::NestedMetaItem::MetaItem(ref mi) = item {
+                        match *mi {
+                            syn::MetaItem::Word(ref kw)
+                                if kw == "no" ||
+                                   kw == "never" ||
+                                   kw == "disable" => return quote! {},
+
+                            syn::MetaItem::NameValue(ref kw, ref val)
+                                if kw == "custom" => match *val {
+                                    syn::Lit::Str(ref s, syn::StrStyle::Cooked) => {
+                                        let id = syn::Ident::from(s.clone());
+                                        return quote! { #id(&mut h, #f) };
+                                    }
+                                    _ => panic!("invalid identifier passed to #[cross_check_hash(custom = ...)]")
+                            }
+
+                            _ => panic!("unknown parameter for #[cross_check_hash]")
+                        }
+                    }
+                }
+            }
+        }
+        // Default implementation
         quote! {
             use cross_check_runtime::hash::CrossCheckHash;
             h.write_u64(CrossCheckHash::cross_check_hash_depth::<__XCHA, __XCHS>(#f, _depth - 1));
