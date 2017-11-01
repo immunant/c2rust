0b0291aba69b6e38aeb4d7ec05b1314c8086ff59
Andrei Homescu <ah@immunant.com>
2017-10-31 18:15:10 -0700

    Pass cross_check hashing attributes from the rustc plugin to derive-macros

--- cross-checks/rust-checks/rustc-plugin/src/lib.rs
+++ cross-checks/rust-checks/rustc-plugin/src/lib.rs
@@ -88,6 +88,13 @@ impl CrossCheckConfig {
                                               .map(|s| cx.parse_tts(String::from(&*s.as_str())))
                                               .unwrap_or_else(|| vec![])
                         }
+
+                        // Ignore arguments for #[derive(CrossCheckHash)]
+                        "custom_hash" |
+                        "ahasher_override" |
+                        "shasher_override" |
+                        "hasher" => (),
+
                         name@_ => panic!("Unknown cross_check item: {}", name)
                     }
                 }
@@ -197,10 +204,11 @@ impl<'a, 'cx> CrossChecker<'a, 'cx> {
                 // Prepend #[derive(CrossCheckHash)] automatically
                 // to every structure definition
                 let mut item_attrs = folded_item.attrs;
+                let xcheck_attr = find_cross_check_attr(&item_attrs).cloned();
                 let xcheck_attr_disabled = {
                     // If the structure has #[cross_check(no)] or #[cross_check(disable)],
                     // do not automatically add CrossCheckHash
-                    find_cross_check_attr(&item_attrs)
+                    xcheck_attr.as_ref()
                         .map(|a| a.parse_meta(self.cx.parse_sess).unwrap())
                         .and_then(|mi| mi.meta_item_list().map(
                             |items| items.iter().any(
@@ -212,6 +220,7 @@ impl<'a, 'cx> CrossChecker<'a, 'cx> {
                 if !xcheck_attr_disabled {
                     let xcheck_hash_attr = quote_attr!(self.cx, #[derive(CrossCheckHash)]);
                     item_attrs.push(xcheck_hash_attr);
+                    item_attrs.extend(xcheck_attr.into_iter());
                 }
                 ast::Item {
                     attrs: item_attrs,
