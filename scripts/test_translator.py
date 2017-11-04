b0e26a8c0ec19af1ff585ab332a6fb0407813bcb
Per Larsen <perl@uci.edu>
2017-11-03 19:38:24 -0700

    Make cbor extraction step work on Linux in test_translator.py

--- scripts/test_translator.py
+++ scripts/test_translator.py
@@ -89,6 +89,9 @@ class TestCase:
 
         # run the extractor
         args = [self.src_c]
+        # make sure we can locate system libraries
+        sys_incl_dirs = get_system_include_dirs()
+        args += ["-extra-arg=-I" + i for i in sys_incl_dirs]
         return ast_extractor[args].run(retcode=None)
 
     def translate(self):
