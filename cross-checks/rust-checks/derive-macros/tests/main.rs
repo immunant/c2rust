fb70d47d5bd74a1c2a08b8e8c2fec5d525c55c6f
Andrei Homescu <ah@immunant.com>
2017-11-09 20:08:49 -0800

    Added test helper macro in derive-macros to simplify testing structures

--- cross-checks/rust-checks/derive-macros/tests/main.rs
+++ cross-checks/rust-checks/derive-macros/tests/main.rs
@@ -1,4 +1,7 @@
 
+#![feature(trace_macros)]
+//trace_macros!(true);
+
 #[macro_use]
 extern crate cross_check_derive;
 extern crate cross_check_runtime;
@@ -7,29 +10,45 @@ use cross_check_runtime::hash::CrossCheckHash as XCH;
 use cross_check_runtime::hash::simple::SimpleHasher;
 use cross_check_runtime::hash::djb2::Djb2Hasher;
 
+macro_rules! test_struct {
+    ([$($attrs:meta)*]
+     {$($field_def:ident:$field_ty:ty),*}
+     ($($field_init:ident:$val:expr),*)
+     $test_fn:expr) => {
+        #[derive(CrossCheckHash)]
+        #[cross_check_hash($($attrs)*)]
+        struct TestStruct {
+            $($field_def: $field_ty),*
+        };
+        let ts = TestStruct { $($field_init: $val),* };
+        $test_fn(ts)
+    }
+}
+
 #[test]
 fn test_custom_hash() {
-    #[derive(CrossCheckHash)]
-    #[cross_check_hash(custom_hash="custom1")]
-    struct A;
-
-    fn custom1<XCHA, XCHS>(_a: &A, _: usize) -> u64 {
+    fn custom1<XCHA, XCHS>(_a: &TestStruct, _: usize) -> u64 {
         0x12345678
     }
 
-    let a = A;
-    assert_eq!(
-        XCH::cross_check_hash::<Djb2Hasher, Djb2Hasher>(&a),
-        0x12345678);
+    test_struct!([custom_hash="custom1"]
+                 {}
+                 ()
+                 |ts| {
+        assert_eq!(
+            XCH::cross_check_hash::<Djb2Hasher, Djb2Hasher>(&ts),
+            0x12345678);
+    });
 }
 
 #[test]
 fn test_empty_struct_djb2() {
-    #[derive(CrossCheckHash)]
-    struct A;
-
-    let a = A;
-    assert_eq!(
-        XCH::cross_check_hash::<Djb2Hasher, Djb2Hasher>(&a),
-        5381_u64);
+    test_struct!([]
+                 {}
+                 ()
+                 |ts| {
+        assert_eq!(
+            XCH::cross_check_hash::<Djb2Hasher, Djb2Hasher>(&ts),
+            5381_u64);
+    });
 }
