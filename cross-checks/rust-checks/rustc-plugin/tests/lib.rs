5b75f87ac332244adb4ad51626dad308001258c0
Andrei Homescu <ah@immunant.com>
2017-11-13 15:51:46 -0800

    Added placeholder tests file for rustc plugin

---
+++ cross-checks/rust-checks/rustc-plugin/tests/lib.rs
@@ -0,0 +1,12 @@
+#![feature(plugin, custom_attribute)]
+#![cross_check]
+
+#[macro_use]
+extern crate cross_check_derive;
+#[macro_use]
+extern crate cross_check_runtime;
+
+#[test]
+fn test() {
+    // TODO
+}
