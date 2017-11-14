acd3295ad0dd411a82cde2b6c4f53106c0680999
Andrei Homescu <ah@immunant.com>
2017-11-13 17:29:38 -0800

    Store cross-checks in a deque data structure and add test infrastructure to assert on added cross-checks

--- cross-checks/rust-checks/rustc-plugin/tests/lib.rs
+++ cross-checks/rust-checks/rustc-plugin/tests/lib.rs
@@ -7,16 +7,31 @@ extern crate cross_check_derive;
 #[macro_use]
 extern crate cross_check_runtime;
 
+use std::collections::VecDeque;
+use std::cell::RefCell;
+
+#[derive(Debug, PartialEq, Eq)]
+struct XCheck(u8, u64);
+
+thread_local! {
+    static XCHECKS: RefCell<VecDeque<XCheck>> = RefCell::new(VecDeque::new())
+}
+
 #[no_mangle]
 pub extern fn rb_xcheck(tag: u8, val: u64) {
-    println!("tag:{} val:{}", tag, val);
+    XCHECKS.with(|xc| xc.borrow_mut().push_back(XCheck(tag, val)));
+}
+
+fn expect_xcheck(tag: u8, val: u64) {
+    let xc = XCHECKS.with(|xc| xc.borrow_mut().pop_front().unwrap());
+    assert_eq!(xc, XCheck(tag, val));
 }
 
 #[test]
 fn test_entry() {
     #[cross_check(yes)]
-    fn foo() { }
+    fn abcd() { }
 
-    foo();
-    // TODO: add expect_xcheck calls
+    abcd();
+    expect_xcheck(1, 0x7c93ee4f_u64);
 }
