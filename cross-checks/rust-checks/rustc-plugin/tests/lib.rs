59db26a5433cc120534377bedc4742d206fdca8b
Andrei Homescu <ah@immunant.com>
2017-11-13 17:14:40 -0800

    Use xcheck-provided runtime feature to provide our own rb_xcheck in the rustc-plugin tests and add a simple test case

--- cross-checks/rust-checks/rustc-plugin/tests/lib.rs
+++ cross-checks/rust-checks/rustc-plugin/tests/lib.rs
@@ -1,12 +1,22 @@
 #![feature(plugin, custom_attribute)]
-#![cross_check]
+#![plugin(cross_check_plugin)]
+#![cross_check(none)]
 
 #[macro_use]
 extern crate cross_check_derive;
 #[macro_use]
 extern crate cross_check_runtime;
 
+#[no_mangle]
+pub extern fn rb_xcheck(tag: u8, val: u64) {
+    println!("tag:{} val:{}", tag, val);
+}
+
 #[test]
-fn test() {
-    // TODO
+fn test_entry() {
+    #[cross_check(yes)]
+    fn foo() { }
+
+    foo();
+    // TODO: add expect_xcheck calls
 }
