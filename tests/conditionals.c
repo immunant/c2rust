3e92891e6bf73561ca55e4c111e17a4c65a4be16
Eric Mertens <emertens@galois.com>
2018-01-09 09:39:41 -0800

    Add ! to conditionals test case

--- tests/conditionals.c
+++ tests/conditionals.c
@@ -1,26 +1,39 @@
 void entry(const unsigned buffer_size, int buffer[const]) {
 
-        if (buffer_size < 10) return;
+        if (buffer_size < 30) return;
+        int i = 0;
 
         // Test function pointer comparisons
         void(* const f0)(unsigned, int*) = 0;
         void(* const f1)(unsigned, int*) = entry;
-        if (f0) { buffer[0] = 1; }
-        if (f1) { buffer[1] = 1; }
+        if (f0) { buffer[i++] = 1; }
+        if (f1) { buffer[i++] = 1; }
 
-        if (0.0) { buffer[2] = 1; }
-        if (1.0) { buffer[3] = 1; }
+        if (!f0) { buffer[i++] = 1; }
+        if (!f1) { buffer[i++] = 1; }
 
-        if (0) { buffer[4] = 1; }
-        if (1) { buffer[5] = 1; }
+        if (0.0) { buffer[i++] = 1; }
+        if (1.0) { buffer[i++] = 1; }
+
+        if (0) { buffer[i++] = 1; }
+        if (1) { buffer[i++] = 1; }
 
         void *p0 = 0;
         const void *p1 = &buffer_size;
 
-        if (p0) { buffer[6] = 1; }
-        if (p1) { buffer[7] = 1; }
+        if (p0) { buffer[i++] = 1; }
+        if (p1) { buffer[i++] = 1; }
 
         // Test simplifiable conditions
-        if (0 == 0) { buffer[8] = 1; }
-        if (p1 && 0 > -3) { buffer[9] = 1; }
+        if (0 == 0) { buffer[i++] = 1; }
+        if (p1 && 0 > -3) { buffer[i++] = 1; }
+
+        if (!p0) { buffer[i++] = 1; }
+        if (!p1) { buffer[i++] = 1; }
+
+        if (p0 && !p0) { buffer[i++] = 1; }
+        if (p0 || !p0) { buffer[i++] = 1; }
+
+        if (p1 && !p1) { buffer[i++] = 1; }
+        if (p1 || !p1) { buffer[i++] = 1; }
 }
