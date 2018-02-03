662d5fb7f907b301b4cc614df481d2abad73a05e
Andrei Homescu <ah@immunant.com>
2018-02-02 16:33:30 -0800

    Added simple fibonacci test crate for rustc cross-check plugin

---
+++ cross-checks/rust-checks/tests/fibo/src/fibo.rs
@@ -0,0 +1,41 @@
+//struct FiboArg(u64, u64);
+//#[derive(Hash)]
+//struct FiboArg<T>(#[cross_check(custom_hash = "tch")] T, #[cross_check(none)] T);
+//struct FiboArg(#[cross_check(custom_hash = "tch")] u64, u64);
+//#[cross_check(custom_hash = "hash_arg")] struct FiboArg(u64, u64, #[cross_check(none)] String);
+//struct FiboArg(#[cross_check(fixed = "0x1234")] u64, #[cross_check(fixed = "0x2345")] u64);
+//struct FiboArg(#[cross_check(fixed = 0x1234)] u64, u64, #[cross_check(none)] String);
+
+struct FiboArg(u64, u64, #[cross_check(none)] String);
+
+#[cross_check(none)]
+fn hash_arg<XCHA, XCHS>(arg: &FiboArg, _: usize) -> u64 {
+    arg.0.wrapping_mul(0x10000).wrapping_add(arg.1)
+}
+
+#[cross_check(none)]
+fn foo(v: &u64) -> u64 {
+    v.wrapping_add(4096)
+}
+
+#[cross_check(none)]
+fn tch<XCHA, XCHS, S, F>(h: &mut XCHA, _: &S, field: F, _: usize)
+        where XCHA: ::cross_check_runtime::hash::CrossCheckHasher,
+              F: ::std::borrow::Borrow<String> {
+    h.write_u64(field.borrow().parse::<u64>().unwrap());
+}
+
+//#[cross_check(none)]
+//#[cross_check(yes, ahasher="::cross_check_runtime::hash::djb2::Djb2Hasher", shasher="::cross_check_runtime::hash::djb2::Djb2Hasher")]
+//#[cross_check(yes, args(n(default)))]
+#[cross_check(yes, all_args(fixed=0x234), args())]
+pub fn fibo(n: u64) -> u64 {
+    //#[cross_check]
+    let fa = FiboArg(n.wrapping_sub(1), n.wrapping_sub(2), n.to_string());
+    cross_check_value!(fa);
+    match n {
+        0 |
+        1 => 1,
+        _ => fibo(n - 1) + fibo(n - 2),
+    }
+}
---
+++ cross-checks/rust-checks/tests/fibo/src/main.rs
@@ -0,0 +1,17 @@
+#![feature(plugin, custom_attribute)]
+#![plugin(cross_check_plugin(config_file = "fibo.c2r"))]
+#![cross_check]
+
+#[macro_use]
+extern crate cross_check_derive;
+#[macro_use]
+extern crate cross_check_runtime;
+
+mod fibo;
+
+fn main() {
+    for i in 0..5 {
+        //println!("fibo({})={}", i, fibo(i));
+        fibo::fibo(i);
+    }
+}
