6d10d9f70bd78cadfd8069e678ce8e1df5854d0e
Eric Mertens <emertens@galois.com>
2018-02-06 10:26:02 -0800

    Add test for malloc

---
+++ tests/malloc.c
@@ -0,0 +1,21 @@
+#include <stdlib.h>
+
+void entry(const unsigned buffer_size, int buffer[const])
+{
+        int i = 0;
+
+        void **p = malloc(sizeof(*p));
+        free(p);
+
+        int *ip = malloc(sizeof(*ip));
+        *ip = 34;
+        buffer[i++] = *ip;
+
+        ip = realloc(ip, 2 * sizeof(*ip));
+        ip[0] = 35;
+        ip[1] = 36;
+        buffer[i++] = ip[0];
+        buffer[i++] = ip[1];
+
+        free(ip);
+}
