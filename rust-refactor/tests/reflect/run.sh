e33fcab5fd2ce71fad172727a4f2e10e4a4fd96a
Per Larsen <perl@immunant.com>
2018-02-07 16:36:27 -0800

    changes to run idiomize testsuite on macOS.

--- rust-refactor/tests/reflect/run.sh
+++ rust-refactor/tests/reflect/run.sh
@@ -1,3 +1,9 @@
 #!/bin/sh
+
+# work around System Integrity Protection on macOS
+if [[ `uname` == 'Darwin' ]]; then
+    export LD_LIBRARY_PATH=$not_LD_LIBRARY_PATH
+fi
+
 $refactor \
     test_reflect -- old.rs $rustflags
--- rust-refactor/tests/retype_argument/run.sh
+++ rust-refactor/tests/retype_argument/run.sh
@@ -1,4 +1,10 @@
 #!/bin/sh
+
+# work around System Integrity Protection on macOS
+if [[ `uname` == 'Darwin' ]]; then
+    export LD_LIBRARY_PATH=$not_LD_LIBRARY_PATH
+fi
+
 $refactor \
     select target 'crate; desc(arg && any_child(match_pat(y)));' \; \
     retype_argument u8 '__old as u8' '__new as i32' -- old.rs $rustflags
