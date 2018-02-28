cd597fc786c9ab15a799305546867da4890478e2
Andrei Homescu <ah@immunant.com>
2018-02-27 17:04:43 -0800

    Modified cross-check-runtime to use the builtin liblibc, instead of the one from crates.io

--- cross-checks/rust-checks/runtime/src/lib.rs
+++ cross-checks/rust-checks/runtime/src/lib.rs
@@ -2,7 +2,8 @@
 #![cfg_attr(feature="xcheck-with-dlsym", feature(const_fn))]
 #![cfg_attr(feature="xcheck-with-dlsym", feature(const_ptr_null_mut))]
 #![cfg_attr(feature="xcheck-with-dlsym", feature(libc))]
-#![cfg_attr(feature="xcheck-with-weak", feature(linkage))]
+#![cfg_attr(feature="xcheck-with-weak",  feature(linkage))]
+#![cfg_attr(feature="libc-hash",         feature(libc))]
 
 #[cfg(feature="djb2-ssse3")]
 extern crate simd;
