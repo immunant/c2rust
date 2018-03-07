d9bb8fab4697e9b2385cb21999a27f8a42676391
Miguel Saldivar <miguel.saldivar22@hotmail.com>
2018-03-06 16:40:59 -0800

    ported arrays

--- tests2/arrays/incomplete_arrays.c
+++ tests2/arrays/incomplete_arrays.c
@@ -1,13 +1,12 @@
-
-
-void entry(const unsigned buffer_size, int buffer[])
+void entry2(const unsigned buffer_size, int buffer[])
 {
     int arr[3] = {1,2,3};
     int (*p_arr)[] = &arr;
     int x = (*p_arr)[0];
+    buffer[0] = x;
 
     const int carr[3] = {1,2,3};
     const int (*p_carr)[] = &carr;
     int cx = (*p_carr)[0];
+    buffer[1] = cx;
 }
-
--- tests2/arrays/test_arrays.rs
+++ tests2/arrays/test_arrays.rs
@@ -1,14 +1,19 @@
 extern crate libc;
 
 use arrays::entry as rust_entry;
+use incomplete_arrays::entry2 as rust_entry2;
 use self::libc::{c_int, c_uint};
 
 extern "C" {
     #[no_mangle]
     fn entry(_: c_uint, _: *mut c_int);
+
+    #[no_mangle]
+    fn entry2(_: c_uint, _: *mut c_int);
 }
 
 const BUFFER_SIZE: usize = 34;
+const BUFFER_SIZE2: usize = 2;
 
 pub fn test_buffer() {
     let mut buffer = [0; BUFFER_SIZE];
@@ -29,4 +34,18 @@ pub fn test_buffer() {
         assert_eq!(buffer[index], rust_buffer[index]);
         assert_eq!(buffer[index], expected_buffer[index], "idx: {}", index);
     }
+}
+
+pub fn test_buffer2() {
+    let mut buffer = [0; BUFFER_SIZE2];
+    let mut rust_buffer = [0; BUFFER_SIZE2];
+    let expected_buffer = [1, 1];
+
+    unsafe {
+       entry2(BUFFER_SIZE2 as u32, buffer.as_mut_ptr());
+       rust_entry2(BUFFER_SIZE2 as u32, rust_buffer.as_mut_ptr());
+    }
+
+    assert_eq!(buffer, rust_buffer);
+    assert_eq!(buffer, expected_buffer);
 }
