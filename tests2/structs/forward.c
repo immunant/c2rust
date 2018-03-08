f0d4a2cd5907d6d49a6adabcd58a124b4a714026
Miguel Saldivar <miguel.saldivar22@hotmail.com>
2018-03-07 16:34:17 -0800

    ported union and some other tests

---
+++ tests2/structs/forward.c
@@ -0,0 +1,12 @@
+
+// forward declaration of structs should not cause problems.
+struct s;
+struct s { int x; }; 
+
+void forward(const unsigned int buffer_size, int buffer[const]){ 
+    if (buffer_size < 1) return;
+
+    struct s foo = { 1 };
+    buffer[0] = foo.x;
+}
+
