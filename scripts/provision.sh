3569b7ebc500e7b108c98e61e998546a8c3006d1
Per Larsen <perl@immunant.com>
2018-03-12 18:39:44 -0700

    Provisioning fixes.

--- scripts/provision.sh
+++ scripts/provision.sh
@@ -11,8 +11,8 @@ SCRIPT_DIR="$(dirname "$0")"
 apt-get update -qq
 # Haven't found a way to upgrade open-vm-tools non-interactively
 # apt-get install -y --only-upgrade open-vm-tools
-apt-get install -qq htop unzip tmux lldb-5.0 vim curl gnupg2 cmake
-apt-get install -qq software-properties-common build-essential llvm-5.0 clang-5.0
+apt-get install -qq htop unzip tmux lldb-5.0 vim curl gnupg2 cmake 
+apt-get install -qq software-properties-common build-essential llvm-5.0 clang-5.0 ninja-build
 
 update-alternatives --install /usr/bin/clang clang /usr/bin/clang-5.0 100
 update-alternatives --install /usr/bin/clang++ clang++ /usr/bin/clang++-5.0 100
