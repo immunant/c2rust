8c47f31a84257bdfbab3f709aa657bea92930f86
Daniel Kolsoi <dkolsoi@pixability.com>
2018-03-13 09:51:09 -0400

    Resolved merge conflict in master

--- scripts/test_translator.py
+++ scripts/test_translator.py
@@ -93,9 +93,11 @@ class CFile:
         # run the extractor
         args = [self.path]
 
-        # make sure we can locate system libraries
-        sys_incl_dirs = get_system_include_dirs()
-        args += ["-extra-arg=-I" + i for i in sys_incl_dirs]
+        # NOTE: it doesn't seem necessary to specify system include
+	# directories and in fact it may cause problems on macOS.
+	## make sure we can locate system include files
+        ## sys_incl_dirs = get_system_include_dirs()
+        ## args += ["-extra-arg=-I" + i for i in sys_incl_dirs]
 
         # log the command in a format that's easy to re-run
         logging.debug("extraction command:\n %s", str(ast_extractor[args]))
