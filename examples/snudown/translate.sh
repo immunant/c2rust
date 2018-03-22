034f3bc8f0b36ec5cbe54ab7f00ec5b485ecd83e
Andrei Homescu <ah@immunant.com>
2018-03-22 15:48:11 -0700

    Use absolute paths in translate.sh

--- examples/snudown/translate.sh
+++ examples/snudown/translate.sh
@@ -2,6 +2,7 @@
 
 MACHINE_NAME=`uname -n`
 C2RUST=$(readlink -f $(dirname $0)/../..)
+SNUDOWN=$(readlink -f $(dirname $0)/repo)
 AST_EXTRACTOR=$C2RUST/dependencies/llvm-6.0.0/build.$MACHINE_NAME/bin/ast-extractor
 AST_IMPORTER=$C2RUST/ast-importer/target/debug/ast_importer
 LIB_PATH=$HOME/.rustup/toolchains/nightly-2018-01-06-x86_64-unknown-linux-gnu/lib/
@@ -12,21 +13,21 @@ XCHECK_PLUGIN=$XCHECK_TOPDIR/rustc-plugin/target/debug/libcross_check_plugin.so
 XCHECK_DERIVE=$XCHECK_TOPDIR/derive-macros/target/debug/libcross_check_derive.so
 XCHECK_RUNTIME=$XCHECK_TOPDIR/runtime/target/debug/libcross_check_runtime.rlib
 
-OUTPUT_DIR=translator-build
+OUTPUT_DIR=$SNUDOWN/translator-build
 
 translate() {
-  $AST_EXTRACTOR src/$1.c
-  env RUST_BACKTRACE=1 LD_LIBRARY_PATH=$LIB_PATH $AST_IMPORTER --reloop-cfgs src/$1.c.cbor > $OUTPUT_DIR/$1.rs
+  $AST_EXTRACTOR $SNUDOWN/src/$1.c
+  env RUST_BACKTRACE=1 LD_LIBRARY_PATH=$LIB_PATH $AST_IMPORTER --reloop-cfgs $SNUDOWN/src/$1.c.cbor > $OUTPUT_DIR/$1.rs
   #$RUSTFMT $OUTPUT_DIR/$1.rs --force
   rustc --crate-type=rlib --crate-name=$1 $OUTPUT_DIR/$1.rs -o $OUTPUT_DIR/lib$1.rlib
 }
 
 translate_xcheck() {
-  $AST_EXTRACTOR src/$1.c
+  $AST_EXTRACTOR $SNUDOWN/src/$1.c
   env RUST_BACKTRACE=1 LD_LIBRARY_PATH=$LIB_PATH \
       $AST_IMPORTER --reloop-cfgs --cross-checks \
       --cross-check-config ../snudown_rust.c2r \
-      -- src/$1.c.cbor > $OUTPUT_DIR/$1.rs
+      -- $SNUDOWN/src/$1.c.cbor > $OUTPUT_DIR/$1.rs
   #$RUSTFMT $OUTPUT_DIR/$1.rs --force
   rustc -g --crate-type=rlib --crate-name=$1 \
       --extern cross_check_plugin=$XCHECK_PLUGIN \
@@ -38,26 +39,26 @@ translate_xcheck() {
 
 compile_commands_entry() {
 
-        cat >> compile_commands.json <<END
+        cat >> $SNUDOWN/compile_commands.json <<END
 {
-  "directory": "${PWD}",
-  "command": "cc -o ${OUTPUT_DIR}/${1}.c.o -c ${PWD}/src/${1}.c -Wwrite-strings -D_FORTIFY_SOURCE=0 -DNDEBUG=1",
-  "file": "${PWD}/src/${1}.c"
+  "directory": "${SNUDOWN}",
+  "command": "cc -o ${OUTPUT_DIR}/${1}.c.o -c ${SNUDOWN}/src/${1}.c -Wwrite-strings -D_FORTIFY_SOURCE=0 -DNDEBUG=1",
+  "file": "${SNUDOWN}/src/${1}.c"
 },
 END
 
 }
 
 # Generate html_entities.h from html_entities.gpers (setup.py used to do this)
-`cd src && gperf html_entities.gperf --output-file=html_entities.h`
+`cd $SNUDOWN/src && gperf html_entities.gperf --output-file=html_entities.h`
 
 if [ "${1}" == "translate" ]; then
-  echo "[" > compile_commands.json
+  echo "[" > $SNUDOWN/compile_commands.json
   compile_commands_entry "autolink"
   compile_commands_entry "buffer"
   compile_commands_entry "stack"
   compile_commands_entry "markdown"
-  echo "]" >> compile_commands.json
+  echo "]" >> $SNUDOWN/compile_commands.json
 
   mkdir $OUTPUT_DIR
  
@@ -66,16 +67,17 @@ if [ "${1}" == "translate" ]; then
   translate "stack"
   translate "markdown"
  
-  rustc --crate-name=snudownrust --crate-type=staticlib -L $OUTPUT_DIR ../snudownrust.rs -o $OUTPUT_DIR/libsnudownrust.a
+  rustc --crate-name=snudownrust --crate-type=staticlib -L $OUTPUT_DIR \
+      $C2RUST/examples/snudown/snudownrust.rs -o $OUTPUT_DIR/libsnudownrust.a
 
 elif [ "$1" == "rustcheck" ]; then
 
-  echo "[" > compile_commands.json
+  echo "[" > $SNUDOWN/compile_commands.json
   compile_commands_entry "autolink"
   compile_commands_entry "buffer"
   compile_commands_entry "stack"
   compile_commands_entry "markdown"
-  echo "]" >> compile_commands.json
+  echo "]" >> $SNUDOWN/compile_commands.json
 
   mkdir $OUTPUT_DIR
 
@@ -87,6 +89,6 @@ elif [ "$1" == "rustcheck" ]; then
   rustc --crate-name=snudownrust --crate-type=staticlib -L $OUTPUT_DIR \
       --extern cross_check_derive=$XCHECK_DERIVE \
       --extern cross_check_runtime=$XCHECK_RUNTIME \
-      ../snudownrustxcheck.rs -o $OUTPUT_DIR/libsnudownrustxcheck.a
+      $C2RUST/examples/snudown/snudownrustxcheck.rs -o $OUTPUT_DIR/libsnudownrustxcheck.a
 
 fi
