7f9c3b4d732d7c2307ce0d7d982c233c4fc3a592
Miguel Saldivar <miguel.saldivar22@hotmail.com>
2018-03-22 16:09:00 -0700

    more tests for typedef

--- tests/misc/test_typedef.rs
+++ tests/misc/test_typedef.rs
@@ -1,6 +1,6 @@
 extern crate libc;
 
-use typedef::rust_entry;
+use typedef::{rust_entry, my_int, int_ptr};
 
 use self::libc::c_int;
 
@@ -17,7 +17,17 @@ pub fn test_typedef() {
     let rust_ret = unsafe {
         rust_entry()
     };
-
     assert_eq!(ret, 0);
     assert_eq!(rust_ret, 0);
+
+    let mut rust_var: my_int = 5;
+    let mut c_var: c_int = 5;
+    assert_eq!(rust_var, c_var);
+
+    let rptr_var: int_ptr = &mut rust_var as *mut my_int;
+    let cptr_var: int_ptr = &mut c_var as *mut c_int;
+
+    unsafe {
+        assert_eq!(*rptr_var, *cptr_var);
+    }
 }
