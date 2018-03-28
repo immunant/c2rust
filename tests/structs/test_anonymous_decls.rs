05f9541938eb0a3d85b7305bcf9a68e2d6ee7906
Eric Mertens <emertens@galois.com>
2018-03-28 08:39:31 -0700

    Fix anonymous decls test, mangle name

--- tests/structs/test_anonymous_decls.rs
+++ tests/structs/test_anonymous_decls.rs
@@ -1,10 +1,10 @@
 extern crate libc;
 
-use anonymous_decls::k;
+use anonymous_decls::rust_k;
 use self::libc::{c_int, c_uint};
 
 pub fn test_anonymous_decl() {
     unsafe {
-        assert_eq!(k.j.l, 0);
+        assert_eq!(rust_k.j.l, 0);
     }
 }
