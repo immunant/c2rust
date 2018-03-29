25904983ba8b794d3eff800f3b8507e34f6c094d
Andrei Homescu <ah@immunant.com>
2018-03-28 18:13:31 -0700

    Add option for libfakechecks to output checks to file with path specified using an environment variable

--- cross-checks/libfakechecks/fakechecks.cpp
+++ cross-checks/libfakechecks/fakechecks.cpp
@@ -1,8 +1,41 @@
 #include <cstdio>
+#include <cstdlib>
 #include <cstddef>
 #include <cstdint>
+#include <atomic>
+#include <mutex>
+
+std::atomic<FILE*> fout_atomic{nullptr};
+std::mutex fout_mutex;
+
+// Thread-safe initialization for fout
+static FILE *get_fout() {
+    FILE *fout = fout_atomic.load();
+    if (fout != nullptr)
+        return fout;
+
+    std::lock_guard<std::mutex> lock(fout_mutex);
+    fout = fout_atomic.load();
+    if (fout != nullptr)
+        return fout;
+
+    const char *out_file = getenv("FAKECHECKS_OUTPUT_FILE");
+    if (out_file == nullptr) {
+        fout = stderr;
+    } else {
+        fout = fopen(out_file, "w");
+        if (fout == nullptr) {
+            fprintf(stderr, "Error opening fakechecks output file '%s', "
+                            "writing to stderr instead!\n", out_file);
+            fout = stderr;
+        }
+    }
+    fout_atomic.store(fout);
+    return fout;
+}
 
 extern "C"
 void rb_xcheck(uint8_t tag, uint64_t item) {
-    fprintf(stderr, "XCHECK(%hhd):%lu/0x%08lx\n", tag, item, item);
+    auto *fout = get_fout();
+    fprintf(fout, "XCHECK(%hhd):%lu/0x%08lx\n", tag, item, item);
 }
