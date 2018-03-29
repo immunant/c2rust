3ce2e579a1d2c7b44f88c73c9b97f064a8d8d312
Per Larsen <perl@immunant.com>
2018-03-29 13:56:40 -0700

    Don't hardcode rust version in dockerfile.

--- docker/docker_build.sh
+++ docker/docker_build.sh
@@ -1,8 +1,17 @@
 #!/bin/bash
 
+set -e
+
 IMG_NAME=c2rust
 SCRIPT_DIR="$(dirname "$0")"
+
+# pull the rust version out of ../scripts/common.py to keep things synched
+PYTHON_DIR="$SCRIPT_DIR/../scripts"
+PYTHON_CMD="from common import CUSTOM_RUST_NAME; print(CUSTOM_RUST_NAME)"
+RUST_VER=$(cd $PYTHON_DIR && python3 -B -c "$PYTHON_CMD")
+
 docker build \
     --build-arg UID=$(id -u $(logname)) \
     --build-arg GID=$(id -g $(logname)) \
+    --build-arg RUST_VER=$RUST_VER \
     --tag $IMG_NAME $SCRIPT_DIR
