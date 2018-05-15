02f3477c5bd64d28b8d455f985b936ce6eb6edc9
Andrei Homescu <ah@immunant.com>
2018-05-15 15:56:12 -0700

    Get the path of build.rs from the CARGO_MANIFEST_DIR environment variable instead of the current directory in the Rust cross checker backends

--- cross-checks/rust-checks/backends/libclevrbuf-sys/build.rs
+++ cross-checks/rust-checks/backends/libclevrbuf-sys/build.rs
@@ -1,6 +1,9 @@
 fn main() {
-    let here = ::std::path::PathBuf::from(::std::env::current_dir().unwrap());
-    let cross_checks_path = here.parent().and_then(|x| x.parent()).unwrap();
+    let here = ::std::path::PathBuf::from(::std::env::("CARGO_MANIFEST_DIR")).unwrap());
+    let cross_checks_path = here.parent()
+        .and_then(|x| x.parent())
+        .and_then(|x| x.parent())
+        .unwrap();
     let libclevrbuf_path = cross_checks_path.join("ReMon").join("libclevrbuf");
     println!("cargo:rustc-link-lib=dylib=clevrbuf");
     println!("cargo:rustc-link-search=native={}", libclevrbuf_path.display());
--- cross-checks/rust-checks/backends/libfakechecks-sys/build.rs
+++ cross-checks/rust-checks/backends/libfakechecks-sys/build.rs
@@ -1,6 +1,9 @@
 fn main() {
-    let here = ::std::path::PathBuf::from(::std::env::current_dir().unwrap());
-    let cross_checks_path = here.parent().and_then(|x| x.parent()).unwrap();
+    let here = ::std::path::PathBuf::from(::std::env::var("CARGO_MANIFEST_DIR").unwrap());
+    let cross_checks_path = here.parent()
+        .and_then(|x| x.parent())
+        .and_then(|x| x.parent())
+        .unwrap();
     let libfakechecks_path = cross_checks_path.join("libfakechecks");
     println!("cargo:rustc-link-lib=dylib=fakechecks");
     println!("cargo:rustc-link-search=native={}", libfakechecks_path.display());
