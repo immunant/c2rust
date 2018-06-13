3ab6c7c3e523912fdccc451112badb1fb8d0707a
Andrei Homescu <ah@immunant.com>
2018-06-13 15:59:04 -0700

    Restore clang-plugin use of llvm::Optional instead of std::optional to simplify implementation

--- cross-checks/c-checks/clang-plugin/plugin/config.h
+++ cross-checks/c-checks/clang-plugin/plugin/config.h
@@ -35,7 +35,6 @@ struct XCheck {
 };
 
 LLVM_YAML_DECLARE_MAPPING_TRAITS(XCheck)
-LLVM_YAML_DECLARE_MAPPING_TRAITS(std::optional<XCheck>)
 LLVM_YAML_DECLARE_ENUM_TRAITS(XCheck::Tag)
 LLVM_YAML_IS_STRING_MAP(XCheck)
 
@@ -62,14 +61,15 @@ inline void yamlize(IO &io, XCheck &xc, bool, EmptyContext &ctx) {
         // for all values not covered above
 
         io.beginMapping();
-#define CHECK_DATA(_s, _dt, _e) do { \
-        std::optional<_dt> data;     \
-        io.mapOptional(_s, data);    \
-        if (data) {                  \
-            xc.type = XCheck::_e;    \
-            xc.data = *data;         \
-            return;                  \
-        }                            \
+#define CHECK_DATA(_s, _dt, _e) do {              \
+        EmptyContext ctx;                         \
+        Optional<_dt> data;                       \
+        io.mapOptionalWithContext(_s, data, ctx); \
+        if (data) {                               \
+            xc.type = XCheck::_e;                 \
+            xc.data = *data;                      \
+            return;                               \
+        }                                         \
     } while (0)
         CHECK_DATA("fixed",  uint64_t,    FIXED);
         CHECK_DATA("djb2",   std::string, DJB2);
@@ -79,19 +79,6 @@ inline void yamlize(IO &io, XCheck &xc, bool, EmptyContext &ctx) {
 
         // We couldn't handle this input, so throw an error
         io.setError("Unknown cross-check type");
-    }
-}
-
-template<>
-inline void yamlize(IO &io, std::optional<XCheck> &oxc,
-                    bool required, EmptyContext &ctx) {
-    XCheck xc;
-    if (io.outputting() && oxc) {
-        xc = *oxc;
-    }
-    yamlize(io, xc, required, ctx);
-    if (!io.outputting()) {
-        oxc = xc;
     }
 }
 } // namespace yaml
@@ -106,11 +93,11 @@ LLVM_YAML_DECLARE_MAPPING_TRAITS(ExtraXCheck)
 LLVM_YAML_IS_SEQUENCE_VECTOR(ExtraXCheck)
 
 struct DefaultsConfig {
-    std::optional<bool> disable_xchecks;
-    std::optional<XCheck> entry;
-    std::optional<XCheck> exit;
-    std::optional<XCheck> all_args;
-    std::optional<XCheck> ret;
+    llvm::Optional<bool> disable_xchecks;
+    llvm::Optional<XCheck> entry;
+    llvm::Optional<XCheck> exit;
+    llvm::Optional<XCheck> all_args;
+    llvm::Optional<XCheck> ret;
     // TODO: do we want entry/exit_extra here???
 
     DefaultsConfig() = default;
@@ -137,14 +124,14 @@ struct DefaultsConfig {
 
 struct FunctionConfig {
     std::string name;
-    std::optional<bool> disable_xchecks;
-    std::optional<XCheck> entry;
-    std::optional<XCheck> exit;
-    std::optional<XCheck> all_args;
+    llvm::Optional<bool> disable_xchecks;
+    llvm::Optional<XCheck> entry;
+    llvm::Optional<XCheck> exit;
+    llvm::Optional<XCheck> all_args;
     std::map<std::string, XCheck> args;
-    std::optional<XCheck> ret;
-    std::optional<std::string> ahasher;
-    std::optional<std::string> shasher;
+    llvm::Optional<XCheck> ret;
+    llvm::Optional<std::string> ahasher;
+    llvm::Optional<std::string> shasher;
     // TODO: nested
     std::vector<ExtraXCheck> entry_extra;
     std::vector<ExtraXCheck> exit_extra;
@@ -190,12 +177,12 @@ struct FunctionConfig {
 
 struct StructConfig {
     std::string name;
-    std::optional<bool> disable_xchecks;
-    std::optional<std::string> field_hasher;
-    std::optional<std::string> custom_hash;
+    llvm::Optional<bool> disable_xchecks;
+    llvm::Optional<std::string> field_hasher;
+    llvm::Optional<std::string> custom_hash;
     std::map<std::string, XCheck> fields;
-    std::optional<std::string> ahasher;
-    std::optional<std::string> shasher;
+    llvm::Optional<std::string> ahasher;
+    llvm::Optional<std::string> shasher;
 
     StructConfig(std::string_view n) : name(n) {}
 
@@ -232,71 +219,5 @@ LLVM_YAML_DECLARE_MAPPING_TRAITS(StructConfig)
 LLVM_YAML_DECLARE_MAPPING_TRAITS(ItemConfig)
 LLVM_YAML_IS_SEQUENCE_VECTOR(ItemConfig)
 LLVM_YAML_IS_STRING_MAP(FileConfig)
-
-namespace llvm {
-namespace yaml {
-// TODO: generalize to ScalarTraits<std::optional<T>>
-template<>
-struct ScalarTraits<std::optional<bool>> {
-    typedef std::optional<bool> OptT;
-
-    static void output(const OptT &val, void *ctx, raw_ostream &out) {
-        ScalarTraits<bool>::output(val.value_or(false), ctx, out);
-    }
-
-    static llvm::StringRef input(llvm::StringRef scalar, void *ctx, OptT &val) {
-        bool x;
-        auto res = ScalarTraits<bool>::input(scalar, ctx, x);
-        val = x;
-        return res;
-    }
-
-    static llvm::yaml::QuotingType mustQuote(llvm::StringRef s) {
-        return ScalarTraits<bool>::mustQuote(s);
-    }
-};
-
-template<>
-struct ScalarTraits<std::optional<uint64_t>> {
-    typedef std::optional<uint64_t> OptT;
-
-    static void output(const OptT &val, void *ctx, raw_ostream &out) {
-        ScalarTraits<uint64_t>::output(val.value_or(0), ctx, out);
-    }
-
-    static llvm::StringRef input(llvm::StringRef scalar, void *ctx, OptT &val) {
-        uint64_t x;
-        auto res = ScalarTraits<uint64_t>::input(scalar, ctx, x);
-        val = x;
-        return res;
-    }
-
-    static llvm::yaml::QuotingType mustQuote(llvm::StringRef s) {
-        return ScalarTraits<uint64_t>::mustQuote(s);
-    }
-};
-
-template<>
-struct ScalarTraits<std::optional<std::string>> {
-    typedef std::optional<std::string> OptT;
-
-    static void output(const OptT &val, void *ctx, raw_ostream &out) {
-        std::string empty_str;
-        ScalarTraits<std::string>::output(val.value_or(empty_str), ctx, out);
-    }
-
-    static llvm::StringRef input(llvm::StringRef scalar, void *ctx, OptT &val) {
-        std::string x;
-        auto res = ScalarTraits<std::string>::input(scalar, ctx, x);
-        val = x;
-        return res;
-    }
-
-    static llvm::yaml::QuotingType mustQuote(llvm::StringRef s) {
-        return ScalarTraits<std::string>::mustQuote(s);
-    }
-};
-} // namespace yaml
-} // namespace llvm
 
 #endif // CROSSCHECK_PLUGIN_CONFIG_H
