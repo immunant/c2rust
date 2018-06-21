b0c47bdb69b9e5a1e457adbfd3aa4d5284635856
Andrei Homescu <ah@immunant.com>
2018-06-20 17:45:13 -0700

    More function cross-check tests

---
+++ cross-checks/c-checks/clang-plugin/test/func6.c
@@ -0,0 +1,21 @@
+// RUN: %clang_xcheck %fakechecks -O2 -o %t %s %xcheck_runtime
+// RUN: %t 2>&1 | FileCheck %s
+
+#include <stdio.h>
+
+#include <cross_checks.h>
+
+int foo() CROSS_CHECK("{ return: { fixed: 0x1234 } }") {
+    return 1;
+}
+
+int main() {
+    foo();
+    return 0;
+}
+// CHECK: XCHECK(1):2090499946/0x7c9a7f6a
+// CHECK: XCHECK(1):193491849/0x0b887389
+// CHECK: XCHECK(2):193491849/0x0b887389
+// CHECK: XCHECK(4):4660/0x00001234
+// CHECK: XCHECK(2):2090499946/0x7c9a7f6a
+// CHECK: XCHECK(4):8680820740569200758/0x7878787878787876
