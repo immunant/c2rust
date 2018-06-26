3bc107d0670dbaa4e558940f3169b8e1b1633cf1
Per Larsen <perl@immunant.com>
2018-06-25 17:03:42 -0700

    Use requirements.txt to provision python packages when building Docker image.

--- docker/docker_build.sh
+++ docker/docker_build.sh
@@ -8,9 +8,13 @@ SCRIPT_DIR="$(dirname "$0")"
 # pull the rust version out of ../rust-toolchain to keep things synched
 RUST_TOOLCHAIN_FILE="$SCRIPT_DIR/../rust-toolchain"
 RUST_VER=$(cat $RUST_TOOLCHAIN_FILE | tr -d '\n')
+# read python packages from ../scripts/requirements.txt
+PY_REQUIREMENTS_FILE="$SCRIPT_DIR/../scripts/requirements.txt"
+PY_REQUIREMENTS=$(cat $PY_REQUIREMENTS_FILE)
 
 docker build \
     --build-arg UID=$(id -u $(logname)) \
     --build-arg GID=$(id -g $(logname)) \
     --build-arg RUST_VER=$RUST_VER \
+    --build-arg PY_REQUIREMENTS="$PY_REQUIREMENTS" \
     --tag $IMG_NAME $SCRIPT_DIR
