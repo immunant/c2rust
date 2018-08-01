5748fa068f8a09b1c085da22badbe102cb05f56b
perl <perl@immunant.com>
2018-07-31 19:21:15 -0700

    Make run-all-tests.sh fail if any individual test failed

--- rust-refactor/tests/run-all-tests.sh
+++ rust-refactor/tests/run-all-tests.sh
@@ -1,4 +1,5 @@
 #!/bin/sh
+exit_code=0
 for f in */; do
     [ -f "$f/run.sh" ] || continue
     ./run-test.sh $f >$f/log 2>&1
@@ -7,5 +8,7 @@ for f in */; do
         echo "[ OK ] ${f%/}"
     else
         echo "[FAIL] ${f%/}"
+	exit_code=$ret
     fi
 done
+exit $exit_code
