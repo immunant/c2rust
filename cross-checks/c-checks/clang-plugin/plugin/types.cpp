5026b9f55feffaffdd7b5eb601a8b6e031b0bc05
Andrei Homescu <ah@immunant.com>
2018-08-03 14:17:49 -0700

    Added Ubuntu-specific paths to blacklist in clang-plugin

--- cross-checks/c-checks/clang-plugin/plugin/types.cpp
+++ cross-checks/c-checks/clang-plugin/plugin/types.cpp
@@ -19,11 +19,15 @@ using namespace std::literals;
 // so we maintain a hard-coded blacklist
 std::set<std::pair<std::string_view, std::string_view>>
 CrossCheckInserter::struct_xcheck_blacklist = {
+    // Regular header paths
     { "/usr/include/bits/_G_config.h"sv,            "_G_fpos_t"sv        },
     { "/usr/include/bits/_G_config.h"sv,            "_G_fpos64_t"sv      },
     { "/usr/include/bits/types/__mbstate_t.h"sv,    "__mbstate_t"sv      },
     { "/usr/include/bits/thread-shared-types.h"sv,  "__pthread_cond_s"sv },
-    { "/usr/include/x86_64-linux-gnu/bits/types/__mbstate_t.h"sv, "__mbstate_t"sv},
+    // Ubuntu header paths
+    { "/usr/include/x86_64-linux-gnu/bits/_G_config.h"sv,           "_G_fpos_t"sv        },
+    { "/usr/include/x86_64-linux-gnu/bits/_G_config.h"sv,           "_G_fpos64_t"sv      },
+    { "/usr/include/x86_64-linux-gnu/bits/types/__mbstate_t.h"sv,   "__mbstate_t"sv},
     { "/usr/include/x86_64-linux-gnu/bits/thread-shared-types.h"sv, "__pthread_cond_s"sv},
 
 };
