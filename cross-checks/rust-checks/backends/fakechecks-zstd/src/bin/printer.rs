c3eeba8490749fa534466507a689921ca753b663
Andrei Homescu <ah@immunant.com>
2018-08-07 20:08:18 -0700

    Optimized xcheck-parser even further by calling std::fmt::write directly

--- cross-checks/rust-checks/backends/fakechecks-zstd/src/bin/printer.rs
+++ cross-checks/rust-checks/backends/fakechecks-zstd/src/bin/printer.rs
@@ -2,6 +2,7 @@
 
 extern crate zstd;
 
+use std::fmt;
 use std::fs::File;
 use std::io;
 use std::io::{Read, Write};
@@ -11,7 +12,7 @@ const BUF_SIZE: usize = 4 * 1024 * 1024; // 4MB buffer
 const MAX_XCHECK_LEN: usize = 50;
 
 pub fn main() -> Result<(), std::io::Error> {
-    let mut out: Vec<u8> = Vec::with_capacity(BUF_SIZE);
+    let mut out = String::with_capacity(BUF_SIZE);
     for arg in env::args() {
         let file = File::open(arg)?;
         let mut reader = zstd::stream::Decoder::new(file)?;
@@ -24,15 +25,16 @@ pub fn main() -> Result<(), std::io::Error> {
             val_buf.copy_from_slice(&buf[1..]);
             let val = u64::from_le(u64::from_bytes(val_buf));
             if out.len() >= BUF_SIZE - MAX_XCHECK_LEN {
-                io::stdout().write_all(&out[..])?;
+                io::stdout().write_all(out.as_bytes())?;
                 out.clear();
             }
             let old_len = out.len();
-            writeln!(out, "XCHECK({0}):{1:}/0x{1:08x}", buf[0], val)?;
+            fmt::write(&mut out, format_args!("XCHECK({0}):{1:}/0x{1:08x}\n", buf[0], val))
+                .expect("Error formatting xcheck");
             assert!(out.len() <= old_len + MAX_XCHECK_LEN);
         }
     }
     // Flush the buffer
-    io::stdout().write_all(&out[..])?;
+    io::stdout().write_all(out.as_bytes())?;
     Ok(())
 }
