f67d0fbca409ec988064c330282197bd11c8e198
Andrei Homescu <ah@immunant.com>
2018-08-07 19:51:47 -0700

    Optimized xcheck-printer using our own Vec-based buffer instead of BufWriter

--- cross-checks/rust-checks/backends/fakechecks-zstd/src/bin/printer.rs
+++ cross-checks/rust-checks/backends/fakechecks-zstd/src/bin/printer.rs
@@ -7,8 +7,11 @@ use std::io;
 use std::io::{Read, Write};
 use std::env;
 
+const BUF_SIZE: usize = 4 * 1024 * 1024; // 4MB buffer
+const MAX_XCHECK_LEN: usize = 50;
+
 pub fn main() -> Result<(), std::io::Error> {
-    let mut out = io::BufWriter::new(io::stdout());
+    let mut out: Vec<u8> = Vec::with_capacity(BUF_SIZE);
     for arg in env::args() {
         let file = File::open(arg)?;
         let mut reader = zstd::stream::Decoder::new(file)?;
@@ -20,8 +23,16 @@ pub fn main() -> Result<(), std::io::Error> {
             let mut val_buf = [0u8; 8];
             val_buf.copy_from_slice(&buf[1..]);
             let val = u64::from_le(u64::from_bytes(val_buf));
+            if out.len() >= BUF_SIZE - MAX_XCHECK_LEN {
+                io::stdout().write_all(&out[..])?;
+                out.clear();
+            }
+            let old_len = out.len();
             writeln!(out, "XCHECK({0}):{1:}/0x{1:08x}", buf[0], val)?;
+            assert!(out.len() <= old_len + MAX_XCHECK_LEN);
         }
     }
+    // Flush the buffer
+    io::stdout().write_all(&out[..])?;
     Ok(())
 }
