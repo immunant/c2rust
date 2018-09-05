faa00ff8ab407718fe88461e9be3348cc3bc61ab
Andrei Homescu <ah@immunant.com>
2018-09-05 15:52:44 -0700

    Added the same libxml2 xmlError test to the clang-plugin test suite

---
+++ cross-checks/c-checks/clang-plugin/test/struct8.c
@@ -0,0 +1,41 @@
+// RUN: %clang_xcheck -O2 -o %t %s %xcheck_runtime %fakechecks
+// RUN: %t 2>&1 | FileCheck %s
+
+#include <stdio.h>
+#include <stdint.h>
+
+#include <cross_checks.h>
+
+// A copy of xmlError from libxml2
+struct xmlError {
+    int domain;
+    int code;
+    char *message;
+    unsigned level;
+    char *file;
+    int line;
+    char *str1;
+    char *str2;
+    char *str3;
+    int int1;
+    int int2;
+    void *ctxt;
+    void *node;
+};
+
+struct xmlError foo(struct xmlError *x DEFAULT_XCHECK) {
+    return *x;
+}
+
+int main() {
+    struct xmlError x = { 5, 68, "h", 2, "t", 975, NULL, NULL, NULL, 0, 2, &x, NULL };
+    foo(&x);
+    return 0;
+}
+// CHECK: XCHECK(Ent):2090499946/0x7c9a7f6a
+// CHECK: XCHECK(Ent):193491849/0x0b887389
+// CHECK: XCHECK(Arg):9325795713530749616/0x816be1ca90dc62b0
+// CHECK: XCHECK(Exi):193491849/0x0b887389
+// CHECK: XCHECK(Ret):9325795713530749616/0x816be1ca90dc62b0
+// CHECK: XCHECK(Exi):2090499946/0x7c9a7f6a
+// CHECK: XCHECK(Ret):8680820740569200758/0x7878787878787876
