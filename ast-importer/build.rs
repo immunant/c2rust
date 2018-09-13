e57e45dc952ef89fa1cfc8af6cac1db69863c28e
Alec Theriault <alec.theriault@gmail.com>
2018-09-13 09:12:55 -0700

    buildrs comments/optimizations

--- ast-importer/build.rs
+++ ast-importer/build.rs
@@ -5,6 +5,9 @@ use std::env;
 use std::path::PathBuf;
 use cmake::Config;
 
+// This build.rs can take a while. Use `cargo build -vv` to more progress
+// output (eg. CMake's).
+
 fn main() {
 
     build_ast_exporter(false, 1);
@@ -59,7 +62,15 @@ fn main() {
 }
 
 // Call out to CMake, build the exporter library, and tell cargo where to look for it.
+// Note that `CMAKE_BUILD_TYPE` gets implicitly determined:
+//
+//    * if `opt-level=0`                              then `CMAKE_BUILD_TYPE=Debug`
+//    * if `opt-level={1,2,3}` and not `debug=false`, then `CMAKE_BUILD_TYPE=RelWithDebInfo`
+//
+// TODO: turn LLVM source into a gitsubmodule.
 fn build_ast_exporter(assertions: bool, max_link_jobs: i32) {
+
+    println!("cargo:rerun-if-changed=../dependencies/llvm-6.0.1/src");
 
     // This takes a while and gives no output. Build progress is outputted to
     // the `../dependencies/llvm-6.0.1/build/.ninja_log` file.
@@ -67,15 +78,23 @@ fn build_ast_exporter(assertions: bool, max_link_jobs: i32) {
         .generator("Ninja")
         .no_build_target(true)
         .out_dir("../dependencies/llvm-6.0.1")
-        .define("CMAKE_C_COMPILER", "clang")
+
+        // General CMake variables
+        .define("CMAKE_C_COMPILER",   "clang")
         .define("CMAKE_CXX_COMPILER", "clang++")
-        .define("LLVM_ENABLE_ASSERTIONS", if assertions { "1" } else { "0" })
-        .define("LLVM_TARGETS_TO_BUILD", "X86")
-        .define("LLVM_INCLUDE_UTILS", "1")
-        .define("BUILD_SHARED_LIBS", "1")  // fiddle with this to get static libraries built instead
+
+        // LLVM/Clang variables (see https://llvm.org/docs/CMake.html#llvm-specific-variables)
+        .define("LLVM_ENABLE_ASSERTIONS",  if assertions { "1" } else { "0" })
+        .define("LLVM_TARGETS_TO_BUILD",   "X86")
+        .define("LLVM_INCLUDE_UTILS",      "1")
+        .define("BUILD_SHARED_LIBS",       "1")  // fiddle with this to get static libraries built instead
         .define("LLVM_PARALLEL_LINK_JOBS", &max_link_jobs.to_string())
+
+        // What to build
+        .build_arg("clangAstExporter")
         .build();
 
+    // Tell Cargo where to find these libraries
     println!("cargo:rustc-link-search={}/build/lib", dst.display());
     println!("cargo:rustc-link-lib={}", "clangAstExporter");
 }
