45579a07c809a5d1b296dec1d0b73d5e7d656053
miguelsaldivar <miguel.saldivar22@hotmail.com>
2018-10-04 16:44:58 -0700

    Paths are now being changed to match the move.
    UseTree's are now being modified to have the correct path, like so:
    `use old_module::some_struct;`
    `use new_module::some_struct;`

--- rust-refactor/src/transform/modules.rs
+++ rust-refactor/src/transform/modules.rs
@@ -1,14 +1,9 @@
 use std::collections::HashMap;
-use std::path::Path;
-use std::fs::File;
-use std::io::Write;
 use rustc::hir::def_id::DefId;
 use rustc::session::Session;
 use syntax::ast::*;
 use syntax::tokenstream::*;
 use syntax::ptr::P;
-use syntax::symbol::Symbol;
-use syntax::print::pprust;
 use syntax::util::small_vector::SmallVector;
 use syntax::codemap::{Span, DUMMY_SP};
 use transform::Transform;
@@ -16,6 +11,7 @@ use transform::Transform;
 use api::*;
 use command::{CommandState, Registry};
 use driver::{self, Phase};
+use util::{IntoIdent, IntoString};
 
 pub struct ReorganizeModules;
 
@@ -65,49 +61,53 @@ impl Transform for ReorganizeModules {
             SmallVector::one(pi)
         });
 
-        /*
-        let krate = fold_nodes(krate, |mut c: Crate| {
-            for module in &modules {
-                let index = find_index(c.module.items.clone());
-                c.module.items.insert(index, mk().pub_().module(module.ident, Vec::new(), c.span));
-            }
-            c
-        });
-        */
-
         let krate = clean_module_items(krate, &mut mod_names);
 
+        // This is where the `old module` items get moved into the `new modules`
         let krate = fold_nodes(krate, |pi: P<Item>| {
-            match pi.node {
-                ItemKind::Mod(ref m) => {
-                    let pi_clone = pi.clone();
-                    return SmallVector::one(pi_clone.map(|i| {
-                        let mod_name = symbol_to_string(i.ident.name);
-                        let modinfo = mod_names.get(&mod_name);
-                        if modinfo.is_none() {
-                            return i;
-                        }
-
-                        let modinfo_items = modinfo.unwrap().items.clone();
-                        let mut m = m.clone();
-                        let mut m_items = m.items.clone();
-                        for item in modinfo_items {
-                            m_items.push(item);
-                        }
-                        m = Mod {
-                            items: m_items.clone(),
-                            .. m
-                        };
-
-                        Item {
-                            node: ItemKind::Mod(m),
-                            .. i
-                        }
-                    }));
-                },
-                _ => {}
+            if !matches!([pi.node] ItemKind::Mod(..)) {
+                return SmallVector::one(pi);
             }
-            SmallVector::one(pi)
+
+            SmallVector::one(pi.map(|i| {
+                unpack!([i.node.clone()] ItemKind::Mod(m));
+                let mut m = m;
+                let mod_name = i.ident.into_string();
+                let modinfo = mod_names.get(&mod_name);
+
+                if modinfo.is_none() {
+                    return i;
+                }
+
+                let modinfo_items = &modinfo.unwrap().items;
+                for item in modinfo_items {
+                    m.items.push(item.clone());
+                }
+
+                Item {
+                    node: ItemKind::Mod(m),
+                    .. i
+                }
+            }))
+        });
+
+        // Change path segments of `old modules` to the to match the `new modules`.
+        // Ex:
+        // use foo_h::some_struct; -> use foo::some_struct;
+
+        // Iterate through mod_names and gather the `old module` identifier's,
+        // use that as the key. Use the `new module` name as the value.
+        let new_names = mod_names.iter().map(|(new_mod, old_mod)| (old_mod.ident.into_string(), new_mod))
+            .collect::<HashMap<_, _>>();
+
+        let krate = fold_nodes(krate, |mut p: Path| {
+            for segment in &mut p.segments {
+                let path_name = segment.ident.into_string();
+                if let Some(new_path) = new_names.get(&path_name) {
+                    segment.ident = new_path.to_string().into_ident();
+                }
+            }
+            p
         });
 
         // print_module_info(modules);
@@ -161,9 +161,9 @@ pub fn match_modules(krate: Crate, modules: Vec<ModInfo>, mod_names: &mut HashMa
         match pi.node {
             ItemKind::Mod(_) => {
                 if !is_from_source_header(&pi.attrs) {
-                    let mod_name = symbol_to_string(pi.ident.name);
+                    let mut mod_name = pi.ident.into_string();
                     for module in &modules {
-                        let mod_info_name = symbol_to_string(module.ident.name);
+                        let mod_info_name = module.ident.into_string();
                         if mod_info_name.contains(&mod_name) && !mod_name.is_empty() {
                             mod_names.insert(mod_name.clone(), module.clone());
                         }
@@ -177,9 +177,10 @@ pub fn match_modules(krate: Crate, modules: Vec<ModInfo>, mod_names: &mut HashMa
     krate
 }
 
+/*
 pub fn print_module_info(modules: Vec<ModInfo>) {
     for module in &modules {
-        let filename_str = symbol_to_string(module.ident.name);
+        let filename_str = symbol_to_string(module);
         let filename = Path::new(&filename_str).with_extension("new");
         let s = pprust::to_string(|s| {
             for p_item in &module.items {
@@ -199,6 +200,7 @@ pub fn print_module_info(modules: Vec<ModInfo>) {
         };
     }
 }
+*/
 
 pub fn get_source_file(sess: &Session) -> String {
     let s = sess.local_crate_source_file.as_ref().cloned();
@@ -211,6 +213,9 @@ pub fn get_source_file(sess: &Session) -> String {
 // `#[cfg(not(source_header = "/some/path"))]`
 pub fn is_from_source_header(attrs: &Vec<Attribute>) -> bool {
 
+    // Recurse down the `TokenTree` till the `Token` is reached,
+    // if the token contains an Ident with `source_tree`, this should be a translated
+    // `old module` then.
     fn parse_token_tree(tree: &TokenTree, is_source_header: &mut bool) {
         match tree {
             TokenTree::Delimited(_, delimited) => {
@@ -223,7 +228,7 @@ pub fn is_from_source_header(attrs: &Vec<Attribute>) -> bool {
             TokenTree::Token(_, token) => {
                 if token.is_ident() {
                     let (ident, _) = token.ident().unwrap();
-                    let name = symbol_to_string(ident.name);
+                    let name = ident.into_string();
                     if name.contains("source_header") {
                         *is_source_header = true;
                     }
@@ -241,10 +246,6 @@ pub fn is_from_source_header(attrs: &Vec<Attribute>) -> bool {
         });
     }
     is_source_header
-}
-
-pub fn symbol_to_string(sym: Symbol) -> String {
-    sym.as_str().to_string()
 }
 
 pub fn register_commands(reg: &mut Registry) {
