fa70ca3b74dcb2d073889ad65f8138330ab415b0
miguelsaldivar <miguel.saldivar22@hotmail.com>
2018-10-17 18:11:25 -0700

    Rebase on top of master

--- rust-refactor/tests/reorganize_modules/basic_example/src/foo.rs
+++ rust-refactor/tests/reorganize_modules/basic_example/src/foo.rs
@@ -1,12 +1,9 @@
-use libc;
-
-#[cfg(
-    not(
-        source_header = "/home/miguelsaldivar/workspace/C2Rust/dependencies/llvm-6.0.1/build.donna/lib/clang/6.0.1/include/stddef.h"
-    )
-)]
-pub mod stddef_h {
-    pub type size_t = libc::c_ulong;
-    use super::libc;
+#[cfg(not(source_header = "/home/miguelsaldivar/workspace/misc/buffer/buffer.h"))]
+pub mod foo_h {
+    #[derive(Copy, Clone)]
+    #[repr(C)]
+    pub struct foo_t {
+        pub len: i32,
+    }
 }
 
