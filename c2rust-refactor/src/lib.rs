4173317afd8a3b92eea4a3a1e72ec607031d4743
Stephen Crane <culda.rinon@gmail.com>
2018-11-21 12:57:56 -0800

    Unify c2rust bins (#196)
    Move all c2rust binaries into the `c2rust` crate. The driver `c2rust` binary then invokes a subcommand (transpile, refactor) as needed using the correct `LD_LIBRARY_PATH`. The `c2rust` crate depends on the subcommand implementation libraries, and will be the crate that we `cargo install` for easy installation.

--- c2rust-refactor/src/lib.rs
+++ c2rust-refactor/src/lib.rs
@@ -6,7 +6,10 @@ extern crate arena;
 extern crate ena;
 extern crate indexmap;
 extern crate libc;
+extern crate cargo;
+extern crate clap;
 extern crate diff;
+extern crate env_logger;
 #[macro_use] extern crate json;
 #[macro_use] extern crate log;
 extern crate regex;
@@ -60,3 +63,327 @@ pub mod transform;
 pub mod mark_adjust;
 pub mod select;
 pub mod print_spans;
+
+use std::collections::HashSet;
+use std::path::{Path, PathBuf};
+use std::str::{self, FromStr};
+use std::sync::Arc;
+use cargo::util::paths;
+use syntax::ast::NodeId;
+use rustc::ty;
+use rustc_data_structures::sync::Lock;
+
+use c2rust_ast_builder::IntoSymbol;
+
+
+
+#[derive(Clone, Debug)]
+pub struct Cursor {
+    file: String,
+    line: u32,
+    col: u32,
+    label: Option<String>,
+    kind: Option<String>,
+}
+
+impl Cursor {
+    pub fn new(
+        file: String,
+        line: u32,
+        col: u32,
+        label: Option<String>,
+        kind: Option<String>,
+    ) -> Self {
+        Cursor {
+            file,
+            line,
+            col,
+            label,
+            kind,
+        }
+    }
+}
+
+#[derive(Clone, Debug)]
+pub struct Mark {
+    id: usize,
+    label: Option<String>,
+}
+
+impl Mark {
+    pub fn new(id: usize, label: Option<String>) -> Self {
+        Mark {
+            id,
+            label,
+        }
+    }
+}
+
+#[derive(Clone, Debug)]
+pub struct Command {
+    pub name: String,
+    pub args: Vec<String>,
+}
+
+#[derive(Clone, Debug)]
+pub enum RustcArgSource {
+    CmdLine(Vec<String>),
+    Cargo,
+}
+
+pub struct Options {
+    pub rewrite_mode: file_io::OutputMode,
+    pub commands: Vec<Command>,
+    pub rustc_args: RustcArgSource,
+    pub cursors: Vec<Cursor>,
+    pub marks: Vec<Mark>,
+
+    pub plugins: Vec<String>,
+    pub plugin_dirs: Vec<String>,
+}
+
+/// Try to find the rustup installation that provides the rustc at the given path.  The input path
+/// should be normalized already.
+fn get_rustup_path(rustc: &Path) -> Option<PathBuf> {
+    use std::ffi::OsStr;
+    use std::fs;
+
+    // `rustc` is already normalized, which resolves the `rustc` -> `rustup` symlink if one is
+    // present.
+    if rustc.file_name() == Some(OsStr::new("rustup")) {
+        return Some(rustc.to_owned());
+    }
+
+    // Otherwise, check for a rustup binary installed alongside rustc.  If they're the same size,
+    // we assume they're the same file (hardlinked or copied).
+    let rustup = rustc.with_file_name("rustup");
+    let rustc_meta = fs::metadata(&rustc).ok()?;
+    let rustup_meta = fs::metadata(&rustup).ok()?;
+    if rustc_meta.len() == rustup_meta.len() {
+        return Some(rustup);
+    }
+
+    None
+}
+
+fn get_rustc_executable(path: &Path) -> String {
+    use std::process::{Command, Stdio};
+
+    let resolved = paths::resolve_executable(path).unwrap();
+    if let Some(rustup_path) = get_rustup_path(&resolved) {
+        let proc = Command::new(rustup_path).arg("which").arg("rustc")
+            .stdout(Stdio::piped())
+            .spawn().unwrap();
+        let output = proc.wait_with_output().unwrap();
+        assert!(output.status.success());
+        let s = str::from_utf8(&output.stdout).unwrap();
+        return s.trim().to_owned();
+    }
+
+    resolved.to_str().unwrap().to_owned()
+}
+
+fn get_rustc_arg_strings(src: RustcArgSource) -> Vec<String> {
+    use std::sync::{Arc, Mutex};
+    use cargo::Config;
+    use cargo::core::{Workspace, PackageId, Target};
+    use cargo::core::compiler::{CompileMode, Executor, DefaultExecutor, Context, Unit};
+    use cargo::core::manifest::TargetKind;
+    use cargo::ops;
+    use cargo::ops::CompileOptions;
+    use cargo::util::{ProcessBuilder, CargoResult};
+    use cargo::util::important_paths::find_root_manifest_for_wd;
+
+    match src {
+        RustcArgSource::CmdLine(mut args) => {
+            let mut rustc_args = vec!(get_rustc_executable(Path::new("rustc")));
+            rustc_args.append(&mut args);
+            return rustc_args;
+        }
+        RustcArgSource::Cargo => {},
+    }
+
+    let config = Config::default().unwrap();
+    let mode = CompileMode::Check { test: false };
+    let compile_opts = CompileOptions::new(&config, mode).unwrap();
+
+    let manifest_path = find_root_manifest_for_wd(config.cwd()).unwrap();
+    let ws = Workspace::new(&manifest_path, &config).unwrap();
+
+    struct LoggingExecutor {
+        default: DefaultExecutor,
+        target_pkg: PackageId,
+        pkg_args: Mutex<Option<Vec<String>>>,
+    }
+
+    impl LoggingExecutor {
+        fn maybe_record_cmd(&self,
+                            cmd: &ProcessBuilder,
+                            id: &PackageId,
+                            target: &Target) {
+            if id != &self.target_pkg {
+                return;
+            }
+
+            let mut g = self.pkg_args.lock().unwrap();
+            match target.kind() {
+                // `lib` builds take priority.  Otherwise, take the first available bin.
+                &TargetKind::Lib(_) => {},
+                &TargetKind::Bin if g.is_none() => {},
+                _ => return,
+            }
+
+            let args = cmd.get_args().iter()
+                .map(|os| os.to_str().unwrap().to_owned())
+                .collect();
+            *g = Some(args);
+        }
+    }
+
+    impl Executor for LoggingExecutor {
+        fn init(&self, cx: &Context, unit: &Unit) {
+            self.default.init(cx, unit);
+        }
+
+        fn exec(&self,
+                cmd: ProcessBuilder,
+                id: &PackageId,
+                target: &Target,
+                mode: CompileMode) -> CargoResult<()> {
+            self.maybe_record_cmd(&cmd, id, target);
+            self.default.exec(cmd, id, target, mode)
+        }
+
+        fn exec_json(&self,
+                     cmd: ProcessBuilder,
+                     id: &PackageId,
+                     target: &Target,
+                     mode: CompileMode,
+                     handle_stdout: &mut FnMut(&str) -> CargoResult<()>,
+                     handle_stderr: &mut FnMut(&str) -> CargoResult<()>) -> CargoResult<()> {
+            self.maybe_record_cmd(&cmd, id, target);
+            self.default.exec_json(cmd, id, target, mode, handle_stdout, handle_stderr)
+        }
+
+        fn force_rebuild(&self, unit: &Unit) -> bool {
+            if unit.pkg.package_id() == &self.target_pkg {
+                return true;
+            }
+            self.default.force_rebuild(unit)
+        }
+    }
+
+    let exec = Arc::new(LoggingExecutor {
+        default: DefaultExecutor,
+        target_pkg: ws.current().unwrap().package_id().clone(),
+        pkg_args: Mutex::new(None),
+    });
+    let exec_dyn: Arc<Executor> = exec.clone();
+
+    ops::compile_with_exec(&ws, &compile_opts, &exec_dyn).unwrap();
+
+    let g = exec.pkg_args.lock().unwrap();
+    let opt_args = g.as_ref().unwrap();
+
+    let mut args = Vec::with_capacity(1 + opt_args.len());
+    let rustc = config.rustc(Some(&ws)).unwrap();
+    args.push(get_rustc_executable(&rustc.path));
+    args.extend(opt_args.iter().cloned());
+    info!("cargo-provided rustc args = {:?}", args);
+    args
+}
+
+fn main_impl(opts: Options) {
+    let mut marks = HashSet::new();
+    for m in &opts.marks {
+        let label = m.label.as_ref().map_or("target", |s| s).into_symbol();
+        marks.insert((NodeId::new(m.id), label));
+    }
+
+    let rustc_args = get_rustc_arg_strings(opts.rustc_args.clone());
+
+    if opts.cursors.len() > 0 {
+        driver::run_compiler(&rustc_args, None, driver::Phase::Phase2, |krate, cx| {
+            for c in &opts.cursors {
+                let kind_result = c.kind.clone().map_or(Ok(pick_node::NodeKind::Any),
+                                                        |s| pick_node::NodeKind::from_str(&s));
+                let kind = match kind_result {
+                    Ok(k) => k,
+                    Err(_) => {
+                        info!("Bad cursor kind: {:?}", c.kind.as_ref().unwrap());
+                        continue;
+                    },
+                };
+
+                let id = match pick_node::pick_node_at_loc(
+                        &krate, &cx, kind, &c.file, c.line, c.col) {
+                    Some(info) => info.id,
+                    None => {
+                        info!("Failed to find {:?} at {}:{}:{}",
+                                 kind, c.file, c.line, c.col);
+                        continue;
+                    },
+                };
+
+                let label = c.label.as_ref().map_or("target", |s| s).into_symbol();
+
+                info!("label {:?} as {:?}", id, label);
+
+                marks.insert((id, label));
+            }
+        });
+    }
+
+
+    let mut cmd_reg = command::Registry::new();
+    transform::register_commands(&mut cmd_reg);
+    mark_adjust::register_commands(&mut cmd_reg);
+    pick_node::register_commands(&mut cmd_reg);
+    print_spans::register_commands(&mut cmd_reg);
+    select::register_commands(&mut cmd_reg);
+    analysis::register_commands(&mut cmd_reg);
+    reflect::register_commands(&mut cmd_reg);
+    command::register_commands(&mut cmd_reg);
+
+    plugin::load_plugins(&opts.plugin_dirs, &opts.plugins, &mut cmd_reg);
+
+    if opts.commands.len() == 1 && opts.commands[0].name == "interact" {
+        interact::interact_command(&opts.commands[0].args,
+                                   rustc_args,
+                                   cmd_reg);
+    } else {
+        let mut state = command::RefactorState::from_rustc_args(
+            &rustc_args,
+            cmd_reg,
+            Arc::new(file_io::RealFileIO::new(opts.rewrite_mode)),
+            marks,
+        );
+
+        state.load_crate();
+
+        for cmd in opts.commands.clone() {
+            if &cmd.name == "interact" {
+                panic!("`interact` must be the only command");
+            } else {
+                match state.run(&cmd.name, &cmd.args) {
+                    Ok(_)=> {},
+                    Err(e) => {
+                        eprintln!("{:?}", e);
+                        std::process::exit(1);
+                    }
+
+                }
+            }
+        }
+
+        state.save_crate();
+    }
+}
+
+pub fn lib_main(opts: Options) {
+    ty::tls::GCX_PTR.set(&Lock::new(0), || {
+        syntax::with_globals(move || {
+            main_impl(opts);
+        });
+    });
+}
--- c2rust-transpile/src/lib.rs
+++ c2rust-transpile/src/lib.rs
@@ -9,8 +9,19 @@ extern crate dtoa;
 extern crate serde;
 extern crate serde_json;
 extern crate libc;
+extern crate clap;
 extern crate c2rust_ast_exporter;
 extern crate c2rust_ast_builder;
+
+use std::io::stdout;
+use std::io::prelude::*;
+use std::fs::File;
+use std::path::{Path, PathBuf};
+use std::process;
+
+use c_ast::*;
+use c_ast::Printer;
+use c2rust_ast_exporter as ast_exporter;
 
 pub mod renamer;
 pub mod convert_type;
@@ -20,9 +31,106 @@ pub mod rust_ast;
 pub mod cfg;
 pub mod with_stmts;
 
-#[cfg(test)]
-mod tests {
-    #[test]
-    fn it_works() {
+pub use translator::ReplaceMode;
+
+/// Configuration settings for the translation process
+#[derive(Debug)]
+pub struct TranspilerConfig {
+    // Debug output options
+    pub dump_untyped_context: bool,
+    pub dump_typed_context: bool,
+    pub pretty_typed_context: bool,
+    pub dump_function_cfgs: bool,
+    pub json_function_cfgs: bool,
+    pub dump_cfg_liveness: bool,
+    pub dump_structures: bool,
+
+    pub incremental_relooper: bool,
+    pub fail_on_multiple: bool,
+    pub debug_relooper_labels: bool,
+    pub cross_checks: bool,
+    pub cross_check_configs: Vec<String>,
+    pub prefix_function_names: Option<String>,
+    pub translate_asm: bool,
+    pub translate_entry: bool,
+    pub use_c_loop_info: bool,
+    pub use_c_multiple_info: bool,
+    pub simplify_structures: bool,
+    pub panic_on_translator_failure: bool,
+    pub emit_module: bool,
+    pub fail_on_error: bool,
+    pub replace_unsupported_decls: ReplaceMode,
+    pub translate_valist: bool,
+    pub reduce_type_annotations: bool,
+    pub reorganize_definitions: bool,
+
+    pub main_file: PathBuf,
+    pub output_file: Option<String>,
+}
+
+/// Main entry point to transpiler. Called from CLI tools with the result of
+/// clap::App::get_matches().
+pub fn transpile(tcfg: TranspilerConfig, input_file: &Path, extra_clang_args: &[&str]) {
+    // Extract the untyped AST from the CBOR file
+    let untyped_context = match ast_exporter::get_untyped_ast(input_file, &extra_clang_args) {
+        Err(e) => {
+            eprintln!("Error: {:}", e);
+            process::exit(1);
+        }
+        Ok(cxt) => cxt,
+    };
+
+    if tcfg.dump_untyped_context {
+        println!("CBOR Clang AST");
+        println!("{:#?}", untyped_context);
     }
+
+    // Convert this into a typed AST
+    let typed_context = {
+        let mut conv = ConversionContext::new(&untyped_context);
+        conv.convert(&untyped_context);
+        conv.typed_context
+    };
+
+    if tcfg.dump_typed_context {
+        println!("Clang AST");
+        println!("{:#?}", typed_context);
+    }
+
+    if tcfg.pretty_typed_context {
+        println!("Pretty-printed Clang AST");
+        println!("{:#?}", Printer::new(stdout()).print(&typed_context));
+    }
+
+    // Perform the translation
+    let translated_string = translator::translate(typed_context, &tcfg);
+    let output_path = get_output_path(&tcfg);
+
+    let mut file = match File::create(output_path) {
+        Ok(file) => file,
+        Err(e) => panic!("Unable to open file for writing: {}", e),
+    };
+
+    match file.write_all(translated_string.as_bytes()) {
+        Ok(()) => (),
+        Err(e) => panic!("Unable to write translation to file: {}", e),
+    };
+}
+
+fn get_output_path(tcfg: &TranspilerConfig) -> PathBuf {
+    if let Some(output_file) = tcfg.output_file.as_ref() {
+        return Path::new(output_file).to_path_buf();
+    }
+
+    // main_file does not have an extension; set_extension will add an .rs
+    // extension
+    let mut path_buf = tcfg.main_file.clone();
+
+    // When an output file name is not explictly specified, we should convert files
+    // with dashes to underscores, as they are not allowed in rust file names.
+    let file_name = path_buf.file_name().unwrap().to_str().unwrap().replace('-', "_");
+
+    path_buf.set_file_name(file_name);
+    path_buf.set_extension("rs");
+    path_buf
 }
