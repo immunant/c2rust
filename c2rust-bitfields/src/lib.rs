9ead06e9dada80e4f20b9157b15772a09fccd418
Daniel Kolsoi <thadan64@gmail.com>
2018-11-26 14:58:12 -0500

    Start fleshing out bit parsing

--- c2rust-bitfields/src/lib.rs
+++ c2rust-bitfields/src/lib.rs
@@ -1,12 +1,19 @@
+#![recursion_limit="128"]
+
 extern crate proc_macro;
 extern crate quote;
 extern crate syn;
 
 use proc_macro::{Span, TokenStream};
 use quote::quote;
-use syn::{Attribute, DeriveInput, Field, Fields, Ident, ItemStruct, Lit, Meta, NestedMeta, Path, PathArguments, PathSegment, Token, TypePath, parse_macro_input};
+use syn::{Attribute, Field, Fields, Ident, ItemStruct, Lit, Meta, NestedMeta, Path, PathArguments, PathSegment, Token, parse_macro_input};
 use syn::punctuated::Punctuated;
 
+#[cfg(target_endian = "big")]
+compile_error!("Big endian architectures are not currently supported");
+
+/// This struct keeps track of a single bitfield attr's params
+/// as well as the bitfield's field name.
 #[derive(Debug)]
 struct BFFieldAttr {
     field_name: Ident,
@@ -73,6 +80,33 @@ fn filter_and_parse_fields(field: &Field) -> Vec<BFFieldAttr> {
     }).collect()
 }
 
+fn parse_bitfield_ty_path(field: &BFFieldAttr) -> Path {
+    let leading_colon = if field.ty.starts_with("::") {
+        Some(Token![::]([Span::call_site().into(), Span::call_site().into()]))
+    } else {
+        None
+    };
+
+    let mut segments = Punctuated::new();
+    let mut segement_strings = field.ty.split("::").peekable();
+
+    while let Some(segment_string) = segement_strings.next() {
+        segments.push_value(PathSegment {
+            ident: Ident::new(segment_string, Span::call_site().into()),
+            arguments: PathArguments::None,
+        });
+
+        if segement_strings.peek().is_some() {
+            segments.push_punct(Token![::]([Span::call_site().into(), Span::call_site().into()]));
+        }
+    }
+
+    Path {
+        leading_colon,
+        segments,
+    }
+}
+
 #[proc_macro_derive(BitfieldStruct, attributes(bitfield))]
 pub fn bitfield_struct(input: TokenStream) -> TokenStream {
     println!("{}", input);
@@ -84,48 +118,57 @@ pub fn bitfield_struct(input: TokenStream) -> TokenStream {
         _ => panic!("Unnamed bitfield fields are not currently supported"),
     };
     let bitfields: Vec<BFFieldAttr> = fields.iter().flat_map(filter_and_parse_fields).collect();
-    let field_types: Vec<_> = bitfields.iter().map(|field| {
-        let leading_colon = if field.ty.starts_with("::") {
-            Some(Token![::]([Span::call_site().into(), Span::call_site().into()]))
-        } else {
-            None
-        };
-
-        let mut segments = Punctuated::new();
-        let mut segement_strings = field.ty.split("::").peekable();
-
-        while let Some(segment_string) = segement_strings.next() {
-            segments.push_value(PathSegment {
-                ident: Ident::new(segment_string, Span::call_site().into()),
-                arguments: PathArguments::None,
-            });
-
-            if segement_strings.peek().is_some() {
-                segments.push_punct(Token![::]([Span::call_site().into(), Span::call_site().into()]));
-            }
-        }
-
-        Path {
-            leading_colon,
-            segments,
-        }
-    }).collect();
-    let field_types2 = field_types.clone();
-    let field_names: Vec<_> = bitfields.iter().map(|field| Ident::new(&field.name, Span::call_site().into())).collect();
-    let field_name_setters: Vec<_> = field_names.iter().map(|field_ident| {
+    let field_types: Vec<_> = bitfields.iter().map(parse_bitfield_ty_path).collect();
+    let field_type_setters = field_types.clone();
+    let method_names: Vec<_> = bitfields.iter().map(|field| Ident::new(&field.name, Span::call_site().into())).collect();
+    let field_name: Vec<_> = bitfields.iter().map(|field| &field.field_name).collect();
+    let method_name_setters: Vec<_> = method_names.iter().map(|field_ident| {
         let span = Span::call_site().into();
         let setter_name = &format!("set_{}", field_ident);
 
         Ident::new(setter_name, span)
     }).collect();
+    let field_bit_info: Vec<_> = bitfields.iter().map(|field| {
+        let bit_string = &field.bits;
+        let mut split = bit_string.split("..=");
+        let lhs: usize = split.next().unwrap().parse().unwrap();
+        let rhs: usize = split.next().unwrap().parse().unwrap();
 
+        quote! { (#lhs, #rhs) }
+    }).collect();
+
+    // TODO: Field visibility determined by struct field visibility?
+    // TODO: Add generic doc strings
     let q = quote! {
         impl #struct_ident {
             #(
-                pub fn #field_name_setters(&self, setter: #field_types) {
+                pub fn #method_name_setters(&mut self, val: #field_type_setters) {
+                    let field = self.#field_name;
+                    let (lhs_bit, rhs_bit) = #field_bit_info;
+                    let bit_width = rhs_bit - lhs_bit;
+                    // let byte_index = field.len() - bit_width / 8;
+                    let start_byte_index = lhs_bit / 8;
+                    let end_byte_index = rhs_bit / 8;
+
+                    // Fits in a single byte
+                    if start_byte_index == end_byte_index {
+                        let mut byte = field[start_byte_index];
+
+                        for bit_pos in 7..=0 {
+                            let bit = val;
+
+                            // byte;
+
+                            // bit_width -= 1;
+                        }
+
+                        return
+                    }
+
+                    // TODO: Multi byte
                 }
 
-                pub fn #field_names(&self) -> #field_types2 {
+                pub fn #method_names(&self) -> #field_types {
                     42
                 }
             )*
--- c2rust-bitfields/tests/test_structs.rs
+++ c2rust-bitfields/tests/test_structs.rs
@@ -2,31 +2,59 @@ extern crate c2rust_bitfields;
 extern crate libc;
 
 use c2rust_bitfields::BitfieldStruct;
+use std::mem::transmute;
 
-#[repr(C, align(8))]
+// *** Dumping AST Record Layout
+//         0 | struct date
+//     0:0-4 |   unsigned char d
+//     1:0-3 |   unsigned char m
+//         2 |   unsigned short y
+//           | [sizeof=4, align=2]
+// struct CompactDate {
+//     unsigned char d: 5;
+//     unsigned char m: 4;
+//     unsigned short y;
+// };
+
+#[repr(C, align(2))]
 #[derive(BitfieldStruct, Copy, Clone)]
-struct Date {
+struct CompactDate {
     // Compact combination of d + m
     // which can't be accessed via ptr in C anyway
     // so we combine the fields into one:
     #[bitfield(name = "d", ty = "libc::c_ulong", bits = "0..=4")]
-    #[bitfield(name = "m", ty = "libc::c_ushort", bits = "5..=8")]
+    #[bitfield(name = "m", ty = "libc::c_ushort", bits = "8..=11")]
     d_m: [u8; 2],
     y: u16,
-    _pad: [u8; 4],
 }
 
 #[test]
-fn it_works() {
-    let date = Date {
-        d_m: [0; 2],
+fn test_compact_date() {
+    // let mut date = CompactDate {
+    //     d_m: [0; 2],
+    //     y: 2014,
+    // };
+    let mut date = CompactDate {
+        d_m: [0b00001100, 0b00011111],
         y: 2014,
-        _pad: [0; 4],
     };
 
     date.set_d(14);
     date.set_m(14);
+    date.y = 2014;
 
     assert_eq!(date.d(), 42);
     assert_eq!(date.m(), 42);
+
+    // assert_eq!(date.d(), 31);
+    // assert_eq!(date.m(), 12);
+    assert_eq!(date.y, 2014);
+
+    // Test byte compatibility: 00000111110111100000110000011111
+    let date_bytes: [u8; 4] = unsafe { transmute(date) };
+
+    assert_eq!(date_bytes, [0b00000111, 0b11011110, 0b00001100, 0b00011111]);
+
+    // 00000111 | 11011110 | 00001100 | 00011111
+    // -2014--> | <--2014- |     -12- |    --31-
 }
