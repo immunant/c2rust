9b2ba515396698d7e3f126fd0c3323c6971db406
Stephen Crane <sjc@immunant.com>
2018-12-07 18:53:48 -0800

    Add spans to instrumentation calls

--- analysis/plugin/src/lib.rs
+++ analysis/plugin/src/lib.rs
@@ -1,18 +1,41 @@
 #![feature(plugin_registrar, rustc_private, quote)]
 
 extern crate rustc_plugin;
+extern crate rustc_data_structures;
 extern crate syntax;
 
+extern crate bincode;
+#[macro_use]
+extern crate error_chain;
+extern crate indexmap;
+
 use std::collections::HashMap;
+use std::fs::File;
+use std::path::PathBuf;
 
 use rustc_plugin::Registry;
-use syntax::ast;
+use syntax::{ast, entry};
 use syntax::ext::base::{SyntaxExtension, ExtCtxt, Annotatable, MultiItemModifier};
 use syntax::ext::build::AstBuilder;
 use syntax::fold::{self, Folder};
 use syntax::ptr::P;
 use syntax::symbol::{Symbol, Ident};
-use syntax::source_map::{Span, DUMMY_SP};
+use syntax::source_map::{Span, DUMMY_SP, FileName};
+
+use indexmap::IndexSet;
+
+use c2rust_analysis_rt::{SourceSpan, BytePos};
+
+pub mod errors {
+    error_chain! {
+        foreign_links {
+            Io(std::io::Error);
+            Bincode(Box<bincode::ErrorKind>);
+        }
+    }
+}
+
+use self::errors::*;
 
 #[plugin_registrar]
 pub fn plugin_registrar(reg: &mut Registry) {
@@ -44,7 +67,9 @@ impl MultiItemModifier for LifetimeAnalysis {
                 match &i.node {
                     ast::ItemKind::Mod(_) => {
                         let mut folder = LifetimeInstrumentation::new(cx);
-                        folder.fold_item(i)
+                        let folded = folder.fold_item(i);
+                        folder.finalize().expect("Error instrumenting lifetimes");
+                        folded
                     }
                     _ => panic!("unexpected item: {:#?}", i),
                 }.into_iter().map(|i| Annotatable::Item(i)).collect()
@@ -66,35 +91,53 @@ const HOOKED_FUNCTIONS: &[&'static str] = &[
     "reallocarray",
 ];
 
+const SPAN_FILENAME: &str = "lifetime_analysis_spans.bincode";
+
 
 struct LifetimeInstrumentation<'a, 'cx: 'a> {
     cx: &'a mut ExtCtxt<'cx>,
     hooked_functions: HashMap<Ident, P<ast::FnDecl>>,
+
+    spans: IndexSet<SourceSpan>,
+    depth: usize,
+    span_file_path: PathBuf,
 }
 
 impl<'a, 'cx> LifetimeInstrumentation<'a, 'cx> {
     fn new(cx: &'a mut ExtCtxt<'cx>) -> Self {
+        let mut path = cx.root_path.canonicalize().unwrap();
+        path.push(SPAN_FILENAME);
         Self {
             cx,
             hooked_functions: HashMap::new(),
+            spans: IndexSet::new(),
+            depth: 0,
+            span_file_path: path,
         }
+    }
+
+    fn finalize(self) -> Result<()> {
+        eprintln!("Writing spans to {:?}", self.span_file_path);
+        let span_file = File::create(self.span_file_path)
+            .chain_err(|| "Could not open span file")?;
+        let spans: Vec<SourceSpan> = self.spans.into_iter().collect();
+        bincode::serialize_into(span_file, &spans)
+            .chain_err(|| "Span serialization failed")
     }
 
     /// Check if the callee expr is a function we've hooked. Returns the name of
     /// the function and its declaration if found.
     fn hooked_fn(&self, callee: &ast::Expr) -> Option<(Ident, &ast::FnDecl)> {
         match &callee.node {
-            ast::ExprKind::Path(None, path) => {
-                if path.segments.len() == 1 {
-                    if let Some(decl) = self.hooked_functions.get(&path.segments[0].ident) {
-                        return Some((path.segments[0].ident, decl));
-                    }
-                }
+            ast::ExprKind::Path(None, path)
+                if path.segments.len() == 1 =>
+            {
+                self.hooked_functions
+                    .get(&path.segments[0].ident)
+                    .and_then(|decl| Some((path.segments[0].ident, &**decl)))
             }
-            _ => (),
+            _ => None,
         }
-
-        None
     }
 
     /// If ty is a Ptr type, return a new expr that is a cast of expr to usize,
@@ -105,29 +148,53 @@ impl<'a, 'cx> LifetimeInstrumentation<'a, 'cx> {
             _ => expr.clone(),
         }
     }
+
+    fn get_source_location_idx(&mut self, span: Span) -> usize {
+        let lo = self.cx.source_map().lookup_byte_offset(span.lo());
+        let hi = self.cx.source_map().lookup_byte_offset(span.hi());
+
+        if lo.sf.start_pos != hi.sf.start_pos {
+            self.cx.span_err(span, "Location crosses source files");
+        }
+        let file_path = match &lo.sf.name {
+            FileName::Real(path) => path
+                .strip_prefix(&self.cx.root_path)
+                .expect("Could not strip root path from source filename")
+                .to_owned(),
+            _ => {
+                self.cx.span_err(span, "Location does not refer to a source file");
+                unreachable!()
+            }
+        };
+
+        let source_span = SourceSpan::new(file_path, BytePos(lo.pos.0), BytePos(hi.pos.0));
+
+        let (idx, _) = self.spans.insert_full(source_span);
+        idx
+    }
+
+    fn instrument_main_block(&self, block: P<ast::Block>) -> P<ast::Block> {
+        let span_file = self.span_file_path.to_str().unwrap();
+        block.map(|mut block| {
+            block.stmts.insert(0, quote_stmt!(
+                self.cx, c2rust_analysis_rt::set_span_file($span_file);
+            ).unwrap());
+            block
+        })
+    }
 }
 
 impl<'a, 'cx> Folder for LifetimeInstrumentation<'a, 'cx> {
-    // Will be needed for other crates?
-    // fn fold_mod(&mut self, m: ast::Mod) -> ast::Mod {
-    //     let mut items = vec![
-    //         quote_item!(self.cx, extern crate c2rust_analysis_rt;).unwrap(),
-    //         quote_item!(self.cx, use c2rust_analysis_rt::*;).unwrap(),
-    //     ];
-    //     items.extend(m.items.move_flat_map(|x| self.fold_item(x)));
-    //     ast::Mod {
-    //         items,
-    //         ..m
-    //     }
-    // }
-
     fn fold_foreign_item_simple(&mut self, item: ast::ForeignItem) -> ast::ForeignItem {
         if let ast::ForeignItemKind::Fn(decl, _) = &item.node {
             if HOOKED_FUNCTIONS.contains(&&*item.ident.name.as_str()) {
                 self.hooked_functions.insert(item.ident, decl.clone());
             }
         }
-        fold::noop_fold_foreign_item_simple(item, self)
+        self.depth += 1;
+        let folded = fold::noop_fold_foreign_item_simple(item, self);
+        self.depth -= 1;
+        folded
     }
 
     fn fold_expr(&mut self, expr: P<ast::Expr>) -> P<ast::Expr> {
@@ -137,13 +204,21 @@ impl<'a, 'cx> Folder for LifetimeInstrumentation<'a, 'cx> {
 
         match &expr.node {
             ast::ExprKind::Call(callee, args) => {
-                if let Some((fn_name, decl)) = self.hooked_fn(callee) {
-                    // Cast all original arguments to usize
-                    let mut hook_args: Vec<P<ast::Expr>> = args
-                        .iter()
-                        .zip(decl.inputs.iter())
-                        .map(|(arg, arg_decl)| self.add_ptr_cast(arg, &arg_decl.ty))
-                        .collect();
+                if self.hooked_fn(callee).is_some() {
+                    let source_loc_idx = self.get_source_location_idx(expr.span);
+                    let mut hook_args: Vec<P<ast::Expr>> = vec![
+                        quote_expr!(self.cx, $source_loc_idx)
+                    ];
+
+                    let (fn_name, decl) = self.hooked_fn(callee).unwrap();
+
+                    // Add all original arguments, casting pointers to usize
+                    hook_args.extend(
+                        args
+                            .iter()
+                            .zip(decl.inputs.iter())
+                            .map(|(arg, arg_decl)| self.add_ptr_cast(arg, &arg_decl.ty))
+                    );
                     // Add the return value of the hooked call.
                     hook_args.push({
                         let ret_expr = quote_expr!(self.cx, ret);
@@ -173,6 +248,33 @@ impl<'a, 'cx> Folder for LifetimeInstrumentation<'a, 'cx> {
                 }
             }
             _ => expr,
+        }
+    }
+
+    fn fold_item_simple(&mut self, item: ast::Item) -> ast::Item {
+        self.depth += 1;
+        let item = fold::noop_fold_item_simple(item, self);
+        self.depth -= 1;
+
+        // Instrument entry point if found
+        match entry::entry_point_type(&item, self.depth) {
+            entry::EntryPointType::MainNamed |
+            entry::EntryPointType::MainAttr |
+            entry::EntryPointType::Start => {
+                ast::Item {
+                    node: {
+                        if let ast::ItemKind::Fn(decl, header, generics, block) = item.node {
+                            ast::ItemKind::Fn(decl, header, generics, {
+                                self.instrument_main_block(block)
+                            })
+                        } else {
+                            panic!("Expected a function item");
+                        }
+                    },
+                    ..item
+                }
+            }
+            _ => item,
         }
     }
 }
--- analysis/runtime/src/lib.rs
+++ analysis/runtime/src/lib.rs
@@ -1,15 +1,76 @@
-pub fn malloc(size: u64, result: usize) {
-    eprintln!("Recording a malloc of size {} at address 0x{:x}", size, result);
+#[macro_use]
+extern crate serde_derive;
+extern crate bincode;
+#[macro_use]
+extern crate lazy_static;
+
+use std::fmt;
+use std::fs::File;
+use std::path::PathBuf;
+use std::sync::RwLock;
+
+lazy_static! {
+    static ref SPAN_FILE_PATH: RwLock<Option<PathBuf>> = RwLock::new(None);
 }
-pub fn free(ptr: usize, result: ()) {
-    eprintln!("Recording a free of address 0x{:x}", ptr);
+
+pub fn set_span_file(file_path: &str) {
+    *SPAN_FILE_PATH.write().unwrap() = Some(PathBuf::from(file_path));
 }
-pub fn calloc(nmemb: u64, size: u64, result: usize) {
-    eprintln!("Recording a calloc of {} members of size {} at address 0x{:x}", nmemb, size, result);
+
+lazy_static! {
+    static ref SOURCE_SPANS: Vec<SourceSpan> = {
+        let path = SPAN_FILE_PATH.read().expect("SPAN_FILE_PATH was locked").clone()
+            .expect("SPAN_FILE_PATH not initialized by the instrumented code");
+ let file = File::open(&path)
+            .expect(&format!("Could not open span file: {:?}", path.to_str()));
+        bincode::deserialize_from(file)
+            .expect("Error deserializing span file")
+    };
 }
-pub fn realloc(ptr: usize, size: u64, result: usize) {
-    eprintln!("Recording a realloc of 0x{:x} to size {} at address 0x{:x}", ptr, size, result);
+
+
+/// A byte offset. Keep this small (currently 32-bits), as AST contains
+/// a lot of them.
+#[derive(Clone, Copy, PartialEq, Eq, Hash, PartialOrd, Ord, Debug, Serialize, Deserialize)]
+pub struct BytePos(pub u32);
+
+#[derive(Serialize, Deserialize, PartialEq, Eq, Hash)]
+pub struct SourceSpan {
+    source: PathBuf,
+    lo: BytePos,
+    hi: BytePos,
 }
-pub fn reallocarray(ptr: usize, nmemb: u64, size: u64, result: usize) {
-    eprintln!("Recording a reallocarray of 0x{:x} to {} members of size {} at address 0x{:x}", ptr, nmemb, size, result);
+
+impl SourceSpan {
+    pub fn new(source: PathBuf, lo: BytePos, hi: BytePos) -> Self {
+        Self {
+            source,
+            lo,
+            hi,
+        }
+    }
+}
+
+impl fmt::Debug for SourceSpan {
+    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
+        write!(f, "{}:{}-{}", self.source.to_str().unwrap(), self.lo.0, self.hi.0)
+    }
+}
+
+
+
+pub fn malloc(span: usize, size: u64, result: usize) {
+    eprintln!("Recording a malloc ({:?}) of size {} at address 0x{:x}", SOURCE_SPANS[span], size, result);
+}
+pub fn free(span: usize, ptr: usize, _result: ()) {
+    eprintln!("Recording a free ({:?}) of address 0x{:x}", SOURCE_SPANS[span], ptr);
+}
+pub fn calloc(span: usize, nmemb: u64, size: u64, result: usize) {
+    eprintln!("Recording a calloc ({:?}) of {} members of size {} at address 0x{:x}", SOURCE_SPANS[span], nmemb, size, result);
+}
+pub fn realloc(span: usize, ptr: usize, size: u64, result: usize) {
+    eprintln!("Recording a realloc ({:?}) of 0x{:x} to size {} at address 0x{:x}", SOURCE_SPANS[span], ptr, size, result);
+}
+pub fn reallocarray(span: usize, ptr: usize, nmemb: u64, size: u64, result: usize) {
+    eprintln!("Recording a reallocarray ({:?}) of 0x{:x} to {} members of size {} at address 0x{:x}", SOURCE_SPANS[span], ptr, nmemb, size, result);
 }
