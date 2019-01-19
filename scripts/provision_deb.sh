c9546a204ed901e0aca7980a4d4aabf040d22b1d
perl <perl@immunant.com>
2019-01-18 16:13:27 -0800

    Fix command in docker README and clean up provisioning script

--- scripts/provision_deb.sh
+++ scripts/provision_deb.sh
@@ -11,19 +11,31 @@ export DEBIAN_FRONTEND=noninteractive
 SCRIPT_DIR="$(dirname "$0")"
 
 apt-get update -qq
-# dirmngr is required for gnupg2 key retrieval
-apt-get install -qq --install-recommends dirmngr
-# libclang-6.0-dev allows builds against pre-built clang/llvm
-apt-get install -qq build-essential clang-6.0 cmake curl \
-    git gnupg2 gperf htop ninja-build python-dev \
-    software-properties-common unzip libssl-dev \
-    libclang-6.0-dev pkg-config
+# gnupg2: required for gnupg2 key retrieval
+# libclang-6.0-dev: for fast builds against host libclang
+apt-get install -qq \
+    build-essential \
+    clang-6.0 \
+    cmake \
+    curl \
+    dirmngr \
+    git \
+    gnupg2 \
+    gperf \
+    htop \
+    libclang-6.0-dev \
+    libssl-dev \
+    ninja-build \
+    pkg-config \
+    python-dev \
+    python3-pip \
+    software-properties-common \
+    unzip
 
 update-alternatives --install /usr/bin/clang clang /usr/bin/clang-6.0 100
 update-alternatives --install /usr/bin/clang++ clang++ /usr/bin/clang++-6.0 100
 # update-alternatives --install /usr/bin/lldb lldb /usr/bin/lldb-6.0 100
 
 # Install python3 and packages
-apt-get install -qq python3-pip
 pip3 install --no-cache-dir --disable-pip-version-check -r $SCRIPT_DIR/requirements.txt
 
