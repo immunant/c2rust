c860012f7e477221daa0a355882d2f338a4fb35d
miguelsaldivar <miguel.saldivar22@hotmail.com>
2019-01-23 13:25:50 -0800

    There needs to be a bin directory inside of the install dir

--- scripts/build_translator.py
+++ scripts/build_translator.py
@@ -155,6 +155,7 @@ def configure_and_build_llvm(args) -> None:
         ninja = get_cmd_or_die("ninja")
         ninja_args = ['c2rust-ast-exporter', 'clangAstExporter',
                       'llvm-config',
+                      'tools/llvm-config/install-llvm-config',
                       'install-clang-headers',
                       'FileCheck', 'count', 'not']
         if args.with_clang:
