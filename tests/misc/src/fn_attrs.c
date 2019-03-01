e7f62b57fbd44d9cd4a769c832cdf9ffb7563d9f
Daniel Kolsoi <thadan64@gmail.com>
2019-03-01 15:18:53 -0500

    Added inline tests for extern fns with always_inline and gnu_inline

--- tests/misc/src/fn_attrs.c
+++ tests/misc/src/fn_attrs.c
@@ -5,7 +5,9 @@ __attribute__((__always_inline__)) void always_inline_nonstatic(void) {}
 __attribute__((noinline)) void noinline_nonstatic(void) {}
 void inline inline_nonstatic(void) {}
 extern void inline inline_extern(void) {}
+extern void inline __attribute__((always_inline)) always_inline_extern(void) {}
 extern void inline __attribute__((__gnu_inline__)) gnu_inline_extern(void) {}
+extern void inline __attribute__((gnu_inline, always_inline)) always_inline_gnu_inline_extern(void) {}
 
 void ensure_use(void) {
     always_inline_static();
@@ -14,5 +16,7 @@ void ensure_use(void) {
     noinline_static();
     inline_nonstatic();
     inline_extern();
+    always_inline_extern();
     gnu_inline_extern();
+    always_inline_gnu_inline_extern();
 }
