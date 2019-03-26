b7204f959d717ad5aef9670ff9cc7029659ef22e
perl <perl@immunant.com>
2019-03-25 19:18:06 -0700

    Update calls to va_list, stop calling va_end

--- c2rust-transpile/src/translator/builtins.rs
+++ c2rust-transpile/src/translator/builtins.rs
@@ -146,8 +146,13 @@ impl<'c> Translation<'c> {
                         let dst = self.convert_expr(ctx.used(), args[0])?;
                         let src = self.convert_expr(ctx.used(), args[1])?;
 
-                        let path = mk().path_expr(vec!["va_copy"]);
-                        let call_expr = mk().call_expr(path, vec![src.val] as Vec<P<Expr>>);
+                        let path = {
+                            let std_or_core = if self.tcfg.emit_no_std { "core"  } else { "std" };
+                            let path = vec!["", std_or_core, "intrinsics", "va_copy"];
+                            mk().path_expr(path)
+                        };
+                        let mut_ref_src = mk().mutbl().addr_of_expr(src.val);
+                        let call_expr = mk().call_expr(path, vec![mut_ref_src] as Vec<P<Expr>>);
                         let assign_expr = mk().assign_expr(dst.val, call_expr);
                         let stmt = mk().semi_stmt(assign_expr);
 
@@ -166,16 +171,19 @@ impl<'c> Translation<'c> {
                             return Ok(WithStmts::new(self.panic("va_end stub")))
                         } else if self.is_copied_va_decl(va_id)  {
                             // call to `va_end` on non-promoted `va_list`
-                            let val = self.convert_expr(ctx.used(), args[0])?;
+                            return Ok(WithStmts::new(self.panic("va_end stub")))
 
-                            let path = mk().path_expr(vec!["va_end"]);
-                            let call_expr = mk().call_expr(path, vec![val.val] as Vec<P<Expr>>);
-
-                            let stmt = mk().semi_stmt(call_expr);
-
-                            let mut res = WithStmts::new(self.panic("va_end stub"));
-                            res.stmts.push(stmt);
-                            return Ok(res);
+//                            let val = self.convert_expr(ctx.used(), args[0])?;
+//
+//                            let path = mk().path_expr(vec!["va_end"]);
+//                            let ref_val = mk().addr_of_expr(val.val);
+//                            let call_expr = mk().call_expr(path, vec![ref_val] as Vec<P<Expr>>);
+//
+//                            let stmt = mk().semi_stmt(call_expr);
+//
+//                            let mut res = WithStmts::new(self.panic("va_end stub"));
+//                            res.stmts.push(stmt);
+//                            return Ok(res);
                         }
                     }
                 }
