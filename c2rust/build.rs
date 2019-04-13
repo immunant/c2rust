d0b6f0bff3bdf467dc9e9b030b3502bdbad4ea18
Stephen Crane <sjc@immunant.com>
2019-04-12 18:48:07 -0700

    Validate nightly version in c2rust crate build script

--- c2rust/build.rs
+++ c2rust/build.rs
@@ -2,7 +2,12 @@ use std::env;
 use std::path::PathBuf;
 use std::process::Command;
 
+use chrono::{Duration, NaiveDate};
+use rustc_version::Channel;
+
 fn main() {
+    check_nightly_version();
+
     let sysroot = Command::new(env::var("RUSTC").unwrap())
         .arg("--print=sysroot")
         .output()
@@ -22,4 +27,34 @@ fn main() {
         .into_string()
         .expect("Unexpected non-Unicode character in rustlib path");
     println!("cargo:rustc-env=RUSTLIB={}", path_string);
+}
+
+// This can be simplified into Cargo.toml if
+// https://github.com/rust-lang/rfcs/pull/2495 ever lands.
+fn check_nightly_version() {
+    let expected_nightly_date = include_str!("../rust-toolchain")
+        .trim()
+        .splitn(2, '-')
+        .nth(1)
+        .and_then(|s| s.parse::<NaiveDate>().ok())
+        .expect("Could not parse expected toolchain version");
+    let expected_commit_date = (expected_nightly_date - Duration::days(1)).to_string();
+    let version = rustc_version::version_meta()
+        .expect("Could not find rustc version");
+    if version.channel != Channel::Nightly {
+        panic!(
+            "C2Rust requires rustc nightly-{}, but version {} was found",
+            expected_nightly_date,
+            version.semver,
+        );
+    }
+    if version.commit_date.clone().unwrap() != expected_commit_date {
+        let nightly_date = (version.commit_date.unwrap().parse::<NaiveDate>().unwrap()
+                           + Duration::days(1)).to_string();
+        panic!(
+            "C2Rust requires rustc nightly-{}, but version nightly-{} was found",
+            expected_nightly_date,
+            nightly_date,
+        );
+    }
 }
