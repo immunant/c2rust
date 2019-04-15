36e568f1d59f1755924ad9a10c898d3a4a22b8b7
Stephen Crane <sjc@immunant.com>
2019-04-15 16:20:19 -0700

    Link rust-toolchain file into c2rust-transpile crate

--- c2rust-transpile/src/build_files/mod.rs
+++ c2rust-transpile/src/build_files/mod.rs
@@ -167,7 +167,7 @@ fn emit_lib_rs(
 /// on a nightly toolchain until the `c_variadics` feature is stable.
 fn emit_rust_toolchain(tcfg: &TranspilerConfig, build_dir: &Path) {
     let output_path = build_dir.join("rust-toolchain");
-    let output = include_str!("../../../rust-toolchain").to_string();
+    let output = include_str!("../../rust-toolchain").to_string();
     maybe_write_to_file(&output_path, output, tcfg.overwrite_existing);
 }
 
