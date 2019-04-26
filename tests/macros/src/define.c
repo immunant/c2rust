5818c7992acb90a54444a4062ae0d7030b92ad78
Stephen Crane <sjc@immunant.com>
2019-04-26 00:57:36 -0700

    Expand macro test

--- tests/macros/src/define.c
+++ tests/macros/src/define.c
@@ -2,10 +2,23 @@
 
 #define TEST_CONST1 1
 #define TEST_NESTED 2
-#define TEST_CONST2 TEST_NESTED + 1
+#define TEST_CONST2 TEST_NESTED
+#define TEST_PARENS (TEST_CONST2 + 1) * 3
+#define NULL 0
 
 int reference_define() {
   int x = TEST_CONST1;
   x += TEST_CONST2;
+  if (3 < TEST_PARENS)
+    x += TEST_PARENS;
   return x;
 }
+
+// Exercise an edge case where a struct initializer needs to be in an unsafe
+// block
+struct fn_ptrs {
+  int (*fn1)(void);
+  int (*fn2)(int);
+};
+
+struct fn_ptrs const fns = {NULL, NULL};
--- tests/macros/src/test_define.rs
+++ tests/macros/src/test_define.rs
@@ -1,7 +1,8 @@
 extern crate libc;
 
-use define::{TEST_CONST1, TEST_CONST2, rust_reference_define};
-use self::libc::c_uint;
+use define::{TEST_CONST1, TEST_CONST2, TEST_PARENS, rust_reference_define};
+use define::{rust_fns};
+use self::libc::{c_int, c_uint};
 
 #[link(name = "test")]
 extern "C" {
@@ -11,5 +12,5 @@ extern "C" {
 
 pub fn test_define() {
     let rust_x = unsafe { rust_reference_define() };
-    assert_eq!(rust_x, TEST_CONST1 + TEST_CONST2);
+    assert_eq!(rust_x, TEST_CONST1 + TEST_CONST2 + TEST_PARENS as c_int);
 }
