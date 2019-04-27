ddcb40b8615be5ee8585c7e765cf94843ca3c723
perl <perl@immunant.com>
2019-04-26 19:41:07 -0700

    Add logging to zstd test script

--- tests/zstd/test.sh
+++ tests/zstd/test.sh
@@ -4,4 +4,4 @@ set -e; set -o pipefail
 SCRIPT_DIR="$(cd "$(dirname "$0" )" && pwd)"
 
 # TODO: run all of `test-fullbench`, compare output to native
-cd $SCRIPT_DIR/repo && cargo run --  -i1
+cd "$SCRIPT_DIR/repo" && cargo run --  -i1 2>&1 | tee ../`basename "$0"`.log
