afc47e47aa9f02523e0f5a740f3d1a6cf0f319ae
perl <perl@immunant.com>
2019-05-14 16:35:57 -0700

    Allow cargo.gen.sh to use custom toolcmain

--- tests/templates.py
+++ tests/templates.py
@@ -29,7 +29,8 @@ set -e; set -o pipefail
 
 SCRIPT_DIR="$(cd "$(dirname "$0" )" && pwd)"
 
-(cd "$SCRIPT_DIR/repo" && cargo build 2>&1 | tee ../`basename "$0"`.log)
+(cd "$SCRIPT_DIR/repo" \
+    && cargo +${TOOLCHAIN:-} build 2>&1 | tee ../`basename "$0"`.log)
 """
 
 
