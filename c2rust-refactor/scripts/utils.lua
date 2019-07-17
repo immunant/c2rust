35d33da75ff6cf4a9741905a6e92dcab24ab1fcf
Daniel Kolsoi <thadan64@gmail.com>
2019-07-17 16:08:18 -0400

    Remove unused code

--- c2rust-refactor/scripts/utils.lua
+++ c2rust-refactor/scripts/utils.lua
@@ -9,16 +9,3 @@ end
 function starts_with(str, start)
     return str:sub(1, #start) == start
 end
-
-Set = {}
-
-function Set.new(items)
-    self = {}
-
-    for _, i in ipairs(items) do self[i] = true end
-
-    setmetatable(self, Set)
-    Set.__index = Set
-
-    return self
-end
