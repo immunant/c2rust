d55a06250d37057f5c32e204e2364c8b89c027a6
Stephen Crane <sjc@immunant.com>
2019-07-30 18:50:53 -0700

    Assign comments on forward declaration to their definition, if available

--- c2rust-transpile/src/translator/comments.rs
+++ c2rust-transpile/src/translator/comments.rs
@@ -10,12 +10,12 @@ struct CommentLocator<'c> {
     ast_context: &'c TypedAstContext,
     comment_context: &'c CommentContext,
     comment_store: &'c mut CommentStore,
-    spans: HashMap<SomeId, Span>,
+    spans: &'c mut HashMap<SomeId, Span>,
     top_decls: &'c HashSet<CDeclId>,
 }
 
 impl<'c> NodeVisitor for CommentLocator<'c> {
-    fn pre(&mut self, id: SomeId) -> bool {
+    fn pre(&mut self, mut id: SomeId) -> bool {
         // Don't traverse into unvisited top-level decls, we should visit those
         // in sorted order.
         if let SomeId::Decl(id) = id {
@@ -26,9 +26,19 @@ impl<'c> NodeVisitor for CommentLocator<'c> {
         if let Some(loc) = self.ast_context.get_src_loc(id) {
             let comments = self.comment_context
                 .get_comments_before(loc.begin(), &self.ast_context);
-            if let Some(pos) = self.comment_store
-                .add_comments(&comments)
-            {
+            if let SomeId::Decl(decl_id) = id {
+                let decl_kind = &self.ast_context[decl_id].kind;
+                if let CDeclKind::NonCanonicalDecl { canonical_decl } = decl_kind {
+                    // Attach non-canonical decl comments to their canonical
+                    // declaration
+                    id = SomeId::Decl(*canonical_decl);
+                }
+            }
+            if let Some(existing) = self.spans.get(&id) {
+                let new_pos = self.comment_store.extend_existing_comments(&comments, Some(existing.lo()));
+                debug!("Attaching more comments {:?} to id {:?} at pos {:?}", comments, id, new_pos);
+                assert_eq!(Some(existing.lo()), new_pos);
+            } else if let Some(pos) = self.comment_store.add_comments(&comments) {
                 debug!("Attaching comments {:?} to id {:?} at pos {:?}", comments, id, pos);
                 let span = pos_to_span(pos);
                 self.spans.insert(id, span);
@@ -68,19 +78,19 @@ impl<'c> Translation<'c> {
             .iter()
             .copied()
             .collect();
+        let mut spans: HashMap<SomeId, Span> = HashMap::new();
         for decl_id in &self.ast_context.c_decls_top {
             top_decls.remove(decl_id);
             let mut visitor = CommentLocator {
                 ast_context: &self.ast_context,
                 comment_context: &self.comment_context,
                 comment_store: &mut *self.comment_store.borrow_mut(),
-                spans: HashMap::new(),
+                spans: &mut spans,
                 top_decls: &top_decls,
             };
             visitor.visit_tree(&self.ast_context, SomeId::Decl(*decl_id));
-            let CommentLocator {spans, ..} = visitor;
-            self.spans.extend(spans);
         }
+        self.spans = spans;
     }
 
     pub fn get_span(&self, id: SomeId) -> Option<Span> {
