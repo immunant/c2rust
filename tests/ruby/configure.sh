c32b21c9a04f807cc73f636a68ec7f0eae52e4f7
perl <perl@immunant.com>
2019-07-30 20:08:07 -0700

    Improve configure script for Ruby 2.6

--- tests/ruby/configure.sh
+++ tests/ruby/configure.sh
@@ -2,6 +2,8 @@
 set -e; set -o pipefail
 
 export CC=clang
-(cd repo && autoconf && ./configure --disable-shared --disable-install-doc --disable-install-rdoc \
-     --disable-install-capi --disable-jit-support -with-out-ext=dbm 2>&1 \
+# cflags=additional cflags; disable gcc's labeled goto's
+export cflags="-DOPT_THREADED_CODE=1" 
+(cd repo && ./configure rb_cv_gcc_atomic_builtins=no rb_cv_gcc_sync_builtins=no --disable-shared --disable-install-doc --disable-install-rdoc \
+     --disable-install-capi --disable-jit-support --with-setjmp-type=setjmp -with-out-ext=dbm 2>&1 \
      | tee ../`basename "$0"`.log)
