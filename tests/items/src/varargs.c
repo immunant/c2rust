c7dce542fcf2da6b74c32e06cf64fea926bfb8a1
perl <perl@immunant.com>
2019-07-31 15:04:38 -0700

    Add a couple of variadics tests

--- tests/items/src/varargs.c
+++ tests/items/src/varargs.c
@@ -1,5 +1,6 @@
 #include <stdarg.h>
 #include <stdio.h>
+#include <math.h>
 
 // Can we correctly call an extern varargs function
 void call_printf(void) {
@@ -60,4 +61,43 @@ void simple_vacopy(const char *fmt, ...) {
   vprintf(fmt, aq);
   va_end(aq);
   va_end(ap);
+}
+
+// mirrors pattern from json-c's sprintbuf
+void restart_valist(const char *fmt, ...) {
+    va_list ap;
+    // start
+    va_start(ap, fmt);
+    vprintf(fmt, ap);
+    va_end(ap);
+    // restart
+    va_start(ap, fmt);
+    vprintf(fmt, ap);
+    va_end(ap);
+}
+
+// From: https://en.cppreference.com/w/c/variadic/va_copy (CC-BY-SA)
+double sample_stddev(int count, ...)
+{
+    /* Compute the mean with args1. */
+    double sum = 0;
+    va_list args1;
+    va_start(args1, count);
+    va_list args2;
+    va_copy(args2, args1);   /* copy va_list object */
+    for (int i = 0; i < count; ++i) {
+        double num = va_arg(args1, double);
+        sum += num;
+    }
+    va_end(args1);
+    double mean = sum / count;
+
+    /* Compute standard deviation with args2 and mean. */
+    double sum_sq_diff = 0;
+    for (int i = 0; i < count; ++i) {
+        double num = va_arg(args2, double);
+        sum_sq_diff += (num-mean) * (num-mean);
+    }
+    va_end(args2);
+    return sqrt(sum_sq_diff / count);
 }
