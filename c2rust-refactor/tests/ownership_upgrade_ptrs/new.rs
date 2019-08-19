08f96ed25f4281c87fc509068fedba602f46a639
Stephen Crane <sjc@immunant.com>
2019-08-19 11:14:45 -0700

    Don't annotate with ownership attributes when upgrading ptrs to refs

--- c2rust-refactor/tests/ownership_upgrade_ptrs/new.rs
+++ c2rust-refactor/tests/ownership_upgrade_ptrs/new.rs
@@ -9,8 +9,6 @@ extern "C" {
 }
 
 #[no_mangle]
-#[ownership_mono("", WRITE, READ)]
-#[ownership_constraints(le(WRITE, _0))]
 pub unsafe extern "C" fn ten_mul(mut acc: Option<&mut f64>, digit: i32, r: Option<&f64>) -> i32 {
     **acc.as_mut().unwrap() *= 10i32 as f64;
     **acc.as_mut().unwrap() += digit as f64;
@@ -21,9 +19,7 @@ pub unsafe extern "C" fn ten_mul(mut acc: Option<&mut f64>, digit: i32, r: Optio
 #[repr(C)]
 #[derive(Copy, Clone)]
 struct SizedData {
-    #[ownership_static(READ)]
     buf: *mut u32,
-    #[ownership_static()]
     bsize: usize,
 }
 
