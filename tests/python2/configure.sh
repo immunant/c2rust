60c3f85feb92603f262d605293833ed426f5fe26
perl <perl@immunant.com>
2019-08-28 16:01:24 -0700

    Don't expect cargo build of Python2 to fail

--- tests/python2/configure.sh
+++ tests/python2/configure.sh
@@ -3,3 +3,9 @@ set -e; set -o pipefail
 
 (cd repo && ./configure --disable-shared --without-computed-gotos 2>&1 \
      | tee ../`basename "$0"`.log)
+
+# The configure script may enable an optimization for va_list's such
+# that they are memcpy'ed rather than va_copy'ed. Disable optimization
+# after configure has run since there is not an option to turn it off.
+SED_EXPR="s/define VA_LIST_IS_ARRAY 1/undef VA_LIST_IS_ARRAY/"
+(cd repo && find . -type f -name pyconfig.h -exec sed -ie "${SED_EXPR}" {} + )
