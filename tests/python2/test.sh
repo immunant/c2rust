681632024735d5bf03f9d66ddfe7e499cedc6272
perl <perl@immunant.com>
2019-09-03 17:22:11 -0700

    Blacklist python2 test requiring IPv6

--- tests/python2/test.sh
+++ tests/python2/test.sh
@@ -9,6 +9,7 @@ fi
 rm repo/python && cp $TARGET_BIN repo/python
 
 NPROC=`nproc`
-
-(cd repo && make test TESTOPTS="-j$NPROC -uall,-cpu -x test_gdb test_ssl test_bsddb3 test_urllibnet test_urllib2_localnet test_httplib -G" 2>&1 \
+# note: test_ftplib can fail inside of docker if ipv6 isn't enabled.
+FLAKY_TESTS="test_gdb test_ssl test_bsddb3 test_urllibnet test_urllib2_localnet test_httplib test_ftplib" 
+(cd repo && make test TESTOPTS="-j$NPROC -uall,-cpu -x $FLAKY_TESTS -G" 2>&1 \
      | tee ../`basename "$BASH_SOURCE"`.log)
