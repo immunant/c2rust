3e99e8d6c6af126daccbd8af5bb03ac538eca182
Stephen Crane <sjc@immunant.com>
2019-09-26 15:59:29 -0700

    Use the new rustfmt now that it supports param attributes

--- c2rust-refactor/tests/ownership_upgrade_ptrs/run.sh
+++ c2rust-refactor/tests/ownership_upgrade_ptrs/run.sh
@@ -6,8 +6,3 @@ if [ `uname` = 'Darwin' ]; then
 fi
 
 $refactor script ../../scripts/test_ownership_upgrade_ptrs.lua -- old.rs $rustflags
-
-# We manually call rustfmt with a newer nightly here because the rustfmt in
-# nightly-2019-06-22 does not handle param attributes. TODO: remove this when we
-# update to a newer nightly
-rustfmt +nightly old.new
