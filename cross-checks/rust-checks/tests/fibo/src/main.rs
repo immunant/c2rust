3c3a894c49a2da6c43f9d39f698a08ae3e79b2b7
Andrei Homescu <ah@immunant.com>
2019-10-14 18:55:31 -0700

    Move cross_check attribute to module and function level in fibo test

--- cross-checks/rust-checks/tests/fibo/src/main.rs
+++ cross-checks/rust-checks/tests/fibo/src/main.rs
@@ -1,6 +1,5 @@
 #![feature(plugin, custom_attribute)]
 #![plugin(c2rust_xcheck_plugin(config_file = "fibo_globs.c2r"))]
-#![cross_check]
 
 #[macro_use]
 extern crate c2rust_xcheck_derive;
@@ -9,8 +8,10 @@ extern crate c2rust_xcheck_runtime;
 
 extern crate c2rust_xcheck_backend_dynamic_dlsym;
 
+#[cross_check]
 mod fibo;
 
+#[cross_check]
 fn main() {
     for i in 0..5 {
         //println!("fibo({})={}", i, fibo(i));
