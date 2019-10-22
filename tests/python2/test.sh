87c1c0e7eb75fe9d15164c588dc7cd8d6d80eb57
Stephen Crane <sjc@immunant.com>
2019-10-21 22:49:12 -0700

    Unset DISPLAY for python tests so it doesn't open gui windows

--- tests/python2/test.sh
+++ tests/python2/test.sh
@@ -10,6 +10,10 @@ rm repo/python && cp $TARGET_BIN repo/python
 
 NPROC=`nproc`
 # note: test_ftplib can fail inside of docker if ipv6 isn't enabled.
-FLAKY_TESTS="test_gdb test_ssl test_bsddb3 test_urllibnet test_urllib2_localnet test_httplib test_ftplib" 
+FLAKY_TESTS="test_gdb test_ssl test_bsddb3 test_urllibnet test_urllib2_localnet test_httplib test_ftplib"
+
+# Don't want to allow gui tests because they flash annoying windows on screen
+unset DISPLAY
+
 (cd repo && make test TESTOPTS="-j$NPROC -uall,-cpu -x $FLAKY_TESTS -G" 2>&1 \
      | tee ../`basename "$BASH_SOURCE"`.log)
