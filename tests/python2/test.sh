f2e9add8e8e465a54b6f728f500775d4ada0953e
perl <perl@immunant.com>
2019-10-30 17:47:36 -0700

    Make python2 unit tests run faster - only run test harness once (buildbottest) - blacklist the two slowest tests (test_xpicle, test_smtpnet)

--- tests/python2/test.sh
+++ tests/python2/test.sh
@@ -8,8 +8,11 @@ fi
 
 rm repo/python && cp $TARGET_BIN repo/python
 
+# https://devguide.python.org/runtests/
 NPROC=`nproc`
 # note: test_ftplib can fail inside of docker if ipv6 isn't enabled.
 FLAKY_TESTS="test_gdb test_ssl test_bsddb3 test_urllibnet test_urllib2_localnet test_httplib test_ftplib" 
-(cd repo && make test TESTOPTS="-j$NPROC -uall,-cpu -x $FLAKY_TESTS -G" 2>&1 \
+# these tests pass but slow us down
+SLOW_TESTS="test_xpickle test_smtpnet"
+(cd repo && make buildbottest TESTOPTS="-j$NPROC -x $FLAKY_TESTS $SLOW_TESTS -G" 2>&1 \
      | tee ../`basename "$BASH_SOURCE"`.log)
