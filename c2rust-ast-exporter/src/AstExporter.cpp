immunant/c2rust

commit 1c954f44c67ebeb2f6ffb022533b88cbba959ddc

Export visibility attribute values

c2rust-ast-exporter/src/AstExporter.cpp
@@ -1697,9 +1697,12 @@ class TranslateASTVisitor final
 
                         if (attr->getKind() == attr::Kind::Alias) {
                             auto aa = def->getAttr<AliasAttr>();
-
                             cbor_encode_text_stringz(
                                 &attr_info, aa->getAliasee().str().c_str());
+                        } else if (attr->getKind() == attr::Kind::Visibility) {
+                            auto va = def->getAttr<VisibilityAttr>();
+                            const char *vis = VisibilityAttr::ConvertVisibilityTypeToStr(va->getVisibility());
+                            cbor_encode_text_stringz(&attr_info, vis);
                         }
                     }
                 }

commit 47ae068d477c26cebb3bf0bb2218e1748615d3dd

Fix AstExporter bug introduced by 1c954f4

c2rust-ast-exporter/src/AstExporter.cpp
@@ -1699,9 +1699,9 @@ class TranslateASTVisitor final
                             auto aa = def->getAttr<AliasAttr>();
                             cbor_encode_text_stringz(
                                 &attr_info, aa->getAliasee().str().c_str());
-                        } else if (attr->getKind() == attr::Kind::Visibility) {
-                            auto va = def->getAttr<VisibilityAttr>();
-                            const char *vis = VisibilityAttr::ConvertVisibilityTypeToStr(va->getVisibility());
+                        } else if (attr->getKind() == attr::Kind::TypeVisibility) {
+                            auto va = def->getAttr<TypeVisibilityAttr>();
+                            const char *vis = TypeVisibilityAttr::ConvertVisibilityTypeToStr(va->getVisibility());
                             cbor_encode_text_stringz(&attr_info, vis);
                         }
                     }