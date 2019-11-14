21f9ef3e580d59b30f514d8fcff2d2b834de7181
Stephen Crane <sjc@immunant.com>
2019-11-13 16:15:44 -0800

    Add snudown to testsuite

---
+++ tests/snudown/check.sh
@@ -0,0 +1,7 @@
+#!/bin/sh
+set -e; set -o pipefail
+
+SCRIPT_DIR="$(cd "$(dirname "$0" )" && pwd)"
+
+cp "$SCRIPT_DIR/repo/target/release/libsnudown.so" "$SCRIPT_DIR/repo/snudown.so"
+python2 "$SCRIPT_DIR/repo/test_snudown.py"
