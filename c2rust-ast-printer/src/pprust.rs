flip1995/rust-clippy

move syntax::parse::lexer::comments -> syntax::util::comments

src/libsyntax/print/pprust.rs
@@ -2,10 +2,10 @@ use crate::ast::{self, BlockCheckMode, PatKind, RangeEnd, RangeSyntax};
 use crate::ast::{SelfKind, GenericBound, TraitBoundModifier};
 use crate::ast::{Attribute, MacDelimiter, GenericArg};
 use crate::util::parser::{self, AssocOp, Fixity};
+use crate::util::comments;
 use crate::attr;
 use crate::source_map::{self, SourceMap, Spanned};
 use crate::token::{self, BinOpToken, DelimToken, Nonterminal, Token, TokenKind};
-use crate::parse::lexer::comments;
 use crate::print::pp::{self, Breaks};
 use crate::print::pp::Breaks::{Consistent, Inconsistent};
 use crate::ptr::P;

immunant/c2rust

c2rust-ast-printer/src/pprust.rs
@@ -2,10 +2,10 @@ use syntax::ast::{self, BlockCheckMode, PatKind, RangeEnd, RangeSyntax};
 use syntax::ast::{SelfKind, GenericBound, TraitBoundModifier};
 use syntax::ast::{Attribute, MacDelimiter, GenericArg};
 use syntax::util::parser::{self, AssocOp, Fixity};
+use syntax::util::comments;
 use syntax::attr;
 use syntax::source_map::{self, SourceMap, Spanned};
 use syntax::token::{self, BinOpToken, DelimToken, Nonterminal, Token, TokenKind};
-use syntax::parse::lexer::comments;
 use syntax::ptr::P;
 use syntax::sess::ParseSess;
 use syntax::symbol::{kw, sym};