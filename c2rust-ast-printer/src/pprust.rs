flip1995/rust-clippy

syntax::parse::sess -> syntax::sess

src/libsyntax/print/pprust.rs
@@ -6,10 +6,11 @@ use crate::attr;
 use crate::source_map::{self, SourceMap, Spanned};
 use crate::parse::token::{self, BinOpToken, DelimToken, Nonterminal, Token, TokenKind};
 use crate::parse::lexer::comments;
-use crate::parse::{self, ParseSess};
+use crate::parse;
 use crate::print::pp::{self, Breaks};
 use crate::print::pp::Breaks::{Consistent, Inconsistent};
 use crate::ptr::P;
+use crate::sess::ParseSess;
 use crate::symbol::{kw, sym};
 use crate::tokenstream::{self, TokenStream, TokenTree};
 

immunant/c2rust

c2rust-ast-printer/src/pprust.rs
@@ -6,8 +6,9 @@ use syntax::attr;
 use syntax::source_map::{self, SourceMap, Spanned};
 use syntax::parse::token::{self, BinOpToken, DelimToken, Nonterminal, Token, TokenKind};
 use syntax::parse::lexer::comments;
-use syntax::parse::{ParseSess};
+use syntax::parse;
 use syntax::ptr::P;
+use syntax::sess::ParseSess;
 use syntax::symbol::{kw, sym};
 use syntax::tokenstream::{self, TokenStream, TokenTree};
 