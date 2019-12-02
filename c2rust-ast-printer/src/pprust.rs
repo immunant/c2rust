flip1995/rust-clippy

syntax::parser::token -> syntax::token

src/libsyntax/print/pprust.rs
@@ -4,7 +4,7 @@ use crate::ast::{Attribute, MacDelimiter, GenericArg};
 use crate::util::parser::{self, AssocOp, Fixity};
 use crate::attr;
 use crate::source_map::{self, SourceMap, Spanned};
-use crate::parse::token::{self, BinOpToken, DelimToken, Nonterminal, Token, TokenKind};
+use crate::token::{self, BinOpToken, DelimToken, Nonterminal, Token, TokenKind};
 use crate::parse::lexer::comments;
 use crate::parse;
 use crate::print::pp::{self, Breaks};

immunant/c2rust

c2rust-ast-printer/src/pprust.rs
@@ -4,7 +4,7 @@ use syntax::ast::{Attribute, MacDelimiter, GenericArg};
 use syntax::util::parser::{self, AssocOp, Fixity};
 use syntax::attr;
 use syntax::source_map::{self, SourceMap, Spanned};
-use syntax::parse::token::{self, BinOpToken, DelimToken, Nonterminal, Token, TokenKind};
+use syntax::token::{self, BinOpToken, DelimToken, Nonterminal, Token, TokenKind};
 use syntax::parse::lexer::comments;
 use syntax::parse;
 use syntax::ptr::P;