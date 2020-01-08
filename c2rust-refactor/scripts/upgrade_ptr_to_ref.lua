0d154e6e3ffa1649f1759ff34cc18b94a8caad4d
Daniel Kolsoi <thadan64@gmail.com>
2020-01-08 10:23:43 -0500

    Separate Rust AST Lua helper objects from upgrade_ptr_to_ref

--- c2rust-refactor/scripts/upgrade_ptr_to_ref.lua
+++ c2rust-refactor/scripts/upgrade_ptr_to_ref.lua
@@ -1,4 +1,5 @@
 require "pl"
+require "rust_ast"
 
 -- Take a set of node ids (locals/params/fields) and turn them (if a pointer)
 -- into a reference or Box
@@ -246,133 +247,6 @@ end
 
 function ConvConfig:is_array()
     return self.conv_type == "array"
-end
-
-Ty = {}
-
-function Ty.new(kind)
-    local self = {}
-
-    self[1] = "Ty"
-    self.id = DUMMY_NODE_ID
-    self.span = DUMMY_SP
-    self.kind = kind
-
-    setmetatable(self, Ty)
-    Ty.__index = Ty
-
-    return self
-end
-
-Expr = {}
-
-function Expr.new(kind, attrs)
-    local self = {}
-
-    self[1] = "Expr"
-    self.id = DUMMY_NODE_ID
-    self.span = DUMMY_SP
-    self.kind = kind
-    self.attrs = attrs or {}
-
-    setmetatable(self, Expr)
-    Expr.__index = Expr
-
-    return self
-end
-
-Pat = {}
-
-function Pat.new(kind)
-    local self = {}
-
-    self[1] = "Pat"
-    self.id = DUMMY_NODE_ID
-    self.span = DUMMY_SP
-    self.kind = kind
-
-    setmetatable(self, Pat)
-    Pat.__index = Pat
-
-    return self
-end
-
-Local = {}
-
-function Local.new(pat, ty, init, attrs)
-    local self = {}
-
-    self[1] = "Local"
-    self.id = DUMMY_NODE_ID
-    self.span = DUMMY_SP
-    self.pat = pat
-    self.ty = ty
-    self.init = init
-    self.attrs = attrs
-
-    return self
-end
-
-Stmt = {}
-
-function Stmt.new(kind)
-    local self = {}
-
-    self[1] = "Stmt"
-    self.kind = kind
-
-    return self
-end
-
-Ident = {}
-
-function Ident.new(name)
-    local self = {}
-
-    self[1] = "Ident"
-    self.name = name
-    self.span = DUMMY_SP
-
-    setmetatable(self, Ident)
-    Ident.__index = Ident
-
-    return self
-end
-
-PathSegment = {}
-
-function PathSegment.new(ident, args)
-    local self = {}
-
-    self[1] = "PathSegment"
-    self.ident = Ident.new(ident)
-    self.args = args
-    self.id = DUMMY_NODE_ID
-
-    setmetatable(self, PathSegment)
-    PathSegment.__index = PathSegment
-
-    return self
-end
-
-Path = {}
-
-function Path.new(segments)
-    local self = {}
-    local path_segments = {}
-
-    for _, segment in ipairs(segments) do
-        table.insert(path_segments, PathSegment.new(segment))
-    end
-
-    self[1] = "Path"
-    self.span = DUMMY_SP
-    self.segments = path_segments
-
-    setmetatable(self, Path)
-    Path.__index = Path
-
-    return self
 end
 
 Visitor = {}
@@ -1593,7 +1467,6 @@ function Visitor:flat_map_stmt(stmt, walk)
 
                 if offset_caller_cfg:is_opt_any() then
                     init = Expr.new{"MethodCall", PathSegment.new("unwrap"), {exprs[1]}}
-                    pretty.dump(init)
                 end
 
                 init = Expr.new{"MethodCall", PathSegment.new("split_at_mut"), {init or exprs[1], offset_expr}}
