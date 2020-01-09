b0e27c04a345f1722dc1cc53984ede25e277559e
Stephen Crane <sjc@immunant.com>
2020-01-08 17:50:29 -0800

    Improve externs::fix_users to support more contexts and types

--- c2rust-refactor/src/transform/externs.rs
+++ c2rust-refactor/src/transform/externs.rs
@@ -1,4 +1,4 @@
-use std::collections::HashMap;
+use std::collections::{HashMap, HashSet};
 use log::Level;
 use rustc::hir::def::DefKind;
 use rustc::hir::def_id::{DefId};
@@ -11,10 +11,11 @@ use crate::ast_manip::{MutVisitNodes, visit_nodes};
 use crate::command::{CommandState, Registry};
 use crate::driver::{Phase};
 use crate::path_edit::fold_resolved_paths_with_id;
-use crate::reflect;
+use crate::reflect::Reflector;
 use crate::resolve;
 use crate::transform::Transform;
 use crate::RefactorCtxt;
+use crate::context::TypeCompare;
 
 
 /// Consult the types of the old and new externs to figure out where we'll need
@@ -23,9 +24,12 @@ pub fn fix_users(
     krate: &mut Crate,
     replace_map: &HashMap<DefId, DefId>,
     path_ids: &HashMap<NodeId, DefId>,
+    new_paths: &HashMap<DefId, (Option<QSelf>, Path)>,
     cx: &RefactorCtxt,
 ) {
     let tcx = cx.ty_ctxt();
+    let reflector = Reflector::new_with_mapping(tcx, new_paths);
+    let ty_compare = TypeCompare::new_with_mapping(cx, replace_map);
 
     /// In `ty_replace_map`, which types need replacing?
     #[derive(Clone, Copy, PartialEq, Eq, PartialOrd, Ord, Debug, Hash)]
@@ -38,6 +42,7 @@ pub fn fix_users(
         Whole,
     }
     let mut ty_replace_map: HashMap<(DefId, TyLoc), (Ty, Ty)> = HashMap::new();
+    let mut changed_fns: HashSet<DefId> = HashSet::new();
 
     for (&old_did, &new_did) in replace_map {
         let old_ty = cx.def_type(old_did);
@@ -45,7 +50,7 @@ pub fn fix_users(
 
         if !matches!([old_ty.kind] TyKind::FnDef(..)) {
             // Non-fn items are easy to handle.
-            if !cx.structural_eq_tys(old_ty, new_ty) {
+            if !ty_compare.structural_eq_tys(old_ty, new_ty) {
                 ty_replace_map.insert((old_did, TyLoc::Whole), (old_ty, new_ty));
             }
             continue;
@@ -79,15 +84,17 @@ pub fn fix_users(
 
         for (i, (&old_ty, &new_ty)) in old_sig.inputs().iter()
             .zip(new_sig.inputs().iter()).enumerate() {
-                if !cx.structural_eq_tys(old_ty, new_ty) {
+                if !ty_compare.eq_tys(old_ty, new_ty) {
                     ty_replace_map.insert((old_did, TyLoc::Arg(i)), (old_ty, new_ty));
+                    changed_fns.insert(old_did);
                 }
             }
 
         let old_ty = old_sig.output();
         let new_ty = new_sig.output();
-        if !cx.structural_eq_tys(old_ty, new_ty) {
+        if !ty_compare.eq_tys(old_ty, new_ty) {
             ty_replace_map.insert((old_did, TyLoc::Ret), (old_ty, new_ty));
+            changed_fns.insert(old_did);
         }
     }
 
@@ -112,44 +119,82 @@ pub fn fix_users(
             // cast around the whole thing.  (This should only be true for statics.)
             if let Some(&(old_ty, _new_ty)) = ty_replace_map.get(&(old_did, TyLoc::Whole)) {
                 // The rewritten expr has type `new_ty`, but its context expects `old_ty`.
-                *e = mk().cast_expr(e.clone(), reflect::reflect_tcx_ty(tcx, old_ty));
+                *e = make_cast(cx, &reflector, e.clone(), old_ty);
             }
         }
 
         // TODO: handle assignments to replaced extern statics
 
-        let callee_old_did = match e.kind {
-            ExprKind::Call(ref f, _) => path_ids.get(&f.id),
-            _ => None,
-        };
-        if let Some(&old_did) = callee_old_did {
-            // This expr is a call to a rewritten extern fn.  Add casts around args and around
-            // the whole expression, as directed by `ty_replace_map`.
-            let arg_count = expect!([e.kind] ExprKind::Call(_, ref a) => a.len());
-            info!("rewriting call - e = {:?}", e);
+        match &e.kind {
+            ExprKind::Call(ref f, _) => if let Some(&old_did) = path_ids.get(&f.id) {
+                // This expr is a call to a rewritten extern fn.  Add casts around args and around
+                // the whole expression, as directed by `ty_replace_map`.
+                let arg_count = expect!([e.kind] ExprKind::Call(_, ref a) => a.len());
+                info!("rewriting call - e = {:?}", e);
 
-            for i in 0 .. arg_count {
-                let k = (old_did, TyLoc::Arg(i));
-                if let Some(&(_old_ty, new_ty)) = ty_replace_map.get(&k) {
-                    expect!([e.kind] ExprKind::Call(_, ref mut args) => {
-                        // The new fn requires `new_ty`, where the old one needed `old_ty`.
-                        let ty_ast = reflect::reflect_tcx_ty(tcx, new_ty);
-                        let new_arg = mk().cast_expr(&args[i], ty_ast);
-                        args[i] = new_arg;
-                    });
-                    info!("  arg {} - rewrote e = {:?}", i, e);
+                for i in 0 .. arg_count {
+                    let k = (old_did, TyLoc::Arg(i));
+                    if let Some(&(_old_ty, new_ty)) = ty_replace_map.get(&k) {
+                        expect!([e.kind] ExprKind::Call(_, ref mut args) => {
+                            if let Some(ty) = cx.opt_node_type(args[i].id) {
+                                if ty_compare.eq_tys(ty, new_ty) {
+                                    // We don't need to conver this type, it already matches
+                                    return;
+                                }
+                            }
+                            // The new fn requires `new_ty`, where the old one needed `old_ty`.
+                            // TODO: fix paths in new_ty based on replacement_map
+                            let new_arg = make_cast(cx, &reflector, args[i].clone(), new_ty);
+                            args[i] = new_arg;
+                        });
+                        info!("  arg {} - rewrote e = {:?}", i, e);
+                    }
                 }
-            }
 
-            if let Some(&(old_ty, _new_ty)) = ty_replace_map.get(&(old_did, TyLoc::Ret)) {
-                // The new fn returns `new_ty`, where the old context requires `old_ty`.
-                *e = mk().cast_expr(e.clone(), reflect::reflect_tcx_ty(tcx, old_ty));
-                info!("  return - rewrote e = {:?}", e);
-            }
+                if let Some(&(old_ty, _new_ty)) = ty_replace_map.get(&(old_did, TyLoc::Ret)) {
+                    // The new fn returns `new_ty`, where the old context requires `old_ty`.
+                    *e = make_cast(cx, &reflector, e.clone(), old_ty);
+                    info!("  return - rewrote e = {:?}", e);
+                }
+            },
+
+            _ => {}
         }
     });
 }
 
+fn make_cast<'a, 'tcx>(
+    cx: &RefactorCtxt<'a, 'tcx>,
+    reflector: &Reflector<'a, 'tcx>,
+    expr: P<Expr>,
+    ty: Ty<'tcx>
+) -> P<Expr> {
+    let ty_ast = reflector.reflect_ty(ty);
+
+    let mut needs_transmute = ty.is_fn_ptr();
+    if let Some(info) = cx.opt_callee_info(&expr) {
+        if let Some(def_id) = info.def_id {
+            if cx.def_path(def_id).segments.last().unwrap().ident.as_str() == "Some"
+                && info.fn_sig.inputs()[0].is_fn_ptr()
+            {
+                needs_transmute = true;
+            }
+        }
+    }
+
+    if needs_transmute {
+        mk().call_expr(mk().path_expr(vec!["", "std", "mem", "transmute"]), vec![expr])
+    } else {
+        let expr = if let ExprKind::AddrOf(_, mutability, _) = expr.kind {
+            // We have to cast to *T where &T is the type of expr before casting
+            // to something else.
+            mk().cast_expr(expr, mk().set_mutbl(mutability).ptr_ty(mk().infer_ty()))
+        } else {
+            expr
+        };
+        mk().cast_expr(expr, ty_ast)
+    }
+}
 
 /// # `canonicalize_externs` Command
 /// 
@@ -231,15 +276,20 @@ impl Transform for CanonicalizeExterns {
         // Maps the NodeId of each rewritten path expr to the DefId of the old extern that was
         // previously referenced by that path.
         let mut path_ids = HashMap::new();
+        let new_paths = replace_map.iter().map(|(old_did, new_did)| {
+            (*old_did, cx.def_qpath(*new_did))
+        }).collect::<HashMap<_, _>>();
         fold_resolved_paths_with_id(krate, cx, |id, qself, path, def| {
             let old_did = match_or!([def[0].opt_def_id()] Some(x) => x; return (qself, path));
-            let new_did = match_or!([replace_map.get(&old_did)] Some(&x) => x;
-                                    return (qself, path));
-            path_ids.insert(id, old_did);
-            cx.def_qpath(new_did)
+            if let Some(new_path) = new_paths.get(&old_did) {
+                path_ids.insert(id, old_did);
+                new_path.clone()
+            } else {
+                (qself, path)
+            }
         });
 
-        fix_users(krate, &replace_map, &path_ids, cx);
+        fix_users(krate, &replace_map, &path_ids, &new_paths, cx);
 
         // Remove the old externs
 
