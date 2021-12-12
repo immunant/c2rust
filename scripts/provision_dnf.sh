a738c25a073bc35c774600bf77b31eacf01ab33f
Per Larsen <perl@immunant.com>
2021-12-11 22:52:48 -0800

    Provision diff on Fedora 34

--- scripts/provision_dnf.sh
+++ scripts/provision_dnf.sh
@@ -7,13 +7,22 @@ SCRIPT_DIR="$(dirname "$0")"
 . /etc/os-release
 
 # TODO: might have to do something similar to support RHEL
-if [ "$NAME" != "Fedora Linux" ]; then
+if [ "$NAME" != "Fedora" ]; then
     echo >&2 "Run this script on a Fedora host."; exit 1;
 fi
 
 # redhat-rpm-config avoids problem when pip3-installing psutils
 dnf install --quiet --assumeyes \
-    ninja-build make cmake llvm-devel clang-devel openssl-devel redhat-rpm-config python3-devel xz
+    clang-devel \
+    cmake \
+    diffutils \
+    llvm-devel \
+    make \
+    ninja-build \
+    openssl-devel \
+    python3-devel \
+    redhat-rpm-config \
+    xz
 
 pip3 install --upgrade pip
 # Current version of scan-build requires setuptools 20.5 or newer to parse
