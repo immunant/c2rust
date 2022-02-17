9426eb5c4320af8a125b6a2784830f8f17a4b3d4
fw <fw@immunant.com>
2022-02-17 15:41:42 -0500

    fix a couple warnings (unused-parens, unused-variable)

--- c2rust-transpile/src/translator/mod.rs
+++ c2rust-transpile/src/translator/mod.rs
@@ -610,8 +610,8 @@ pub fn translate(
         // Helper function that returns true if there is either a matching typedef or its
         // corresponding struct/union/enum
         fn contains(prenamed_decls: &IndexMap<CDeclId, CDeclId>, decl_id: &CDeclId) -> bool {
-            (prenamed_decls.contains_key(decl_id)
-                || prenamed_decls.values().find(|id| *id == decl_id).is_some())
+            prenamed_decls.contains_key(decl_id)
+                || prenamed_decls.values().find(|id| *id == decl_id).is_some()
         }
 
         // Populate renamer with top-level names
