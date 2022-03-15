03c338ff519eba2353ae5bf2aba0bf227b1dbc96
Stuart Pernsteiner <spernsteiner@galois.com>
2022-03-14 17:08:43 -0700

    analyze: run polonius and dump outputs

--- c2rust-analyze/src/dump.rs
+++ c2rust-analyze/src/dump.rs
@@ -1,11 +1,14 @@
 /// Copied partly from rustc `compiler/rustc_borrowck/src/facts.rs`, which is dual-licensed MIT and
 /// Apache 2.0.
+use std::collections::{BTreeMap, BTreeSet};
 use std::error::Error;
-use std::fmt::Debug;
+use std::fmt::{Debug, Write as _};
 use std::fs::{self, File};
+use std::hash::Hash;
 use std::io::{BufWriter, Write};
 use std::path;
-use crate::atoms::{AllFacts, AtomMaps, Origin, Loan, Point, Variable, Path};
+use rustc_hash::{FxHashMap, FxHashSet};
+use crate::atoms::{AllFacts, Output, AtomMaps, Origin, Loan, Point, Variable, Path};
 
 pub fn dump_facts_to_dir(
     facts: &AllFacts,
@@ -52,6 +55,51 @@ pub fn dump_facts_to_dir(
     Ok(())
 }
 
+pub fn dump_output_to_dir(
+    output: &Output,
+    maps: &AtomMaps,
+    dir: impl AsRef<path::Path>,
+) -> Result<(), Box<dyn Error>> {
+    let dir: &path::Path = dir.as_ref();
+    fs::create_dir_all(dir)?;
+    let wr = FactWriter { maps, dir };
+    macro_rules! write_output_to_path {
+        ($wr:ident . write_output_to_path($this:ident . [
+            $($field:ident,)*
+        ])) => {
+            let Output { $(ref $field,)* } = $this;
+            $(
+                $wr.write_output_to_path(
+                    $field,
+                    &format!("{}.output", stringify!($field))
+                )?;
+            )*
+        }
+    }
+    write_output_to_path! {
+        wr.write_output_to_path(output.[
+            errors,
+            subset_errors,
+            move_errors,
+            dump_enabled,
+            loan_live_at,
+            origin_contains_loan_at,
+            origin_contains_loan_anywhere,
+            origin_live_on_entry,
+            loan_invalidated_at,
+            subset,
+            subset_anywhere,
+            var_live_on_entry,
+            var_drop_live_on_entry,
+            path_maybe_initialized_on_exit,
+            path_maybe_uninitialized_on_exit,
+            known_contains,
+            var_maybe_partly_initialized_on_exit,
+        ])
+    }
+    Ok(())
+}
+
 struct FactWriter<'w> {
     maps: &'w AtomMaps,
     dir: &'w path::Path,
@@ -69,6 +117,16 @@ impl<'w> FactWriter<'w> {
         }
         Ok(())
     }
+
+    fn write_output_to_path<T>(&self, rows: &T, file_name: &str) -> Result<(), Box<dyn Error>>
+    where
+        T: OutputTable,
+    {
+        let file = &self.dir.join(file_name);
+        let mut file = BufWriter::new(File::create(file)?);
+        rows.write(&mut file, self.maps)?;
+        Ok(())
+    }
 }
 
 trait FactRow {
@@ -81,8 +139,8 @@ trait FactRow {
 
 impl<A, B> FactRow for (A, B)
 where
-    A: FactCell,
-    B: FactCell,
+    A: Render,
+    B: Render,
 {
     fn write(
         &self,
@@ -95,9 +153,9 @@ where
 
 impl<A, B, C> FactRow for (A, B, C)
 where
-    A: FactCell,
-    B: FactCell,
-    C: FactCell,
+    A: Render,
+    B: Render,
+    C: Render,
 {
     fn write(
         &self,
@@ -110,10 +168,10 @@ where
 
 impl<A, B, C, D> FactRow for (A, B, C, D)
 where
-    A: FactCell,
-    B: FactCell,
-    C: FactCell,
-    D: FactCell,
+    A: Render,
+    B: Render,
+    C: Render,
+    D: Render,
 {
     fn write(
         &self,
@@ -127,7 +185,7 @@ where
 fn write_row(
     out: &mut dyn Write,
     maps: &AtomMaps,
-    columns: &[&dyn FactCell],
+    columns: &[&dyn Render],
 ) -> Result<(), Box<dyn Error>> {
     for (index, c) in columns.iter().enumerate() {
         let tail = if index == columns.len() - 1 { "\n" } else { "\t" };
@@ -136,27 +194,173 @@ fn write_row(
     Ok(())
 }
 
-trait FactCell {
+
+trait OutputTable {
+    fn write(
+        &self,
+        out: &mut dyn Write,
+        maps: &AtomMaps,
+    ) -> Result<(), Box<dyn Error>>;
+}
+
+impl<K: Render, V: Render> OutputTable for FxHashMap<K, V> {
+    fn write(
+        &self,
+        out: &mut dyn Write,
+        maps: &AtomMaps,
+    ) -> Result<(), Box<dyn Error>> {
+        for (k, v) in self {
+            write!(out, "{}: {}", k.to_string(maps), v.to_string(maps))?;
+        }
+        Ok(())
+    }
+}
+
+impl OutputTable for bool {
+    fn write(
+        &self,
+        out: &mut dyn Write,
+        maps: &AtomMaps,
+    ) -> Result<(), Box<dyn Error>> {
+        write!(out, "{}", self)?;
+        Ok(())
+    }
+}
+
+
+trait Render {
     fn to_string(&self, maps: &AtomMaps) -> String;
 }
 
-impl FactCell for Point {
+impl<K: Render + Hash + Eq, V: Render> Render for FxHashMap<K, V> {
+    fn to_string(&self, maps: &AtomMaps) -> String {
+        let mut s = String::new();
+        write!(s, "{{").unwrap();
+        let mut first = true;
+        for (k, v) in self {
+            if !first {
+                write!(s, ",").unwrap();
+            }
+            write!(s, " {}: {}", k.to_string(maps), v.to_string(maps)).unwrap();
+        }
+        if !first {
+            write!(s, " ").unwrap();
+        }
+        write!(s, "}}").unwrap();
+        s
+    }
+}
+
+impl<T: Render + Hash + Eq> Render for FxHashSet<T> {
+    fn to_string(&self, maps: &AtomMaps) -> String {
+        let mut s = String::new();
+        write!(s, "{{").unwrap();
+        let mut first = true;
+        for x in self {
+            if !first {
+                write!(s, ",").unwrap();
+            }
+            write!(s, " {}", x.to_string(maps)).unwrap();
+        }
+        if !first {
+            write!(s, " ").unwrap();
+        }
+        write!(s, "}}").unwrap();
+        s
+    }
+}
+
+impl<K: Render + Ord, V: Render> Render for BTreeMap<K, V> {
+    fn to_string(&self, maps: &AtomMaps) -> String {
+        let mut s = String::new();
+        write!(s, "{{").unwrap();
+        let mut first = true;
+        for (k, v) in self {
+            if !first {
+                write!(s, ",").unwrap();
+            }
+            write!(s, " {}: {}", k.to_string(maps), v.to_string(maps)).unwrap();
+        }
+        if !first {
+            write!(s, " ").unwrap();
+        }
+        write!(s, "}}").unwrap();
+        s
+    }
+}
+
+impl<T: Render + Ord> Render for BTreeSet<T> {
+    fn to_string(&self, maps: &AtomMaps) -> String {
+        let mut s = String::new();
+        write!(s, "{{").unwrap();
+        let mut first = true;
+        for x in self {
+            if !first {
+                write!(s, ",").unwrap();
+            }
+            write!(s, " {}", x.to_string(maps)).unwrap();
+        }
+        if !first {
+            write!(s, " ").unwrap();
+        }
+        write!(s, "}}").unwrap();
+        s
+    }
+}
+
+impl<T: Render> Render for Vec<T> {
+    fn to_string(&self, maps: &AtomMaps) -> String {
+        let mut s = String::new();
+        write!(s, "[").unwrap();
+        let mut first = true;
+        for x in self {
+            if !first {
+                write!(s, ", ").unwrap();
+            }
+            write!(s, "{}", x.to_string(maps)).unwrap();
+        }
+        write!(s, "]").unwrap();
+        s
+    }
+}
+
+impl<A: Render, B: Render> Render for (A, B) {
+    fn to_string(&self, maps: &AtomMaps) -> String {
+        format!("({}, {})", self.0.to_string(maps), self.1.to_string(maps))
+    }
+}
+
+impl Render for Origin {
+    fn to_string(&self, maps: &AtomMaps) -> String {
+        // TODO
+        format!("{:?}", self)
+    }
+}
+
+impl Render for Loan {
+    fn to_string(&self, maps: &AtomMaps) -> String {
+        // TODO
+        format!("{:?}", self)
+    }
+}
+
+impl Render for Point {
     fn to_string(&self, maps: &AtomMaps) -> String {
         let (bb, idx, sub) = maps.get_point(*self);
         format!("{:?}({:?}[{}])", sub, bb, idx)
     }
 }
 
-/*
-impl<A: Debug> FactCell for A {
-    default fn to_string(&self, _maps: &AtomMaps) -> String {
-        format!("{:?}", self)
+impl Render for Variable {
+    fn to_string(&self, maps: &AtomMaps) -> String {
+        // TODO
+        format!("{:?}", maps.get_variable(*self))
     }
 }
 
-impl FactCell for LocationIndex {
+impl Render for Path {
     fn to_string(&self, maps: &AtomMaps) -> String {
-        format!("{:?}", maps.to_location(*self))
+        // TODO
+        format!("{:?}", self)
     }
 }
-*/
