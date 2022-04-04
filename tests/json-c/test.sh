93d7715a4fc0228c0f863d6cf914cf07467bb520
Frances Wingerter <fw@immunant.com>
2022-04-04 12:02:33 -0700

    use LDFLAGS from rustc --print native-static-libs for json-c test
    rustc may emit calls to fma which depend on libm.so (for example), so we can't simply assume json-c's old LDFLAGS are sufficient

--- tests/json-c/test.sh
+++ tests/json-c/test.sh
@@ -10,6 +10,9 @@ fi
 
 cp ${TARGET_BIN} $SCRIPT_DIR/repo/.libs/libjson-c.a
 
+# determine ldflags needed to use the static lib we built
+LDFLAGS=$(cd ${SCRIPT_DIR}/repo && RUSTFLAGS="--print native-static-libs" cargo build)
+
 (cd $SCRIPT_DIR/repo/tests && \
-    make clean && make check) 2>&1 \
+    make clean && make check LDFLAGS=$LDFLAGS) 2>&1 \
     | tee `basename "$0"`.log
