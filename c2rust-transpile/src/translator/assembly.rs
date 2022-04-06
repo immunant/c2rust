38a1903b12a86c554aa843ad16027f4e16d77a20
fw <fw@immunant.com>
2022-04-06 13:50:38 -0400

    bring asm conversion to basic completion
    options are not yet translated and it may be buggy

--- c2rust-transpile/src/translator/assembly.rs
+++ c2rust-transpile/src/translator/assembly.rs
@@ -5,7 +5,8 @@ use super::*;
 use proc_macro2::{TokenStream, TokenTree};
 use syn::__private::ToTokens;
 
-enum RegHandling {
+/// An argument direction specifier for a Rust asm! expression
+enum ArgDirSpec {
     In,
     Out,
     InOut,
@@ -13,9 +14,9 @@ enum RegHandling {
     InLateOut,
 }
 
-impl ToString for RegHandling {
+impl ToString for ArgDirSpec {
     fn to_string(&self) -> String {
-        use RegHandling::*;
+        use ArgDirSpec::*;
         match self {
             In => "in",
             Out => "out",
@@ -26,9 +27,28 @@ impl ToString for RegHandling {
     }
 }
 
-fn parse_constraints(mut constraints: &str) -> Option<(RegHandling, bool, String)> {
-    use RegHandling::*;
-    let mut is_output = match constraints.chars().next() {
+impl ArgDirSpec {
+    fn with_in(&self) -> Self {
+        use ArgDirSpec::*;
+        match self {
+            In => In,
+            Out => InOut,
+            InOut => InOut,
+            LateOut => InLateOut,
+            InLateOut => InLateOut,
+        }
+    }
+}
+
+fn parse_constraints(mut constraints: &str) ->
+    Result<(ArgDirSpec, bool, String), TranslationError> {
+    let parse_error = |constraints| {
+        Err(TranslationError::new(None, failure::err_msg(
+            "Inline assembly constraints could not be parsed: ".to_owned() + constraints,
+        ).context(TranslationErrorKind::Generic)))
+    };
+    use ArgDirSpec::*;
+    let mut is_input = match constraints.chars().next() {
         Some('+') => {
             constraints = &constraints[1..];
             true
@@ -37,7 +57,7 @@ fn parse_constraints(mut constraints: &str) -> Option<(RegHandling, bool, String
             constraints = &constraints[1..];
             false
         },
-        _ => false,
+        _ => true,
     };
 
     let early_clobber = if constraints.starts_with('&') {
@@ -58,15 +78,15 @@ fn parse_constraints(mut constraints: &str) -> Option<(RegHandling, bool, String
     constraints = match split.next() {
         Some(c) => c,
         // Parse error
-        _ => return None,
+        _ => return parse_error(constraints),
     };
     // If a comma is present, this is an output of form =[&]foo,N
     if split.next().is_some() {
-        if !is_output {
-            is_output = true;
+        if !is_input {
+            is_input = true;
         } else {
             // '+' followed by ',' is a parse error
-            return None;
+            return parse_error(constraints);
         }
     }
 
@@ -81,18 +101,18 @@ fn parse_constraints(mut constraints: &str) -> Option<(RegHandling, bool, String
     let mode = if mem_only {
         In
     } else {
-        if is_output {
+        if is_input {
             if early_clobber {InOut} else {InLateOut}
         } else {
             if early_clobber {Out} else {LateOut}
         }
     };
 
-    Some((mode, mem_only, constraints))
+    Ok((mode, mem_only, constraints))
 }
 
-// References of the form $0 need to be converted to {0}, and references
-// that are mem-only need to be converted to [{0}].
+/// References of the form $0 need to be converted to {0}, and references
+/// that are mem-only need to be converted to [{0}].
 fn rewrite_asm<F: Fn(&str) -> bool>(asm: &str, is_mem_only: F) -> String {
     let mut chunks = asm.split('$');
     let mut out = chunks.next().unwrap().to_owned();
@@ -159,7 +179,7 @@ impl<'c> Translation<'c> {
         }
 
         fn operand_is_mem_only(operand: &AsmOperand) -> bool {
-            if let Some((_handling, mem_only, _parsed)) = parse_constraints(&operand.constraints) {
+            if let Ok((_dir_spec, mem_only, _parsed)) = parse_constraints(&operand.constraints) {
                 mem_only
             } else {
                 println!("could not parse asm constraints: {}", operand.constraints);
@@ -182,33 +202,85 @@ impl<'c> Translation<'c> {
         });
         push_expr(&mut tokens, mk().lit_expr(rewritten_asm));
 
+        // Detect and pair inputs/outputs that constrain themselves to the same register
+        let mut inputs_by_register = HashMap::new();
+        for (i, input) in inputs.iter().enumerate() {
+            let (_dir_spec, mem_only, parsed) = parse_constraints(&input.constraints)?;
+            inputs_by_register.insert(parsed, (i, input.clone()));
+        }
+
+        // Convert gcc asm arguments (input and output lists) into a single list
+        // of operands with explicit arg dir specs (asm!-style)
+        struct BidirAsmOperand {
+            dir_spec: ArgDirSpec,
+            mem_only: bool,
+            constraints: String,
+            // At least one of these is non-None
+            in_expr: Option<(usize, CExprId)>,
+            out_expr: Option<(usize, CExprId)>,
+        }
+        // The unified arg list
+        let mut args = Vec::<BidirAsmOperand>::new();
+
+        // Add outputs as inout if a matching input is found, else as outputs
+        for (i, output) in outputs.into_iter().enumerate() {
+            match parse_constraints(&output.constraints) {
+                Ok((mut dir_spec, mem_only, parsed)) => {
+                    // Add to args list; if a matching in_expr is found, this is
+                    // an inout and we remove the output from the outputs list
+                    let in_expr = inputs_by_register.remove(&parsed)
+                        .map(|(i, operand)| (i, operand.expression));
+                    // For inouts, change the dirspec to include 'in'
+                    if in_expr.is_some() {
+                        dir_spec = dir_spec.with_in();
+                    }
+                    args.push(BidirAsmOperand {
+                        dir_spec,
+                        mem_only,
+                        constraints: parsed,
+                        in_expr,
+                        out_expr: Some((i, output.expression))
+                    });
+                },
+                // Constraint could not be parsed, drop it
+                Err(e) => eprintln!("{}", e),
+            }
+        }
+        // Add unmatched inputs
+        for (_, (i, input)) in inputs_by_register {
+            let (dir_spec, mem_only, parsed) = match parse_constraints(&input.constraints) {
+                Ok(x) => x,
+                Err(e) => {eprintln!("{}", e); continue;}
+            };
+            args.push(BidirAsmOperand {
+                dir_spec,
+                mem_only,
+                constraints: parsed,
+                in_expr: Some((i, input.expression)),
+                out_expr: None,
+            });
+        }
+
         // Outputs and Inputs
         let mut operand_renames = HashMap::new();
-        for &(list, is_output) in &[(outputs, true), (inputs, false)] {
-            for (operand_idx, &AsmOperand {
-                ref constraints,
-                expression,
-            }) in list.iter().enumerate()
-            {
-                tokens.push(TokenTree::Punct(Punct::new(',', Alone)));
+        for operand in args {
+            tokens.push(TokenTree::Punct(Punct::new(',', Alone)));
 
-                let mut result = self.convert_expr(ctx.used(), expression)?;
-                stmts.append(result.stmts_mut());
+            // First, convert output expr if present
+            let out_expr = if let Some((output_idx, out_expr)) = operand.out_expr {
+                let mut out_expr = self.convert_expr(ctx.used(), out_expr)?;
+                stmts.append(out_expr.stmts_mut());
+                let mut out_expr = out_expr.into_value();
 
-                let mut result = result.into_value();
-                if constraints.contains('*') {
+                if operand.mem_only {
                     // If the constraint string contains `*`, then
                     // c2rust-ast-exporter added it (there's no gcc equivalent);
                     // in this case, we need to do what clang does and pass in
                     // the operand by-address instead of by-value
-                    if is_output {
-                        result = mk().mutbl().addr_of_expr(result);
-                    } else {
-                        result = mk().addr_of_expr(result);
-                    }
+                    out_expr = mk().mutbl().addr_of_expr(out_expr);
                 }
 
-                if let Some(tied_operand) = tied_operands.get(&(operand_idx, is_output)) {
+                if let Some(tied_operand) = tied_operands.get(&(output_idx, true)) {
                     // If we have an input operand tied to an output operand,
                     // we need to replicate clang's behavior: the inline assembly
                     // uses the larger type internally, and the smaller value gets
@@ -216,80 +288,105 @@ impl<'c> Translation<'c> {
                     // back after (if needed). For portability, we moved the
                     // type conversions into the `c2rust-asm-casts` crate,
                     // so we call into that one from here.
-                    if is_output {
-                        // Convert `x` into `let freshN = &mut x; *x`
-                        let output_name = self.renamer.borrow_mut().fresh();
-                        let output_local = mk().local(
-                            mk().ident_pat(&output_name),
-                            None as Option<Box<Type>>,
-                            Some(mk().mutbl().addr_of_expr(result)),
-                        );
-                        stmts.push(mk().local_stmt(Box::new(output_local)));
 
-                        // `let mut freshN;`
-                        let inner_name = self.renamer.borrow_mut().fresh();
-                        let inner_local = mk().local(
-                            mk().ident_pat(&inner_name),
-                            None as Option<Box<Type>>,
-                            None as Option<Box<Expr>>,
-                        );
-                        stmts.push(mk().local_stmt(Box::new(inner_local)));
+                    // Convert `x` into `let freshN = &mut x; *x`
+                    let output_name = self.renamer.borrow_mut().fresh();
+                    let output_local = mk().local(
+                        mk().ident_pat(&output_name),
+                        None as Option<Box<Type>>,
+                        Some(mk().mutbl().addr_of_expr(out_expr)),
+                    );
+                    stmts.push(mk().local_stmt(Box::new(output_local)));
 
-                        result = mk().ident_expr(&inner_name);
-                        operand_renames.insert(operand_idx, (output_name, inner_name));
-                    } else {
-                        self.use_crate(ExternCrate::C2RustAsmCasts);
+                    // `let mut freshN;`
+                    let inner_name = self.renamer.borrow_mut().fresh();
+                    let inner_local = mk().local(
+                        mk().ident_pat(&inner_name),
+                        None as Option<Box<Type>>,
+                        None as Option<Box<Expr>>,
+                    );
+                    stmts.push(mk().local_stmt(Box::new(inner_local)));
 
-                        // Import the trait into scope
-                        self.with_cur_file_item_store(|item_store| {
-                            item_store.add_use(vec!["c2rust_asm_casts".into()], "AsmCastTrait");
-                        });
-
-                        let (output_name, inner_name) = operand_renames
-                            .get(&tied_operand)
-                            .unwrap();
-
-                        let input_name = self.renamer.borrow_mut().fresh();
-                        let input_local = mk().local(
-                            mk().ident_pat(&input_name),
-                            None as Option<Box<Type>>,
-                            Some(result),
-                        );
-                        stmts.push(mk().local_stmt(Box::new(input_local)));
-
-                        // Replace `result` with
-                        // `c2rust_asm_casts::AsmCast::cast_in(output, input)`
-                        let path_expr = mk().path_expr(
-                            vec!["c2rust_asm_casts", "AsmCast", "cast_in"]
-                        );
-                        let output = mk().ident_expr(output_name);
-                        let input = mk().ident_expr(input_name);
-                        result = mk().call_expr(path_expr, vec![output.clone(), input.clone()]);
-
-                        // Append the cast-out call after the assembly macro:
-                        // `c2rust_asm_casts::AsmCast::cast_out(output, input, inner);`
-                        let path_expr = mk().path_expr(
-                            vec!["c2rust_asm_casts", "AsmCast", "cast_out"]);
-                        let inner = mk().ident_expr(inner_name);
-                        let cast_out = mk().call_expr(path_expr, vec![output, input, inner]);
-                        post_stmts.push(mk().semi_stmt(cast_out));
-                    }
+                    out_expr = mk().ident_expr(&inner_name);
+                    operand_renames.insert(output_idx, (output_name, inner_name));
                 }
+                Some(out_expr)
+            } else {
+                None
+            };
 
-                let (reg_handling, mem_only, parsed_constraints) = match parse_constraints(constraints) {
-                    Some(x) => x,
-                    None => return Err(TranslationError::new(None, failure::err_msg(
-                        "Inline assembly constraints could not be parsed: ".to_owned() + constraints,
-                    ).context(TranslationErrorKind::Generic)))
-                };
-                push_expr(&mut tokens, mk().ident_expr(reg_handling.to_string()));
-                push_expr(&mut tokens, mk().paren_expr(if parsed_constraints.contains('"') {
-                    mk().lit_expr(parsed_constraints.trim_matches('"'))
-                } else {
-                    mk().ident_expr(parsed_constraints)
-                }));
-                tokens.push(TokenTree::Punct(Punct::new(' ', Alone)));
-                push_expr(&mut tokens, result);
+            // Then, handle input expr if present
+            let in_expr = if let Some((input_idx, in_expr)) = operand.in_expr {
+                let mut in_expr = self.convert_expr(ctx.used(), in_expr)?;
+                stmts.append(in_expr.stmts_mut());
+                let mut in_expr = in_expr.into_value();
+
+                if operand.mem_only {
+                    in_expr = mk().addr_of_expr(in_expr);
+                }
+                if let Some(tied_operand) = tied_operands.get(&(input_idx, false)) {
+                    self.use_crate(ExternCrate::C2RustAsmCasts);
+
+                    // Import the trait into scope
+                    self.with_cur_file_item_store(|item_store| {
+                        item_store.add_use(vec!["c2rust_asm_casts".into()], "AsmCastTrait");
+                    });
+
+                    let (output_name, inner_name) = operand_renames
+                        .get(&tied_operand)
+                        .unwrap();
+
+                    let input_name = self.renamer.borrow_mut().fresh();
+                    let input_local = mk().local(
+                        mk().ident_pat(&input_name),
+                        None as Option<Box<Type>>,
+                        Some(in_expr),
+                    );
+                    stmts.push(mk().local_stmt(Box::new(input_local)));
+
+                    // Replace `in_expr` with
+                    // `c2rust_asm_casts::AsmCast::cast_in(output, input)`
+                    let path_expr = mk().path_expr(
+                        vec!["c2rust_asm_casts", "AsmCast", "cast_in"]
+                    );
+                    let output = mk().ident_expr(output_name);
+                    let input = mk().ident_expr(input_name);
+                    in_expr = mk().call_expr(path_expr, vec![output.clone(), input.clone()]);
+
+                    // Append the cast-out call after the assembly macro:
+                    // `c2rust_asm_casts::AsmCast::cast_out(output, input, inner);`
+                    let path_expr = mk().path_expr(
+                        vec!["c2rust_asm_casts", "AsmCast", "cast_out"]);
+                    let inner = mk().ident_expr(inner_name);
+                    let cast_out = mk().call_expr(path_expr, vec![output, input, inner]);
+                    post_stmts.push(mk().semi_stmt(cast_out));
+                }
+                Some(in_expr)
+            } else {
+                None
+            };
+
+            // Emit dir_spec(constraint), quoting constraint if needed
+            push_expr(&mut tokens, mk().ident_expr(operand.dir_spec.to_string()));
+            let is_regname_or_int = operand.constraints.contains('"') ||
+                operand.constraints.starts_with(|c: char| c.is_ascii_digit());
+            let constraints_ident = if is_regname_or_int {
+                mk().lit_expr(operand.constraints.trim_matches('"'))
+            } else {
+                mk().ident_expr(operand.constraints)
+            };
+
+            // Emit input and/or output expressions, separated by "=>" if both
+            push_expr(&mut tokens, mk().paren_expr(constraints_ident));
+            if let Some(in_expr) = in_expr {
+                push_expr(&mut tokens, in_expr);
+                if out_expr.is_some() {
+                    tokens.push(TokenTree::Punct(Punct::new('=', Joint)));
+                    tokens.push(TokenTree::Punct(Punct::new('>', Joint)));
+                }
+            }
+            if let Some(out_expr) = out_expr {
+                push_expr(&mut tokens, out_expr);
             }
         }
 
