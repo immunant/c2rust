d75e429e3c7ddaf9f448290e89259e1c66190f5c
Stuart Pernsteiner <spernsteiner@galois.com>
2022-05-02 10:55:59 -0700

    analyze: infer READ and WRITE permissions

--- c2rust-analyze/src/dataflow/mod.rs
+++ c2rust-analyze/src/dataflow/mod.rs
@@ -5,60 +5,51 @@ mod type_check;
 
 
 #[derive(Clone, Debug)]
-struct Edge {
-    /// For each bit in `mask`, a positive edge sets that bit in `dest` if it's set in `source`; a
-    /// negative edge clears the bit in `dest` if it's cleared in `source`.
-    positive: bool,
-    source: PointerId,
-    dest: PointerId,
-    mask: PermissionSet,
-}
-
-#[derive(Clone, Debug)]
-struct Fixed {
-    /// For each bit in `mask`, a positive fixed constraint sets the bit in `dest`; a negative
-    /// constraint clear the bit in `dest` instead.
-    positive: bool,
-    dest: PointerId,
-    mask: PermissionSet,
+enum Constraint {
+    /// Pointer `.0` must have a subset of the permissions of poniter `.1`.
+    Subset(PointerId, PointerId),
+    /// Pointer `.0` must have all the permissions in `.1`.
+    AllPerms(PointerId, PermissionSet),
+    /// Pointer `.0` must not have any of the permissions in `.1`.
+    NoPerms(PointerId, PermissionSet),
 }
 
 #[derive(Clone, Debug, Default)]
 pub struct DataflowConstraints {
-    fixed: Vec<Fixed>,
-    edges: Vec<Edge>,
+    constraints: Vec<Constraint>,
 }
 
 impl DataflowConstraints {
-    fn add_edge(
+    fn add_subset(
         &mut self,
-        positive: bool,
-        source: PointerId,
-        dest: PointerId,
-        mask: PermissionSet,
+        a: PointerId,
+        b: PointerId,
     ) {
-        self.edges.push(Edge { positive, source, dest, mask });
+        self.constraints.push(Constraint::Subset(a, b));
     }
 
-    fn add_fixed(
+    fn add_all_perms(
         &mut self,
-        positive: bool,
-        dest: PointerId,
-        mask: PermissionSet,
+        ptr: PointerId,
+        perms: PermissionSet,
     ) {
-        self.fixed.push(Fixed { positive, dest, mask });
+        self.constraints.push(Constraint::AllPerms(ptr, perms));
+    }
+
+    fn add_no_perms(
+        &mut self,
+        ptr: PointerId,
+        perms: PermissionSet,
+    ) {
+        self.constraints.push(Constraint::NoPerms(ptr, perms));
     }
 
     /// Update the pointer permissions in `hypothesis` to satisfy these constraints.
     pub fn propagate(&self, hypothesis: &mut [PermissionSet]) -> bool {
         eprintln!("=== propagating ===");
-        eprintln!("fixed:");
-        for f in &self.fixed {
-            eprintln!("  {:?}", f);
-        }
-        eprintln!("edges:");
-        for e in &self.edges {
-            eprintln!("  {:?}", e);
+        eprintln!("constraints:");
+        for c in &self.constraints {
+            eprintln!("  {:?}", c);
         }
         eprintln!("hypothesis:");
         for (i, p) in hypothesis.iter().enumerate() {
@@ -73,40 +64,84 @@ impl DataflowConstraints {
     }
 
     fn propagate_inner(&self, hypothesis: &mut [PermissionSet]) -> Result<bool, String> {
-        for f in &self.fixed {
-            if f.positive {
-                hypothesis[f.dest.index()].insert(f.mask);
-            } else {
-                hypothesis[f.dest.index()].remove(f.mask);
-            }
-        }
-
         let mut changed = false;
         let mut dirty = vec![true; hypothesis.len()];
         let mut i = 0;
         loop {
-            if i > hypothesis.len() + self.edges.len() {
+            if i > hypothesis.len() + self.constraints.len() {
                 return Err(format!("infinite loop in dataflow edges"));
             }
             i += 1;
 
             let mut new_dirty = vec![false; hypothesis.len()];
             let mut any_new_dirty = false;
-            for e in &self.edges {
-                if !dirty[e.source.index()] {
-                    continue;
-                }
-                let old = hypothesis[e.dest.index()];
-                if e.positive {
-                    hypothesis[e.dest.index()].insert(e.mask & hypothesis[e.source.index()]);
-                } else {
-                    hypothesis[e.dest.index()].remove(e.mask & !hypothesis[e.source.index()]);
-                }
-                if hypothesis[e.dest.index()] != old {
-                    eprintln!("changed {:?}: {:?} => {:?}", e.dest, old, hypothesis[e.dest.index()]);
-                    new_dirty[e.dest.index()] = true;
-                    any_new_dirty = true;
-                    changed = true;
+            for c in &self.constraints {
+                match *c {
+                    Constraint::Subset(a, b) => {
+                        if !dirty[a.index()] && !dirty[b.index()] {
+                            continue;
+                        }
+
+                        // These should be `const`s, but that produces `error[E0015]: cannot call
+                        // non-const operator in constants`.
+
+                        // Permissions that should be propagated "down": if the superset (`b`)
+                        // doesn't have it, then the subset (`a`) should have it removed.
+                        #[allow(bad_style)]
+                        let PROPAGATE_DOWN =
+                            PermissionSet::UNIQUE;
+                        // Permissions that should be propagated "up": if the subset (`a`) has it,
+                        // then the superset (`b`) should be given it.
+                        #[allow(bad_style)]
+                        let PROPAGATE_UP =
+                            PermissionSet::READ |
+                            PermissionSet::WRITE;
+
+                        let old_a = hypothesis[a.index()];
+                        let old_b = hypothesis[b.index()];
+                        let new_a = old_a & !(!old_b & PROPAGATE_DOWN);
+                        let new_b = old_b | (old_a & PROPAGATE_UP);
+                        if new_a != old_a {
+                            eprintln!("changed {:?}: {:?} => {:?}", a, old_a, new_a);
+                            hypothesis[a.index()] = new_a;
+                            new_dirty[a.index()] = true;
+                            any_new_dirty = true;
+                        }
+                        if new_b != old_b {
+                            eprintln!("changed {:?}: {:?} => {:?}", b, old_b, new_b);
+                            hypothesis[b.index()] = new_b;
+                            new_dirty[b.index()] = true;
+                            any_new_dirty = true;
+                        }
+                    },
+
+                    Constraint::AllPerms(ptr, perms) => {
+                        if !dirty[ptr.index()] {
+                            continue;
+                        }
+                        let old = hypothesis[ptr.index()];
+                        let new = old | perms;
+                        if new != old {
+                            eprintln!("changed {:?}: {:?} => {:?}", ptr, old, new);
+                            hypothesis[ptr.index()] = new;
+                            new_dirty[ptr.index()] = true;
+                            any_new_dirty = true;
+                        }
+                    },
+
+                    Constraint::NoPerms(ptr, perms) => {
+                        if !dirty[ptr.index()] {
+                            continue;
+                        }
+                        let old = hypothesis[ptr.index()];
+                        let new = old & !perms;
+                        if new != old {
+                            hypothesis[ptr.index()] = new;
+                            eprintln!("changed {:?}: {:?} => {:?}", ptr, old, new);
+                            new_dirty[ptr.index()] = true;
+                            any_new_dirty = true;
+                        }
+                    },
                 }
             }
 
@@ -114,17 +149,6 @@ impl DataflowConstraints {
                 break;
             }
             dirty = new_dirty;
-        }
-
-        for f in &self.fixed {
-            let ok = if f.positive {
-                hypothesis[f.dest.index()].contains(f.mask)
-            } else {
-                (!hypothesis[f.dest.index()]).contains(f.mask)
-            };
-            if !ok {
-                return Err(format!("fixed dataflow constraint was violated: {:?}", f));
-            }
         }
 
         Ok(changed)
--- c2rust-analyze/src/dataflow/type_check.rs
+++ c2rust-analyze/src/dataflow/type_check.rs
@@ -1,14 +1,17 @@
 use std::collections::HashMap;
 use rustc_index::vec::IndexVec;
 use rustc_middle::mir::{
-    Body, Statement, StatementKind, Terminator, TerminatorKind, Rvalue, Place, Operand, BorrowKind,
-    Local, LocalDecl, Location, ProjectionElem,
+    Body, Statement, StatementKind, Terminator, TerminatorKind, Rvalue, Place, PlaceRef, Operand,
+    BorrowKind, Local, LocalDecl, Location, ProjectionElem, Mutability,
 };
+use rustc_middle::mir::visit::{PlaceContext, NonMutatingUseContext, MutatingUseContext};
 use crate::context::{PermissionSet, PointerId, AnalysisCtxt, LTy};
 use crate::util::{describe_rvalue, RvalueDesc};
 use super::DataflowConstraints;
 
 
+/// Visitor that walks over the MIR, computing types of rvalues/operands/places and generating
+/// constraints as a side effect.
 struct TypeChecker<'tcx, 'a> {
     acx: &'a AnalysisCtxt<'tcx>,
     constraints: DataflowConstraints,
@@ -16,17 +19,42 @@ struct TypeChecker<'tcx, 'a> {
 
 impl<'tcx> TypeChecker<'tcx, '_> {
     fn add_edge(&mut self, src: PointerId, dest: PointerId) {
-        // Forward dataflow: if src is non-UNIQUE, then dest must be non-UNIQUE.
-        self.constraints.add_edge(false, src, dest, PermissionSet::UNIQUE);
-        // Backward dataflow: if dest is READ/WRITE, then src must also be READ/WRITE.
-        self.constraints.add_edge(true, dest, src, PermissionSet::READ | PermissionSet::WRITE);
+        // Copying `src` to `dest` can discard permissions, but can't add new ones.
+        self.constraints.add_subset(dest, src);
     }
 
-    pub fn visit_place(&mut self, pl: Place<'tcx>) -> LTy<'tcx> {
+    fn record_access(&mut self, ptr: PointerId, mutbl: Mutability) {
+        eprintln!("record_access({:?}, {:?})", ptr, mutbl);
+        if ptr == PointerId::NONE {
+            return;
+        }
+        match mutbl {
+            Mutability::Mut => {
+                self.constraints.add_all_perms(ptr, PermissionSet::READ | PermissionSet::WRITE);
+            },
+            Mutability::Not => {
+                self.constraints.add_all_perms(ptr, PermissionSet::READ);
+            },
+        }
+    }
+
+    pub fn visit_place(&mut self, pl: Place<'tcx>, ctx: PlaceContext) -> LTy<'tcx> {
+        self.visit_place_ref(pl.as_ref(), ctx)
+    }
+
+    pub fn visit_place_ref(&mut self, pl: PlaceRef<'tcx>, ctx: PlaceContext) -> LTy<'tcx> {
         let mut lty = self.acx.local_tys[pl.local];
+        let mut prev_deref_ptr = None;
+
         for proj in pl.projection {
             match proj {
                 ProjectionElem::Deref => {
+                    // All derefs except the last are loads, to retrieve the pointer for the next
+                    // deref.  The last deref is either a load or a store, depending on `ctx`.
+                    if let Some(ptr) = prev_deref_ptr.take() {
+                        self.record_access(ptr, Mutability::Not);
+                    }
+                    prev_deref_ptr = Some(lty.label);
                     assert_eq!(lty.args.len(), 1);
                     lty = lty.args[0];
                 },
@@ -34,13 +62,54 @@ impl<'tcx> TypeChecker<'tcx, '_> {
                 ref proj => panic!("unsupported projection {:?} in {:?}", proj, pl),
             }
         }
+
+        if let Some(ptr) = prev_deref_ptr.take() {
+            match ctx {
+                PlaceContext::NonMutatingUse(..) => {
+                    self.record_access(ptr, Mutability::Not);
+                },
+                PlaceContext::MutatingUse(..) => {
+                    self.record_access(ptr, Mutability::Mut);
+                },
+                PlaceContext::NonUse(..) => {},
+            }
+        }
+
         lty
     }
 
-    pub fn visit_operand(&mut self, op: &Operand<'tcx>) -> LTy<'tcx> {
+    pub fn visit_rvalue(&mut self, rv: &Rvalue<'tcx>) -> PointerId {
+        eprintln!("visit_rvalue({:?}), desc = {:?}", rv, describe_rvalue(rv));
+
+        match describe_rvalue(rv) {
+            Some(RvalueDesc::Project { base, proj: _ }) => {
+                let ctx = PlaceContext::NonMutatingUse(NonMutatingUseContext::Copy);
+                let base_ty = self.visit_place_ref(base, ctx);
+                base_ty.label
+            },
+            Some(RvalueDesc::AddrOfLocal { local, proj: _ }) => {
+                self.acx.addr_of_local[local]
+            },
+            None => match *rv {
+                Rvalue::Use(ref op) => self.visit_operand(op).label,
+                _ => panic!("TODO: handle assignment of {:?}", rv),
+            },
+        }        
+    }
+
+    pub fn visit_operand(
+        &mut self,
+        op: &Operand<'tcx>,
+    ) -> LTy<'tcx> {
         match *op {
-            Operand::Copy(pl) |
-            Operand::Move(pl) => self.visit_place(pl),
+            Operand::Copy(pl) => {
+                let ctx = PlaceContext::NonMutatingUse(NonMutatingUseContext::Copy);
+                self.visit_place(pl, ctx)
+            },
+            Operand::Move(pl) => {
+                let ctx = PlaceContext::NonMutatingUse(NonMutatingUseContext::Move);
+                self.visit_place(pl, ctx)
+            },
             Operand::Constant(ref c) => {
                 let ty = c.ty();
                 // TODO
@@ -50,27 +119,21 @@ impl<'tcx> TypeChecker<'tcx, '_> {
     }
 
     pub fn visit_statement(&mut self, stmt: &Statement<'tcx>) {
+        eprintln!("visit_statement({:?})", stmt);
         match stmt.kind {
             StatementKind::Assign(ref x) => {
                 let (pl, ref rv) = **x;
-                let pl_lty = self.visit_place(pl);
+                let ctx = PlaceContext::MutatingUse(MutatingUseContext::Store);
+                let pl_lty = self.visit_place(pl, ctx);
                 let pl_ptr = pl_lty.label;
-                if pl_ptr == PointerId::NONE {
-                    return;
-                }
 
-                let rv_ptr = match describe_rvalue(rv) {
-                    Some(RvalueDesc::Project { base, proj: _ }) => {
-                        self.acx.ptr_of(base)
-                            .unwrap_or_else(|| panic!("missing pointer ID for {:?}", base))
-                    },
-                    Some(RvalueDesc::AddrOfLocal { local, proj: _ }) => {
-                        self.acx.addr_of_local[local]
-                    },
-                    None => panic!("TODO: handle assignment of {:?}", rv),
-                };
-                assert!(rv_ptr != PointerId::NONE);
-                self.add_edge(rv_ptr, pl_ptr);
+                let rv_ptr = self.visit_rvalue(rv);
+
+                if pl_ptr != PointerId::NONE || rv_ptr != PointerId::NONE {
+                    assert!(pl_ptr != PointerId::NONE);
+                    assert!(rv_ptr != PointerId::NONE);
+                    self.add_edge(rv_ptr, pl_ptr);
+                }
             },
             _ => {},
         }
