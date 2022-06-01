b2c5db18d5c7ecc53dadb59519b853e7861a3708
Khyber Sen <kkysen@gmail.com>
2022-06-01 18:03:24 -0400

    Only use `sudo` if `sudo` exists.
    It's nice to be able to run `provision_cross_tests.sh` locally
    as well as in CI (where `sudo` doesn't exist but you're running as root (I think)).
    But locally, some other commands you might not want to give sudo to.
    
    Also, don't use `cargo quickinstall` actually.

--- scripts/provision_cross_tests.sh
+++ scripts/provision_cross_tests.sh
@@ -2,19 +2,24 @@
 
 set -e
 
-cargo install cargo-quickinstall
-cargo quickinstall ripgrep
-cargo quickinstall fd-find
-cargo quickinstall cargo-zigbuild
+if command -v sudo > /dev/null; then
+    sudo=sudo
+else
+    sudo=""
+fi
 
-sudo apt install -y qemu-user qemu-user-static binfmt-support
+cargo install ripgrep
+cargo install fd-find
+cargo install cargo-zigbuild
+
+${sudo} apt install -y qemu-user qemu-user-static binfmt-support
 native_target="$(rustc -vV | rg '^host: (.*)$' --replace '$1')"
 fd '^target-tuple$' "$(dirname "${0}")/.." --exec-batch cat | while read -r rust_target; do
     if [[ "${rust_target}" != "${native_target}" ]]; then
         c_target="${rust_target/-unknown/}"
         arch="${rust_target/-*/}"
 		rustup target add "${rust_target}"
-		sudo update-binfmts --enable "qemu-${arch}"
-		sudo apt install -y "gcc-${c_target}"
+		${sudo} update-binfmts --enable "qemu-${arch}"
+		${sudo} apt install -y "gcc-${c_target}"
 	fi
 done
