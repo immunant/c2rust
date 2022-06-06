420821066a1ee930a798c435baa002788d27ac64
Khyber Sen <kkysen@gmail.com>
2022-06-06 10:00:08 -0700

    Moved `docker_run.sh` comments to inline with the arguments using the `\`#\`` trick.

--- docker/docker_run.sh
+++ docker/docker_run.sh
@@ -6,18 +6,15 @@ CONTAINER_NAME=c2rust
 SCRIPT_DIR=$( cd "$( dirname $0 )" && pwd )
 C2RUST_HOME="$(dirname "$SCRIPT_DIR")"
 
-# args
-# 1st: names container
-# 2nd: sets hostname
-# 3rd: keeps STDIN open, allocates a pseudo-TTY
-# 4-5th: maps pardir on host into docker guest
-# NOTE: ssh forwarding does not work with Docker for Mac ATM.
-# More info here https://github.com/docker/for-mac/issues/483
 docker run \
-    --name $CONTAINER_NAME \
-    --hostname docker \
-    -it \
+    `#--name $CONTAINER_NAME` `# names container` \
+    --hostname docker `# sets hostname` \
+    --interactive \
+    --tty \
+    `# maps pardir on host into docker guest` \
     --volume $C2RUST_HOME:/home/docker/C2Rust \
+    `# NOTE: ssh forwarding does not work with Docker for Mac ATM.` \
+    `# More info here https://github.com/docker/for-mac/issues/483` \
     --volume $(dirname $SSH_AUTH_SOCK):$(dirname $SSH_AUTH_SOCK) \
     --env SSH_AUTH_SOCK=$SSH_AUTH_SOCK \
     --user $USER \
