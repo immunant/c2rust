9b6bb7909cf18b4c5b032d22ec47765d04a7160c
Khyber Sen <kkysen@gmail.com>
2022-06-16 14:03:40 -0700

    Updated `pdg.sh` to take test directory so it can run on `analysis/test` or `lighttpd`.

--- scripts/pdg.sh
+++ scripts/pdg.sh
@@ -7,7 +7,11 @@ cargo() {
 }
 
 main() {
-    local profile_dir_name="${1:-debug}"
+    local test_dir="${1}"
+
+    local profile_dir_name="${PROFILE:-debug}"
+    local cwd="${PWD}"
+
     if [[ "${profile_dir_name}" != "debug" ]]; then
         echo >&2 "only debug works right now, see https://github.com/immunant/c2rust/issues/448"
         return 1
@@ -20,22 +24,22 @@ main() {
     fi
     export CARGO_PROFILE="${profile}"
 
-    test_dir=analysis/test
-
     cargo build --features dynamic-instrumentation
 
     export RUST_BACKTRACE=1
     unset RUSTC_WRAPPER
 
-    rustc_path="$(rustup which rustc)"
-    toolchain_dir="$(dirname "$(dirname "${rustc_path}")")"
+    local rustc_path="$(rustup which rustc)"
+    local toolchain_dir="$(dirname "$(dirname "${rustc_path}")")"
+
+    local c2rust="${cwd}/${profile_dir}/c2rust"
+    local runtime="${cwd}/analysis/runtime/"
 
     (cd "${test_dir}"
         cargo clean
-        LD_LIBRARY_PATH="${toolchain_dir}/lib" ../../${profile_dir}/c2rust instrument metadata.bc ../runtime/ -- --profile "${CARGO_PROFILE}"
-        INSTRUMENT_BACKEND=log INSTRUMENT_OUTPUT=log.bc METADATA_FILE=metadata.bc ${profile_dir}/c2rust-analysis-test
+        LD_LIBRARY_PATH="${toolchain_dir}/lib" "${c2rust}" instrument metadata.bc "${runtime}" -- --profile "${CARGO_PROFILE}"
+        INSTRUMENT_BACKEND=log INSTRUMENT_OUTPUT=log.bc METADATA_FILE=metadata.bc cargo run
     )
-    return
 
     (cd pdg
         RUST_LOG=info METADATA_FILE="../${test_dir}/metadata.bc" cargo run -- "../${test_dir}/log.bc"
