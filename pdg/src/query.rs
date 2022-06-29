bf47a0b6a46f7af9ac2bc6110a97fb2ed017db30
Khyber Sen <kkysen@gmail.com>
2022-06-29 09:59:48 -0700

    Rewrote `Graph::needs_write_permissions`'s docs from @aneksteind's notes.

--- pdg/src/query.rs
+++ pdg/src/query.rs
@@ -2,37 +2,25 @@ use linked_hash_set::LinkedHashSet;
 
 use crate::graph::{Graph, NodeId, NodeKind};
 
+// TODO(kkysen, aneksteind)
+// it would be nice to test properties like "an object is created in foo,
+// and its object graph contains a StoreAddr node".
+// essentially, if Stuart thinks the lighttpd example PDG looks good,
+// it would be nice to convert those mappings into test cases
+
 impl Graph {
-    /// it would be nice to test properties like "an object is created in foo,
-    /// and its object graph contains a StoreAddr node".
-    /// essentially, if Stuart thinks the lighttpd example PDG looks good,
-    /// it would be nice to convert those mappings into test cases
+    /// Query an object [`Graph`] to determine which of its [`Node`]s (returned as [`NodeId`]s)
+    /// need write permissions for future refactors into Rust references instead of raw pointers.
     ///
-    /// Wanted to give you some more info about the query. It should basically say for each node in each object graph 
-    /// whether the node needs write permissions, based on whether or not there is a path to a StoreAddr node
-    /// (within the same object graph, but there should be no paths out of the object graph anyway).
-    /// The way the current representation of the PDG is, it's actually easiest to work backwards from StoreAddr nodes 
-    /// and mark all ancestor nodes as needing write permissions (I've avoided changing the representation 
-    /// and am aligning with Stuart's original as much as possible, but if there is a strong case for doing a forward search 
-    /// then I think tweaking the representation should be fine). We can also query on a per-object basis 
-    /// and only run the query for "the object that was created in fnode_init " for example . 
-    /// Essentially, the meat of the query might be something like
+    /// This is calculated based on whether or not there is a path to a [`StoreAddr`] node,
+    /// which is a write, from the current [`Node`] we are testing
+    /// (in the same object [`Graph`], though there shouldn't be any paths out of an object [`Graph`] anyways).
+    ///
+    /// The way the PDG/[`Graph`]s is/are represented, it is actually easiest to work backwards from [`StoreAddr`] nodes
+    /// and mark all ancestor nodes as needing write permissions.
     /// 
-    /// ```psuedocode
-    /// write_permissions_required:
-    ///   needs_write = []
-    ///   not_needs_write = []
-    ///   for node in nodes.reversed():
-    ///     if node not in needs_write and node not in not_needs_write
-    ///       if node.type == StoreAddr:
-    ///         cur = node
-    ///         # walk ancestry
-    ///         while let Some(parent) = cur.parent:
-    ///             needs_write.add(parent)
-    ///             cur = parent
-    ///       else: not_needs_write.add(node)
-    ///   return needs_write # or query_node in needs_write
-    /// ```
+    /// [`StoreAddr`]: NodeKind::StoreAddr
+    /// [`Node`]: crate::graph::Node
     pub fn needs_write_permission(&self) -> impl Iterator<Item = NodeId> {
         let mut needs_write = LinkedHashSet::new();
         let mut not_needs_write = LinkedHashSet::new();
