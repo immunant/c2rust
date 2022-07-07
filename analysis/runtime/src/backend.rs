0f9d7e16df16e35fcfdb52b4638bb9badc0f120a
Khyber Sen <kkysen@gmail.com>
2022-07-06 19:16:09 -0700

    Fixed the previous issue by using a `OnceCell<()>` to run the finalizer so it can only run once.  `Event`s are dropped after the finalizer as they are before the initializer.

--- analysis/runtime/src/backend.rs
+++ analysis/runtime/src/backend.rs
@@ -1,11 +1,9 @@
 use enum_dispatch::enum_dispatch;
-use once_cell::race::OnceBool;
 use once_cell::sync::{Lazy, OnceCell};
 use std::env;
 use std::error::Error;
 use std::fs::File;
 use std::io::BufWriter;
-use std::mem::forget;
 use std::path::Path;
 use std::sync::mpsc::{self, Receiver, SyncSender};
 use std::sync::{Condvar, Mutex};
@@ -22,45 +20,62 @@ static FINISHED: Lazy<(Mutex<bool>, Condvar)> = Lazy::new(|| (Mutex::new(false),
 
 pub struct Runtime {
     tx: SyncSender<Event>,
+    finalized: OnceCell<()>,
 }
 
 impl Runtime {
     /// Initialize the [`Runtime`], which includes [`thread::spawn`]ing, so it must be run post-`main`.
-    /// 
+    ///
     /// It returns an error if [`Backend::detect`] returns an error.
     pub fn try_init() -> Result<Self, AnyError> {
         let mut backend = Backend::detect()?;
         let (tx, rx) = mpsc::sync_channel(1024);
         thread::spawn(move || backend.run(rx));
-        Ok(Self { tx })
+        Ok(Self {
+            tx,
+            finalized: OnceCell::new(),
+        })
     }
 
     /// Finalize the [`Runtime`], shutting it down.
     ///
-    /// This should not be called directly;
-    /// call [`Runtime::finalize`] or [`Runtime::drop`] instead.
+    /// This can be called any number of times; it only finalizes once.
     ///
-    /// This is only here because [`Drop::drop`] takes `&mut self` instead of `self`,
-    /// but we want [`Runtime::finalize`] to take `self`.
-    fn _finalize(&mut self) {
-        // Notify the backend that we're done
-        self.tx.send(Event::done()).unwrap();
+    /// This does the same thing as [`Runtime::drop`]
+    /// except, of course, it's not a destructor.
+    pub fn finalize(&self) {
+        // only run finalizer once
+        self.finalized.get_or_init(|| {
+            // Notify the backend that we're done.
+            self.tx.send(Event::done()).unwrap();
 
-        // Wait for the backend thread to finish
-        let (lock, cvar) = &*FINISHED;
-        let mut finished = lock.lock().unwrap();
-        while !*finished {
-            finished = cvar.wait(finished).unwrap();
+            // Wait for the backend thread to finish.
+            let (lock, cvar) = &*FINISHED;
+            let mut finished = lock.lock().unwrap();
+            while !*finished {
+                finished = cvar.wait(finished).unwrap();
+            }
+        });
+        // Don't need to `forget(self)` since the finalizer can only run once anyways.
+    }
+
+    /// Send an [`Event`] to the [`Runtime`].
+    ///
+    /// If the [`Runtime`] has already been [`Runtime::finalized`]d, 
+    /// then the [`Event`] is silently dropped.
+    /// Otherwise, it sends the [`Event`] to the channel,
+    /// panicking if there is a [`SendError`].
+    pub fn send_event(&self, event: Event) {
+        match self.finalized.get() {
+            None => {
+                // `.unwrap()` as we're in no place to handle an error here,
+                // unless we should silently drop the [`Event`] instead.
+                self.tx.send(event).unwrap();
+            }
+            Some(()) => {
+                // Silently drop the [`Event`] as the [`Runtime`] has already been [`Runtime::finalize`]d.
+            }
         }
-    }
-
-    /// Finalize the [`Runtime`], shutting it down.
-    ///
-    /// This does the same thing as [`Runtime::drop`],
-    /// except it consumes `self`.
-    pub fn finalize(mut self) {
-        self._finalize();
-        forget(self); // don't `finalize` twice
     }
 }
 
@@ -69,7 +84,7 @@ impl Drop for Runtime {
     ///
     /// This does the same thing as [`Runtime::finalize`].
     fn drop(&mut self) {
-        self._finalize();
+        self.finalize();
     }
 }
 
@@ -171,10 +186,10 @@ impl GlobalRuntime {
     }
 
     /// Send an [`Event`] to the [`GlobalRuntime`].
-    /// 
+    ///
     /// If the [`Runtime`] has been initialized, then it sends the [`Event`] to it,
     /// panicking if there is a [`SendError`].
-    /// 
+    ///
     /// If the [`Runtime`] has not yet been initialized,
     /// which is done so by [`c2rust_analysis_rt::initialize`],
     /// which is called at the start of `main`,
@@ -182,28 +197,43 @@ impl GlobalRuntime {
     /// perhaps in an `.init_array` or `.ctors` section,
     /// and we cannot use threads yet as the Rust runtime is not fully initialized yet.
     /// Therefore, we silently drop the [`Event`].
+    /// 
+    /// It also silently drops the [`Event`] if the [`Runtime`]
+    /// has been [`Runtime::finalized`]d/[`GlobalRuntime::finalize`]d.
     pub fn send_event(&self, event: Event) {
         match self.runtime.get() {
             None => {
-                // silently drop the [`Event`] as the [`Runtime`] isn't ready yet
-            },
+                // Silently drop the [`Event`] as the [`Runtime`] isn't ready/initialized yet.
+            }
             Some(runtime) => {
-                runtime.tx.send(event).unwrap();
-            },
+                runtime.send_event(event);
+            }
         }
     }
 
+    /// Try to initialize the [`GlobalRuntime`] with [`Runtime::try_init`].
+    /// 
+    /// This (or [`GlobalRuntime::init`]), on [`RUNTIME`], should be called at the top of `main`.
     pub fn try_init(&self) -> Result<&Runtime, AnyError> {
         self.runtime.get_or_try_init(Runtime::try_init)
     }
 
+    /// Same as [`GlobalRuntime::try_init`] except the [`Result`] is `.unwrap()`ed.
+    /// 
+    /// This (or [`GlobalRuntime::try_init`]), on [`RUNTIME`], should be called at the top of `main`.
     pub fn init(&self) {
         self.try_init().unwrap();
     }
 
-    pub fn finalize(&mut self) {
-        if let Some(runtime) = self.runtime.take() {
-            runtime.finalize()
+    /// Finalize the [`GlobalRuntime`] by finalizing the [`Runtime`] if it has been initialized.
+    ///
+    /// This can be called as many times as you want.
+    ///
+    /// When used from [`RUNTIME`], a `static`, this must be called at the end of `main` to properly finalize,
+    /// as `static` destructors are not run.
+    pub fn finalize(&self) {
+        if let Some(runtime) = self.runtime.get() {
+            runtime.finalize();
         }
     }
 }
