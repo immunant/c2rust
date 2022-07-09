0b59466bb033352bc5eb68cc2558091013260f45
Khyber Sen <kkysen@gmail.com>
2022-07-09 01:25:40 -0700

    Specify `--features dynamic-instrumentation` instead of `--all-features` for commands that run (`cargo build`, `cargo test`) as we don't want `--features llvm-static` in those cases.

--- scripts/check.sh
+++ scripts/check.sh
@@ -6,8 +6,9 @@ export RUSTFLAGS="-D warnings"
 export RUSTDOCFLAGS="-D warnings"
 
 cargo fmt --check
-cargo build --all-features
+cargo check --all-features
+cargo build --features dynamic-instrumentation
 cargo doc --all-features --document-private-items
-cargo test --all-features
+cargo test --features dynamic-instrumentation
 cargo clippy --tests --all --all-features
 ./scripts/test_translator.py tests/
