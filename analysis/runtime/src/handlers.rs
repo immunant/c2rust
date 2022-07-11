ce1d28d4869dffd458b862f5dd6637a734745632
Khyber Sen <kkysen@gmail.com>
2022-07-11 12:28:59 -0700

    Revert "Added `hook_fn!` so that hook functions are explicitly linked to the function being hooked and its handler."
    I think this should go in a separate PR if we do want it,
    and `reallocarray` doesn't exists on macOS/Darwin,
    meaning that matching the `libc` functions is of little use outside of documenting it,
    as we really are matching the transpiled functions, which are slightly different.
    
    This reverts commit cb2a40a96cb214c74358fd42433ef63d8ed01cc0.

--- analysis/runtime/src/handlers.rs
+++ analysis/runtime/src/handlers.rs
@@ -74,41 +74,18 @@ pub fn offset(mir_loc: MirLocId, ptr: usize, offset: isize, new_ptr: usize) {
     .unwrap();
 }
 
-type Pointer = *const ();
-
-/// Ensure the function exists both as a handler above and as a function in transpiled code.
-/// Usually that means either [`std`], [`core`], or [`libc`].
-/// 
-/// Type-checking is not performed, as that's too complicated,
-/// but this at least ensures names aren't changed and it's clear what needs to match up with what.
-/// 
-/// Note that uses of [`hook_fn`] use [`libc`],
-/// even though the transpiled versions are slightly different from [`libc`].
-/// For example, they may use [`libc::size_t`] ([`usize`]) instead of [`libc::c_ulong`] ([`u64`]).
-/// We're just checking function names, though, so this is okay.
-/// 
-/// This also allows IDEs like rust-analyzer and IntelliJ Rust to see both the hooked function and it's handler.
-macro_rules! hook_fn {
-    ($module:ident::$name:ident) => {{
-        // Ensure it exists and allow rust-analyzer to see through it.
-        let _ = self::$name;
-        let _ = $module::$name;
-        stringify!($name)
-    }}
-}
-
 /// List of functions we want hooked for the lifetime analyis runtime.
 /// 
 /// For functions in [`HOOK_FUNCTIONS`], the tracing passes 
 /// the return value of the traced function as the last argument to the trace hook for it.
 /// See the `if after_call` block in `apply_instrumentation` in `dynamic_instrumentation/src/instrument_memory.rs`.
 pub const HOOK_FUNCTIONS: &[&str] = &[
-    hook_fn!(libc::malloc),
-    hook_fn!(libc::free),
-    hook_fn!(libc::calloc),
-    hook_fn!(libc::realloc),
-    hook_fn!(libc::reallocarray),
-    hook_fn!(Pointer::offset),
+    "malloc",
+    "free",
+    "calloc",
+    "realloc",
+    "reallocarray",
+    "offset",
 ];
 
 pub fn ptr_field(mir_loc: MirLocId, ptr: usize, field_id: u32) {
