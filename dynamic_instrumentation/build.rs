4bdbefb7c4245407c2a656483e386a26bd70df5a
Khyber Sen <kkysen@gmail.com>
2022-07-13 00:58:07 -0700

    Switch the `build.rs`s to use `rustc-private-link`.

--- dynamic_instrumentation/build.rs
+++ dynamic_instrumentation/build.rs
@@ -1,25 +1,7 @@
-use std::env;
-use std::path::Path;
-use std::process::Command;
-use std::str;
+use rustc_private_link::SysRoot;
 
 fn main() {
-    // Add the toolchain lib/ directory to `-L`.  This fixes the linker error "cannot find
-    // -lLLVM-13-rust-1.60.0-nightly".
-    let out = Command::new("rustup")
-        .args(&["which", "rustc"])
-        .output()
-        .unwrap();
-    assert!(out.status.success());
-    let rustc_path = Path::new(str::from_utf8(&out.stdout).unwrap().trim_end());
-    let lib_dir = rustc_path.parent().unwrap().parent().unwrap().join("lib");
-    println!("cargo:rustc-link-search={}", lib_dir.display());
-
-    let out = Command::new(env::var("RUSTC").unwrap())
-        .arg("--print=sysroot")
-        .output()
-        .expect("Could not invoke rustc to find rust sysroot");
-    assert!(out.status.success());
-    let sysroot = str::from_utf8(&out.stdout).unwrap();
-    println!("cargo:rustc-env=RUST_SYSROOT={}", sysroot)
+    let sysroot = SysRoot::resolve();
+    sysroot.link_rustc_private();
+    sysroot.set_env();
 }
