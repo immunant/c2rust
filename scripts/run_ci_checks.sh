0121d08f0b9b863c8085bcfc8a616da54f2b4786
Khyber Sen <kkysen@gmail.com>
2022-07-13 17:42:29 -0700

    Added inline documentation to `run_ci_checks.sh`.

--- scripts/run_ci_checks.sh
+++ scripts/run_ci_checks.sh
@@ -2,14 +2,38 @@
 
 set -euox pipefail
 
+# Run the same checks that are run in CI in `azure-pipelines.yml`.
+
+# Deny all warnings, including in documentation.
 export RUSTFLAGS="-D warnings"
 export RUSTDOCFLAGS="-D warnings"
 
 cargo fmt --check
+
+# Soon to be superceded by the commented out `cargo clippy` below.
+# This is different from `cargo build`
+# as that uses `--features dynamic-instrumention` (see below)
+# and this uses `--all-features` to check everything.
 cargo check --tests --all-features
 # cargo clippy --tests --all-features
+
 cargo doc --all-features --document-private-items
+
+# At this point, we could unset `RUSTFLAGS` and `RUSTDOCFLAGS`,
+# as we've already checked all the code,
+# but doing so and then re-compiling would flush the caches,
+# so we leave them until we're done compiling.
+
+# Build these with `--features dynamic-instrumentation`
+# instead of `--all-features` as `--all-features` includes `--features llvm-static`,
+# which we don't want to test here (it doesn't work out of the box on Arch and Fedora;
+# see https://github.com/immunant/c2rust/issues/500).
 cargo build --features dynamic-instrumentation --release
 cargo test --features dynamic-instrumentation --release
-unset RUSTFLAGS # transpiled code has tons of warnings; don't allow `-D warnings`
+
+# `test_translatory.py` compiles translated code,
+# which has tons of warnings.
+# `RUSTFLAGS="-D warnings"` would be inherited by that,
+# causing tons of errors, so unset that.
+unset RUSTFLAGS
 ./scripts/test_translator.py tests/
