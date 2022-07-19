5f70d422bb64a911ae0515658712f5470742aa87
Stuart Pernsteiner <spernsteiner@galois.com>
2022-07-19 11:50:06 -0700

    analyze: use rustc-private-link to find rustc library paths

--- c2rust-analyze/build.rs
+++ c2rust-analyze/build.rs
@@ -1,21 +1,9 @@
-use std::env;
-use std::path::Path;
-use std::process::Command;
-use std::str;
+use rustc_private_link::SysRoot;
 
 fn main() {
-    let rustc = env::var("RUSTC").unwrap();
-    // Add the toolchain lib/ directory to `-L`.  This fixes the linker error "cannot find
-    // -lLLVM-13-rust-1.60.0-nightly".
-    let out = Command::new(&rustc)
-        .args(&["--print", "sysroot"])
-        .output().unwrap();
-    assert!(out.status.success());
-    let sysroot = Path::new(str::from_utf8(&out.stdout).unwrap().trim_end());
-    let lib_dir = sysroot.join("lib");
-    println!("cargo:rustc-link-search={}", lib_dir.display());
+    let sysroot = SysRoot::resolve();
+    sysroot.link_rustc_private();
 
-    let target = env::var("HOST").unwrap();
-    let target_lib_dir = lib_dir.join("rustlib").join(target).join("lib");
-    println!("cargo:rustc-env=C2RUST_TARGET_LIB_DIR={}", target_lib_dir.display());
+    print!("cargo:rustc-env=C2RUST_TARGET_LIB_DIR=");
+    print_bytes::println_bytes(&sysroot.rustlib());
 }
