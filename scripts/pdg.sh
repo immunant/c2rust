e3d221e9113073822d40a7a863fc227ce9c00776
Khyber Sen <kkysen@gmail.com>
2022-07-22 11:21:30 -0700

    Updated (and able to shorten considerably) the `pdg.sh` documentation.

--- scripts/pdg.sh
+++ scripts/pdg.sh
@@ -14,28 +14,14 @@ SCRIPT_DIR="${CWD}/$(dirname "${SCRIPT_PATH}")"
 # 
 # Instrument and run a test crate and create its PDG.
 # 
-# 1. Compile the whole `c2rust` workspace, including `c2rust instrument`.
-# 2. If the `c2rust-instrument` binary has changed,
-#    re-instrument the test crate using `c2rust instrument`.
-#    The produced metadata is saved to `metadata.bc`.
-#    It always has to be `cargo clean`ed first,
-#    for not quite known reasons, which may be:
-#    * interactions between the default `cargo` run and the `cargo` invocation in `c2rust instrument`
-#    * and/or incremental compilation
-# 3. Redirect `c2rust instrument` output to `instrument.out.log` and `instrument.err.jsonl`.
-#    These are in the test crate directory.
-#    If there is an error, `instrument.err.jsonl` is pretty-printed.
+# 1. Compile `c2rust-dynamic-instrumentation`.
+# 3. Redirect `c2rust-dynamic-instrumentation` stdout to `instrument.out.log` in the test crate directory,
+#    as it prints a lot of debugging info currently.
 # 4. Run the instrumented binary directly.
-#    Note that `cargo run` can't be used because
-#    that will recompile without instrumentation.
 #    The `bincode`-encoded event log is written to `log.bc`.
 # 5. Using the `metadata.bc` metadata and the `log.bc` event log,
 #    run `c2rust-pdg` to generate the pdg.
-#    The output is saved to `pdg.log` (relative to the test crate directory),
-#    This output is just a debug representation of the PDG.
-#    Except for the `=>`-containing lines, 
-#    which are from the `latest_assignments` `HashMap`,
-#    we want this output to remain stable.
+#    The output is saved to `pdg.log` (relative to the test crate directory).
 main() {
     local test_dir="${1}"
     local args=("${@:2}")
@@ -49,15 +35,15 @@ main() {
     fi
     local profile_args=(--profile "${profile}")
 
-    cargo build "${profile_args[@]}" --features dynamic-instrumentation
-
-    export RUST_BACKTRACE=1
+    local instrument="c2rust-dynamic-instrumentation"
+    cargo build "${profile_args[@]}" --bin "${instrument}"
 
     local c2rust="${CWD}/${profile_dir}/c2rust"
-    local c2rust_instrument="${CWD}/${profile_dir}/c2rust-dynamic-instrumentation"
+    local c2rust_instrument="${CWD}/${profile_dir}/${instrument}"
     local metadata="${CWD}/${test_dir}/metadata.bc"
 
     (cd "${test_dir}"
+        export RUST_BACKTRACE=1
         export INSTRUMENT_BACKEND=log
         export INSTRUMENT_OUTPUT=log.bc
         export INSTRUMENT_OUTPUT_APPEND=false
