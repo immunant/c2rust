c035e75e88465371541e2398436a050abee714c5
Khyber Sen <kkysen@gmail.com>
2022-07-27 22:23:35 -0700

    In the non-instrumenting case, use `RunCompiler` instead of invoking `rustc`, as it guarantees the same version is used.

--- dynamic_instrumentation/src/main.rs
+++ dynamic_instrumentation/src/main.rs
@@ -11,7 +11,7 @@ use std::{
 };
 
 use anyhow::anyhow;
-use c2rust_dynamic_instrumentation::{MirTransformCallbacks, INSTRUMENTER};
+use c2rust_dynamic_instrumentation::{MirTransformCallbacks, NullCallbacks, INSTRUMENTER};
 use camino::Utf8Path;
 use cargo_metadata::MetadataCommand;
 use clap::Parser;
@@ -48,48 +48,28 @@ fn get_sysroot_slow() -> anyhow::Result<PathBuf> {
     todo!("use `rustc --print sysroot` to support non-`rustup` cases, which @fw-immunant uses")
 }
 
+/// Resolve the current `rustc` sysroot.
+/// 
+/// Normally, `rustc` looks up the sysroot by the location of its own binary.
+/// This works because the `rustc` on `$PATH` is actually `rustup`,
+/// and `rustup` invokes the real `rustc`, which is in a location relative to the sysroot.
+/// As we invoke `rustc_driver` directly here, we are `rustc`,
+/// and thus we have to explicitly specify the sysroot that the real `rustc` would normally use.
+///
+/// Note that the sysroot contains the toolchain and host target name,
+/// but this has no effect on cross-compiling.
+/// Every toolchain's `rustc` is able to itself cross-compile.
+/// I'm not sure why the host target needs to be in the sysroot directory name, but it is.
+///
+/// Also note that this sysroot lookup should be done at runtime,
+/// not at compile-time in the `build.rs`,
+/// as the toolchain locations could be different
+/// from where this binary was compiled and where it is running
+/// (it could be on a different machine with a different `$RUSTUP_HOME`).
 fn get_sysroot() -> anyhow::Result<PathBuf> {
     get_sysroot_fast()
         .ok_or(())
         .or_else(|()| get_sysroot_slow())
-}
-
-fn passthrough_rustc(at_args: &[String], sysroot: &Path) -> anyhow::Result<ExitStatus> {
-    // We can skip a `rustup` `rustc` -> real `rustc` resolution by just invoking the real `rustc` ourselves.
-    // TODO(kkysen) this might not work without `rustup`
-    let rustc = sysroot.join("bin/rustc");
-    let status = Command::new(rustc).args(at_args.iter().skip(1)).status()?;
-    Ok(status)
-}
-
-fn instrument_rustc(
-    mut at_args: Vec<String>,
-    sysroot: &Path,
-    metadata: &Path,
-) -> anyhow::Result<()> {
-    // Normally, `rustc` looks up the sysroot by the location of its own binary.
-    // This works because the `rustc` on `$PATH` is actually `rustup`,
-    // and `rustup` invokes the real `rustc`, which is in a location relative to the sysroot.
-    // As we invoke `rustc_driver` directly here, we are `rustc`,
-    // and thus we have to explicitly specify the sysroot that the real `rustc` would normally use.
-    //
-    // Note that the sysroot contains the toolchain and host target name,
-    // but this has no effect on cross-compiling.
-    // Every toolchain's `rustc` is able to itself cross-compile.
-    // I'm not sure why the host target needs to be in the sysroot directory name, but it is.
-    //
-    // Also note that this sysroot lookup should be done at runtime,
-    // not at compile-time in the `build.rs`,
-    // as the toolchain locations could be different
-    // from where this binary was compiled and where it is running
-    // (it could be on a different machine with a different `$RUSTUP_HOME`).
-    let sysroot: &Utf8Path = sysroot.try_into()?;
-    at_args.extend(["--sysroot".into(), sysroot.as_str().into()]);
-    RunCompiler::new(&at_args, &mut MirTransformCallbacks)
-        .run()
-        .map_err(|_| anyhow!("`rustc` failed"))?;
-    INSTRUMENTER.finalize(metadata)?;
-    Ok(())
 }
 
 fn main() -> anyhow::Result<()> {
@@ -100,16 +80,30 @@ fn main() -> anyhow::Result<()> {
 
     let wrapping_rustc = env::var_os(rustc_wrapper_var).as_deref() == Some(own_exe.as_os_str());
     if wrapping_rustc {
-        let sysroot = get_sysroot()?;
-        let at_args = env::args().skip(1).collect::<Vec<_>>();
         let is_primary_package = env::var("CARGO_PRIMARY_PACKAGE").is_ok();
-        if is_primary_package {
+        let should_instrument = is_primary_package;
+        let mut at_args = env::args().skip(1).collect::<Vec<_>>();
+        let sysroot = get_sysroot()?;
+        let sysroot: &Utf8Path = sysroot.as_path().try_into()?;
+        at_args.extend(["--sysroot".into(), sysroot.as_str().into()]);
+        let result = if should_instrument {
+            RunCompiler::new(&at_args, &mut MirTransformCallbacks).run()
+        } else {
+            // Always use the dynamically linked `librustc_driver-{hash}.so`,
+            // as it is guaranteed to be the same version as the instrumented version.
+            // Furthermore, we can't accidentally load the wrong `librustc_driver-{hash}.so`,
+            // as it contains its hash.
+            // This also avoids an extra `rustc` (and potentially `rustup` `rustc`) invocation.
+            RunCompiler::new(&at_args, &mut NullCallbacks).run()
+        };
+        // `ErrorReported` means the error has already been reported to the user,
+        // so we just have to fail/exit with a failing exit code.
+        // There is no `impl Error for ErrorReported`.
+        result.map_err(|_| anyhow!("`rustc` failed"))?;
+        if should_instrument {
             let metadata =
                 env::var_os(metadata_var).ok_or_else(|| anyhow!("we should've set this"))?;
-            instrument_rustc(at_args, &sysroot, Path::new(&metadata))?;
-        } else {
-            let status = passthrough_rustc(&at_args, &sysroot)?;
-            exit_with_status(status);
+            INSTRUMENTER.finalize(Path::new(&metadata))?;
         }
     } else {
         let Args {
@@ -124,6 +118,7 @@ fn main() -> anyhow::Result<()> {
         let root_package = cargo_metadata
             .root_package()
             .ok_or_else(|| anyhow!("no root package found by `cargo`"))?;
+
         let status = Command::new(&cargo)
             .args(&["clean", "--package", root_package.name.as_str()])
             .status()?;
