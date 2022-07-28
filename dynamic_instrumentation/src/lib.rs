0ab0a3084626c238f20227e459ededb1d6c3fa78
Khyber Sen <kkysen@gmail.com>
2022-07-27 22:42:20 -0700

    Removed the old `cargo` dependency version and all its dependencies, which include the `c2rust-instrument` binary in the `c2rust` crate and `--feature dynamic-instrumentation`.

--- dynamic_instrumentation/src/lib.rs
+++ dynamic_instrumentation/src/lib.rs
@@ -15,15 +15,6 @@ extern crate rustc_target;
 mod instrument_memory;
 use instrument_memory::InstrumentMemoryOps;
 
-use cargo::core::compiler::{CompileMode, Context, Executor, Unit};
-use cargo::core::{PackageId, Target, Verbosity, Workspace};
-use cargo::ops;
-use cargo::ops::CompileOptions;
-use cargo::util::important_paths::find_root_manifest_for_wd;
-use cargo::util::CargoResult;
-use cargo::Config;
-use cargo_util::ProcessBuilder;
-
 use rustc_ast::ast::{Item, ItemKind, Visibility, VisibilityKind};
 use rustc_ast::node_id::NodeId;
 use rustc_ast::ptr::P;
@@ -39,21 +30,11 @@ use rustc_span::def_id::LocalDefId;
 use rustc_span::symbol::Ident;
 use rustc_span::DUMMY_SP;
 
-use anyhow::anyhow;
 use lazy_static::lazy_static;
-use std::env;
-use std::ffi::OsString;
-use std::path::Path;
-use std::sync::atomic::{AtomicBool, Ordering};
-use std::sync::{Arc, Mutex};
 
 lazy_static! {
     pub static ref INSTRUMENTER: InstrumentMemoryOps = InstrumentMemoryOps::new();
 }
-
-pub struct NullCallbacks;
-
-impl rustc_driver::Callbacks for NullCallbacks {}
 
 pub struct MirTransformCallbacks;
 
@@ -112,100 +93,4 @@ fn override_queries(
 
         tcx.alloc_steal_mir(mir)
     };
-}
-
-pub fn instrument(
-    metadata_file_path: &Path,
-    rt_path: &Path,
-    _args: &[String],
-) -> anyhow::Result<()> {
-    let config = Config::default().unwrap();
-    config.shell().set_verbosity(Verbosity::Quiet);
-    let mode = CompileMode::Build;
-    let compile_opts = CompileOptions::new(&config, mode).unwrap();
-
-    let manifest_path = find_root_manifest_for_wd(config.cwd()).unwrap();
-    let ws = Workspace::new(&manifest_path, &config).unwrap();
-
-    let rt_manifest_path = find_root_manifest_for_wd(rt_path).unwrap();
-    let mut rt_ws = Workspace::new(&rt_manifest_path, &config).unwrap();
-    rt_ws.set_target_dir(ws.target_dir());
-
-    let exec = Arc::new(InstrumentationExecutor {
-        target_pkg: ws.current().unwrap().package_id(),
-        rt_crate_path: Mutex::new(String::new()),
-        building_rt: AtomicBool::new(true),
-    });
-    let exec_dyn: Arc<dyn Executor> = exec.clone();
-
-    let cwd = env::current_dir().unwrap();
-
-    env::set_current_dir(rt_ws.root()).unwrap();
-    ops::compile_with_exec(&rt_ws, &compile_opts, &exec_dyn)?;
-
-    exec.building_rt.store(false, Ordering::Relaxed);
-    env::set_current_dir(cwd).unwrap();
-    ops::compile_with_exec(&ws, &compile_opts, &exec_dyn)?;
-
-    INSTRUMENTER.finalize(metadata_file_path)
-}
-struct InstrumentationExecutor {
-    target_pkg: PackageId,
-    rt_crate_path: Mutex<String>,
-    building_rt: AtomicBool,
-}
-
-impl Executor for InstrumentationExecutor {
-    fn init(&self, cx: &Context<'_, '_>, unit: &Unit) {
-        if self.building_rt.load(Ordering::Relaxed) && cx.is_primary_package(unit) {
-            *self.rt_crate_path.lock().unwrap() = cx.outputs(unit).unwrap()[0]
-                .path
-                .to_str()
-                .unwrap()
-                .to_owned();
-        }
-    }
-
-    fn exec(
-        &self,
-        cmd: &ProcessBuilder,
-        id: PackageId,
-        target: &Target,
-        _mode: CompileMode,
-        _on_stdout_line: &mut dyn FnMut(&str) -> CargoResult<()>,
-        _on_stderr_line: &mut dyn FnMut(&str) -> CargoResult<()>,
-    ) -> CargoResult<()> {
-        let mut args: Vec<String> = cmd
-            .get_args()
-            .iter()
-            .map(|a| a.to_str().unwrap().to_string())
-            .collect();
-        args.insert(0, cmd.get_program().to_str().unwrap().to_string());
-        // We need to point the rust compiler libraries to the corresponding sysroot
-        args.push(format!("--sysroot={}", env!("RUST_SYSROOT")));
-        for (var, val) in cmd.get_envs() {
-            env::set_var(var, val.as_ref().unwrap_or(&OsString::new()));
-        }
-        if id == self.target_pkg && !target.for_host() {
-            args.push("--extern".to_string());
-            args.push(format!(
-                "c2rust_analysis_rt={}",
-                self.rt_crate_path.lock().unwrap()
-            ));
-            let mut callbacks = MirTransformCallbacks;
-            // TODO: Capture stdout and pass it back to cargo
-            rustc_driver::RunCompiler::new(&args, &mut callbacks)
-                .run()
-                .map_err(|_| anyhow!("Compilation failed"))
-        } else {
-            let mut callbacks = NullCallbacks;
-            rustc_driver::RunCompiler::new(&args, &mut callbacks)
-                .run()
-                .map_err(|_| anyhow!("Compilation failed"))
-        }
-    }
-
-    fn force_rebuild(&self, _unit: &Unit) -> bool {
-        self.building_rt.load(Ordering::Relaxed)
-    }
 }
