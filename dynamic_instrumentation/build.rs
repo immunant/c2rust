6265cfa1f8d0e52883d4475a62d01d5fe9599d1b
Khyber Sen <kkysen@gmail.com>
2022-07-28 00:26:34 -0700

    Removed `$RUST_SYSROOT` code as it's no longer used anywhere.

--- dynamic_instrumentation/build.rs
+++ dynamic_instrumentation/build.rs
@@ -2,7 +2,6 @@ use rustc_private_link::SysRoot;
 
 fn main() {
     let sysroot = SysRoot::resolve();
-    sysroot.set_env_rust_sysroot();
     // Not strictly needed here since this is not used as this is a library crate (it's needed in `c2rust`),
     // but it doesn't hurt, and in case this ever adds a binary crate, it's useful.
     sysroot.link_rustc_private();
