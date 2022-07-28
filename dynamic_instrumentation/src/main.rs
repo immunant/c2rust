407d39c49360dde1d0dd6a67532f1f7f1bfe71d5
Khyber Sen <kkysen@gmail.com>
2022-07-27 22:47:07 -0700

    Removed unused `extern crate rustc_target`.

--- dynamic_instrumentation/src/main.rs
+++ dynamic_instrumentation/src/main.rs
@@ -1,18 +1,18 @@
 #![feature(rustc_private)]
-extern crate rustc_driver;
-extern crate rustc_session;
-extern crate rustc_span;
 extern crate rustc_ast;
 extern crate rustc_const_eval;
 extern crate rustc_data_structures;
+extern crate rustc_driver;
 extern crate rustc_index;
 extern crate rustc_interface;
 extern crate rustc_middle;
 extern crate rustc_mir_build;
 extern crate rustc_mir_transform;
-extern crate rustc_target;
+extern crate rustc_session;
+extern crate rustc_span;
 
 mod instrument_memory;
+
 use instrument_memory::InstrumentMemoryOps;
 
 use std::{
