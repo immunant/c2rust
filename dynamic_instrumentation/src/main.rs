dd5f89e37d3558f19b947a1cf710bc9762dc5535
Khyber Sen <kkysen@gmail.com>
2022-07-28 22:51:44 -0700

    Add a `--runtime` argument, which is used by `cargo add c2rust-analysis-rt` instead of the user having to manually do it.  However, we have to use `+stable` as `cargo add` was added in 1.62 (on 1.60 now, but upgrading in https://github.com/immunant/c2rust/pull/513).

--- dynamic_instrumentation/src/main.rs
+++ dynamic_instrumentation/src/main.rs
@@ -48,6 +48,10 @@ struct Args {
     #[clap(long, value_parser)]
     metadata: PathBuf,
 
+    /// Path to the `c2rust-analysis-rt` crate.
+    #[clap(long, value_parser)]
+    runtime: Option<PathBuf>,
+
     /// `cargo` args.
     cargo_args: Vec<OsString>,
 }
@@ -286,6 +290,7 @@ fn set_rust_toolchain() -> anyhow::Result<()> {
 fn cargo_wrapper(rustc_wrapper: &Path) -> anyhow::Result<()> {
     let Args {
         metadata,
+        runtime,
         mut cargo_args,
     } = Args::parse();
 
@@ -310,6 +315,23 @@ fn cargo_wrapper(rustc_wrapper: &Path) -> anyhow::Result<()> {
         // and if we only need to rebuild the primary package,
         // it usually isn't that slow.
         cmd.args(&["clean", "--package", root_package.name.as_str()]);
+    })?;
+
+    // TODO(kkysen) Once we upgrade to 1.62, we can just use `cargo` and not specify `+stable`.
+    // The problem currently is that `+stable` doesn't work on a resolved `$CARGO`,
+    // which happens when this runs inside of `cargo test`,
+    // and our current nightly is 1.60, which is before `cargo add` got added
+    // (though it's been in `cargo-edit` for a while).
+    Cargo {
+        path: "cargo".into(),
+    }
+    .run(|cmd| {
+        cmd.args(&["+stable", "add", "--optional", "c2rust-analysis-rt"]);
+        if let Some(runtime) = runtime {
+            // Since it's a local path, we don't need the internet,
+            // and running it offline saves a slow index sync.
+            cmd.args(&["--offline", "--path"]).arg(runtime);
+        }
     })?;
 
     // Create and truncate the metadata file for the [`rustc_wrapper`]s to append to.
