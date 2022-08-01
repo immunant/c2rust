afbee3e5ac4cd51e00de3501e91e08992d368b8d
Khyber Sen <kkysen@gmail.com>
2022-08-01 15:47:21 -0700

    Reverted an `clippy::if_same_then_else` fix and suppressed the warning with a comment saying they will not remain the same.

--- c2rust-analyze/src/type_desc.rs
+++ c2rust-analyze/src/type_desc.rs
@@ -44,8 +44,12 @@ pub fn perms_to_desc(perms: PermissionSet, flags: FlagSet) -> (Ownership, Quanti
         Ownership::Imm
     };
 
-    let qty = if perms.contains(PermissionSet::OFFSET_SUB) || perms.contains(PermissionSet::OFFSET_ADD) {
-        // TODO: should be Quantity::OffsetPtr, but that's not implemented yet
+    // TODO(spernsteiner): will not remain identical branches
+    #[allow(clippy::if_same_then_else)]
+    let qty = if perms.contains(PermissionSet::OFFSET_SUB) {
+        // TODO(spernsteiner): should be [`Quantity::OffsetPtr`], but that's not implemented yet
+        Quantity::Slice
+    } else if perms.contains(PermissionSet::OFFSET_ADD) {
         Quantity::Slice
     } else {
         Quantity::Single
