006ff8959d48683d012eefc3aa0b922ed8dc3451
Khyber Sen <kkysen@gmail.com>
2022-08-24 15:07:44 -0700

    Thoroughly documented `fn pdg_snapshot(_inner)`.

--- pdg/src/main.rs
+++ pdg/src/main.rs
@@ -258,6 +258,41 @@ mod tests {
         Ok(repo_dir.to_owned())
     }
 
+    /// Instrument and run a test crate and return a snapshot (an `impl `[`Display`]) of its [`Pdg`].
+    ///
+    /// # Args
+    /// * `test_dir` is the directory of the test crate.
+    ///   It must contain a `Cargo.toml`.
+    ///
+    /// * `profile` is the [`Profile`] the test crate is compiled and run as.
+    ///
+    /// * `to_print` are the [`ToPrint`]s that should be printed in the [`Pdg`] snapshot.
+    ///
+    /// # Overview
+    ///
+    /// This instruments the `test_dir` crate using `c2rust-instrument` through `cargo run --bin c2rust-instrument`.
+    /// It is used through a separate binary and its CLI because `c2rust-instrument`
+    /// must have control over its `main` in order to invoke itself as a `$RUSTC_WRAPPER`.
+    ///
+    /// The instrumented binary, compiled with `profile`,
+    /// is then run via a `cargo run`, but done through `c2rust-instrument ... -- run`.
+    ///
+    /// Then the metadata and event log files created are read in by the `c2rust-pdg` code here.
+    /// All assertion tests are checked on the [`Pdg`]'s [`Graphs`](crate::Graphs).
+    /// Then, finally, the [`Pdg`] is snapshotted into an `impl `[`Display`], printing the [`ToPrint`]s in `to_print`.
+    ///
+    /// # Details
+    ///
+    /// `c2rust-instrument` is compiled with the same `$PROFILE`/`--profile`/`--release` that this current crate is.
+    ///
+    /// The metadata file and event log are placed in the same directory as the test binary built.
+    /// Thus, there should be no conflicts when building with different [`Profile`]s simultaneously.
+    ///
+    /// `--set-runtime` and `--runtime-path` are also passed to `c2rust-instrument`,
+    /// setting the runtime dependency to the correct path in case it's out-of-date.
+    ///
+    /// `$INSTRUMENT_OUTPUT_APPEND` is set to `false` as this runs the test binary only once,
+    /// so appending is not yet necessary.
     fn pdg_snapshot_inner(
         test_dir: &Path,
         profile: Profile,
@@ -302,6 +337,23 @@ mod tests {
         Ok(repr.to_string())
     }
 
+    /// Instrument and run a test crate and return a snapshot (an `impl `[`Display`]) of its [`Pdg`].
+    ///
+    /// # Args
+    /// * `test_dir` is the directory of the test crate.
+    ///   It must contain a `Cargo.toml`.
+    ///
+    /// * `profile` is the [`Profile`] the test crate is compiled and run as.
+    ///
+    /// * `to_print` are the [`ToPrint`]s that should be printed in the [`Pdg`] snapshot.
+    ///
+    /// # Overview
+    ///
+    /// This instruments the `test_dir` crate using `c2rust-instrument`, creating a metadata file.
+    /// The instrumented binary, compiled with `profile`, is then run, creating an event log.
+    /// Those are then read in by the `c2rust-pdg` code here to create a [`Pdg`].
+    /// All assertion tests are checked on the [`Pdg`]'s [`Graphs`](crate::Graphs).
+    /// Then, finally, the [`Pdg`] is snapshotted into an `impl `[`Display`], printing the [`ToPrint`]s in `to_print`.
     pub fn pdg_snapshot(
         test_dir: impl AsRef<Path>,
         profile: Profile,
