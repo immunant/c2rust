828c18dc00624860ee7a98025ed808db5665b861
Peter Faith Boyland <pb@immunant.com>
2022-08-26 16:05:55 -0700

    Added docs for `NodeInfo`: general idea and what it is meant to contain later.

--- pdg/src/info.rs
+++ pdg/src/info.rs
@@ -1,6 +1,9 @@
 use crate::Graphs;
-use std::fmt::{self, Debug, Formatter, Display};
+use std::fmt::{self, Debug, Display, Formatter};
 
+///Information generated from the PDG proper which is used as data for static analysis.
+///Eventually will include information about what kinds of nodes the node flows to, as
+///well as its ability to be used as a mutable reference.
 #[derive(Hash, Clone, PartialEq, Debug)]
 pub struct NodeInfo {}
 
@@ -9,7 +12,6 @@ impl Display for NodeInfo {
         write!(f, "")
     }
 }
-
 
 pub fn add_flags(pdg: &mut Graphs) {
     for g in &mut pdg.graphs {
