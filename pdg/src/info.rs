55a95262ca6715c178445de990a35459799f4eb7
boyland-pf <66194159+boyland-pf@users.noreply.github.com>
2022-08-26 18:16:26 -0500

    Add missing spaces in doc comments.
    Co-authored-by: Khyber Sen <kkysen@gmail.com>

--- pdg/src/info.rs
+++ pdg/src/info.rs
@@ -1,9 +1,10 @@
 use crate::Graphs;
 use std::fmt::{self, Debug, Display, Formatter};
 
-///Information generated from the PDG proper which is used as data for static analysis.
+/// Information generated from the PDG proper which is used as data for static analysis.
+///
 ///Eventually will include information about what kinds of nodes the node flows to, as
-///well as its ability to be used as a mutable reference.
+/// well as its ability to be used as a mutable reference.
 #[derive(Hash, Clone, PartialEq, Debug)]
 pub struct NodeInfo {}
 
