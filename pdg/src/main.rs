5836246d49271c7a24ead54c9308fe2d6d36bbbe
Khyber Sen <kkysen@gmail.com>
2022-08-25 19:12:45 -0700

    Remove `pdg_snapshot` wrapper around `pdg_snapshot_inner` (now only `pdg_snapshot`), de-duplicating the docs and removing the generics.

--- pdg/src/main.rs
+++ pdg/src/main.rs
@@ -293,7 +293,7 @@ mod tests {
     ///
     /// `$INSTRUMENT_OUTPUT_APPEND` is set to `false` as this runs the test binary only once,
     /// so appending is not yet necessary.
-    fn pdg_snapshot_inner(
+    fn pdg_snapshot(
         test_crate_dir: &Path,
         profile: Profile,
         to_print: &[ToPrint],
@@ -337,33 +337,8 @@ mod tests {
         Ok(repr.to_string())
     }
 
-    /// Instrument and run a test crate and return a snapshot (an `impl `[`Display`]) of its [`Pdg`].
-    ///
-    /// # Args
-    /// * `test_crate_dir` is the directory of the test crate.
-    ///   It must contain a `Cargo.toml`.
-    ///
-    /// * `profile` is the [`Profile`] the test crate is compiled and run as.
-    ///
-    /// * `to_print` are the [`ToPrint`]s that should be printed in the [`Pdg`] snapshot.
-    ///
-    /// # Overview
-    ///
-    /// This instruments the `test_crate_dir` crate using `c2rust-instrument`, creating a metadata file.
-    /// The instrumented binary, compiled with `profile`, is then run, creating an event log.
-    /// Those are then read in by the `c2rust-pdg` code here to create a [`Pdg`].
-    /// All assertion tests are checked on the [`Pdg`]'s [`Graphs`](crate::Graphs).
-    /// Then, finally, the [`Pdg`] is snapshotted into an `impl `[`Display`], printing the [`ToPrint`]s in `to_print`.
-    pub fn pdg_snapshot(
-        test_crate_dir: impl AsRef<Path>,
-        profile: Profile,
-        to_print: &[ToPrint],
-    ) -> eyre::Result<impl Display> {
-        pdg_snapshot_inner(test_crate_dir.as_ref(), profile, to_print)
-    }
-
     fn analysis_test_pdg_snapshot(profile: Profile) -> eyre::Result<impl Display> {
-        pdg_snapshot(repo_dir()?.join("analysis/test"), profile, {
+        pdg_snapshot(repo_dir()?.join("analysis/test").as_path(), profile, {
             use ToPrint::*;
             &[Graphs, WritePermissions, Counts]
         })
