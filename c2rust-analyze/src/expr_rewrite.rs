9766e967bfcc2fc13f23f7fceec9b285aaa28020
Khyber Sen <kkysen@gmail.com>
2022-09-27 20:17:31 -0700

    Replaced `StatementKind::CopyNonOverlapping` with `StatementKind::Intrinsic`.

--- c2rust-analyze/src/expr_rewrite.rs
+++ c2rust-analyze/src/expr_rewrite.rs
@@ -138,7 +138,7 @@ impl<'a, 'tcx> ExprRewriteVisitor<'a, 'tcx> {
             StatementKind::Retag(..) => {}
             StatementKind::AscribeUserType(..) => {}
             StatementKind::Coverage(..) => {}
-            StatementKind::CopyNonOverlapping(..) => todo!("statement {:?}", stmt),
+            StatementKind::Intrinsic(..) => todo!("statement {:?}", stmt),
             StatementKind::Nop => {}
         }
     }
