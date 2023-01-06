aa67f6d292132a53fd6e776bd9618ea67e99dee7
David Anekstein <da@immunant.com>
2023-01-06 16:33:53 -0500

    update lighttpd-minimal example to reflect support for ADT fields

--- analysis/tests/lighttpd-minimal/src/main.rs
+++ analysis/tests/lighttpd-minimal/src/main.rs
@@ -63,7 +63,6 @@ unsafe extern "C" fn fdnode_init() -> *mut libc::c_void /*TODO: handle *mut fdno
     if fdn.is_null() {
         // println!("It's null");
     }
-
     return fdn;
 }
 
@@ -71,8 +70,14 @@ unsafe extern "C" fn fdnode_init() -> *mut libc::c_void /*TODO: handle *mut fdno
 pub unsafe extern "C" fn connection_accepted(
     mut srv: *mut server,
     mut cnt: libc::c_int,
-) -> *mut libc::c_void /* TODO: handle *mut connection */ {
-    let con = malloc(::std::mem::size_of::<connection>() as libc::c_ulong); // TODO: handle as *mut connection;
+    fdn: *mut fdnode,     // TODO: remove when casts from c_void are handled
+    con: *mut connection, // TODO: remove when casts from c_void are handled
+) -> *mut connection {
+    // let con = malloc(::std::mem::size_of::<connection>() as libc::c_ulong); // TODO: handle as *mut connection;
+    (*con).fd = cnt;
+    (*con).fdn = fdn;
+    let x = &*(*(*(*srv).ev).fdarray).offset(0);
+    (*(*(*(*srv).ev).fdarray).offset(0)).fd = 0;
     return con;
 }
 
@@ -90,11 +95,23 @@ pub unsafe extern "C" fn fdevent_fdnode_event_del(mut ev: *mut fdevents, mut fdn
     }
 }
 
-unsafe extern "C" fn fdevent_fdnode_event_unsetter(mut ev: *mut fdevents, mut fdn: *mut fdnode) {}
+unsafe extern "C" fn fdevent_fdnode_event_unsetter(mut ev: *mut fdevents, mut fdn: *mut fdnode) {
+    if -(1 as libc::c_int) == (*fdn).fde_ndx {
+        return;
+    }
+    (*fdn).fde_ndx = -(1 as libc::c_int);
+    (*fdn).events = 0 as libc::c_int;
+}
 
 #[no_mangle]
-pub unsafe extern "C" fn fdevent_unregister(mut fds: *mut *mut fdnode, mut fd: libc::c_int) {
-    let mut fdn: *mut fdnode = *(fds).offset(fd as isize);
+pub unsafe extern "C" fn fdevent_unregister(mut ev: *mut fdevents, mut fd: libc::c_int) {
+    let mut fdn: *mut fdnode = *((*ev).fdarray).offset(fd as isize);
+    if fdn as uintptr_t & 0x3 as libc::c_int as usize != 0 as uintptr_t {
+        return;
+    }
+    let ref mut fresh1 = *((*ev).fdarray).offset(fd as isize);
+    // *fresh1 = 0 as *mut fdnode;
+    // fdnode_free(fdn);
 }
 
 unsafe extern "C" fn fdnode_free(fdn: *mut libc::c_void) {
