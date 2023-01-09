d14c0480d42996a4d6b0de2e02e19fea924173f5
David Anekstein <da@immunant.com>
2023-01-09 14:37:36 -0500

    push subset_base of fields

--- c2rust-analyze/src/borrowck/type_check.rs
+++ c2rust-analyze/src/borrowck/type_check.rs
@@ -280,6 +280,22 @@ impl<'tcx> TypeChecker<'tcx, '_> {
             let point = self.current_point(SubPoint::Mid);
             self.facts.subset_base.push((rv_origin, pl_origin, point));
         }
+
+        let pl_origin_params = pl_lty.label.origin_params;
+        let rv_origin_params = rv_lty.label.origin_params;
+        if let (Some(pl_origin_params), Some(rv_origin_params)) =
+            (pl_origin_params, rv_origin_params)
+        {
+            assert_eq!(pl_origin_params.len(), rv_origin_params.len());
+            for (pl_origin_param, rv_origin_param) in
+                pl_origin_params.iter().zip(rv_origin_params.iter())
+            {
+                let point = self.current_point(SubPoint::Mid);
+                self.facts
+                    .subset_base
+                    .push((rv_origin_param.1, pl_origin_param.1, point));
+            }
+        }
     }
 
     pub fn visit_statement(&mut self, stmt: &Statement<'tcx>) {
