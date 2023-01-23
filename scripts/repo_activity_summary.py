75d31dfe245225757193b08745fe30bbb1e40102
Khyber Sen <kkysen@gmail.com>
2023-01-22 17:00:01 -0800

    (`repo_activity_summary.py`) Separate printed sections with newlines.

--- scripts/repo_activity_summary.py
+++ scripts/repo_activity_summary.py
@@ -239,7 +239,7 @@ def main() -> None:
         time_name = get_time.__name__
 
         in_time_range = [t for t in list(T) if get_time(t) in time_range]
-        print(f"{time_name} {len(in_time_range)} {T.name()}s")
+        print(f"\n{time_name} {len(in_time_range)} {T.name()}s")
 
         for by in (by_collaborators, by_community):
             by_them = [t for t in in_time_range if by(t.author)]
