275679b8f44871f3162a310f36055af73ae07efa
Khyber Sen <kkysen@gmail.com>
2023-02-15 19:19:47 -0800

    (`c2rust-analyze/tests`) Find the `c2rust-analyze` binary to run directly, rather than going through `cargo` again.
    Skipping going through `cargo` makes the tests run much faster,
    but more importantly, prevents them from competing for the `target/` lock,
    which caused issues in CI on macOS sometimes.
    It's also a bit simpler now, not needing to go through `cargo`.

--- c2rust-analyze/tests/analyze.rs
+++ c2rust-analyze/tests/analyze.rs
@@ -4,15 +4,15 @@ pub mod common;
 
 #[test]
 fn string_literals() {
-    Analyze::new().run("tests/analyze/string_literals.rs");
+    Analyze::resolve().run("tests/analyze/string_literals.rs");
 }
 
 #[test]
 fn string_casts() {
-    Analyze::new().run("tests/analyze/string_casts.rs");
+    Analyze::resolve().run("tests/analyze/string_casts.rs");
 }
 
 #[test]
 fn lighttpd_minimal() {
-    Analyze::new().run("../analysis/tests/lighttpd-minimal/src/main.rs");
+    Analyze::resolve().run("../analysis/tests/lighttpd-minimal/src/main.rs");
 }
