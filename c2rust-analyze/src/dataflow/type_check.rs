68461db7a4e58a7a9d7cce5be35584cbe82bc594
Khyber Sen <kkysen@gmail.com>
2023-02-16 14:17:56 -0800

    (`c2rust-analyze`) Clarified that `do_unify` now requires only compatible/safetly transmutable types, not identical.

--- c2rust-analyze/src/dataflow/type_check.rs
+++ c2rust-analyze/src/dataflow/type_check.rs
@@ -202,8 +202,9 @@ impl<'tcx> TypeChecker<'tcx, '_> {
 
     /// Unify corresponding `PointerId`s in `lty1` and `lty2`.
     ///
-    /// The two inputs must have identical underlying types.  For any position where the underlying
-    /// type has a pointer, this function unifies the `PointerId`s that `lty1` and `lty2` have at
+    /// The two inputs must have compatible ([safely transmutable](are_transmutable)) underlying types.
+    /// For any position where the underlying type has a pointer,
+    /// this function unifies the `PointerId`s that `lty1` and `lty2` have at
     /// that position.  For example, given `lty1 = *mut /*l1*/ *const /*l2*/ u8` and `lty2 = *mut
     /// /*l3*/ *const /*l4*/ u8`, this function will unify `l1` with `l3` and `l2` with `l4`.
     fn do_unify(&mut self, lty1: LTy<'tcx>, lty2: LTy<'tcx>) {
