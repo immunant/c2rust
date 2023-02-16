722d54b1ff06fd6fccff10db4868ed2185ca4808
Khyber Sen <kkysen@gmail.com>
2023-02-15 23:06:19 -0800

    (`c2rust-analyze/tests`) Add tests for string literals and casts.  They're `cfg`ed off as they're not yet supported.

--- c2rust-analyze/tests/analyze.rs
+++ c2rust-analyze/tests/analyze.rs
@@ -3,6 +3,16 @@ use common::Analyze;
 pub mod common;
 
 #[test]
+fn string_literals() {
+    Analyze::new().run("tests/analyze/string_literals.rs");
+}
+
+#[test]
+fn string_casts() {
+    Analyze::new().run("tests/analyze/string_casts.rs");
+}
+
+#[test]
 fn lighttpd_minimal() {
     Analyze::new().run("../analysis/tests/lighttpd-minimal/src/main.rs");
 }
