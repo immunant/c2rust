f34a3c1bb93a0c23c99166e31a6092b22d35d0fa
Khyber Sen <kkysen@gmail.com>
2023-02-18 20:47:37 -0800

    (`c2rust-analyze/tests`) Added more `string_literals.rs` tests to narrow done at which point things crash.

--- c2rust-analyze/tests/analyze/string_literals.rs
+++ c2rust-analyze/tests/analyze/string_literals.rs
@@ -1,9 +1,20 @@
+pub fn inline_desugared_bstr() -> &'static [u8; 0] {
+    &[]
+}
+
+pub const DESUGARED_BSTR: &'static [u8; 0] = &[];
+
 #[cfg(any())]
-pub fn str() {
-    "";
+pub fn const_desugared_bstr() -> &'static [u8; 0] {
+    DESUGARED_BSTR
 }
 
 #[cfg(any())]
-pub fn bstr() {
-    b"";
+pub fn bstr() -> &'static [u8; 0] {
+    b""
+}
+
+#[cfg(any())]
+pub fn str() -> &'static str {
+    ""
 }
