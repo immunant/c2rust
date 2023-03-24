07bda9d11dd93e03a1df59e015fc717c1a5e21ef
Stuart Pernsteiner <spernsteiner@galois.com>
2023-03-24 15:33:34 -0700

    analyze: use @LINE instead of #@LINE in filecheck tests

--- c2rust-analyze/tests/filecheck/field_temp.rs
+++ c2rust-analyze/tests/filecheck/field_temp.rs
@@ -8,13 +8,13 @@ pub struct MyList {
 // CHECK-LABEL: final labeling for "list_get"
 pub unsafe fn list_get(l: *const MyList, i: usize) -> i32 {
     // The temporary `(*l).data` requires a MIR `MutToImm` rewrite.
-    // CHECK: ([[#@LINE+2]]: (*l).data): &[i32]
-    // CHECK: rewrite Ref(Deref(Identity), Not) at {{.*}}:[[#@LINE+1]]:6: [[#@LINE+1]]:15
+    // CHECK: ([[@LINE+2]]: (*l).data): &[i32]
+    // CHECK: rewrite Ref(Deref(Identity), Not) at {{.*}}:[[@LINE+1]]:6: [[@LINE+1]]:15
     *(*l).data.offset(i as isize)
 }
 
 // CHECK-LABEL: final labeling for "list_set"
 pub unsafe fn list_set(l: *const MyList, i: usize, x: i32) {
-    // CHECK: ([[#@LINE+1]]: (*l).data): &mut [i32]
+    // CHECK: ([[@LINE+1]]: (*l).data): &mut [i32]
     *(*l).data.offset(i as isize) = x;
 }
--- c2rust-analyze/tests/filecheck/insertion_sort_rewrites.rs
+++ c2rust-analyze/tests/filecheck/insertion_sort_rewrites.rs
@@ -9,20 +9,20 @@ extern crate libc;
 pub unsafe extern "C" fn insertion_sort(n: libc::c_int, p: *mut libc::c_int) {
     let mut i: libc::c_int = 1 as libc::c_int;
     while i < n {
-        // CHECK-DAG: [[#@LINE+2]]: p: &*$e
-        // CHECK-DAG: [[#@LINE+1]]: p.offset({{.*}}): &(&$0[($1 as usize) ..])[0]
+        // CHECK-DAG: [[@LINE+2]]: p: &*$e
+        // CHECK-DAG: [[@LINE+1]]: p.offset({{.*}}): &(&$0[($1 as usize) ..])[0]
         let tmp: libc::c_int = *p.offset(i as isize);
         let mut j: libc::c_int = i;
-        // CHECK-DAG: [[#@LINE+2]]: p: &*$e
-        // CHECK-DAG: [[#@LINE+1]]: p.offset({{.*}}): &(&$0[($1 as usize) ..])[0]
+        // CHECK-DAG: [[@LINE+2]]: p: &*$e
+        // CHECK-DAG: [[@LINE+1]]: p.offset({{.*}}): &(&$0[($1 as usize) ..])[0]
         while j > 0 as libc::c_int && *p.offset((j - 1 as libc::c_int) as isize) > tmp {
-            // CHECK-DAG: [[#@LINE+3]]: p: &*$e
-            // CHECK-DAG: [[#@LINE+2]]: p.offset({{.*}}): &(&$0[($1 as usize) ..])[0]
-            // CHECK-DAG: [[#@LINE+1]]: p.offset({{.*}}): &mut (&mut $0[($1 as usize) ..])[0]
+            // CHECK-DAG: [[@LINE+3]]: p: &*$e
+            // CHECK-DAG: [[@LINE+2]]: p.offset({{.*}}): &(&$0[($1 as usize) ..])[0]
+            // CHECK-DAG: [[@LINE+1]]: p.offset({{.*}}): &mut (&mut $0[($1 as usize) ..])[0]
             *p.offset(j as isize) = *p.offset((j - 1 as libc::c_int) as isize);
             j -= 1
         }
-        // CHECK-DAG: [[#@LINE+1]]: p.offset({{.*}}): &mut (&mut $0[($1 as usize) ..])[0]
+        // CHECK-DAG: [[@LINE+1]]: p.offset({{.*}}): &mut (&mut $0[($1 as usize) ..])[0]
         *p.offset(j as isize) = tmp;
         i += 1
     }
