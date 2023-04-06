96ad50ccac4bd209506cf18e08e17bf0a24a8602
Khyber Sen <kkysen@gmail.com>
2023-04-06 02:42:28 -0700

    (`c2rust-analyze/tests`) Split `string_literals` test cases in inline (local) vs. outline (global) versions.

--- c2rust-analyze/tests/analyze/string_literals.rs
+++ c2rust-analyze/tests/analyze/string_literals.rs
@@ -2,19 +2,33 @@ pub fn inline_desugared_bstr() -> &'static [u8; 0] {
     &[]
 }
 
-pub const DESUGARED_BSTR: &'static [u8; 0] = &[];
+const DESUGARED_BSTR: &'static [u8; 0] = &[];
 
 #[cfg(any())]
-pub fn const_desugared_bstr() -> &'static [u8; 0] {
+pub fn outline_desugared_bstr() -> &'static [u8; 0] {
     DESUGARED_BSTR
 }
 
 #[cfg(any())]
-pub fn bstr() -> &'static [u8; 0] {
+pub fn inline_bstr() -> &'static [u8; 0] {
     b""
 }
 
+const BSTR: &'static [u8; 0] = b"";
+
 #[cfg(any())]
-pub fn str() -> &'static str {
+pub fn outline_bstr() -> &'static [u8; 0] {
+    BSTR
+}
+
+#[cfg(any())]
+pub fn inline_str() -> &'static str {
     ""
+}
+
+const STR: &'static str = "";
+
+#[cfg(any())]
+pub fn outline_str() -> &'static str {
+    STR
 }
