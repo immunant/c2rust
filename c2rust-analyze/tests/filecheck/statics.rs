2e84d6c77d8b48c2fed7e4c0bd782fb1a30d4b5b
fw <fw@immunant.com>
2023-04-21 13:13:46 -0700

    analyze/tests/statics: add FileCheck annotations, fix warnings

--- c2rust-analyze/tests/filecheck/statics.rs
+++ c2rust-analyze/tests/filecheck/statics.rs
@@ -1,12 +1,31 @@
-static unused: usize = 2;
-static mut unused_mut: usize = 6;
-static read: usize = 9;
-static mut read_mut: usize = 21;
-static mut written_mut: usize = 3;
+#![allow(dead_code)]
+
+// CHECK: final labeling for static items:
+// CHECK-DAG: "UNUSED": addr_of = UNIQUE, type = UNIQUE#&usize[(empty)#usize[]]
+static UNUSED: usize = 2;
+// CHECK-DAG: "UNUSED_MUT": addr_of = UNIQUE, type = UNIQUE#*mut usize[(empty)#usize[]]
+static mut UNUSED_MUT: usize = 6;
+// CHECK-DAG: "READ": addr_of = READ | UNIQUE, type = READ | UNIQUE#&usize[(empty)#usize[]]
+static READ: usize = 9;
+// CHECK-DAG: "READ_MUT": addr_of = READ | UNIQUE, type = READ | UNIQUE#*mut usize[(empty)#usize[]]
+static mut READ_MUT: usize = 21;
+// CHECK-DAG: "WRITTEN_MUT": addr_of = READ | WRITE | UNIQUE, type = READ | WRITE | UNIQUE#*mut usize[(empty)#usize[]]
+static mut WRITTEN_MUT: usize = 3;
+
+// CHECK: generated 2 static rewrites:
+// CHECK-DAG: static mut UNUS ...  = 6;: static (-mut) $s
+// CHECK-DAG: static mut READ ... = 21;: static (-mut) $s
 
 fn main() {
-    let x = read;
-    if x > unsafe { read_mut } {
-         unsafe { written_mut = 6000; }
+    let x = READ;
+    if x > unsafe { READ_MUT } {
+        unsafe {
+            WRITTEN_MUT = 6000;
+        }
     }
 }
+
+// CHECK:  ===== BEGIN
+// CHECK: static UNUSED_MUT: usize = 6;
+// CHECK: static READ_MUT: usize = 21;
+// CHECK: static mut WRITTEN_MUT: usize = 3;
