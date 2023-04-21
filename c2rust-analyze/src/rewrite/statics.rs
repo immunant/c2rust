a18fc516f46abfdb73c9228aab05dfd2fbc07657
fw <fw@immunant.com>
2023-04-21 16:02:13 -0400

    analyze: rewrite never-written `static mut`s without mut

---
+++ c2rust-analyze/src/rewrite/statics.rs
@@ -0,0 +1,55 @@
+use crate::context::{PermissionSet, PointerId};
+use crate::rewrite::Rewrite;
+use crate::{GlobalAnalysisCtxt, GlobalAssignment};
+use rustc_hir::{ItemKind, Mutability, Node};
+use rustc_span::Span;
+
+/// For every static, if its write permission does not match its declared mutability, emit a rewrite
+/// changing the declaration to match observed/analyzed usage.
+pub fn gen_static_rewrites<'tcx>(
+    gacx: &GlobalAnalysisCtxt<'tcx>,
+    gasn: &GlobalAssignment,
+) -> Vec<(Span, Rewrite)> {
+    let mut hir_rewrites = Vec::new();
+    for (did, lty) in gacx.static_tys.iter() {
+        // The map of statics and their ty + permissions tracks statics by did; map this to an Item
+        // node to look at the static's spans and declared mutability.
+        let item = if let Some(Node::Item(item)) = gacx.tcx.hir().get_if_local(*did) {
+            item
+        } else {
+            panic!("def id {:?} not found", did);
+        };
+        if let ItemKind::Static(_ty, mutbl, _body_id) = item.kind {
+            let perms = if lty.label == PointerId::NONE {
+                PermissionSet::empty()
+            } else {
+                gasn.perms[lty.label]
+            };
+            let written_to = perms.contains(PermissionSet::WRITE);
+            let is_mutable = mutbl == Mutability::Mut;
+            if written_to != is_mutable {
+                let ident = gacx
+                    .tcx
+                    .opt_item_ident(*did)
+                    .expect("did not find ident when trying to generate rewrite for static item");
+                // Generate a span from beginning of ident to end of body.
+                let span = ident.span.with_hi(item.span.hi());
+                hir_rewrites.push((
+                    item.span,
+                    Rewrite::StaticMut(
+                        if written_to {
+                            Mutability::Mut
+                        } else {
+                            Mutability::Not
+                        },
+                        span,
+                    ),
+                ))
+            }
+        } else {
+            panic!("expected item {:?} to be a `static`", item);
+        }
+    }
+
+    hir_rewrites
+}
