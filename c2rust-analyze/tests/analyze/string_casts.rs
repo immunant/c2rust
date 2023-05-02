e41cec17c237135bd767bd94e01d1e2e552d9779
Khyber Sen <kkysen@gmail.com>
2023-05-02 11:52:02 -0700

    (`c2rust-analyze/tests`) Add an explicit version of the `cast_from_literal` test.

--- c2rust-analyze/tests/analyze/string_casts.rs
+++ c2rust-analyze/tests/analyze/string_casts.rs
@@ -10,6 +10,15 @@ pub fn cast_slice_ptr_to_ptr(s: *const [u8]) {
     s as *const u8;
 }
 
+/// For the below disabled (`#[cfg(any())]`ed) tests, they currently crash in the rewriter
+/// due to it not being able to handle implicitly inserted `std::ptr::addr_of!`s yet.
+/// Thus, they also have `*_explicit` versions where the `addr_of!` is made explicit.
+///
+/// Also note that `addr_of!` (with a `use std::ptr::addr_of`)
+/// and `::core::ptr::addr_of!` don't work either,
+/// though `std::ptr::addr_of`, `::std::ptr::addr_of!`,
+/// and `core::ptr::addr_of!` do work.
+
 #[cfg(any())]
 pub fn cast_array_to_slice_ptr(s: &[u8; 0]) {
     s as *const [u8];
@@ -20,15 +29,6 @@ pub fn cast_array_to_ptr(s: &[u8; 0]) {
     s as *const u8;
 }
 
-/// An explicit version of `cast_array_to_ptr` above
-/// where we use an explicit `std::ptr::addr_of!`
-/// instead of having one inserted.
-/// The rewriter can only currently handle explicit ones.
-///
-/// Also note that `addr_of!` (with a `use std::ptr::addr_of`)
-/// and `::core::ptr::addr_of!` don't work either,
-/// though `std::ptr::addr_of`, `::std::ptr::addr_of!`,
-/// and `core::ptr::addr_of!` do work.
 pub fn cast_array_to_ptr_explicit(s: &[u8; 0]) {
     std::ptr::addr_of!(*s) as *const u8;
 }
@@ -36,4 +36,8 @@ pub fn cast_array_to_ptr_explicit(s: &[u8; 0]) {
 #[cfg(any())]
 pub fn cast_from_literal() {
     b"" as *const u8 as *const core::ffi::c_char;
+}
+
+pub fn cast_from_literal_explicit() {
+    std::ptr::addr_of!(*b"") as *const u8 as *const core::ffi::c_char;
 }
