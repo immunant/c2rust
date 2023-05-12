397da0d4455311137c2507ddfd3b8a8e809fa4ca
Stuart Pernsteiner <spernsteiner@galois.com>
2023-05-12 16:03:15 -0700

    analyze: rewrite::mir_op: ignore lifetime differences in cast pointee types

--- c2rust-analyze/src/rewrite/expr/mir_op.rs
+++ c2rust-analyze/src/rewrite/expr/mir_op.rs
@@ -471,12 +471,17 @@ impl<'a, 'tcx> ExprRewriteVisitor<'a, 'tcx> {
             self.acx.tcx().erase_regions(to.pointee_ty),
         );
 
+        let orig_from = from;
+        let mut from = orig_from;
+
+        // Ignore differences in lifetimes.
+        if from.pointee_ty != to.pointee_ty {
+            from.pointee_ty = to.pointee_ty;
+        }
+
         if from == to {
             return;
         }
-
-        let orig_from = from;
-        let mut from = orig_from;
 
         if (from.qty, to.qty) == (Quantity::OffsetPtr, Quantity::Slice) {
             // TODO: emit rewrite
