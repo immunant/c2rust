b34d4e1465f3c1c0cf73811955c1e383aa25c2fd
fw <fw@immunant.com>
2023-05-11 20:09:50 -0400

    tests: builtins: test 1 and 2 argument builtins

--- tests/builtins/src/mem_x_fns.c
+++ tests/builtins/src/mem_x_fns.c
@@ -7,6 +7,10 @@ void mem_x(const char src[4], char dest[4]) {
     __builtin_memcmp(dest, src, strlen(src)+1);
     __builtin_memmove(dest, src, strlen(src)+1);
     __builtin_memset(dest, 'a', 2);
+    __builtin_strcspn(dest, "a");
+    __builtin_strchr(dest, 'a');
+    __builtin_strndup(dest, 4);
+    __builtin_strdup(dest);
 }
 
 void* assume_aligned(void* ptr) {
