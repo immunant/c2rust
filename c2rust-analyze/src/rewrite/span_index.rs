13d628cc6e38f724614e9a9ef8e346280528ecbb
Stuart Pernsteiner <spernsteiner@galois.com>
2023-05-16 11:35:39 -0700

    analyze: rewrite: add SpanIndex::lookup_exact method

--- c2rust-analyze/src/rewrite/span_index.rs
+++ c2rust-analyze/src/rewrite/span_index.rs
@@ -4,6 +4,7 @@ use std::slice;
 
 /// A mapping from [`Span`]s to values of type `T`.  Allows looking up all items whose span is a
 /// subset of some target span.
+#[derive(Clone, Debug)]
 pub struct SpanIndex<T> {
     v: Vec<(Span, T)>,
 }
@@ -24,6 +25,18 @@ impl<T> SpanIndex<T> {
         RangeIter {
             inner: self.v[start..].iter(),
             hi: data.hi,
+        }
+    }
+
+    /// Iterate over items whose spans are exactly equal to `span`.  The order of the returned
+    /// items is unspecified.
+    pub fn lookup_exact(&self, span: Span) -> ExactIter<T> {
+        let data = span.data();
+        let start = self.v.partition_point(|(span, _)| span.lo() < data.lo);
+        ExactIter {
+            inner: self.v[start..].iter(),
+            lo: data.lo,
+            span: span,
         }
     }
 }
@@ -49,6 +62,35 @@ impl<'a, T> Iterator for RangeIter<'a, T> {
                 continue;
             }
             return Some((span, value));
+        }
+    }
+}
+
+pub struct ExactIter<'a, T> {
+    inner: slice::Iter<'a, (Span, T)>,
+    lo: BytePos,
+    span: Span,
+}
+
+impl<'a, T> Iterator for ExactIter<'a, T> {
+    type Item = &'a T;
+
+    fn next(&mut self) -> Option<&'a T> {
+        loop {
+            let &(span, ref value) = self.inner.next()?;
+            let data = span.data();
+            if data.lo != self.lo {
+                // The list is sorted by `lo`, so once we move past `self.lo`, we won't see it
+                // again.
+                return None;
+            }
+            if span != self.span {
+                // This is not the requested span.  Note we compare the entire span, not just `lo`
+                // and `hi`, because spans with the same `lo` and `hi` can come from two different
+                // macro expansions (thus having different `SyntaxContext`s).
+                continue;
+            }
+            return Some(value);
         }
     }
 }
