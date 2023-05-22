31ccf69e8b1ac0ad62cd9b23aacb7288b6b4ffdb
Stuart Pernsteiner <spernsteiner@galois.com>
2023-05-22 10:47:30 -0700

    analyze: add slice cases to call_cast test

--- c2rust-analyze/tests/filecheck/call_cast.rs
+++ c2rust-analyze/tests/filecheck/call_cast.rs
@@ -4,12 +4,30 @@
 
 // CHECK-LABEL: fn use_single(x: *mut i32)
 #[c2rust_analyze_test::fixed_signature]
+#[c2rust_analyze_test::skip_rewrite]
 unsafe fn use_single(x: *mut i32) {
     *x = 1;
 }
+
+// CHECK-LABEL: fn use_slice(x: *mut i32)
+#[c2rust_analyze_test::fixed_signature]
+#[c2rust_analyze_test::skip_rewrite]
+unsafe fn use_slice(x: *mut i32) {
+    *x.offset(1) = 1;
+}
+
 
 // CHECK-LABEL: fn f(x: &mut (i32))
 unsafe fn f(x: *mut i32) {
     // CHECK: use_single(core::ptr::addr_of_mut!(*(x)))
     use_single(x);
+}
+
+// CHECK-LABEL: fn g(x: &mut [(i32)])
+unsafe fn g(x: *mut i32) {
+    // CHECK: use_single(core::ptr::addr_of_mut!(*&mut (x)[0]))
+    use_single(x);
+
+    // CHECK: use_slice(core::ptr::addr_of_mut!(*&mut (x)[0]))
+    use_slice(x);
 }
