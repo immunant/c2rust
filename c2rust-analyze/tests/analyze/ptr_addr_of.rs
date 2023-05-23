c52954eeaedf29661ea8c77dc56cbb09ddb97f45
Khyber Sen <kkysen@gmail.com>
2023-05-22 20:34:50 -0700

    (`c2rust-analyze/tests`) Add `let _ = `s to remove unused warnings for `ptr_addr_of.rs`.

--- c2rust-analyze/tests/analyze/ptr_addr_of.rs
+++ c2rust-analyze/tests/analyze/ptr_addr_of.rs
@@ -1,27 +1,27 @@
 pub fn std_ptr_addr_of(x: ()) {
-    std::ptr::addr_of!(x);
+    let _ = std::ptr::addr_of!(x);
 }
 
 pub fn abs_std_ptr_addr_of(x: ()) {
-    ::std::ptr::addr_of!(x);
+    let _ = ::std::ptr::addr_of!(x);
 }
 
 pub fn core_ptr_addr_of(x: ()) {
-    core::ptr::addr_of!(x);
+    let _ = core::ptr::addr_of!(x);
 }
 
 #[cfg(any())]
 pub fn abs_core_ptr_addr_of(x: ()) {
-    ::core::ptr::addr_of!(x);
+    let _ = ::core::ptr::addr_of!(x);
 }
 
 pub fn use_std_ptr_addr_of(x: ()) {
     use std::ptr::addr_of;
-    addr_of!(x);
+    let _ = addr_of!(x);
 }
 
 #[cfg(any())]
 pub fn use_core_ptr_addr_of(x: ()) {
     use core::ptr::addr_of;
-    addr_of!(x);
+    let _ = addr_of!(x);
 }
