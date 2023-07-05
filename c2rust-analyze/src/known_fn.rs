fdbbcf9691b1980d4bde25ae2a65b52c64d3f504
Khyber Sen <kkysen@gmail.com>
2023-07-05 15:47:45 -0700

    (`c2rust-analyze`) Add `known_fns` for declaring the permissions of known (`libc`) `fn`s while also checking these definitions match `libc`'s.

--- c2rust-analyze/src/known_fn.rs
+++ c2rust-analyze/src/known_fn.rs
@@ -42,6 +42,12 @@ impl KnownFnTy {
         assert!(self.perms.len() == num_ptrs);
         self
     }
+
+    #[cfg(test)]
+    const fn named(self, name: &'static str) -> Self {
+        let Self { name: _, ty, perms } = self;
+        Self { name, ty, perms }
+    }
 }
 
 #[cfg(test)]
@@ -61,6 +67,45 @@ macro_rules! known_fn_ty {
             perms: &[],
         }
         .checked()
+    }};
+}
+
+#[derive(Debug, PartialEq, Eq)]
+pub struct KnownFn {
+    name: &'static str,
+    inputs: &'static [KnownFnTy],
+    output: KnownFnTy,
+}
+
+#[cfg(test)]
+macro_rules! known_fns {
+    {
+        mod $module:ident {
+
+            $(
+                fn $name:ident(
+                    $($arg_name:ident: $arg_ty:ty$(: $arg_perms:tt)?,)*
+                ) -> $return_ty:ty$(: $return_perms:tt)?
+            );*;
+
+        }
+    } => {{
+        use $module::*;
+
+        const_slice!(KnownFn, [$({
+            unsafe extern "C" fn $name($($arg_name: $arg_ty,)*) -> $return_ty {
+                $(let _ = $arg_name;)*
+                todo!()
+            }
+            // ensure the definitions match
+            [$name, $module::$name];
+
+            KnownFn {
+                name: stringify!($name),
+                inputs: const_slice!(KnownFnTy, [$(known_fn_ty!($arg_ty$(: $arg_perms)?).named(stringify!($arg_name)),)*]),
+                output: known_fn_ty!($return_ty$(: $return_perms)?),
+            }
+        },)*])
     }};
 }
 
@@ -164,6 +209,154 @@ mod tests {
                     ]
                 ),
             }
+        );
+    }
+
+    #[test]
+    fn __errno_location() {
+        assert_eq!(
+            known_fns! {
+                mod libc {
+
+                    fn __errno_location() -> *mut c_int: [READ | WRITE];
+
+                }
+            },
+            const_slice!(
+                KnownFn,
+                [KnownFn {
+                    name: "__errno_location",
+                    inputs: &[],
+                    output: KnownFnTy {
+                        name: "",
+                        ty: "*mut c_int",
+                        perms: const_slice!(
+                            PermissionSet,
+                            [PermissionSet::union_all([
+                                PermissionSet::READ,
+                                PermissionSet::WRITE
+                            ])]
+                        )
+                    }
+                }]
+            )
+        );
+    }
+
+    #[test]
+    fn read() {
+        assert_eq!(
+            known_fns! {
+                mod libc {
+
+                    fn read(
+                        fd: c_int,
+                        buf: *mut c_void: [WRITE | OFFSET_ADD],
+                        count: size_t,
+                    ) -> ssize_t;
+
+                }
+            },
+            const_slice!(
+                KnownFn,
+                [KnownFn {
+                    name: "read",
+                    inputs: const_slice!(
+                        KnownFnTy,
+                        [
+                            KnownFnTy {
+                                name: "fd",
+                                ty: "c_int",
+                                perms: &[],
+                            },
+                            KnownFnTy {
+                                name: "buf",
+                                ty: "*mut c_void",
+                                perms: const_slice!(
+                                    PermissionSet,
+                                    [PermissionSet::union_all([
+                                        PermissionSet::WRITE,
+                                        PermissionSet::OFFSET_ADD
+                                    ])]
+                                ),
+                            },
+                            KnownFnTy {
+                                name: "count",
+                                ty: "size_t",
+                                perms: &[],
+                            },
+                        ]
+                    ),
+                    output: KnownFnTy {
+                        name: "",
+                        ty: "ssize_t",
+                        perms: &[],
+                    }
+                }]
+            )
+        );
+    }
+
+    #[test]
+    fn strtol() {
+        assert_eq!(
+            known_fns! {
+                mod libc {
+
+                    fn strtol(
+                        s: *const c_char: [READ | OFFSET_ADD],
+                        endp: *mut *mut c_char: [WRITE, WRITE | OFFSET_ADD],
+                        base: c_int,
+                    ) -> c_long;
+
+                }
+            },
+            const_slice!(
+                KnownFn,
+                [KnownFn {
+                    name: "strtol",
+                    inputs: const_slice!(
+                        KnownFnTy,
+                        [
+                            KnownFnTy {
+                                name: "s",
+                                ty: "*const c_char",
+                                perms: const_slice!(
+                                    PermissionSet,
+                                    [PermissionSet::union_all([
+                                        PermissionSet::READ,
+                                        PermissionSet::OFFSET_ADD
+                                    ])]
+                                ),
+                            },
+                            KnownFnTy {
+                                name: "endp",
+                                ty: "*mut *mut c_char",
+                                perms: const_slice!(
+                                    PermissionSet,
+                                    [
+                                        PermissionSet::union_all([PermissionSet::WRITE,]),
+                                        PermissionSet::union_all([
+                                            PermissionSet::WRITE,
+                                            PermissionSet::OFFSET_ADD
+                                        ])
+                                    ]
+                                ),
+                            },
+                            KnownFnTy {
+                                name: "base",
+                                ty: "c_int",
+                                perms: &[],
+                            },
+                        ]
+                    ),
+                    output: KnownFnTy {
+                        name: "",
+                        ty: "c_long",
+                        perms: &[],
+                    }
+                }]
+            )
         );
     }
 }
