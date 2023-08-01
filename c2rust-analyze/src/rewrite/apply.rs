3fcec9cc91ed32118ee8e61f25df3aab0702a926
David Anekstein <da@immunant.com>
2023-08-01 18:39:45 -0400

    replace allow_unused with leading underscore for TyGenericParams

--- c2rust-analyze/src/rewrite/apply.rs
+++ c2rust-analyze/src/rewrite/apply.rs
@@ -323,7 +323,7 @@ impl<S: Sink> Emitter<'_, S> {
             Rewrite::LitZero => self.emit_str("0"),
 
             Rewrite::Print(ref s) => self.emit_str(s),
-            Rewrite::TyGenericParams(ref rws) => {
+            Rewrite::_TyGenericParams(ref rws) => {
                 self.emit_str("<")?;
                 for (index, rw) in rws.iter().enumerate() {
                     self.emit(rw, 0)?;
