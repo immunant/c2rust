209f4214c2781505f8ff1b9bf2a41176be49f43e
hkalbasi <hamidrezakalbasi@protonmail.com>
2023-12-01 16:05:10 +0330

    Fix nix build

--- default.nix
+++ default.nix
@@ -1,5 +1,5 @@
 let
-  nixpkgs = import <nixpkgs> {};
+  nixpkgs = import <nixpkgs> { };
   inherit (nixpkgs) pkgs llvmPackages;
   stdenv = pkgs.clangStdenv;
 in
@@ -9,11 +9,14 @@ stdenv.mkDerivation {
     pkgs.clang
     pkgs.cmake
     pkgs.llvm
+    pkgs.libllvm
     pkgs.openssl
     pkgs.pkgconfig
     pkgs.python3
     pkgs.rustup
     pkgs.zlib
   ];
-  LIBCLANG_PATH="${llvmPackages.libclang}/lib";
+  LIBCLANG_PATH = "${pkgs.libclang.lib}/lib";
+  CMAKE_LLVM_DIR = "${llvmPackages.libllvm.dev}/lib/cmake/llvm";
+  CMAKE_CLANG_DIR = "${llvmPackages.libclang.dev}/lib/cmake/clang";
 }
