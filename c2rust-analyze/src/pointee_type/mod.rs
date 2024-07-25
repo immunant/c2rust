386f7da1984a0e95eb6dbf30e5bb1fdd8577e2ff
Stuart Pernsteiner <spernsteiner@galois.com>
2024-07-25 15:42:18 -0700

    analyze: pointee_type: factor out VarTable from ConstraintSet

--- c2rust-analyze/src/pointee_type/mod.rs
+++ c2rust-analyze/src/pointee_type/mod.rs
@@ -7,14 +7,15 @@ mod constraint_set;
 mod solve;
 mod type_check;
 
-pub use self::constraint_set::{CTy, Constraint, ConstraintSet};
+pub use self::constraint_set::{CTy, Constraint, ConstraintSet, VarTable};
 pub use self::solve::{solve_constraints, PointeeTypes};
 
 pub fn generate_constraints<'tcx>(
     acx: &AnalysisCtxt<'_, 'tcx>,
     mir: &Body<'tcx>,
+    vars: &mut VarTable<'tcx>,
 ) -> ConstraintSet<'tcx> {
-    type_check::visit(acx, mir)
+    type_check::visit(acx, mir, vars)
 }
 
 pub fn remap_pointers_global<'tcx>(
