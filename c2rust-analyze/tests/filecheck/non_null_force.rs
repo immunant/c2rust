b2e3f94198ea5d3dbcd79bfa3667bf3f4df53e6a
Stuart Pernsteiner <spernsteiner@galois.com>
2024-08-21 16:48:51 -0700

    [wip] require WRITE for `&mut *p` (RvalueDesc::Project with no projections)

--- c2rust-analyze/tests/filecheck/non_null_force.rs
+++ c2rust-analyze/tests/filecheck/non_null_force.rs
@@ -6,26 +6,26 @@ use std::ptr;
 // CHECK-LABEL: final labeling for "f"
 fn f(cond: bool) {
     let x = 1_i32;
-    // CHECK: ([[@LINE+1]]: mut y): {{.*}}, type = UNIQUE | STACK#
+    // CHECK: ([[@LINE+1]]: mut y): {{.*}}, type = READ | UNIQUE | STACK#
     let mut y = ptr::addr_of!(x);
     if cond {
         y = 0 as *const _;
     }
     // The expression `y` is considered nullable even though it's passed for argument `p` of `g`,
     // which is forced to be `NON_NULL`.
-    // CHECK: ([[@LINE+1]]: y): {{.*}}, type = UNIQUE | STACK#
+    // CHECK: ([[@LINE+1]]: y): {{.*}}, type = READ | UNIQUE | STACK#
     g(cond, y);
 }
 
 // CHECK-LABEL: final labeling for "g"
 // `p` should be non-null, as it's forced to be by the attribute.  This emulates the "unsound" PDG
 // case, where a variable is forced to stay `NON_NULL` even though a null possibly flows into it.
-// CHECK: ([[@LINE+2]]: p): {{.*}}, type = UNIQUE | NON_NULL | STACK#
+// CHECK: ([[@LINE+2]]: p): {{.*}}, type = READ | UNIQUE | NON_NULL | STACK#
 #[c2rust_analyze_test::force_non_null_args]
 fn g(cond: bool, p: *const i32) {
     // `q` is not forced to be `NON_NULL`, so it should be inferred nullable due to the null
     // assignment below.
-    // CHECK: ([[@LINE+1]]: mut q): {{.*}}, type = UNIQUE | STACK#
+    // CHECK: ([[@LINE+1]]: mut q): {{.*}}, type = READ | UNIQUE | STACK#
     let mut q = p;
     if cond {
         q = 0 as *const _;
