b6cbea1d9bd03afbbb7a13d172ae255ce68ae2d9
Gordon P. Hemsley <me@gphemsley.org>
2024-10-12 12:54:22 -0400

    WIP: operators tests

---
+++ tests/operators/src/test_binary.rs
@@ -0,0 +1,129 @@
+use crate::binary::*;
+use libc::*;
+
+#[link(name = "test")]
+extern "C" {
+}
+
+pub fn test_multiply() {
+
+}
+
+pub fn test_divide() {
+
+}
+
+pub fn test_modulus() {
+
+}
+
+pub fn test_add() {
+
+}
+
+pub fn test_subtract() {
+
+}
+
+pub fn test_shiftleft() {
+
+}
+
+pub fn test_shiftright() {
+
+}
+
+pub fn test_less() {
+
+}
+
+pub fn test_greater() {
+
+}
+
+pub fn test_lessequal() {
+
+}
+
+pub fn test_greaterequal() {
+
+}
+
+pub fn test_equalequal() {
+
+}
+
+pub fn test_notequal() {
+
+}
+
+pub fn test_bitand() {
+
+}
+
+pub fn test_bitxor() {
+
+}
+
+pub fn test_bitor() {
+
+}
+
+pub fn test_and() {
+
+}
+
+pub fn test_or() {
+
+}
+
+
+pub fn test_assignadd() {
+
+}
+
+pub fn test_assignsubtract() {
+
+}
+
+pub fn test_assignmultiply() {
+
+}
+
+pub fn test_assigndivide() {
+
+}
+
+pub fn test_assignmodulus() {
+
+}
+
+pub fn test_assignbitxor() {
+
+}
+
+pub fn test_assignshiftleft() {
+
+}
+
+pub fn test_assignshiftright() {
+
+}
+
+pub fn test_assignbitor() {
+
+}
+
+pub fn test_assignbitand() {
+
+}
+
+
+pub fn test_assign() {
+
+}
+
+pub fn test_comma() {
+
+}
+
