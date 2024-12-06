42aa3f743b1b8868e94dd2c0ec694a61f0f6fc9c
Yeaseen <yeaseen.arafat96@gmail.com>
2024-12-06 02:26:05 -0700

    address reviewer suggestions

--- tests/enums/src/enum_compound.c
+++ tests/enums/src/enum_compound.c
@@ -1,12 +1,13 @@
 
 typedef enum {
-    b=2
+    B = 2
 } a;
 
-void entry6(const unsigned buffer_size, int buffer[]){
-  if (buffer_size < 1) { return; }
+void entry6(const unsigned buffer_size, int buffer[]) {
+  if (buffer_size < 1) {
+    return; 
+  }
   
   // Using a compound literal with enum to assign the value
-  buffer[0] = (a){b};
-
+  buffer[0] = (a) { B };
 }
