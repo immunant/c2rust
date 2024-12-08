25cf53ef5061857cdb27779cbf50494d4746651d
Yeaseen <yeaseen.arafat96@gmail.com>
2024-12-08 16:00:47 -0700

    fix: allow compound literals with address-of operator

---
+++ tests/structs/src/test_struct_with_exp.rs
@@ -0,0 +1,23 @@
+use crate::struct_with_exp::rust_struct_with_exp;
+use libc::{c_int, c_uint, size_t};
+
+#[link(name = "test")]
+extern "C" {
+    fn struct_with_exp(_: c_uint, _: *mut c_int);
+}
+
+const BUFFER_SIZE: usize = 1;
+
+pub fn test_struct_with_exp() {
+  let mut buffer = [0; BUFFER_SIZE];
+  let mut rust_buffer = [0; BUFFER_SIZE];
+  let expected_buffer = [42];
+
+  unsafe {
+      struct_with_exp(BUFFER_SIZE as u32, buffer.as_mut_ptr());
+      rust_struct_with_exp(BUFFER_SIZE as u32, rust_buffer.as_mut_ptr());
+  }
+
+  assert_eq!(buffer, rust_buffer);
+  assert_eq!(buffer, expected_buffer);
+}
