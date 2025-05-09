4c3a0a756aaf7c64cd545165706ae965a1a76b2e
Josh Triplett <josh@joshtriplett.org>
2025-05-09 09:52:46 +0100

    WIP: Try LLVM 19 on macOS

--- scripts/provision_mac.sh
+++ scripts/provision_mac.sh
@@ -17,10 +17,8 @@ done
 SCRIPT_DIR="$(dirname "$0")"
 export HOMEBREW_NO_AUTO_UPDATE=1
 
-# NOTE: Pin LLVM to a known good version since new releases
-# tend not to be backwards compatible
 # `bash` needed b/c macOS ships with bash 3, which doesn't support arrays properly
-brew install -q python cmake ninja gpg llvm@17 bash
+brew install -q python cmake ninja gpg llvm@19 bash
 
 # Python 3 packages
 python3 -m pip install --break-system-packages --user --upgrade pip
