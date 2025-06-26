5dac3b51c0e620da877f85964193cae171eed21d
Andrei Homescu <ah@immunant.com>
2025-06-25 21:33:58 -0700

    c2rust-refactor: Import all internal rustc crates as extern crates

--- c2rust-refactor/src/lib.rs
+++ c2rust-refactor/src/lib.rs
@@ -9,6 +9,28 @@
 )]
 #![cfg_attr(feature = "profile", feature(proc_macro_hygiene))]
 
+extern crate rustc_arena;
+extern crate rustc_ast;
+extern crate rustc_ast_pretty;
+extern crate rustc_const_eval;
+extern crate rustc_data_structures;
+extern crate rustc_driver;
+extern crate rustc_errors;
+extern crate rustc_hash;
+extern crate rustc_hir;
+extern crate rustc_incremental;
+extern crate rustc_index;
+extern crate rustc_interface;
+extern crate rustc_metadata;
+extern crate rustc_middle;
+extern crate rustc_parse;
+extern crate rustc_privacy;
+extern crate rustc_session;
+extern crate rustc_span;
+extern crate rustc_target;
+extern crate rustc_typeck;
+extern crate rustc_type_ir;
+
 mod macros;
 
 pub mod ast_manip;
