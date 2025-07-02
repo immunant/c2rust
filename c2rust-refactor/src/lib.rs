f98520aab20c1595240a8548eb5e105e62e77e2a
Andrei Homescu <ah@immunant.com>
2025-07-01 21:07:46 -0700

    c2rust-refactor: Import rustc_lint::LintStore

--- c2rust-refactor/src/lib.rs
+++ c2rust-refactor/src/lib.rs
@@ -22,6 +22,7 @@ extern crate rustc_hir;
 extern crate rustc_incremental;
 extern crate rustc_index;
 extern crate rustc_interface;
+extern crate rustc_lint;
 extern crate rustc_metadata;
 extern crate rustc_middle;
 extern crate rustc_parse;
