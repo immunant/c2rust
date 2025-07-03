05f6d4c6565cef20394b763ea8c033b43f60cc0f
Andrei Homescu <ah@immunant.com>
2025-07-02 22:58:02 -0700

    c2rust-refactor: Add rustc_lexer crate

--- c2rust-refactor/src/lib.rs
+++ c2rust-refactor/src/lib.rs
@@ -22,6 +22,7 @@ extern crate rustc_hir;
 extern crate rustc_incremental;
 extern crate rustc_index;
 extern crate rustc_interface;
+extern crate rustc_lexer;
 extern crate rustc_lint;
 extern crate rustc_metadata;
 extern crate rustc_middle;
