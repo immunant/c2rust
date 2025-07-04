94d184129702e3175450f3eff4a08f59bbd87bfe
Andrei Homescu <ah@immunant.com>
2025-07-03 17:07:19 -0700

    c2rust-refactor: Update SelfTy

--- c2rust-refactor/src/context.rs
+++ c2rust-refactor/src/context.rs
@@ -213,7 +213,7 @@ impl<'a, 'tcx> RefactorCtxt<'a, 'tcx> {
             }
             Res::Local(id) => Some(*id),
 
-            Res::PrimTy(_) | Res::SelfTy(..) | Res::ToolMod | Res::NonMacroAttr(_) | Res::Err => {
+            Res::PrimTy(_) | Res::SelfTy { .. } | Res::ToolMod | Res::NonMacroAttr(_) | Res::Err => {
                 None
             }
         }
