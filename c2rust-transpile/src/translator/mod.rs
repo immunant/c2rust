5b0f63f08f008b671a14d57fbf314238699ce863
fw <fw@immunant.com>
2025-07-05 03:05:48 -0400

    tweak clippy allow syntax
    'reason' isn't supported in our nightly but putting the reason on the same line as the allow makes the intent more obvious

--- c2rust-transpile/src/translator/mod.rs
+++ c2rust-transpile/src/translator/mod.rs
@@ -3182,8 +3182,7 @@ impl<'c> Translation<'c> {
         Ok(WithStmts::new_val(call))
     }
 
-    // Fixing this would require major refactors for marginal benefit.
-    #[allow(clippy::vec_box)]
+    #[allow(clippy::vec_box/*, reason = "not worth a substantial refactor"*/)]
     fn convert_exprs(
         &self,
         ctx: ExprContext,
