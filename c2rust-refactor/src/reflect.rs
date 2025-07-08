f2d9f1bf81d5f0ac81b3222ad314c3c6587507e7
Andrei Homescu <ah@immunant.com>
2025-07-07 18:18:23 -0700

    c2rust-refactor: Update TyKind::Error

--- c2rust-refactor/src/reflect.rs
+++ c2rust-refactor/src/reflect.rs
@@ -118,7 +118,7 @@ impl<'a, 'tcx> Reflector<'a, 'tcx> {
             // No idea what `Placeholder` is, but it sounds like something rustc-internal.
             TyKind::Placeholder(..) => mk().infer_ty(),
             TyKind::Infer(_) => mk().infer_ty(),
-            TyKind::Error => mk().infer_ty(), // unsupported
+            TyKind::Error(_) => mk().infer_ty(), // unsupported
         }
     }
 
