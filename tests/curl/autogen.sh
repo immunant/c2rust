13cc2d92a207379289b734ea3478ab1bd8db8994
fw <fw@immunant.com>
2025-07-09 15:19:22 -0400

    fix curl ldap compilation error

--- tests/curl/autogen.sh
+++ tests/curl/autogen.sh
@@ -2,4 +2,8 @@
 set -e; set -o pipefail
 
 SCRIPT_DIR="$(cd "$(dirname "$0" )" && pwd)"
-(cd "$SCRIPT_DIR/repo" && ./buildconf) 2>&1 > "$(basename "$0")".log
+(cd "$SCRIPT_DIR/repo" &&
+# ldap now exports `ldap_connect` with a different type. to avoid errors, rename
+# curl's definition.
+sed -i -E -e 's/\bldap_connect\b/curl_ldap_connect/g' 'lib/openldap.c' &&
+./buildconf) 2>&1 > "$(basename "$0")".log
