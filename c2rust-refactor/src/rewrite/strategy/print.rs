d3b693045f1fb6ba5490e4fd67028991f36b3282
Andrei Homescu <ah@immunant.com>
2025-07-10 20:40:03 -0700

    c2rust-refactor: Import LazyTokenStream and Spacing into some modules

--- c2rust-refactor/src/rewrite/strategy/print.rs
+++ c2rust-refactor/src/rewrite/strategy/print.rs
@@ -27,7 +27,7 @@ use rustc_ast::ptr::P;
 use rustc_ast_pretty::pprust::{self, PrintState};
 use rustc_span::source_map::{BytePos, FileName, SourceFile, Span, Spanned};
 use rustc_span::symbol::{Ident, Symbol};
-use rustc_ast::tokenstream::{DelimSpan, TokenStream, TokenTree};
+use rustc_ast::tokenstream::{DelimSpan, LazyTokenStream, Spacing, TokenStream, TokenTree};
 use rustc_ast::util::parser;
 use rustc_data_structures::thin_vec::ThinVec;
 use rustc_span::DUMMY_SP;
