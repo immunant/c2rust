4d4b6d3b64fa31262be33798f9cd71a04a48d2c4
Andrei Homescu <ah@immunant.com>
2025-07-14 22:11:01 -0700

    c2rust-refactor: Fix expect macro invocations that contain braces

--- c2rust-refactor/src/macros.rs
+++ c2rust-refactor/src/macros.rs
@@ -12,12 +12,11 @@ macro_rules! match_or {
 macro_rules! expect {
     ([$e:expr] $arm_pat:pat => $arm_body:expr) => {
         $crate::match_or!([$e] $arm_pat => $arm_body;
-            panic!(concat!("expected ", stringify!($arm_pat))))
+            panic!("expected {}", stringify!($arm_pat)))
     };
     ([$e:expr] $($arm_pat:pat => $arm_body:expr),*) => {
         $crate::match_or!([$e] $($arm_pat => $arm_body),*;
-            panic!(concat!("expected one of: ",
-                           stringify!($($arm_pat),*))))
+            panic!("expected one of: {}", stringify!($($arm_pat),*)))
     };
 }
 
