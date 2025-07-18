c09391022e227fe3f1c757de3467e83e0b19d7d3
Andrei Homescu <ah@immunant.com>
2025-07-17 19:21:56 -0700

    c2rust-refactor: Added some missing borrows

--- c2rust-refactor/src/ast_manip/util.rs
+++ c2rust-refactor/src/ast_manip/util.rs
@@ -251,8 +251,8 @@ pub fn join_visibility(vis1: &VisibilityKind, vis2: &VisibilityKind) -> Visibili
     use rustc_ast::VisibilityKind::*;
     match (vis1, vis2) {
         (Public, _) | (_, Public) => Public,
-        (Restricted { path, .. }, _) if path.eq(kw::Crate) => vis1.clone(),
-        (_, Restricted { path, .. }) if path.eq(kw::Crate) => vis2.clone(),
+        (Restricted { path, .. }, _) if path.eq(&kw::Crate) => vis1.clone(),
+        (_, Restricted { path, .. }) if path.eq(&kw::Crate) => vis2.clone(),
         (Restricted { path: path1, .. }, Restricted { path: path2, .. }) => {
             if path1.ast_equiv(&path2) {
                 vis1.clone()
