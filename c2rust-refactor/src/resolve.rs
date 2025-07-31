f759627ec9147c4ef7ad0e40670fa195007a59c2
Andrei Homescu <ah@immunant.com>
2025-07-30 20:46:33 -0700

    c2rust-refactor: Convert some LocalDefId values to DefIs where expected

--- c2rust-refactor/src/resolve.rs
+++ c2rust-refactor/src/resolve.rs
@@ -11,7 +11,7 @@ fn push_hir_mod_children(tcx: TyCtxt, m: &Mod, children: &mut Vec<(Symbol, Res)>
     for &iid in &m.item_ids {
         let node = tcx.hir().get(iid.id);
         let item = expect!([node] Node::Item(i) => i);
-        let item_did = tcx.hir().local_def_id(item.hir_id());
+        let item_did = tcx.hir().local_def_id(item.hir_id()).to_def_id();
 
         match item.kind {
             ForeignMod { ref items, .. } => {
@@ -46,7 +46,7 @@ fn push_hir_mod_children(tcx: TyCtxt, m: &Mod, children: &mut Vec<(Symbol, Res)>
 
 fn push_hir_foreign_mod_children(tcx: TyCtxt, items: &[ForeignItemRef], children: &mut Vec<(Symbol, Res)>) {
     for fi in &items {
-        let did = tcx.hir().local_def_id(fi.id.hir_id());
+        let did = tcx.hir().local_def_id(fi.id.hir_id()).to_def_id();
         if let Some(def) = tcx.def_kind(did) {
             children.push((fi.ident.name, Res::Def(def, did)));
         }
