d0027046b9c2a73c105a132c477e87c0c2a55c9c
Khyber Sen <kkysen@gmail.com>
2025-07-31 04:23:31 -0700

    transpile: add `macros.c` snapshot test copied from `tests/macros/src/define.c`
    A few adjustments are made so that it should transpile the same on multiple platforms
    and doesn't test `f128`, which requires the `f128` crate,
    which the snapshot tests aren't set up to test yet.

---
+++ c2rust-transpile/tests/snapshots/macros.c
@@ -0,0 +1,85 @@
+#include <stddef.h>
+
+typedef unsigned long long U64;
+
+#define TEST_FN_MACRO(x) ((x) * (x))
+
+int test_fn_macro(int x) { return TEST_FN_MACRO(x); }
+
+#define TEST_CONST1 1
+#define TEST_NESTED 2
+#define TEST_CONST2 TEST_NESTED
+#define TEST_PARENS (TEST_CONST2 + 1) * 3
+
+int reference_define() {
+  int x = TEST_CONST1;
+  x += TEST_CONST2;
+  if (3 < TEST_PARENS)
+    x += TEST_PARENS;
+  return x;
+}
+
+// Exercise an edge case where a struct initializer needs to be in an unsafe
+// block
+struct fn_ptrs {
+  void *v;
+  int (*fn1)(void);
+  int (*fn2)(int);
+};
+
+typedef int (*fn_ptr_ty)(char);
+
+const struct fn_ptrs fns = {NULL, NULL, NULL};
+
+// Make sure we can't refer to globals in a const macro
+#define GLOBAL_REF &fns
+const struct fn_ptrs *p = GLOBAL_REF;
+
+// `size_t` not used since it translates differently on macOS,
+// and we don't care about testing that here.
+// `long` should suffice for testing as well.
+// typedef size_t zstd_platform_dependent_type;
+typedef long zstd_platform_dependent_type;
+
+#define ZSTD_STATIC_ASSERT(c) (void)sizeof(char[(c) ? 1 : -1])
+#define ZSTD_WINDOWLOG_MAX_32 30
+#define ZSTD_WINDOWLOG_MAX_64 31
+#define ZSTD_WINDOWLOG_MAX                                                     \
+  ((int)(sizeof(zstd_platform_dependent_type) == 4 ? ZSTD_WINDOWLOG_MAX_32     \
+                                                   : ZSTD_WINDOWLOG_MAX_64))
+U64 test_zstd() {
+  // This static assert was causing us trouble by somehow giving a valid
+  // expression for ZSTD_WINDOWLOG_MAX which shouldn't be possible to translate
+  // to a const.
+  ZSTD_STATIC_ASSERT(ZSTD_WINDOWLOG_MAX <= 31);
+  return ZSTD_WINDOWLOG_MAX;
+}
+
+#define inc(ptr)                                                               \
+  ({                                                                           \
+    (*ptr)++;                                                                  \
+    *ptr;                                                                      \
+  })
+
+// Ensure the macro generated stmt expr block is codegen'd
+int stmt_expr_inc(void) {
+  int a = 0;
+  int *b = &a;
+
+  // unused
+  inc(b);
+
+  // used
+  return inc(b);
+}
+
+int test_switch(int x) {
+  switch (x) {
+  case TEST_CONST1:
+    return 10;
+  case TEST_NESTED:
+    return 20;
+  }
+
+  return 0;
+}
