39eba456a12f6223f48186ffe967522628bb0c40
Khyber Sen <kkysen@gmail.com>
2025-08-02 01:33:49 -0700

    transpile: unconditionally make `const`s use `unsafe` blocks for `--translate-const-macros conservative`
    Some operations, such as ptr arithmetic, are `unsafe` and can be done in const macros.
    So as an initially overly conservative implementation,
    just make all `const`s use `unsafe` blocks in case `unsafe` operations are used.
    This is what we already do for `fn`s, for example,
    even if all of the operations in a `fn` are safe.

--- c2rust-transpile/src/translator/mod.rs
+++ c2rust-transpile/src/translator/mod.rs
@@ -2187,7 +2187,7 @@ impl<'c> Translation<'c> {
         ctx: ExprContext,
         expansions: &[CExprId],
     ) -> TranslationResult<(Box<Expr>, CTypeId)> {
-        let (val, ty) = expansions
+        let (mut val, ty) = expansions
             .iter()
             .try_fold::<Option<(WithStmts<Box<Expr>>, CTypeId)>, _, _>(None, |canonical, &id| {
                 self.can_convert_const_macro_expansion(id)?;
@@ -2224,6 +2224,7 @@ impl<'c> Translation<'c> {
             })?
             .ok_or_else(|| format_err!("Could not find a valid type for macro"))?;
 
+        val.set_unsafe();
         val.to_unsafe_pure_expr()
             .map(|val| (val, ty))
             .ok_or_else(|| TranslationError::generic("Macro expansion is not a pure expression"))
