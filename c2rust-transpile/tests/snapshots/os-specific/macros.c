568620a89f132e20ef007ab8b89533644427da96
Khyber Sen <kkysen@gmail.com>
2025-08-20 22:53:45 -0700

    transpile: add `sizeof` tests to `macros.c` snapshot (os-specific)

--- c2rust-transpile/tests/snapshots/os-specific/macros.c
+++ c2rust-transpile/tests/snapshots/os-specific/macros.c
@@ -2,3 +2,18 @@
 #include <stdbool.h>
 
 bool errno_is_error() { return errno != 0; }
+
+int size_of_const() {
+  int a[10];
+#define SIZE sizeof(a)
+  return SIZE;
+}
+
+#if 0
+// TODO VLA error
+int size_of_dynamic(int n) {
+  int a[n];
+#define SIZE sizeof(a)
+  return SIZE;
+}
+#endif
