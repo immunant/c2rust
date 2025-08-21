195e8cdb20db232449d5ba9406ff947ec734b72f
Khyber Sen <kkysen@gmail.com>
2025-08-20 22:56:59 -0700

    transpile: add `memcpy` of str literal macro test to `macros.c` snapshot (os-specific)

--- c2rust-transpile/tests/snapshots/os-specific/macros.c
+++ c2rust-transpile/tests/snapshots/os-specific/macros.c
@@ -1,5 +1,6 @@
 #include <errno.h>
 #include <stdbool.h>
+#include <string.h>
 
 bool errno_is_error() { return errno != 0; }
 
@@ -17,3 +18,13 @@ int size_of_dynamic(int n) {
   return SIZE;
 }
 #endif
+
+// From Lua's `lobject.c`.
+
+#define POS "\"]"
+/* number of chars of a literal string without the ending \0 */
+#define LL(x) (sizeof(x) / sizeof(char) - 1)
+
+void memcpy_str_literal(char *out) {
+  memcpy(out, POS, (LL(POS) + 1) * sizeof(char));
+}
