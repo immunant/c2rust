0d2476ccb3aa0aa340b66c3670794929aec61950
Andrei Homescu <ah@immunant.com>
2025-08-29 21:20:09 -0700

    c2rust-refactor: Outline modules by default

--- c2rust-refactor/src/ast_builder/builder.rs
+++ c2rust-refactor/src/ast_builder/builder.rs
@@ -352,6 +352,7 @@ pub struct Builder {
     unsafety: Unsafe,
     constness: Const,
     ext: Extern,
+    inline: Inline,
     attrs: AttrVec,
     span: Span,
     id: NodeId,
@@ -371,6 +372,7 @@ impl Builder {
             unsafety: Unsafe::No,
             constness: Const::No,
             ext: Extern::None,
+            inline: Inline::No,
             attrs: AttrVec::new(),
             span: DUMMY_SP,
             id: DUMMY_NODE_ID,
@@ -386,6 +388,13 @@ impl Builder {
 
     pub fn pub_(self) -> Self {
         self.vis("pub")
+    }
+
+    pub fn inline(self) -> Self {
+        Builder {
+            inline: Inline::Yes,
+            ..self
+        }
     }
 
     pub fn set_mutbl<M: Make<Mutability>>(self, mutbl: M) -> Self {
@@ -1776,7 +1785,7 @@ impl Builder {
             inner_span: self.span,
             inject_use_span: DUMMY_SP,
         };
-        ModKind::Loaded(items, Inline::Yes, spans)
+        ModKind::Loaded(items, self.inline, spans)
     }
 
     pub fn mac_item<M>(self, mac: M) -> P<Item>
