210e58d24f073b432e672277961c637a3ab04006
bungcip <bungcip@gmail.com>
2025-08-31 19:37:04 +0700

    add snapshot test for unary expression

---
+++ c2rust-transpile/tests/snapshots/exprs.c
@@ -0,0 +1,35 @@
+int puts(const char *str);
+
+static int side_effect(){
+    puts("the return of side effect");
+    return 10;
+}
+
+void unary_without_side_effect(){
+    int i = 5;
+    -i;
+    +i;
+    ~i;
+    !i;
+    &i;
+    *&i;
+    i++;
+    i--;
+    --i;
+    ++i;
+}
+
+void unary_with_side_effect(){
+    char* arr[1] = {0};
+
+    -side_effect();
+    +side_effect();
+    ~side_effect();
+    !side_effect();
+    &""[side_effect()];
+    *arr[side_effect()];
+    ++arr[side_effect()];
+    --arr[side_effect()];
+    arr[side_effect()]++;
+    arr[side_effect()]--;
+}
