5cc2167b0fe710e3ae6b7a6b72c10741ff3cd7f1
Khyber Sen <kkysen@gmail.com>
2025-10-17 12:53:41 +0200

    exporter: tag all SVE types w/ `TagSve` (unhandled)
    Fixes #1252.
    
    There are a lot of SVE types and we were only handling a couple of them.
    Inside clang, this is done by `#include "clang/include/clang/Basic/AArch64ACLETypes.def"`
    and defining a macro beforehand, but we can't do that in an extern clang tool,
    so I just copied all of the variants from that file.
    Alternatively, we could vendor that file.

--- c2rust-ast-exporter/src/AstExporter.cpp
+++ c2rust-ast-exporter/src/AstExporter.cpp
@@ -422,11 +422,69 @@ class TypeEncoder final : public TypeVisitor<TypeEncoder> {
             case BuiltinType::WChar_S: return TagSWChar;
             case BuiltinType::WChar_U: return TagUWChar;
 #if CLANG_VERSION_MAJOR >= 17
-            case BuiltinType::SveCount: return TagSveCount;
-            case BuiltinType::SveBool: return TagSveBool;
-            case BuiltinType::SveBoolx2: return TagSveBoolx2;
-            case BuiltinType::SveBoolx4: return TagSveBoolx4;
             case BuiltinType::Float128: return TagFloat128;
+
+            // From `clang/include/clang/Basic/AArch64ACLETypes.def`,
+            // but we can't `#include` it in an external clang tool.
+            case BuiltinType::SveInt8:
+            case BuiltinType::SveInt16:
+            case BuiltinType::SveInt32:
+            case BuiltinType::SveInt64:
+            case BuiltinType::SveUint8:
+            case BuiltinType::SveUint16:
+            case BuiltinType::SveUint32:
+            case BuiltinType::SveUint64:
+            case BuiltinType::SveFloat16:
+            case BuiltinType::SveFloat32:
+            case BuiltinType::SveFloat64:
+            case BuiltinType::SveBFloat16:
+            case BuiltinType::SveMFloat8:
+            case BuiltinType::SveInt8x2:
+            case BuiltinType::SveInt16x2:
+            case BuiltinType::SveInt32x2:
+            case BuiltinType::SveInt64x2:
+            case BuiltinType::SveUint8x2:
+            case BuiltinType::SveUint16x2:
+            case BuiltinType::SveUint32x2:
+            case BuiltinType::SveUint64x2:
+            case BuiltinType::SveFloat16x2:
+            case BuiltinType::SveFloat32x2:
+            case BuiltinType::SveFloat64x2:
+            case BuiltinType::SveBFloat16x2:
+            case BuiltinType::SveMFloat8x2:
+            case BuiltinType::SveInt8x3:
+            case BuiltinType::SveInt16x3:
+            case BuiltinType::SveInt32x3:
+            case BuiltinType::SveInt64x3:
+            case BuiltinType::SveUint8x3:
+            case BuiltinType::SveUint16x3:
+            case BuiltinType::SveUint32x3:
+            case BuiltinType::SveUint64x3:
+            case BuiltinType::SveFloat16x3:
+            case BuiltinType::SveFloat32x3:
+            case BuiltinType::SveFloat64x3:
+            case BuiltinType::SveBFloat16x3:
+            case BuiltinType::SveMFloat8x3:
+            case BuiltinType::SveInt8x4:
+            case BuiltinType::SveInt16x4:
+            case BuiltinType::SveInt32x4:
+            case BuiltinType::SveInt64x4:
+            case BuiltinType::SveUint8x4:
+            case BuiltinType::SveUint16x4:
+            case BuiltinType::SveUint32x4:
+            case BuiltinType::SveUint64x4:
+            case BuiltinType::SveFloat16x4:
+            case BuiltinType::SveFloat32x4:
+            case BuiltinType::SveFloat64x4:
+            case BuiltinType::SveBFloat16x4:
+            case BuiltinType::SveMFloat8x4:
+            case BuiltinType::SveBool:
+            case BuiltinType::SveBoolx2:
+            case BuiltinType::SveBoolx4:
+            case BuiltinType::SveCount:
+            case BuiltinType::MFloat8:
+                return TagSve;
+
 #endif
             default:
                 auto pol = clang::PrintingPolicy(Context->getLangOpts());
