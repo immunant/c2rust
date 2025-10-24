00751bbfd39461351f06cdc4515173fcc86f0e9f
Nicole LeGare <legaren@immunant.com>
2025-10-24 13:14:59 -0700

    Support filtering C files when running tests

---
+++ tests/unit/gotos/src/test_goto_error.rs
@@ -0,0 +1,6 @@
+use crate::goto_error::rust_goto_error;
+
+// This test case just makes sure that the function imported actually was
+// translated. Compilation suffices.
+#[test]
+pub fn test_nothing() {}
