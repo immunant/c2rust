d12fe300b974a5d5c9867366bd737a96bd73b336
Nicole LeGare <legaren@immunant.com>
2025-10-27 14:48:34 -0700

    Get basic goto error example working
    ... by not generating a break if there's only one entry following a simple shape.

--- c2rust-transpile/src/translator/mod.rs
+++ c2rust-transpile/src/translator/mod.rs
@@ -2462,7 +2462,7 @@ impl<'c> Translation<'c> {
         );
 
         if self.tcfg.dump_structures {
-            eprintln!("Relooped structures:");
+            eprintln!("Relooped structures for {name}:");
             for s in &relooped {
                 eprintln!("{:#?}", s);
             }
