e51b8b70950a05a42a5010c30256b28fc237d363
Nicole LeGare <legaren@immunant.com>
2025-10-27 11:52:12 -0700

    Setup simplified goto error test

--- c2rust-transpile/src/translator/mod.rs
+++ c2rust-transpile/src/translator/mod.rs
@@ -2648,7 +2648,7 @@ impl<'c> Translation<'c> {
         if self.tcfg.dump_structures {
             eprintln!("Relooped structures:");
             for s in &relooped {
-                eprintln!("  {:#?}", s);
+                eprintln!("{:#?}", s);
             }
         }
 
