aed794f8ce43410799318e19b9eae1451a3a84f8
Nicole LeGare <legaren@immunant.com>
2025-10-28 13:54:25 -0700

    Setup "oops all gotos" test case

--- tests/gotos/src/test_goto_error.rs
+++ tests/gotos/src/test_goto_error.rs
@@ -1,17 +1,33 @@
 #[test]
-pub fn test_goto_error() {
+pub fn test_goto_error_a() {
     use crate::goto_error::rust_goto_error_a;
-    use crate::goto_error::rust_goto_error_b;
     unsafe {
         assert_eq!(rust_goto_error_a(10), 15);
         assert_eq!(rust_goto_error_a(20), 18);
         assert_eq!(rust_goto_error_a(1), 2);
         assert_eq!(rust_goto_error_a(0), -4);
+    }
+}
 
+#[test]
+pub fn test_goto_error_b() {
+    use crate::goto_error::rust_goto_error_b;
+    unsafe {
         assert_eq!(rust_goto_error_b(10), 15);
         assert_eq!(rust_goto_error_b(18), 18);
         assert_eq!(rust_goto_error_b(1), 2);
         assert_eq!(rust_goto_error_b(2), -4);
+    }
+}
+
+#[test]
+pub fn goto_error_but_its_all_gotos() {
+    use crate::goto_error::rust_goto_error_but_its_all_gotos;
+    unsafe {
+        assert_eq!(rust_goto_error_but_its_all_gotos(10), 15);
+        assert_eq!(rust_goto_error_but_its_all_gotos(18), 18);
+        assert_eq!(rust_goto_error_but_its_all_gotos(1), 2);
+        assert_eq!(rust_goto_error_but_its_all_gotos(2), -4);
     }
 }
 
