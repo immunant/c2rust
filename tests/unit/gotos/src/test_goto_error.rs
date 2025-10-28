483587b19bac29e76ed422da29e9d10993e108a6
Nicole LeGare <legaren@immunant.com>
2025-10-27 17:38:25 -0700

    Make goto_error into proper test

--- tests/unit/gotos/src/test_goto_error.rs
+++ tests/unit/gotos/src/test_goto_error.rs
@@ -1,4 +1,24 @@
-// This test case just makes sure that the function imported actually was
-// translated. Compilation suffices.
+#[cfg(none)]
 #[test]
-pub fn test_nothing() {}
+pub fn test_simple_test() {
+    use crate::goto_error::rust_simple_test;
+    unsafe {
+        assert_eq!(rust_simple_test(10), 1);
+        assert_eq!(rust_simple_test(5), 2);
+        assert_eq!(rust_simple_test(0), 3);
+    }
+}
+
+#[test]
+pub fn test_goto_error() {
+    use crate::goto_error::rust_goto_error;
+    unsafe {
+        assert_eq!(rust_goto_error(10), 15);
+        assert_eq!(rust_goto_error(20), 18);
+        assert_eq!(rust_goto_error(1), 2);
+        assert_eq!(rust_goto_error(0), -4);
+    }
+}
+
+#[test]
+pub fn dummy_test() {}
