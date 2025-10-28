34e2c2df5ecd7eaeb75d006bd1ad5ec78db44c8a
Nicole LeGare <legaren@immunant.com>
2025-10-27 17:08:27 -0700

    Fix terminator for empty simple case

--- c2rust-transpile/src/translator/mod.rs
+++ c2rust-transpile/src/translator/mod.rs
@@ -2451,6 +2451,8 @@ impl<'c> Translation<'c> {
             graph
                 .dump_json_graph(&store, format!("dumps/{name}_cfg.json"))
                 .expect("Failed to write CFG .json file");
+
+            eprintln!("Wrote CFG json for {name} to dumps/{name}_cfg.json");
         }
 
         let (lifted_stmts, relooped) = cfg::relooper::reloop(
@@ -2465,6 +2467,7 @@ impl<'c> Translation<'c> {
         if self.tcfg.dump_structures {
             use std::fmt::Write;
 
+            // TODO: Do this more efficiently lol
             let mut dump = String::new();
             for s in &relooped {
                 writeln!(&mut dump, "{:#?}", s).unwrap();
