11e14560401ed50dee82b345a335c44dcd8d3bcc
Nicole LeGare <legaren@immunant.com>
2025-10-28 13:43:15 -0700

    Don't generate blocks for empty branches
    This is probably the right thing to do.

--- tests/unit/gotos/src/test_goto_error.rs
+++ tests/unit/gotos/src/test_goto_error.rs
@@ -1,22 +1,17 @@
-#[cfg(none)]
-#[test]
-pub fn test_simple_test() {
-    use crate::goto_error::rust_simple_test;
-    unsafe {
-        assert_eq!(rust_simple_test(10), 1);
-        assert_eq!(rust_simple_test(5), 2);
-        assert_eq!(rust_simple_test(0), 3);
-    }
-}
-
 #[test]
 pub fn test_goto_error() {
-    use crate::goto_error::rust_goto_error;
+    use crate::goto_error::rust_goto_error_a;
+    use crate::goto_error::rust_goto_error_b;
     unsafe {
-        assert_eq!(rust_goto_error(10), 15);
-        assert_eq!(rust_goto_error(20), 18);
-        assert_eq!(rust_goto_error(1), 2);
-        assert_eq!(rust_goto_error(0), -4);
+        assert_eq!(rust_goto_error_a(10), 15);
+        assert_eq!(rust_goto_error_a(20), 18);
+        assert_eq!(rust_goto_error_a(1), 2);
+        assert_eq!(rust_goto_error_a(0), -4);
+
+        assert_eq!(rust_goto_error_b(10), 15);
+        assert_eq!(rust_goto_error_b(18), 18);
+        assert_eq!(rust_goto_error_b(1), 2);
+        assert_eq!(rust_goto_error_b(2), -4);
     }
 }
 
