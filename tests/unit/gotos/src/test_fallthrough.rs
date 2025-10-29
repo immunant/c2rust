b09f465ab6998625af2541861a05aaf2f7c504c0
Nicole LeGare <legaren@immunant.com>
2025-10-29 13:18:58 -0700

    Add test for fallthrough with early return

--- tests/unit/gotos/src/test_fallthrough.rs
+++ tests/unit/gotos/src/test_fallthrough.rs
@@ -27,3 +27,14 @@ pub fn test_switch_fallthrough_no_default() {
         assert_eq!(rust_fallthrough_without_default(4), 4);
     }
 }
+
+#[test]
+pub fn test_fallthrough_with_early_return() {
+    use crate::fallthrough::rust_fallthrough_with_early_return;
+    unsafe {
+        assert_eq!(rust_fallthrough_with_early_return(0), 10);
+        assert_eq!(rust_fallthrough_with_early_return(1), 3);
+        assert_eq!(rust_fallthrough_with_early_return(2), 3);
+        assert_eq!(rust_fallthrough_with_early_return(4), 5);
+    }
+}
