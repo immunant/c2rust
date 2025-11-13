8e8456fee09ca44722d1984a8184a48751d11167
Stuart Pernsteiner <spernsteiner@galois.com>
2025-11-12 16:56:20 -0800

    add tools/merge_rust/

---
+++ tools/merge_rust/src/main.rs
@@ -0,0 +1,54 @@
+use std::collections::HashMap;
+use std::env;
+use std::fs::{self, File};
+use rust_util::collect::FileCollector;
+use rust_util::item_span::item_spans;
+use serde_json;
+
+fn main() {
+    let src_root_path = env::args().nth(1).unwrap();
+    let new_snippet_json_path = env::args().nth(2).unwrap();
+
+    let new_snippets_file = File::open(&new_snippet_json_path).unwrap();
+    let new_snippets: HashMap<String, String> =
+        serde_json::from_reader(new_snippets_file).unwrap();
+
+    let mut fc = FileCollector::default();
+    fc.parse(&src_root_path, vec![], true).unwrap();
+    for &(ref file_path, ref mod_path, ref ast) in &fc.files {
+        eprintln!("visit {:?}", file_path);
+        let old_src = fs::read_to_string(file_path).unwrap();
+
+        let mut rewrites = Vec::new();
+        for (item_path, lo, hi) in item_spans(mod_path.to_owned(), ast) {
+            let old_snippet = &old_src[lo .. hi];
+            let item_path_str = item_path.join("::");
+            if let Some(new_snippet) = new_snippets.get(&item_path_str) {
+                if new_snippet != old_snippet {
+                    rewrites.push((lo, hi, new_snippet));
+                }
+            }
+        }
+
+        if rewrites.len() == 0 {
+            continue;
+        }
+
+        rewrites.sort_by_key(|&(lo, _, _)| lo);
+        let mut new_src = String::with_capacity(old_src.len());
+        let mut pos = 0;
+        for &(lo, hi, new_snippet) in &rewrites {
+            assert!(lo >= pos, "overlapping rewrites: previous rewrite ended at {}, \
+                but current rewrite covers {} .. {}", pos, lo, hi);
+            new_src.push_str(&old_src[pos .. lo]);
+            new_src.push_str(new_snippet);
+            pos = hi;
+        }
+        new_src.push_str(&old_src[pos..]);
+
+        let tmp_path = file_path.with_extension(".new");
+        fs::write(&tmp_path, &new_src).unwrap();
+        fs::rename(&tmp_path, file_path).unwrap();
+        eprintln!("applied {} rewrites to {:?}", rewrites.len(), file_path);
+    }
+}
