44298f8a785c45e0f0aa3c771477c51fb8a220bd
Andrei Homescu <ah@immunant.com>
2025-11-19 23:07:57 -0800

    Force python2 to compile as C99 code

--- tests/python2/make.sh
+++ tests/python2/make.sh
@@ -3,6 +3,10 @@ set -e; set -o pipefail
 
 SCRIPT_DIR="$(cd "$(dirname "$0" )" && pwd)"
 
+# Force the C standard to C99 for compatibility
+# with newer compilers that default to a different version
+export EXTRA_CFLAGS="-std=c99"
+
 (cd "$SCRIPT_DIR"
 make -C repo clean && rm -f compile_commands.json 
 intercept-build make -C repo -j`nproc` python) 2>&1 | tee `basename "$0"`.log
