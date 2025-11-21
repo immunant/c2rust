3fe8b33fd6ae814647ee18f6704acd453cd4efce
Nicole LeGare <legaren@immunant.com>
2025-11-21 15:28:52 -0800

    Add some slightly weirder test cases
    Just for funsies.

--- tests/unit/gotos/src/idiomatic_nested_loops.c
+++ tests/unit/gotos/src/idiomatic_nested_loops.c
@@ -6,6 +6,18 @@ int simple_loop(int x) {
     return x;
 }
 
+int infinite_loop_with_break(int x) {
+    // Also we have a different comment here, just to be difficult.
+    for (;;) {
+        // Imagine we have a comment here. If we smoosh this into a `while x` loop, where should the comment go?
+        if (!x)
+            break;
+        x--;
+    }
+
+    return x;
+}
+
 int nested_for(int x) {
     for (int i = 0; i < 5; i++) {
         for (int j = 0; j < 5; j++) {
@@ -29,6 +41,24 @@ int nested_loop(int x) {
 int trivially_nested_loop(int x) {
     while (x < 5) {
         while (x < 5) {
+            if (x < 2)
+                goto break_outer;
+            x++;
+        }
+    }
+
+break_outer:
+    return x;
+}
+
+// The same cfg as `trivially_nested_loop`, but expressed in a weirder way. TBD
+// if we want to generate the same code for both of these functions.
+int weird_nested_loops(int x) {
+outer:
+    while (x < 5) {
+        for (;;) {
+            if (!(x < 5))
+                goto outer; // Explicitly jump back to the top of the outer loop instead of breaking forward out of the inner loop.
             if (x < 2)
                 goto break_outer;
             x++;
