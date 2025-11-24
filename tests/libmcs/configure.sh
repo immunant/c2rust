c0ce6aa430a9c1202021e5b3392aa6a874676077
Khyber Sen <kkysen@gmail.com>
2025-11-24 01:09:02 -0800

    tests: add where `libmcs` configure flags come from (Hayroll)

--- tests/libmcs/configure.sh
+++ tests/libmcs/configure.sh
@@ -3,6 +3,7 @@ set -e; set -o pipefail
 
 SCRIPT_DIR="$(cd "$(dirname "$0")" && pwd)"
 
+# Flags copied to match <https://github.com/UW-HARVEST/Hayroll/blob/8d88b7403650132521e837947667b4cd0999fd1a/prerequisites.bash#L264-L270>.
 (cd "${SCRIPT_DIR}/repo" && ./configure \
      --cross-compile="" \
      --compilation-flags="" \
