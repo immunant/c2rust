immunant/c2rust-testsuite

test.py: make logic for finding scripts clearer
We search for and find non-generated scripts (`.sh`, but not `.gen.sh`),
whereas we record all generated scripts, which we then combine to get all scripts.

tests/__init__.py
@@ -22,14 +22,15 @@ class Test(object):
         "check": ["check.sh", "test.sh"]
     }
 
-    def __init__(self, directory: str, generated_scripts: set[Path]):
-        self.scripts = {
-            f.name for f in Path(directory).iterdir() if f.suffix == ".sh" and
-            (".gen" not in f.suffixes or any(f.samefile(script) for script in generated_scripts))
-        }
-        self.dir = directory
-        self.conf_file = os.path.join(directory, CONF_YML)
-        self.name = os.path.basename(directory)
+    def __init__(self, directory: Path, generated_scripts: set[Path]):
+        # We only want scripts that have been generated by us now,
+        # but non `*.gen*` scripts we can search for.
+        non_gen_script_paths = { f for f in directory.iterdir() if f.suffix == ".sh" and ".gen" not in f.suffixes }
+        script_paths = non_gen_script_paths | generated_scripts
+        self.scripts = { str(script.relative_to(directory)) for script in script_paths }
+        self.dir = str(directory)
+        self.conf_file = str(directory / CONF_YML)
+        self.name = directory.name
 
     def run_script(self, stage, script, verbose=False, xfail=False) -> bool:
         """
@@ -213,7 +214,7 @@ def run_tests(conf: Config, generated_scripts: set[Path]):
     if not conf.ignore_requirements:
         check(conf)
 
-    tests = [Test(td, generated_scripts) for td in conf.project_dirs]
+    tests = [Test(Path(td), generated_scripts) for td in conf.project_dirs]
 
     failure = False
     for tt in tests:

immunant/c2rust

tests/integration/tests/__init__.py
@@ -26,14 +26,16 @@ class Test(object):
         "check": ["check.sh", "test.sh"]
     }
 
-    def __init__(self, directory: str, generated_scripts: set[Path]):
-        self.scripts = {
-            f.name for f in Path(directory).iterdir() if f.suffix == ".sh" and
-            (".gen" not in f.suffixes or any(f.samefile(script) for script in generated_scripts))
-        }
-        self.dir = directory
-        self.conf_file = os.path.join(directory, CONF_YML)
-        self.name = os.path.basename(directory)
+    def __init__(self, directory: Path, generated_scripts: set[Path]):
+        # We only want scripts that have been generated by us now,
+        # but non `*.gen*` scripts we can search for.
+        non_gen_script_paths = { f for f in directory.iterdir() if f.suffix == ".sh" and ".gen" not in f.suffixes }
+        gen_script_paths = { path for path in generated_scripts if path.is_relative_to(directory) }
+        script_paths = non_gen_script_paths | gen_script_paths
+        self.scripts = { str(script.relative_to(directory)) for script in script_paths }
+        self.dir = str(directory)
+        self.conf_file = str(directory / CONF_YML)
+        self.name = directory.name
 
     def run_script(self, stage, script, verbose=False, xfail=False) -> bool:
         """
@@ -217,7 +219,7 @@ def run_tests(conf: Config, generated_scripts: set[Path]):
     if not conf.ignore_requirements:
         check(conf)
 
-    tests = [Test(td, generated_scripts) for td in conf.project_dirs]
+    tests = [Test(Path(td), generated_scripts) for td in conf.project_dirs]
 
     def run(test: Test) -> TestResult:
         start = perf_counter()