7bc8270539643732e1dcff1f03a8104e64b7de66
Nicole LeGare <legaren@immunant.com>
2026-01-05 14:33:22 -0800

    Flesh out fallthrough with early return test

--- tests/unit/gotos/src/fallthrough.c
+++ tests/unit/gotos/src/fallthrough.c
@@ -1,5 +1,3 @@
-void stuff(const char *);
-
 int fallthrough_2(int x) {
     switch (x)
     {
@@ -41,11 +39,19 @@ int fallthrough_with_early_return(int x) {
     switch (x)
     {
     case 0:
-        return 10;
+        x += 1;
     case 1:
-        x += 1;
+        x += 2;
+    case 2:
+        x += 3;
+        return x;
+    case 3:
+        x += 4;
+    case 4:
+        x += 5;
     default:
-        x += 1;
+        x += 6;
     }
+    x += 7;
     return x;
 }
