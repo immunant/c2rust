34a25df744367fb6116692fff91696c673490977
Rua <ruawhitepaw@gmail.com>
2026-01-06 18:12:43 +0100

    transpile: Assert that newly picked names contain "c2rust"

--- c2rust-transpile/src/renamer.rs
+++ c2rust-transpile/src/renamer.rs
@@ -105,12 +105,14 @@ impl<T: Clone + Eq + Hash> Renamer<T> {
     }
 
     pub fn pick_name(&mut self, basename: &str) -> String {
+        assert!(basename.to_ascii_lowercase().contains("c2rust"));
         self.pick_name_in_scope(basename, None)
     }
 
     /// Permanently assign a name that doesn't collide with anything
     /// currently in scope, and also never goes out of scope
     pub fn pick_name_root(&mut self, basename: &str) -> String {
+        assert!(basename.to_ascii_lowercase().contains("c2rust"));
         self.pick_name_in_scope(basename, Some(0))
     }
 
