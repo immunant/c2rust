0078e0500a64a34be73a7c400f90a636fe23e44f
Andrei Homescu <ah@immunant.com>
2026-01-28 21:54:48 -0800

    cross-checks: c-checks: Add c2rust_cross_check_value support

--- cross-checks/c-checks/clang-plugin/include/cross_checks.h
+++ cross-checks/c-checks/clang-plugin/include/cross_checks.h
@@ -3,6 +3,8 @@
 
 #pragma once
 
+#include <stdint.h>
+
 #define CROSS_CHECK(x)          __attribute__((annotate("cross_check:" x)))
 #define DISABLE_XCHECKS(x)      CROSS_CHECK("{ disable_xchecks: " #x " }")
 #define DEFAULT_XCHECK          CROSS_CHECK("default")
@@ -10,5 +12,7 @@
 #define FIXED_XCHECK(x)         CROSS_CHECK("{ fixed: " x " }")
 #define CUSTOM_XCHECK(x)        CROSS_CHECK("{ custom: \"" x "\" }")
 #define CUSTOM_HASH_XCHECK(x)   CROSS_CHECK("{ custom_hash: \"" x "\" }")
+
+extern void c2rust_cross_check_value(uint8_t tag, ...);
 
 #endif // CROSS_CHECKS_H
