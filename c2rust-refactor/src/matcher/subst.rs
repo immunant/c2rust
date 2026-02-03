dab5ad30bdc2366cb50289336818099b2d83c24b
Andrei Homescu <ah@immunant.com>
2026-02-02 17:36:07 -0800

    refactor: Handle the new parse! substitution that parses a node as is from its arguments

--- c2rust-refactor/src/matcher/subst.rs
+++ c2rust-refactor/src/matcher/subst.rs
@@ -20,7 +20,11 @@
 use rustc_ast::mut_visit::{self, MutVisitor};
 use rustc_ast::ptr::P;
 use rustc_ast::MacCall;
-use rustc_ast::{Expr, ExprKind, Item, Label, Pat, Path, Stmt, Ty};
+use rustc_ast::{
+    Expr, ExprKind, Item, ItemKind, Label, MacArgs, Pat, PatKind, Path, Stmt, StmtKind, Ty, TyKind,
+};
+use rustc_ast_pretty::pprust;
+use rustc_parse::parser::{ForceCollect, Parser};
 use rustc_span::symbol::Ident;
 use smallvec::smallvec;
 use smallvec::SmallVec;
@@ -59,6 +63,16 @@ impl<'a, 'tcx> SubstFolder<'a, 'tcx> {
             }
         }
     }
+}
+
+fn unwrap_or_panic_tts<T, E>(x: Result<T, E>, args: &MacArgs, kind: &str) -> T
+where
+    E: std::fmt::Debug,
+{
+    x.unwrap_or_else(|e| {
+        let tts = pprust::tts_to_string(&args.inner_tokens());
+        panic!("Failed to parse {kind} parse!({tts}): {e:?}");
+    })
 }
 
 impl<'a, 'tcx> MutVisitor for SubstFolder<'a, 'tcx> {
@@ -114,6 +128,17 @@ impl<'a, 'tcx> MutVisitor for SubstFolder<'a, 'tcx> {
             | ExprKind::Continue(ref mut label) => {
                 self.subst_opt_label(label);
             }
+
+            ExprKind::MacCall(ref mc) if mc.path.is_named("parse") => {
+                let mut parser = Parser::new(
+                    &self.cx.session().parse_sess,
+                    mc.args.inner_tokens().clone(),
+                    false,
+                    None,
+                );
+                *e = unwrap_or_panic_tts(parser.parse_expr(), &mc.args, "Expr");
+            }
+
             _ => {}
         }
 
@@ -128,6 +153,16 @@ impl<'a, 'tcx> MutVisitor for SubstFolder<'a, 'tcx> {
             *p = binding.clone();
         }
 
+        if let PatKind::MacCall(mc) = &p.kind && mc.path.is_named("parse") {
+            let mut parser = Parser::new(
+                &self.cx.session().parse_sess,
+                mc.args.inner_tokens().clone(),
+                false,
+                None,
+            );
+            *p = unwrap_or_panic_tts(parser.parse_pat_no_top_alt(None), &mc.args, "Pat");
+        }
+
         mut_visit::noop_visit_pat(p, self);
     }
 
@@ -138,6 +173,16 @@ impl<'a, 'tcx> MutVisitor for SubstFolder<'a, 'tcx> {
             } else if let Some(Some(binding)) = self.bindings.get_opt::<_, P<Ty>>(sym) {
                 *ty = binding.clone();
             }
+        }
+
+        if let TyKind::MacCall(mc) = &ty.kind && mc.path.is_named("parse") {
+            let mut parser = Parser::new(
+                &self.cx.session().parse_sess,
+                mc.args.inner_tokens().clone(),
+                false,
+                None,
+            );
+            *ty = unwrap_or_panic_tts(parser.parse_ty(), &mc.args, "Ty");
         }
 
         mut_visit::noop_visit_ty(ty, self)
@@ -154,6 +199,18 @@ impl<'a, 'tcx> MutVisitor for SubstFolder<'a, 'tcx> {
             .and_then(|sym| self.bindings.get::<_, Vec<Stmt>>(sym))
         {
             SmallVec::from_vec(stmts.clone())
+        } else if let StmtKind::MacCall(mcs) = &s.kind && mcs.mac.path.is_named("parse") {
+            let mut parser = Parser::new(
+                &self.cx.session().parse_sess,
+                mcs.mac.args.inner_tokens().clone(),
+                false,
+                None,
+            );
+            unwrap_or_panic_tts(parser.parse_stmt(ForceCollect::No), &mcs.mac.args, "Stmt")
+                .into_iter()
+                .map(|s| mut_visit::noop_flat_map_stmt(s, self))
+                .flatten()
+                .collect()
         } else {
             mut_visit::noop_flat_map_stmt(s, self)
         }
@@ -165,6 +222,18 @@ impl<'a, 'tcx> MutVisitor for SubstFolder<'a, 'tcx> {
             .and_then(|sym| self.bindings.get::<_, P<Item>>(sym))
         {
             smallvec![item.clone()]
+        } else if let ItemKind::MacCall(mc) = &i.kind && mc.path.is_named("parse") {
+            let mut parser = Parser::new(
+                &self.cx.session().parse_sess,
+                mc.args.inner_tokens().clone(),
+                false,
+                None,
+            );
+            unwrap_or_panic_tts(parser.parse_item(ForceCollect::No), &mc.args, "Item")
+                .into_iter()
+                .map(|i| mut_visit::noop_flat_map_item(i, self))
+                .flatten()
+                .collect()
         } else {
             mut_visit::noop_flat_map_item(i, self)
         }
