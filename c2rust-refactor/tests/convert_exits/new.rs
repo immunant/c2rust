49d59f534e646e79ae99686c9c8e585bad43b5ca
Nicole LeGare <legaren@immunant.com>
2026-02-06 16:42:25 -0800

    Remove leading :: from generated paths

--- c2rust-refactor/tests/convert_exits/new.rs
+++ c2rust-refactor/tests/convert_exits/new.rs
@@ -9,26 +9,26 @@ extern "C" {
 
 fn test_abort() {
     unsafe {
-        ::std::process::abort();
+        std::process::abort();
     }
 }
 
 fn test_exit() {
     unsafe {
-        ::std::process::exit(0);
+        std::process::exit(0);
     }
 }
 
 fn test_exit_error() {
     unsafe {
-        ::std::process::exit(1);
+        std::process::exit(1);
     }
 }
 
 fn test_exit_variable() {
     let code = 42;
     unsafe {
-        ::std::process::exit(code);
+        std::process::exit(code);
     }
 }
 
