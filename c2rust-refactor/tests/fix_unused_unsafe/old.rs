07265f78b9facb5a165e111f92986551ec3a8f20
Nicole LeGare <legaren@immunant.com>
2026-02-13 13:08:30 -0800

    Add more cases for fix_unused_unsafe test

--- c2rust-refactor/tests/fix_unused_unsafe/old.rs
+++ c2rust-refactor/tests/fix_unused_unsafe/old.rs
@@ -1,5 +1,16 @@
 fn main() {
+    // Has unsafe operations, shouldn't be modified.
+    unsafe {
+        let val = 123;
+        let ptr = &val as *const i32;
+        let _safe_val = *ptr;
+    }
+
     unsafe {
         let x = 42;
     }
+
+    unsafe {}
+
+    let _ = unsafe { 5 + 5 };
 }
