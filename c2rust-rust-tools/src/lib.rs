4c419758c69b4301ae0d03d6e42b5a6cad86b2a3
Khyber Sen <kkysen@gmail.com>
2026-02-21 23:28:07 -0800

    rust-tools: have better `rustc` error messages

--- c2rust-rust-tools/src/lib.rs
+++ c2rust-rust-tools/src/lib.rs
@@ -102,23 +102,24 @@ fn run_rustc(rs_path: &Path, crate_name: &str, expect_error: bool) {
     // There's no good way to not create an output with `rustc`,
     // so just create an `.rlib` and then delete it immediately.
     let rlib_path = rs_path.with_file_name(format!("lib{crate_name}.rlib"));
-    let status = Command::new("rustc")
-        .args([
-            "+nightly-2023-04-15",
-            "--crate-type",
-            "lib",
-            "--edition",
-            EDITION,
-            "--crate-name",
-            crate_name,
-            "-Awarnings", // Disable warnings.
-            "-o",
-        ])
-        .args([&rlib_path, rs_path])
-        .status()
-        .unwrap();
-    assert_eq!(!expect_error, status.success());
-    if !expect_error {
+    let mut cmd = Command::new("rustc");
+    cmd.args([
+        "+nightly-2023-04-15",
+        "--crate-type",
+        "lib",
+        "--edition",
+        EDITION,
+        "--crate-name",
+        crate_name,
+        "-Awarnings", // Disable warnings.
+        "-o",
+    ]);
+    cmd.args([&rlib_path, rs_path]);
+    let status = cmd.status().unwrap();
+    if expect_error {
+        assert!(!status.success(), "expected error, but succeeded: {cmd:?}");
+    } else {
+        assert!(status.success(), "failed with {status}: {cmd:?}");
         fs_err::remove_file(&rlib_path).unwrap();
     }
 }
