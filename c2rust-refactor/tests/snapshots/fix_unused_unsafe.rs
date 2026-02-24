0d1ef2c27f2b836061d70179eb4cedc9ce5c8261
Nicole LeGare <legaren@immunant.com>
2026-02-23 16:03:14 -0800

    Add edge cases to snapshot test

--- c2rust-refactor/tests/snapshots/fix_unused_unsafe.rs
+++ c2rust-refactor/tests/snapshots/fix_unused_unsafe.rs
@@ -24,4 +24,19 @@ fn main() {
 
     // Block expression should stay.
     let _ = unsafe { 5 + 5 };
+
+    unsafe {
+        // Contains a comment but no body. In theory we'd like to preserve this,
+        // but it's hard to detect the comment since it doesn't have an AST node
+        // it's attached to. For now we allow this to be deleted.
+    }
+
+    unsafe {
+        unsafe {
+            // Nested unsafe blocks. Not that hard to handle, but also not a
+            // case that we need to cover since c2rust doesn't generate code
+            // like this and later refactoring is unlikely to introduce it.
+            let y = 99;
+        }
+    }
 }
