4aea439b8bb86df4a040997d62af7066ed200304
Khyber Sen <kkysen@gmail.com>
2026-02-24 17:13:06 -0800

    refactor: tests: port `collapse_cfg` to a snapshot test

--- c2rust-refactor/tests/snapshots.rs
+++ c2rust-refactor/tests/snapshots.rs
@@ -224,6 +224,13 @@ fn test_bitcast_retype() {
 }
 
 #[test]
+fn test_collapse_cfg() {
+    refactor("test_one_plus_one")
+        .named("collapse_cfg.rs")
+        .test();
+}
+
+#[test]
 fn test_collapse_cfg_attr() {
     refactor("test_one_plus_one")
         .named("collapse_cfg_attr.rs")
