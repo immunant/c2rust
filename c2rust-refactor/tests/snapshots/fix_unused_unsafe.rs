8025e77c97cc5d040342a4b80161c140b18a60c8
Nicole LeGare <legaren@immunant.com>
2026-02-27 12:30:54 -0800

    Support removing block from block exprs

--- c2rust-refactor/tests/snapshots/fix_unused_unsafe.rs
+++ c2rust-refactor/tests/snapshots/fix_unused_unsafe.rs
@@ -7,8 +7,8 @@ fn main() {
     }
 
     unsafe {
-        // Block with no comment and no destructors, block should be removed but
-        // contents kept.
+        // Block expr with no comment and no destructors, block should be
+        // removed but contents kept.
         let x = 42;
     }
 
@@ -22,8 +22,21 @@ fn main() {
         let _string = String::from("string");
     }
 
-    // Block expression should stay.
+    // Block expr with no statements, we can remove the block.
     let _ = unsafe { 5 + 5 };
+
+    // Block expr with statements, keep the block.
+    let _ = unsafe {
+        let _ = 5;
+        5 + 5
+    };
+
+    // TODO: Uh oh, if there's a comment on the tail expr we eat it.
+    // let _ = unsafe {
+    //     // Comment on the expr.
+    //     5 + 5
+    // };
+    let _ = {};
 
     unsafe {
         // Contains a comment but no body. In theory we'd like to preserve this,
@@ -40,10 +53,11 @@ fn main() {
         }
     }
 
-    // Trailing expr in an unsafe block used as a statement, make sure we don't
-    // just remove the block since that would turn the statement into an
-    // expression.
     unsafe {
+        // Trailing expr in an unsafe block used as a statement, make sure we
+        // don't just remove the block since that would turn the statement into
+        // an expression.
+        // TODO: Why is there extra whitespace between lines of the comment?
         1 + 2
     };
 }
