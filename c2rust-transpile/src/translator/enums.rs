4edc754464353d86f65e68ae1aeac085a64e91bf
Rua <ruawhitepaw@gmail.com>
2026-03-07 13:11:00 +0100

    transpile: Split off `expr_is_expanded_macro` helper function

--- c2rust-transpile/src/translator/enums.rs
+++ c2rust-transpile/src/translator/enums.rs
@@ -97,10 +97,7 @@ impl<'c> Translation<'c> {
                     if self.is_variant_of_enum(enum_id, enum_constant_id) =>
                 {
                     // `enum`s shouldn't need portable `override_ty`s.
-                    let expr_is_macro = matches!(
-                        self.convert_const_macro_expansion(ctx, expr, None),
-                        Ok(Some(_))
-                    );
+                    let expr_is_macro = self.expr_is_expanded_macro(ctx, expr, None);
 
                     // If this DeclRef expanded to a const macro, we actually need to insert a cast,
                     // because the translation of a const macro skips implicit casts in its context.
