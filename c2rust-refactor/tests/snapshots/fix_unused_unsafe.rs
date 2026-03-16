0cbf3dea805d0f387d8f4969e2935bb86efac07c
Nicole L <dlegare.1001@gmail.com>
2026-03-16 10:47:03 -0700

    refactor: Remove unnecessary blocks when cleaning up unused unsafe (#1611)

--- c2rust-refactor/tests/snapshots/fix_unused_unsafe.rs
+++ c2rust-refactor/tests/snapshots/fix_unused_unsafe.rs
@@ -7,10 +7,74 @@ fn main() {
     }
 
     unsafe {
+        // Block expr with no comment and no destructors, block should be
+        // removed but contents kept.
         let x = 42;
     }
 
+    // Empty block expr should stay a block, empty block statement can be
+    // removed entirely.
+    let _ = unsafe {};
     unsafe {}
 
+    // Has a comment, shouldn't be removed.
+    unsafe {}
+
+    unsafe {
+        // Contains a destructor, keep the block to avoid changing drop order.
+        let _string = String::from("string");
+    }
+
+    // Block expr with no statements, we can remove the block.
     let _ = unsafe { 5 + 5 };
+
+    let _ = unsafe {
+        // Comment on the tail expr, keep the block because it's not easy to
+        // lift the comment to the outer statement.
+        5 + 5
+    };
+
+    // Block expr with statements, keep the block.
+    let _ = unsafe {
+        let _ = 5;
+        5 + 5
+    };
+
+    unsafe {
+        // Contains a comment but no body. In theory we'd like to preserve this,
+        // but it's hard to detect the comment since it doesn't have an AST node
+        // it's attached to. For now we allow this to be deleted.
+    }
+
+    unsafe {
+        unsafe {
+            // Nested unsafe blocks. Not that hard to handle, but also not a
+            // case that we need to cover since c2rust doesn't generate code
+            // like this and later refactoring is unlikely to introduce it.
+            let y = 99;
+        }
+    }
+
+    unsafe {
+        // Trailing expr in an unsafe block used as a statement, make sure we
+        // don't just remove the block since that would turn the statement into
+        // an expression.
+        // TODO: Why is there extra whitespace between lines of the comment?
+        1 + 2
+    };
+}
+
+// In a generic context, all non-`Copy` type params show up as requiring drop,
+// so we won't be able to fully remove the block.
+fn generic_non_copy<T: Default>() {
+    unsafe {
+        let _val = T::default();
+    }
+}
+
+// If the type parameter is `Copy` then we should be able to remove the block.
+fn generic_copy<T: Copy + Default>() {
+    unsafe {
+        let _val = T::default();
+    }
 }
