03e64391ed72ffb9df2761261e50f6a583a25ac2
Khyber Sen <kkysen@gmail.com>
2026-03-19 13:25:51 -0700

    transpile: rename `e` to `val` in `fn convert_pointer_is_null`
    Co-authored-by: Rua <ruawhitepaw@gmail.com>

--- c2rust-transpile/src/translator/pointers.rs
+++ c2rust-transpile/src/translator/pointers.rs
@@ -569,12 +569,12 @@ impl<'c> Translation<'c> {
         &self,
         ctx: ExprContext,
         ptr_type: CTypeId,
-        e: Box<Expr>,
+        val: Box<Expr>,
         is_null: bool,
     ) -> TranslationResult<Box<Expr>> {
         Ok(if self.ast_context.is_function_pointer(ptr_type) {
             let method = if is_null { "is_none" } else { "is_some" };
-            mk().method_call_expr(e, method, vec![])
+            mk().method_call_expr(val, method, vec![])
         } else {
             // TODO: `pointer::is_null` becomes stably const in Rust 1.84.
             if ctx.is_const {
@@ -583,11 +583,11 @@ impl<'c> Translation<'c> {
                     "cannot check nullity of pointer in `const` context",
                 ));
             }
-            let is_null_val = mk().method_call_expr(e, "is_null", vec![]);
+            let val = mk().method_call_expr(val, "is_null", vec![]);
             if !is_null {
-                mk().unary_expr(UnOp::Not(Default::default()), is_null_val)
+                mk().unary_expr(UnOp::Not(Default::default()), val)
             } else {
-                is_null_val
+                val
             }
         })
     }
