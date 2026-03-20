90987944aba55a32e5c42ced3505d2e3c6bbd1fb
Rua <ruawhitepaw@gmail.com>
2026-03-20 12:27:07 +0100

    transpile: Translate enums into newtype structs

--- c2rust-transpile/src/translator/pointers.rs
+++ c2rust-transpile/src/translator/pointers.rs
@@ -262,6 +262,10 @@ impl<'c> Translation<'c> {
         let lhs_node_type = lhs_node
             .get_type()
             .ok_or_else(|| format_err!("lhs node bad type"))?;
+        let rhs_node_type = rhs_node
+            .get_type()
+            .ok_or_else(|| format_err!("rhs node bad type"))?;
+
         if self
             .ast_context
             .resolve_type(lhs_node_type)
@@ -275,7 +279,12 @@ impl<'c> Translation<'c> {
         }
 
         let rhs = self.convert_expr(ctx.used(), rhs, None)?;
-        rhs.and_then(|rhs| {
+        rhs.and_then(|mut rhs| {
+            // C allows enums to index arrays directly without inserting a numeric cast.
+            if let CTypeKind::Enum(..) = self.ast_context.resolve_type(rhs_node_type).kind {
+                rhs = self.integer_from_enum(rhs);
+            }
+
             let simple_index_array = if ctx.needs_address() {
                 // We can't necessarily index into an array if we're using
                 // that element to compute an address.
@@ -558,7 +567,7 @@ impl<'c> Translation<'c> {
         } else if let &CTypeKind::Enum(enum_decl_id) = target_ty_kind {
             let expr = expr.ok_or_else(|| format_err!("Casts to enums require a C ExprId"))?;
             val.result_map(|val| {
-                self.convert_cast_to_enum(ctx, target_cty, enum_decl_id, Some(expr), val)
+                self.convert_cast_to_enum(ctx, enum_decl_id, Some(expr), source_cty, val)
             })
         } else {
             Ok(val.map(|val| mk().cast_expr(val, target_ty)))
