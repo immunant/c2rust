3a9237c8ae8f50071faecbf8f5e79a2041991d4e
Rua <ruawhitepaw@gmail.com>
2026-03-31 14:29:47 +0200

    transpile: Add more snapshot tests to `volatile.c`

--- c2rust-transpile/tests/snapshots/volatile.c
+++ c2rust-transpile/tests/snapshots/volatile.c
@@ -1,6 +1,14 @@
 struct S { int * volatile p; } volatile_global_struct;
 
 void test_volatile(void) {
+    volatile int vi = 0;
+    int i = vi;
+    vi = i;
+
+    volatile int *pvi = &vi;
+    i = *pvi;
+    *pvi = i;
+
     // https://github.com/immunant/c2rust/issues/1237
     --(volatile_global_struct.p);
 }
