b9d32dd8bc9e2dafe7a469d7d7f744d43ce1394c
Rua <ruawhitepaw@gmail.com>
2026-03-31 21:00:34 +0200

    transpile: Add `asm.c` snapshot test

--- c2rust-transpile/tests/snapshots.rs
+++ c2rust-transpile/tests/snapshots.rs
@@ -414,6 +414,17 @@ fn test_str_init() {
 // arch-specific
 
 #[test]
+fn test_asm() {
+    #[cfg(target_arch = "x86_64")]
+    transpile("asm.c")
+        .arch_specific(true)
+        .expect_unresolved_import("c2rust_asm_casts")
+        .run();
+    #[cfg(not(target_arch = "x86_64"))]
+    transpile("asm.c").arch_specific(true).run();
+}
+
+#[test]
 fn test_spin() {
     transpile("spin.c").arch_specific(true).run();
 }
