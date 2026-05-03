0d8ec5e409795a05d6f6ea26694736192ad519ff
Rua <ruawhitepaw@gmail.com>
2026-05-03 12:11:40 +0200

    transpile: let `can_propagate_cast` handle casting `EnumConstant` to its own enum

--- c2rust-transpile/src/translator/pointers.rs
+++ c2rust-transpile/src/translator/pointers.rs
@@ -611,7 +611,6 @@ impl<'c> Translation<'c> {
         source_cty: CQualTypeId,
         target_cty: CQualTypeId,
         val: WithStmts<Box<Expr>>,
-        expr: Option<CExprId>,
     ) -> TranslationResult<WithStmts<Box<Expr>>> {
         if ctx.is_const {
             return Err(format_translation_err!(
@@ -654,7 +653,6 @@ impl<'c> Translation<'c> {
                         ctx,
                         CQualTypeId::new(size_type_id),
                         enum_decl_id,
-                        expr,
                         val,
                     )
                 })
