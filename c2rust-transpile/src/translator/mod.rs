05af591dea55dccaf14b0ab5ee960932a28f1441
Rua <ruawhitepaw@gmail.com>
2026-05-05 17:37:55 +0200

    transpile: Add `EnumMode` to select enum output type

--- c2rust-transpile/src/translator/mod.rs
+++ c2rust-transpile/src/translator/mod.rs
@@ -4364,3 +4364,8 @@ fn neg_expr(arg: Box<Expr>) -> Box<Expr> {
 fn wrapping_neg_expr(arg: Box<Expr>) -> Box<Expr> {
     mk().method_call_expr(arg, "wrapping_neg", vec![])
 }
+
+#[derive(Debug, Clone, Copy, PartialEq, Eq)]
+pub enum EnumMode {
+    Consts,
+}
