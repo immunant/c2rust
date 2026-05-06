b104d5112406f1c86804568aa3a7254bbe9cba18
Rua <ruawhitepaw@gmail.com>
2026-05-06 16:12:44 +0200

    transpile: Add more side-effect snapshot tests to `exprs.c`

--- c2rust-transpile/tests/snapshots/exprs.c
+++ c2rust-transpile/tests/snapshots/exprs.c
@@ -2,7 +2,13 @@ int puts(const char *str);
 
 static int side_effect(){
     puts("the return of side effect");
-    return 10;
+    return 0;
+}
+
+static int* lvalue_side_effect(){
+    puts("the return of side effect");
+    static int VAL = 42;
+    return &VAL;
 }
 
 void unary_without_side_effect(){
@@ -20,18 +26,43 @@ void unary_without_side_effect(){
 }
 
 void unary_with_side_effect(){
-    char* arr[1] = {0};
+    char *arr[1] = {0};
 
     -side_effect();
     +side_effect();
     ~side_effect();
     !side_effect();
     &""[side_effect()];
-    *arr[side_effect()];
+}
+
+void inc_decl_with_rvalue_side_effect() {
+    int arr[1] = {0};
+
+    // Increment/decrement, expression value not used
     ++arr[side_effect()];
     --arr[side_effect()];
     arr[side_effect()]++;
     arr[side_effect()]--;
+
+    // Increment/decrement, expression value is used
+    int pre_inc = ++arr[side_effect()];
+    int pre_dec = --arr[side_effect()];
+    int post_inc = arr[side_effect()]++;
+    int post_dec = arr[side_effect()]--;
+}
+
+void inc_decl_with_lvalue_side_effect() {
+    // Increment/decrement, expression value not used
+    ++*lvalue_side_effect();
+    --*lvalue_side_effect();
+    (*lvalue_side_effect())++;
+    (*lvalue_side_effect())--;
+
+    // Increment/decrement, expression value is used
+    int pre_inc = ++*lvalue_side_effect();
+    int pre_dec = --*lvalue_side_effect();
+    int post_inc = (*lvalue_side_effect())++;
+    int post_dec = (*lvalue_side_effect())--;
 }
 
 void unsigned_compound_desugaring(void) {
