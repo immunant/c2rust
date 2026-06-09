71daf71e7b6ee6bd81220d22885543cd7a06f535
Per Larsen <perl@immunant.com>
2026-06-08 18:36:07 -0700

    tests: integration: json-c: Fix configure step
    Don't treat calloc-transposed-args and implicit-function-declaration
    lints as an errors. Issue encountered on Ubuntu 26.04/GCC 15.

--- tests/integration/tests/json-c/configure.sh
+++ tests/integration/tests/json-c/configure.sh
@@ -3,6 +3,7 @@ set -e; set -o pipefail
 
 SCRIPT_DIR="$(cd "$(dirname "$0" )" && pwd)"
 
+export CFLAGS="-Wno-calloc-transposed-args -Wno-implicit-function-declaration"
 export LDFLAGS="-pthread -lquadmath -Wl,--no-as-needed -ldl"
 
 (cd "${SCRIPT_DIR}/repo" && ./configure --disable-shared 2>&1 \
