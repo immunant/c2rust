e97c718f2360177509385b0045267d410324ecfe
Rua <ruawhitepaw@gmail.com>
2026-06-19 12:43:56 +0200

    transpile: Use raw pointers for lvalues with side effects

--- c2rust-transpile/src/translator/named_references.rs
+++ c2rust-transpile/src/translator/named_references.rs
@@ -124,11 +124,14 @@ impl<'c> Translation<'c> {
 
                 let ptr_name = self.renamer.borrow_mut().pick_name("c2rust_lvalue_ptr");
 
-                // let ref mut p = lhs;
+                // let p = &raw mut lhs;
+                // Use a raw pointer here so that it doesn't create borrow conflicts and
+                // doesn't invalidate other pointers.
+                self.use_feature("raw_ref_op");
                 let compute_ref = mk().local_stmt(Box::new(mk().local(
-                    mk().mutbl().ident_ref_pat(&ptr_name),
+                    mk().ident_pat(&ptr_name),
                     None,
-                    Some(reference),
+                    Some(mk().mutbl().raw_borrow_expr(reference)),
                 )));
 
                 let write =
