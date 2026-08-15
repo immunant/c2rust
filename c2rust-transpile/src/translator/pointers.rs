0cc2795496d4aec36479b9cb8168fcd8e7e67781
Rua <ruawhitepaw@gmail.com>
2026-08-15 12:11:27 +0200

    transpile: Unset `needs_address` in various places

--- c2rust-transpile/src/translator/pointers.rs
+++ c2rust-transpile/src/translator/pointers.rs
@@ -201,7 +201,7 @@ impl<'c> Translation<'c> {
             return self.convert_expr(ctx.used(), arg, None);
         }
 
-        self.convert_expr(ctx.used(), arg, None)?
+        self.convert_expr(ctx.used().set_needs_address(false), arg, None)?
             .try_map(|val: Box<Expr>| {
                 if let CTypeKind::Function(..) =
                     self.ast_context.resolve_type(cqual_type.ctype).kind
@@ -291,13 +291,14 @@ impl<'c> Translation<'c> {
                 ref other => panic!("Unexpected array type {:?}", other),
             };
 
-            let array_rs = self.convert_expr(ctx.used(), array_id, None)?;
+            let array_rs =
+                self.convert_expr(ctx.used().set_needs_address(false), array_id, None)?;
 
             // Don't dereference the offset if we're still within the variable portion
             let val = if let Some(elt_type_id) = var_elt_type_id {
                 let target_type_id = self.ast_context.type_for_kind(&CTypeKind::SSize);
                 let offset_rs = self.convert_expr_with_cast(
-                    ctx.used(),
+                    ctx.used().set_needs_address(false),
                     CQualTypeId::new(target_type_id),
                     offset_id,
                 )?;
@@ -307,7 +308,7 @@ impl<'c> Translation<'c> {
             } else {
                 let target_type_id = self.ast_context.type_for_kind(&CTypeKind::Size);
                 let offset_rs = self.convert_expr_with_cast(
-                    ctx.used(),
+                    ctx.used().set_needs_address(false),
                     CQualTypeId::new(target_type_id),
                     offset_id,
                 )?;
@@ -331,10 +332,14 @@ impl<'c> Translation<'c> {
             };
 
             // LHS must be ref decayed for the offset method call's self param
-            let pointer_rs = self.convert_expr(ctx.used().decay_ref(), pointer_id, None)?;
+            let pointer_rs = self.convert_expr(
+                ctx.used().set_needs_address(false).decay_ref(),
+                pointer_id,
+                None,
+            )?;
             let target_type_id = self.ast_context.type_for_kind(&CTypeKind::SSize);
             let offset_rs = self.convert_expr_with_cast(
-                ctx.used(),
+                ctx.used().set_needs_address(false),
                 CQualTypeId::new(target_type_id),
                 offset_id,
             )?;
