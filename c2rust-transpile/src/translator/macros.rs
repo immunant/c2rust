272bcf79ff7b3d0a84cbfc5d1f96716c5e36f9db
Rua <ruawhitepaw@gmail.com>
2026-09-02 14:59:32 +0200

    transpile: Use default `ExprContext` for macro decls

--- c2rust-transpile/src/translator/macros.rs
+++ c2rust-transpile/src/translator/macros.rs
@@ -12,13 +12,7 @@ use crate::with_stmts::WithStmts;
 use crate::TranslateMacros;
 
 impl<'c> Translation<'c> {
-    pub fn convert_macro(
-        &self,
-        ctx: ExprContext,
-        decl_id: CDeclId,
-        span: Span,
-        name: &str,
-    ) -> ConvertedDecl {
+    pub fn convert_macro(&self, decl_id: CDeclId, span: Span, name: &str) -> ConvertedDecl {
         trace!(
             "Expanding macro {:?}: {:?}",
             decl_id,
@@ -26,7 +20,7 @@ impl<'c> Translation<'c> {
         );
 
         self.recreate_const_macro_from_expansions(
-            ctx.const_().set_expanding_macro(decl_id),
+            ExprContext::default().const_().set_expanding_macro(decl_id),
             &self.ast_context.macro_expansions[&decl_id],
         )
         .and_then(|(replacement, converted)| {
@@ -193,7 +187,7 @@ impl<'c> Translation<'c> {
 
             // We haven't tried to convert it yet.
             None => {
-                self.convert_decl(ctx.not_pattern(), *macro_id)?;
+                self.convert_decl(ctx, *macro_id)?;
                 let maybe_converted = self.converted_macros.borrow().get(macro_id).cloned();
                 if let Some(Some(converted)) = maybe_converted {
                     converted
--- c2rust-transpile/src/translator/mod.rs
+++ c2rust-transpile/src/translator/mod.rs
@@ -2335,7 +2335,7 @@ impl<'c> Translation<'c> {
                     .get(&decl_id)
                     .expect("Macro object not named");
 
-                Ok(self.convert_macro(ctx, decl_id, span, &name))
+                Ok(self.convert_macro(decl_id, span, &name))
             }
 
             // We aren't doing anything with the definitions of function-like
