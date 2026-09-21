11a4b91414cc47ad7c9818a8e510da78bd0b4664
Rua <ruawhitepaw@gmail.com>
2026-09-21 17:38:08 +0200

    transpile: Add more f128 tests

--- c2rust-transpile/tests/snapshots.rs
+++ c2rust-transpile/tests/snapshots.rs
@@ -550,6 +550,7 @@ fn test_f128() {
         .expect_unresolved_import("f128")
         .expect_unresolved_import("num_traits")
         .os_specific(true)
+        .expect_compile_error(true)
         .run();
 }
 
