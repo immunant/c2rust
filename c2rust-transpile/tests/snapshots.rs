cdd019aa40a90c496fa1bca1000b56d61be9f6d8
crasbe <crasbe@gmail.com>
2026-09-26 20:30:03 +0200

    fixup! c2rust-transpile/tests/snapshots: add RISC-V 32 and 64 test cases

--- c2rust-transpile/tests/snapshots.rs
+++ c2rust-transpile/tests/snapshots.rs
@@ -355,14 +355,14 @@ fn test_empty_init() {
 #[test]
 fn test_empty_riscv32() {
     transpile("empty_rv32.h")
-        .extra_clang_args(&["-target", "riscv32-unknown-elf"])
+        .extra_clang_args(&["-target", "riscv32"])
         .run();
 }
 
 #[test]
 fn test_empty_riscv64() {
     transpile("empty_rv64.h")
-        .extra_clang_args(&["-target", "riscv64-unknown-elf"])
+        .extra_clang_args(&["-target", "riscv64"])
         .run();
 }
 
